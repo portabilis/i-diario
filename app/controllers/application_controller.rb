6aa4e775da0d3a32f62506dcc53d269bd63db5e6
Ricardo Henrique <ricardohsd@gmail.com>
2014-09-05 01:08:46 -0300

    Configurado gem responders
    portabilis/novo-educacao#1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
+require "application_responder"
+
 class ApplicationController < ActionController::Base
+  self.responder = ApplicationResponder
+  respond_to :html
+
   include Pundit
   around_action :handle_customer
 
