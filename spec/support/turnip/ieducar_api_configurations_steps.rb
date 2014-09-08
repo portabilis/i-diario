cb017477bfd0f4867f9dd61bad289ca64afff44e
Ricardo Henrique <ricardohsd@gmail.com>
2014-09-08 11:49:53 -0300

    Alterado i18n da funcionalidade de configuração da API do i-Educar
    portabilis/novo-educacao#14

--- spec/support/turnip/ieducar_api_configurations_steps.rb
+++ spec/support/turnip/ieducar_api_configurations_steps.rb
@@ -2,22 +2,22 @@
 module Turnip
   module IeducarApiConfigurationsSteps
     step "acesso a edição da API" do
-      click_***REMOVED*** 'Configurações > Configurações da API'
+      click_***REMOVED*** 'Configurações > API de integração'
     end
 
     step 'poderei alterar a configuração da API' do
       fill_in 'URL do i-Educar', with: 'https://ieducar.com.br'
-      fill_in 'API token', with: 'asdqweas'
-      fill_in 'API secret token', with: '123asdqweas'
+      fill_in 'Chave de acesso', with: 'asdqweas'
+      fill_in 'Chave secreta', with: '123asdqweas'
       fill_in 'Código da instituição', with: '123'
 
       click_on 'Salvar'
 
-      expect(page).to have_text 'Configuração da API foi alterada com sucesso.'
+      expect(page).to have_text 'API de integração foi alterada com sucesso.'
 
       expect(page).to have_field 'URL do i-Educar', with: 'https://ieducar.com.br'
-      expect(page).to have_field 'API token', with: 'asdqweas'
-      expect(page).to have_field 'API secret token', with: '123asdqweas'
+      expect(page).to have_field 'Chave de acesso', with: 'asdqweas'
+      expect(page).to have_field 'Chave secreta', with: '123asdqweas'
       expect(page).to have_field 'Código da instituição', with: '123'
     end
   end
