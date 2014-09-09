995a6938f2448c9d98d4148c4d1cdfe681f9827e
Ricardo Henrique <ricardohsd@gmail.com>
2014-09-09 11:45:09 -0300

    Movido inserção de permissões iniciais para uma migration
    Estas permissões são padrões e devem sempre ser criadas.
    
    portabilis/novo-educacao#6

---
+++ db/migrate/20140909144227_insert_profile_roles.rb
@@ -0,0 +1,19 @@
+class InsertProfileRoles < ActiveRecord::Migration
+  def change
+    execute <<-SQL
+      DELETE FROM profiles;
+
+      INSERT INTO profiles (role, manage_users, manage_profiles, created_at, updated_at)
+      VALUES ('admin', 'f', 'f', now(), now());
+
+      INSERT INTO profiles (role, manage_users, manage_profiles, created_at, updated_at)
+      VALUES ('parent', 'f', 'f', now(), now());
+
+      INSERT INTO profiles (role, manage_users, manage_profiles, created_at, updated_at)
+      VALUES ('servant', 'f', 'f', now(), now());
+
+      INSERT INTO profiles (role, manage_users, manage_profiles, created_at, updated_at)
+      VALUES ('student', 'f', 'f', now(), now());
+    SQL
+  end
+end
