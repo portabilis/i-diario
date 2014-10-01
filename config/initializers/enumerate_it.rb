b852ec9f0c09c5a5edb82b1ed8e52fab36e3e4ed
Matias Leidemer <matiasleidemer@gmail.com>
2014-10-01 18:35:16 -0300

    Adicionado métodos auxiliares no EnumerateIt
    portabilis/novo-educacao#33

--- config/initializers/enumerate_it.rb
+++ config/initializers/enumerate_it.rb
@@ -1,1 +1,13 @@
 ActiveRecord::Base.extend EnumerateIt
+
+module EnumerateIt
+  class Base
+    def self.to_select
+      to_a.map { |arr| { id: arr[1], name: arr[0] } }
+    end
+
+    def self.to_hash
+      Hash[to_a]
+    end
+  end
+end
