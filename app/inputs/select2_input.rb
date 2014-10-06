b0672cdffd69178bcad337998d7f68a6ee513eee
Matias Leidemer <matiasleidemer@gmail.com>
2014-10-06 10:26:43 -0300

    Atualizo select2_input para parsear collections
    portabilis/novo-educacao#34

--- app/inputs/select2_input.rb
+++ app/inputs/select2_input.rb
@@ -2,8 +2,16 @@ class Select2Input < SimpleForm::Inputs::StringInput
   def input
     input_html_options[:type] = "hidden"
     input_html_options[:style] = "width: 100%;" + (input_html_options[:style] || "")
-    input_html_options[:data] = (input_html_options[:data] || {}).merge({elements: options[:elements]})
+    input_html_options[:data] = (input_html_options[:data] || {}).merge({ elements: parse_collection })
 
     @builder.text_field(attribute_name, input_html_options)
+  end
+
+  private
+
+  def parse_collection
+    return options[:elements] if options[:elements].is_a? String
+
+    options[:elements].map { |g| { id: g.id, name: g.to_s, text: g.to_s } }.to_json
   end
 end
