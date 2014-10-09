35d4e497267796e55b95ed160b1b2433540dbe99
Matias Leidemer <matiasleidemer@gmail.com>
2014-10-09 13:41:25 -0300

    Adicionado controller de MaterialRequests
    portabilis/novo-educacao#36

--- config/routes.rb
+++ config/routes.rb
@@ -38,5 +38,6 @@ Rails.application.routes.draw do
     resources :***REMOVED***
     resources :***REMOVED***
     resources :***REMOVED***
+    resources :***REMOVED***
   end
 end
---
+++ spec/controllers/material_requests_controller_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe ***REMOVED***RequestsController, :type => :controller do
+end
