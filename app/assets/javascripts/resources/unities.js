e6fe1c0e23f7b03fb61d6c98269a94b45fd0fea9
Matias Leidemer <matiasleidemer@gmail.com>
2014-10-17 09:48:53 -0300

    Corrigido problema com código ieducar na tela de unidades
    portabilis/novo-educacao#35

--- app/assets/javascripts/resources/unities.js
+++ app/assets/javascripts/resources/unities.js
@@ -1,12 +1,19 @@
 $(function() {
-  $dataApiCode = $("[data-api-code]");
+  var $dataApiCode = $("[data-unity-api-code]"),
+      $unitType = $("#unity_unit_type");
+
+  toggleApiCode($unitType.val() !== 'cost_center');
 
   $('#unity_unit_type').on("change", function(e) {
-    if (e.val === 'cost_center') {
+    toggleApiCode(e.val !== 'cost_center');
+  });
+
+  function toggleApiCode(show) {
+    if (show) {
+      $dataApiCode.show();
+    } else {
       $dataApiCode.hide();
       $dataApiCode.find('input').val('');
-    } else {
-      $dataApiCode.show();
     }
-  });
+  }
 });
