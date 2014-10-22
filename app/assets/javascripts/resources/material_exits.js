6ff2bcce601f032baae5b2fb11facc0eac3b6aad
Matias Leidemer <matiasleidemer@gmail.com>
2014-10-22 17:00:10 -0200

    Adicionada regra para esconder unidade de destino na saída de materiais
    Ao selecionar tipo da movimentação "Consumo" a "Unidade de destino" é removida

--- app/assets/javascripts/resources/material_exits.js
+++ app/assets/javascripts/resources/material_exits.js
@@ -1,16 +1,23 @@
 $(function() {
   var $returnReasonDiv = $("[data-material-exit-return-reason]"),
+      $requestAuthorizationDiv = $("[data-material-exit-request-authorization]"),
+      $destinationUnityDiv = $("[data-material-exit-destination-unity]"),
       $returnReason = $("#material_exit_return_reason"),
       $materialExitKind = $("#material_exit_kind"),
       $requestAuthorization = $("#material_exit_material_request_authorization_id"),
+      $destinationUnity = $("#material_exit_destination_unity_id"),
       $materialItems = $("fieldset#material_exit_items div"),
       flashMessages = new FlashMessages(),
       itemTemplate = $("#material_exit_items a.add_fields").attr("data-association-insertion-template");
 
   toggleReturnReason($materialExitKind.val() === 'return');
+  toggleRequestAuthorization($materialExitKind.val() === 'transfer');
+  toggleDestinationUnity($materialExitKind.val() !== 'consumption');
 
   $materialExitKind.on('change', function(e) {
     toggleReturnReason(e.val === 'return');
+    toggleRequestAuthorization(e.val === 'transfer');
+    toggleDestinationUnity(e.val !== 'consumption');
   });
 
   $requestAuthorization.on('change', function(e) {
@@ -65,6 +72,24 @@ $(function() {
     } else {
       $returnReasonDiv.hide();
       $returnReason.val('');
+    }
+  }
+
+  function toggleRequestAuthorization(show) {
+    if (show) {
+      $requestAuthorizationDiv.show();
+    } else {
+      $requestAuthorizationDiv.hide();
+      $requestAuthorization.select2('val', '');
+    }
+  }
+
+  function toggleDestinationUnity(show) {
+    if (show) {
+      $destinationUnityDiv.show();
+    } else {
+      $destinationUnityDiv.hide();
+      $destinationUnity.select2('val', '');
     }
   }
 });
