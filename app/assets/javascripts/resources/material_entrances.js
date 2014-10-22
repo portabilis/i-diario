cb4ef0f6a288e8a69259b33d3eecd62eaf82199a
Matias Leidemer <matiasleidemer@gmail.com>
2014-10-22 13:22:06 -0200

    Criado cadastro de entradas de materiais
    portabilis/novo-educacao#40

---
+++ app/assets/javascripts/resources/material_entrances.js
@@ -0,0 +1,84 @@
+$(function() {
+  var $supplier = $("[data-material-entrance-supplier]"),
+      $materialExitFields = $("[data-material-entrance-material-exit]"),
+      $kind = $("#material_entrance_kind"),
+      $materialItems = $("[data-material-entrance-items-container]"),
+      $materialExit = $("#material_entrance_material_exit_id"),
+      flashMessages = new FlashMessages(),
+      itemTemplate = $("#material_entrance_items a.add_fields").attr("data-association-insertion-template");
+
+  toggle***REMOVED***($kind.val() === 'supplier');
+  toggle***REMOVED***($kind.val() === 'return' || $kind.val() === 'transfer');
+
+  $kind.on('change', function(e) {
+    toggle***REMOVED***(e.val === 'supplier');
+    toggle***REMOVED***(e.val === 'return' || e.val === 'transfer');
+  });
+
+  $materialExit.on('change', function(e) {
+    fetchExitItems(e.val);
+  });
+
+  $("fieldset#material_entrance_items a.add_fields").
+    data("association-insertion-method", 'append').
+    data("association-insertion-traversal", 'closest').
+    data("association-insertion-node", 'fieldset#material_entrance_items');
+
+  function fetchExitItems(exitId) {
+    $.ajax({
+      url: '/saidas-de-materiais/' + exitId + '/items-de-saidas-de-materiais.json',
+      success: renderExitItems,
+      error: handleError
+    });
+  }
+
+  function renderExitItems(items) {
+    var output = [];
+
+    $materialItems.hide();
+    $materialItems.find("a.remove_fields").trigger("click");
+
+    _.each(items, function(item) {
+      item.quantity = parseFloat(item.quantity).toFixed(2);
+      output.push(updateTemplate(item));
+    });
+
+    $materialItems.append(output);
+    $('form').trigger('cocoon:after-insert');
+    $materialItems.show();
+  }
+
+  function updateTemplate(item) {
+    var output = [];
+
+    output = $(itemTemplate.replace(/new_items/g, new Date().getTime()));
+
+    output.find("[id$=quantity]").val(item.quantity);
+    output.find("[id$=material_id]").val(item.material.id);
+    output.find("span.measuring-unit").html(item.material.measuring_unit);
+
+    return output;
+  }
+
+  function handleError() {
+    flashMessages.error('Problemas ao buscar items da saída.');
+  }
+
+  function toggle***REMOVED***(show) {
+    if (show) {
+      $supplier.show();
+    } else {
+      $supplier.hide().find("input").val("");
+      $supplier.find("input.select2").select2("val", "");
+    }
+  }
+
+  function toggle***REMOVED***(show) {
+    if (show) {
+      $materialExitFields.show();
+    } else {
+      $materialExitFields.hide().
+        find("input.select2").select2("val", "");
+    }
+  }
+});
