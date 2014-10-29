2bb809e521a2fe5c2d16adef58b6ac2d915cd745
Matias Leidemer <matiasleidemer@gmail.com>
2014-10-29 15:00:03 -0200

    Criado model de associação entre cardápio e alimentos
    Para que o nested funcione corretamente, é preciso de um model
    intermediário.
    
    portabilis/novo-educacao#45

---
+++ app/models/food_menu.rb
@@ -0,0 +1,4 @@
+class ***REMOVED***Menu < ActiveRecord::Base
+  belongs_to :food
+  belongs_to :***REMOVED***
+end
---
+++ spec/models/food_menu_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe ***REMOVED***Menu, :type => :model do
+  describe "associations" do
+    it { should belong_to :food }
+    it { should belong_to :***REMOVED*** }
+  end
+end
