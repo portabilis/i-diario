f877d8fb36e6d0bb39dbadb600935433ab724da4
Ricardo Henrique <ricardohsd@gmail.com>
2014-11-19 22:19:18 -0200

    Melhorado cobertura de testes de funcionalidade

--- app/controllers/disciplinary_incidents_controller.rb
+++ app/controllers/disciplinary_incidents_controller.rb
@@ -1,9 +1,13 @@
 # encoding: utf-8
 class DisciplinaryIncidentsController < ApplicationController
   def index
-    result = api.fetch(aluno_id: students_code)
+    begin
+      result = api.fetch(aluno_id: students_code)
 
-    @disciplinary_incidents = DisciplinaryIncident.all(result["ocorrencias_disciplinares"])
+      @disciplinary_incidents = DisciplinaryIncident.all(result["ocorrencias_disciplinares"])
+    rescue IeducarApi::Base::ApiError => e
+      redirect_to root_path, alert: e.message
+    end
   end
 
   protected
@@ -12,10 +16,7 @@ class DisciplinaryIncidentsController < ApplicationController
     @api ||= IeducarApi::DisciplinaryIncidents.new(current_configuration.to_api)
   end
 
-  # TODO: por enquanto está retornando os 20 primeiros alunos sincronizados
-  # Quando for feito o vínculo do pai com os alunos este método deverá retornar
-  # somente os alunos vinculados
   def students_code
-    Student.limit(20).pluck(:api_code)
+    current_user.students.pluck(:api_code)
   end
 end
--- spec/controllers/disciplinary_incidents_controller_spec.rb
+++ spec/controllers/disciplinary_incidents_controller_spec.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'spec_helper'
 
 RSpec.describe DisciplinaryIncidentsController, :type => :controller do
@@ -8,6 +9,22 @@ RSpec.describe DisciplinaryIncidentsController, :type => :controller do
         action: "index",
         locale: "pt-BR"
       )
+    end
+  end
+
+  describe "#index" do
+    context "without students" do
+      it "should redirect to root_path and display the error message" do
+        user = users(:mary_jane)
+
+        controller.stub(:current_user).and_return(user)
+        controller.stub(:authenticate_user!)
+
+        get :index, locale: 'pt-BR'
+
+        expect(response).to redirect_to(root_path)
+        expect(flash[:alert]).to match(/É necessário informar os códigos dos alunos: aluno_id/)
+      end
     end
   end
 end
