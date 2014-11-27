96d3f05667d083272945598cc71c5d093607e9b8
Ricardo Henrique <ricardohsd@gmail.com>
2014-11-27 16:00:00 -0200

    Removido environment do path do arquivo publicado no S3
    portabilis/novo-educacao#50

--- app/uploaders/backup_file_uploader.rb
+++ app/uploaders/backup_file_uploader.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 class BackupFileUploader < CarrierWave::Uploader::Base
   def store_dir
-    "uploads/#{Rails.env}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 end
