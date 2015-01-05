1393f1bb42cc7b9fa2d0e5c341e3d52cf24b82db
Lucas Schmoeller da Silva <lucasschmoellerdasilva@gmail.com>
2015-01-05 19:04:37 -0200

    Criada permissões padrões nas configurações gerais
    portabilis/novo-educacao#82

--- app/controllers/general_configurations_controller.rb
+++ app/controllers/general_configurations_controller.rb
@@ -30,7 +30,7 @@ class GeneralConfigurationsController < ApplicationController
 
   def permitted_attributes
     params.require(:general_configuration).permit(
-      :security_level, :students_default_role_id, :employees_default_role_id, :students_default_role_id
+      :security_level, :students_default_role_id, :employees_default_role_id, :parents_default_role_id
       )
   end
 end
