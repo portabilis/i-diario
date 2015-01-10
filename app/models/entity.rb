f4acabbc7ceab105b4beb6c809e280eec8787b8d
Ricardo Henrique <ricardohsd@gmail.com>
2015-01-10 12:13:41 -0200

    Corrigido recuperação de senha do usuário
    Corrigido formulário e link que é enviado.
    
    portabilis/novo-educacao#80

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -4,6 +4,18 @@ class Entity < ActiveRecord::Base
   validates :name, :domain, :config, presence: true
   validates :domain, uniqueness: { case_sensitive: false }, allow_blank: true
 
+  def self.current
+    raise Exception.new("More than 1 entity registered") if all.count != 1
+
+    all.first
+  end
+
+  def self.current_domain
+    raise Exception.new("More than 1 entity registered") if all.count != 1
+
+    all.first.domain
+  end
+
   def using_connection(&block)
     ActiveRecord::Base.using_connection(id, connection_spec, &block)
   end
