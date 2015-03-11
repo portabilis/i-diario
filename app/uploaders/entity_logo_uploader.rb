4b4e2ddc06b9aa8e1a6f3c1f105eea49562d119f
Ricardo Henrique <ricardohsd@gmail.com>
2015-03-11 10:03:58 -0300

    Corrigido EntityLogoUploader
    O stode_dir deve apontar para a path correto no bucket. Ao usar Time.now
    ele sempre irá pegar a data e hora do momento da requisição.

--- app/uploaders/entity_logo_uploader.rb
+++ app/uploaders/entity_logo_uploader.rb
@@ -1,10 +1,10 @@
 # encoding: utf-8
 class EntityLogoUploader < CarrierWave::Uploader::Base
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{Time.now.to_i}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-  
+
   def extension_white_list
     %w(jpg jpeg gif png)
-  end  
+  end
 end
