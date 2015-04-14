bd815fa862647940b9f0eea8561860b5a87f030a
Lucas Schmoeller da Silva <lucasschmoellerdasilva@gmail.com>
2015-04-14 16:07:03 -0300

    Removido funções de debug
    portabilis/novo-educacao#139

--- app/services/ieducar_api/classrooms.rb
+++ app/services/ieducar_api/classrooms.rb
@@ -2,11 +2,7 @@
 module IeducarApi
   class Classrooms < Base
     def fetch(params = {})
-      Rails.logger.debug params.inspect
       params.reverse_merge!(path: "module/Api/Turma", resource: "turmas-por-escola")
-      Rails.logger.debug "@@@@@@@@@@@@@@@@@"
-      Rails.logger.debug params.inspect
-      Rails.logger.debug "@@@@@@@@@@@@@@@@@"
 
       raise ApiError.new("É necessário informar o ano") if params[:ano].blank?
       super
