a7a8685035f7539f9d641ab5e7b04ffc1910cbd7
Lucas Schmoeller da Silva <lucasschmoellerdasilva@gmail.com>
2015-07-02 19:50:07 -0300

    WIP Padronizado design de tabelas internas (nesteds)
    portabilis/novo-educacao#157

--- app/assets/javascripts/resources/material_requests.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-  $("fieldset#items a.add_fields").
-    data("association-insertion-method", 'append').
-    data("association-insertion-traversal", 'closest').
-    data("association-insertion-node", 'fieldset#items');
- });
