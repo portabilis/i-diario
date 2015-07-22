817e4e9b8136e74b750da2da82988d4f641cb8fb
Marcelo B. Alexandre <marcelobalexandre@gmail.com>
2015-07-22 15:18:21 -0300

    Criar parametrização de nota máxima e número de casas decimais para lançamento de notas Correção de bug relacionado as casas decimais do campo peso. Adicionado teste de aceitação. portabilis/novo-educacao#175

--- spec/acceptance/test_settings.feature
+++ spec/acceptance/test_settings.feature
@@ -18,3 +18,11 @@ Funcionalidade: Configurações de avaliação
   Cenário: Excluir uma configuração de avaliação
     Dado que existe uma configuração de avaliação cadastrada
     Então poderei excluir uma configuração de avaliação
+
+  Cenário: Cadastrar uma avaliação com nota máxima 2, número de casas decimais 2 e fixar duas avaliações com peso 5,00 e sem tipo de avaliação
+    Dado que acesso a listagem de configuração de avaliação
+    Quando eu entrar no formulário de nova configuração de avaliação
+    E cadastrar uma nova configuração de avaliação com nota máxima 2, número de casas decimais 2 e fixar duas avaliações com peso 5,00 e sem tipo de avaliação
+    Então devo visualizar uma mensagem de tipo de avaliação não pode ficar em branco
+    E devo visualizar os pesos das avaliações formatados corretamente
+
--- spec/support/turnip/test_settings_steps.rb
+++ spec/support/turnip/test_settings_steps.rb
@@ -1,4 +1,5 @@
 # encoding: utf-8
+
 module Turnip
   module TestSettingsSteps
     step 'que acesso a listagem de configuração de avaliação' do
@@ -50,6 +51,50 @@ module Turnip
 
       expect(page).to have_content 'Configuração de avaliação foi apagada com sucesso'
       expect(page).to have_no_content '2014 10 2 Não'
+    end
+
+    step 'cadastrar uma nova configuração de avaliação com nota máxima 2, número de casas decimais 2 e fixar duas avaliações com peso 5,00 e sem tipo de avaliação' do
+      fill_in 'Ano', with: '2010'
+      fill_in 'Nota máxima', with: '2'
+      fill_in 'Número de casas decimais', with: '2'
+
+      find(:css, '#test_setting_fix_tests').trigger('click')
+
+      click_on 'Adicionar avaliação'
+
+      within '#test-settings-tests > tr:nth-child(1)' do
+        fill_in 'Avaliação', with: 'Avaliação 01'
+        fill_in 'Peso', with: '5,00'
+      end
+
+      click_on 'Adicionar avaliação'
+
+      within '#test-settings-tests > tr:nth-child(2)' do
+        fill_in 'Avaliação', with: 'Avaliação 02'
+        fill_in 'Peso', with: '5,00'
+      end
+
+      click_on 'Salvar'
+    end
+
+    step 'devo visualizar uma mensagem de tipo de avaliação não pode ficar em branco' do
+      within '#test-settings-tests > tr:nth-child(1) > td > div.test_setting_tests_test_type' do
+        expect(page).to have_content('não pode ficar em branco')
+      end
+
+      within '#test-settings-tests > tr:nth-child(2) > td > div.test_setting_tests_test_type' do
+        expect(page).to have_content('não pode ficar em branco')
+      end
+    end
+
+    step 'devo visualizar os pesos das avaliações formatados corretamente' do
+      within '#test-settings-tests > tr:nth-child(1)' do
+        expect(find_field('Peso').value).to eql('5,00')
+      end
+
+      within '#test-settings-tests > tr:nth-child(2)' do
+        expect(find_field('Peso').value).to eql('5,00')
+      end
     end
   end
 end
