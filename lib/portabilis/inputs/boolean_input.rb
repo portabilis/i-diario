e6da30b259769e73f3e216169d53b1f28f19fb6e
Marcelo B. Alexandre <marcelobalexandre@gmail.com>
2015-08-06 16:49:58 -0300

    Atualizar Rails para a versão 4.2.3
    Corrigido erros relacionados a atualização da versão do Rails. Corrigido deprecation warnings.
    
    portabilis/novo-educacao#222

--- lib/portabilis/inputs/boolean_input.rb
+++ lib/portabilis/inputs/boolean_input.rb
@@ -7,7 +7,7 @@ module Portabilis
         if nested_boolean_style?
           build_hidden_field_for_checkbox +
             template.label_tag(nil, class: "checkbox") {
-              build_check_box_without_hidden_field + inline_label + template.send(:content_tag, :i)
+              build_check_box_without_hidden_field(merged_input_options) + inline_label + template.send(:content_tag, :i)
             }
         else
           build_check_box(unchecked_value, merged_input_options)
