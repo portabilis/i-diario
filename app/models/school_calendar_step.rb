37e4279f201e9e270e1af51787b02142468bcd31
Lucas Schmoeller da Silva <lucasschmoellerdasilva@gmail.com>
2015-08-14 09:12:25 -0300

    Criado controle de período de postagem de avaliações por etapa
    Ajustes e validações solicitadas
    
    portabilis/novo-educacao#223

--- app/models/school_calendar_step.rb
+++ app/models/school_calendar_step.rb
@@ -11,6 +11,9 @@ class SchoolCalendarStep < ActiveRecord::Base
   validate :end_at_must_be_in_school_calendar_year, if: :school_calendar
   validate :start_at_must_be_less_than_end_at
 
+  validate :dates_for_posting_less_than_start_date
+  validate :end_date_less_than_start_date_for_posting
+
   scope :ordered, -> { order(arel_table[:start_at]) }
   scope :started_after_and_before, lambda { |date| where(arel_table[:start_at].lteq(date)).
                                                   where(arel_table[:end_at].gteq(date)) }
@@ -38,6 +41,19 @@ class SchoolCalendarStep < ActiveRecord::Base
 
     if start_at.to_date >= end_at.to_date
       errors.add(:start_at, :must_be_less_than_end_at)
+    end
+  end
+
+  def dates_for_posting_less_than_start_date
+    if start_at
+      errors.add(:start_date_for_posting, :must_be_greater_than_start_at) if start_date_for_posting && start_date_for_posting < start_at
+      errors.add(:end_date_for_posting, :must_be_greater_than_start_at) if end_date_for_posting && end_date_for_posting < start_at
+    end
+  end
+
+  def end_date_less_than_start_date_for_posting
+    if start_date_for_posting && end_date_for_posting
+      errors.add(:end_date_for_posting, :must_be_greater_than_start_date_for_posting) if end_date_for_posting < start_date_for_posting
     end
   end
 end
--- spec/models/school_calendar_step_spec.rb
+++ spec/models/school_calendar_step_spec.rb
@@ -30,6 +30,30 @@ RSpec.describe SchoolCalendarStep, type: :model do
       expect(subject.errors.messages[:start_at]).to include('não pode ser maior ou igual a data final')
     end
 
+    it "validates that start_at is less than start_date_for_posting" do
+      subject.start_at = "02/01/2020"
+      subject.start_date_for_posting = "01/01/2020"
+
+      expect(subject).to_not be_valid
+      expect(subject.errors.messages[:start_date_for_posting]).to include("não pode ser menor que a data inicial")
+    end
+
+    it "validates that start_at is less than end_date_for_posting " do
+      subject.start_at = "02/01/2020"
+      subject.end_date_for_posting = "01/01/2020"
+
+      expect(subject).to_not be_valid
+      expect(subject.errors.messages[:end_date_for_posting]).to include("não pode ser menor que a data inicial")
+    end
+
+    it "validates that start_date_for_posting is less than end_date_for_posting" do
+      subject.start_date_for_posting = "02/01/2020"
+      subject.end_date_for_posting = "01/01/2020"
+
+      expect(subject).to_not be_valid
+      expect(subject.errors.messages[:end_date_for_posting]).to include("não pode ser menor que a data inicial para postagem")
+    end
+
     it "validates that end_at is in school_calendar year" do
       school_calendar = build(:school_calendar, year: 2020)
       subject.school_calendar = school_calendar
