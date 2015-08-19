8f63f706afef8b3e1c41a1eb0c63997f0e1a233e
Marcelo B. Alexandre <marcelobalexandre@gmail.com>
2015-08-19 15:10:36 -0300

    Correção de testes

--- app/decorators/weekdays_decorator.rb
+++ app/decorators/weekdays_decorator.rb
@@ -1,7 +1,13 @@
 class WeekdaysDecorator
   def self.checkbox_collection
-    I18n.t('date.day_names').each_with_index.map do |obj, index|
-      OpenStruct.new({weekday: obj, id: index.to_s})
+    I18n.t('date.day_names').each_with_index.map do |day_name, index|
+      OpenStruct.new({ weekday: day_name, id: index.to_s })
     end
+  end
+
+  def self.data_for_select2
+    I18n.t('date.day_names').each_with_index.map do |day_name, index|
+      { id: index.to_s, name: day_name, text: day_name }
+    end.to_json
   end
 end
