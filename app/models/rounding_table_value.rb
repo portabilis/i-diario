20e15eda0db5ca4eab9d23b1521dcfdc59175686
Marcelo B. Alexandre <marcelobalexandre@gmail.com>
2015-08-31 10:04:14 -0300

    Removido validação do atributo value do model RoundingTableValue

--- app/models/rounding_table_value.rb
+++ app/models/rounding_table_value.rb
@@ -2,7 +2,7 @@ class RoundingTableValue < ActiveRecord::Base
   acts_as_copy_target
   belongs_to :rounding_table
 
-  validates :label, :rounding_table_api_code, :value, presence: true
+  validates :label, :rounding_table_api_code, presence: true
 
   scope :ordered, -> { order(arel_table[:value].desc) }
 
