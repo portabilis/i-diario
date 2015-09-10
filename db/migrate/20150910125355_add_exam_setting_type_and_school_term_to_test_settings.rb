42616367c5a5574f739a0fbe1b3113696042af17
Marcelo B. Alexandre <marcelobalexandre@gmail.com>
2015-09-10 11:24:41 -0300

    Alterar funcionalidade de configuração de avaliações para atender casos com configurações por etapa Alterado nome da funcionalidade para "Avaliações numéricas". Adicionado campo para identificar se a configuração é "Geral" ou "Por etapa". Criado migrações e implementado validações. portabilis/novo-educacao#267

---
+++ db/migrate/20150910125355_add_exam_setting_type_and_school_term_to_test_settings.rb
@@ -0,0 +1,14 @@
+class AddExamSettingTypeAndSchoolTermToTestSettings < ActiveRecord::Migration
+  def change
+    add_column :test_settings, :exam_setting_type, :string, null: true
+    add_column :test_settings, :school_term,       :string, null: true
+
+    execute <<-SQL
+      UPDATE test_settings SET exam_setting_type = 'general';
+    SQL
+
+    change_column :test_settings, :exam_setting_type, :string, null: false
+
+    add_index :test_settings, [:year, :school_term], unique: true
+  end
+end
