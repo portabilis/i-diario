934d2f269b9b0a0a802418367f1959c989b5b90c
Marcelo B. Alexandre <marcelobalexandre@gmail.com>
2015-09-14 13:45:05 -0300

    Alterado funcionalidade de postagem para permitir o envio de diferentes etapas

--- app/controllers/ieducar_api_exam_postings_controller.rb
+++ app/controllers/ieducar_api_exam_postings_controller.rb
@@ -5,16 +5,21 @@ class IeducarApiExamPostingsController < ApplicationController
   before_action :require_current_posting_step
 
   def index
-    ApiPostingTypes.each_value do |value|
-      instance_variable_set "@#{value}_posting", IeducarApiExamPosting.where(author_id: current_user.id).send(value).last
+    @school_calendar = current_school_calendar
+
+    @school_calendar.steps.each do |step|
+      ApiPostingTypes.each_value do |value|
+        ieducar_api_exam_posting = IeducarApiExamPosting.where(school_calendar_step: step.id, author_id: current_user.id).send(value).last
+        instance_variable_set("@school_calendar_step_#{step.id}_#{value}_posting", ieducar_api_exam_posting)
+      end
     end
   end
 
   def create
-    authorize IeducarApiExamPosting.new
+    authorize(IeducarApiExamPosting.new)
+
     ieducar_api_exam_posting = IeducarApiExamPosting.new(permitted_attributes)
     ieducar_api_exam_posting.author = current_user
-    ieducar_api_exam_posting.school_calendar_step = current_school_calendar.posting_step Date.today
     ieducar_api_exam_posting.status = ApiSyncronizationStatus::STARTED
     ieducar_api_exam_posting.ieducar_api_configuration = IeducarApiConfiguration.current
 
@@ -28,13 +33,16 @@ class IeducarApiExamPostingsController < ApplicationController
   protected
 
   def permitted_attributes
-    params.permit(:post_type)
+    params.permit(:school_calendar_step_id,
+                  :post_type)
   end
 
   def require_current_posting_step
     return unless current_school_calendar
-    unless current_school_calendar.posting_step Date.today
+
+    unless current_school_calendar.posting_step(Date.today)
       flash[:alert] = t('errors.ieducar_api_exam_postings.require_current_posting_step')
+
       redirect_to root_path
     end
   end
--- app/models/ieducar_api_exam_posting.rb
+++ app/models/ieducar_api_exam_posting.rb
@@ -1,13 +1,13 @@
 class IeducarApiExamPosting < ActiveRecord::Base
   acts_as_copy_target
 
-  has_enumeration_for :post_type, with: ApiPostingTypes, create_scopes: true
-  has_enumeration_for :status, with: ApiPostingStatus, create_helpers: true,
-    create_scopes: true
+  has_enumeration_for :post_type, with: ApiPostingTypes,  create_scopes: true
+  has_enumeration_for :status,    with: ApiPostingStatus, create_helpers: true,
+                                                          create_scopes: true
 
   belongs_to :ieducar_api_configuration
   belongs_to :school_calendar_step
-  belongs_to :author, class_name: "User"
+  belongs_to :author, class_name: 'User'
 
   validates :ieducar_api_configuration, :school_calendar_step, presence: true
 
