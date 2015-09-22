496d8b614276028d850f0ff8c620ad0219206d7b
Marcelo B. Alexandre <marcelobalexandre@gmail.com>
2015-09-22 09:57:20 -0300

    Permitir ressincronizar os calendários letivos com datas diferentes do i-Educar Implementado funcionalidade que permite ressincronizar os calendários letivos. portabilis/novo-educacao#274

--- app/services/school_calendars_parser.rb
+++ app/services/school_calendars_parser.rb
@@ -4,7 +4,8 @@ class SchoolCalendarsParser
   end
 
   def parse!
-    build_records(api.fetch(ano: Date.today.year)['escolas'])
+    school_calendars_from_api = api.fetch(ano: Date.today.year)['escolas']
+    build_school_calendars_to_synchronize(school_calendars_from_api)
   end
 
   def self.parse!(configuration)
@@ -19,26 +20,72 @@ class SchoolCalendarsParser
     IeducarApi::SchoolCalendars.new(configuration.to_api)
   end
 
-  def build_records(collection)
-    new_school_calendars = []
+  def build_school_calendars_to_synchronize(school_calendars_from_api)
+    build_new_school_calendars(school_calendars_from_api) + build_existing_school_calendars(school_calendars_from_api)
+  end
 
-    collection.each do |record|
-      if Unity.exists?(api_code: record['escola_id']) && !SchoolCalendar.joins(:unity).exists?(unities: { api_code: record['escola_id'] })
-        unity = Unity.find_by_api_code(record['escola_id'])
-        new_school_calendar = SchoolCalendar.new(unity: unity,
-                                                 year: Date.today.year,
-                                                 number_of_classes: 4)
-        record['etapas'].each do |step|
-          new_school_calendar.steps.build(start_at: step['data_inicio'],
-                                          end_at:   step['data_fim'],
-                                          start_date_for_posting: step['data_inicio'],
-                                          end_date_for_posting: step['data_fim'])
-        end
+  def build_new_school_calendars(school_calendars_from_api)
+    school_calendars_to_synchronize = []
 
-        new_school_calendars << new_school_calendar
-      end
+    new_school_calendars_from_api = school_calendars_from_api.select do |school_calendar_from_api|
+      unity_api_code = school_calendar_from_api['escola_id']
+      Unity.exists?(api_code: unity_api_code) &&
+      !SchoolCalendar.joins(:unity).exists?(unities: { api_code: unity_api_code })
     end
 
-    new_school_calendars
+    new_school_calendars_from_api.each do |new_school_calendar_from_api|
+      unity_api_code = new_school_calendar_from_api['escola_id']
+      unity = Unity.find_by(api_code: unity_api_code)
+
+      new_school_calendar = SchoolCalendar.new(unity: unity,
+                                               year: Date.today.year,
+                                               number_of_classes: 4)
+
+      new_school_calendar_from_api['etapas'].each do |step|
+        new_school_calendar.steps.build(start_at: step['data_inicio'],
+                                        end_at:   step['data_fim'],
+                                        start_date_for_posting: step['data_inicio'],
+                                        end_date_for_posting:   step['data_fim'])
+      end
+
+      school_calendars_to_synchronize << new_school_calendar
+    end
+
+    school_calendars_to_synchronize
+  end
+
+  def build_existing_school_calendars(school_calendars_from_api)
+    school_calendars_to_synchronize = []
+
+    existing_school_calendars_from_api = school_calendars_from_api.select do |school_calendar_from_api|
+      unity_api_code = school_calendar_from_api['escola_id']
+      Unity.exists?(api_code: unity_api_code) &&
+      SchoolCalendar.joins(:unity).exists?(unities: { api_code: unity_api_code })
+    end
+
+    existing_school_calendars_from_api.each do |existing_school_calendar_from_api|
+      unity_api_code = existing_school_calendar_from_api['escola_id']
+      unity = Unity.find_by(api_code: unity_api_code)
+      school_calendar = SchoolCalendar.joins(:unity).find_by(unities: { api_code: unity_api_code })
+
+      need_to_synchronize = false
+      existing_school_calendar_from_api['etapas'].each_with_index do |step, index|
+        if school_calendar.steps[index].start_at != Date.parse(step['data_inicio'])
+          need_to_synchronize = true
+          school_calendar.steps[index].start_at = step['data_inicio']
+          school_calendar.steps[index].start_date_for_posting = step['data_inicio']
+        end
+
+        if school_calendar.steps[index].end_at != Date.parse(step['data_fim'])
+          need_to_synchronize = true
+          school_calendar.steps[index].end_at = step['data_fim']
+          school_calendar.steps[index].end_date_for_posting = step['data_fim']
+        end
+      end
+
+      school_calendars_to_synchronize << school_calendar if need_to_synchronize
+    end
+
+    school_calendars_to_synchronize
   end
 end
