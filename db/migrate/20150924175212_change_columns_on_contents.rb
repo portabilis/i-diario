491c8c022f830f38fc2069951317deb4d0eb3412
matheusportabilis <matheus@portabilis.com.br>
2015-09-24 14:53:25 -0300

    Alterar funcionalidade de registro de conteúdos para plano de aula Corrigido migrate. portabilis/novo-educacao#255

--- db/migrate/20150924175212_change_columns_on_contents.rb
+++ db/migrate/20150924175212_change_columns_on_contents.rb
@@ -1,11 +1,12 @@
 class ChangeColumnsOnContents < ActiveRecord::Migration
   def change
     change_column :contents, :description, :text, null: true
-    change_column :contents, :theme, :text, null: false
 
     execute <<-SQL
       UPDATE contents SET theme = description;
       UPDATE contents SET description = '';
     SQL
+
+    change_column :contents, :theme, :text, null: false
   end
 end
