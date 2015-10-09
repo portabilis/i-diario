6b00509c98a960c307aea4db65004a7c4c79705c
Marcelo B. Alexandre <marcelobalexandre@gmail.com>
2015-10-09 14:56:51 -0300

    Alterar funcionalidade de planos de aula para permitir a criação por área de conhecimento ou disciplina Criado migração para atualizar as permissões. portabilis/novo-educacao#290

---
+++ db/migrate/20151009175304_migrate_role_permissions.rb
@@ -0,0 +1,9 @@
+class MigrateRolePermissions < ActiveRecord::Migration
+  def change
+    execute <<-SQL
+      INSERT INTO role_permissions(role_id, feature, permission, created_at, updated_at) SELECT role_id, 'knowledge_area_lesson_plans', permission, localtimestamp, localtimestamp FROM role_permissions WHERE feature = 'contents';
+      INSERT INTO role_permissions(role_id, feature, permission, created_at, updated_at) SELECT role_id, 'discipline_lesson_plans', permission, localtimestamp, localtimestamp FROM role_permissions WHERE feature = 'contents';
+      DELETE FROM role_permissions WHERE feature = 'contents';
+    SQL
+  end
+end
