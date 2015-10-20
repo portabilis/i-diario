ab112f1dbb9c87821146c46c73836553348ed6fa
Marcelo B. Alexandre <marcelobalexandre@gmail.com>
2015-10-20 16:09:27 -0200

    Alteração na mensagem de manutenção

--- app/assets/javascripts/resources/maintenance_message.js
+++
@@ -1,4 +0,0 @@
-$(function () {
-  var flashMessages = new FlashMessages();
-  flashMessages.warning('Para melhorarmos ainda mais o produto, precisamos fazer uma pequena paradinha para manutenção. O serviço poderá ficar instável entre as 11:00h e as 13:00h desta quarta-feira (21/10). Agradecemos sua compreensão.');
-});
