a9147c80ee6a28a97053a10283dd9640a8aa092f
Marcelo B. Alexandre <marcelobalexandre@gmail.com>
2015-11-27 10:41:46 -0200

    Remover validação no envio de faltas
    * Removido necessidade de lançar todas as faltas dos dias letivos para enviar as faltas.
    
    portabilis/novo-educacao#345

--- app/services/absence_posting.rb
+++ app/services/absence_posting.rb
@@ -46,35 +46,23 @@ class AbsencePosting
       daily_frequencies = DailyFrequency.by_classroom_id(classroom.id)
         .by_frequency_date_between(step_start_at, step_end_at)
 
-      if daily_frequencies.blank? || daily_frequencies.count < posting.school_calendar_step.number_of_school_days
-        warning_dates = posting.school_calendar_step.school_day_dates
 
-        if daily_frequencies.present?
-          daily_frequencies.each do |daily_frequency|
-            warning_dates.delete daily_frequency.frequency_date
-          end
-        end
+      students_ids = []
+      daily_frequencies.each { |d| students_ids << d.students.map(&:student_id) }
+      students_ids.flatten!.uniq! if students_ids.any?
+      students = Student.find(students_ids)
 
-        string_warning_dates = warning_dates.collect { |date| I18n.l(date) }.join(', ')
-        raise IeducarApi::Base::ApiError.new("Não foi possível enviar as faltas da turma #{classroom} pois existem aulas que não foram lançadas no diário de frequência. Datas: #{string_warning_dates}.")
-      else
-        students_ids = []
-        daily_frequencies.each { |d| students_ids << d.students.map(&:student_id) }
-        students_ids.flatten!.uniq! if students_ids.any?
-        students = Student.find(students_ids)
+      students.each do |student|
+        daily_frequency_students = DailyFrequencyStudent.general_by_classroom_student_date_between(
+          classroom,
+          student.id,
+          step_start_at,
+          step_end_at
+        )
 
-        students.each do |student|
-          daily_frequency_students = DailyFrequencyStudent.general_by_classroom_student_date_between(
-            classroom,
-            student.id,
-            step_start_at,
-            step_end_at
-          )
-
-          classrooms[classroom.api_code]['turma_id'] = classroom.api_code
-          classrooms[classroom.api_code]['alunos'][student.api_code]['aluno_id'] = student.api_code
-          classrooms[classroom.api_code]['alunos'][student.api_code]['faltas'] = daily_frequency_students.absences.count
-        end
+        classrooms[classroom.api_code]['turma_id'] = classroom.api_code
+        classrooms[classroom.api_code]['alunos'][student.api_code]['aluno_id'] = student.api_code
+        classrooms[classroom.api_code]['alunos'][student.api_code]['faltas'] = daily_frequency_students.absences.count
       end
     end
 
