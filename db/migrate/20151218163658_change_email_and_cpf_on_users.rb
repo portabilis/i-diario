ba8d3c1a64c3806803d05986b9b94c7f32048462
Marcelo B. Alexandre <marcelobalexandre@gmail.com>
2015-12-22 09:00:37 -0200

    Permitir criação de contas com CPF
    * Corrigido migração para alterar valores em branco na coluna CPF da tabela de usuários para NULL antes de adicionar o Unique Index.
    
    portabilis/novo-educacao#369

--- db/migrate/20151218163658_change_email_and_cpf_on_users.rb
+++ db/migrate/20151218163658_change_email_and_cpf_on_users.rb
@@ -1,6 +1,11 @@
 class ChangeEmailAndCpfOnUsers < ActiveRecord::Migration
   def change
     change_column :users, :email, :string, null: true, default: nil
+
+    execute <<-SQL
+      UPDATE users SET cpf = null WHERE cpf = '';
+    SQL
+
     add_index :users, :cpf, unique: true
   end
 end
