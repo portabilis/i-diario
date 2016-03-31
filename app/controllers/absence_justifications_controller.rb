16d71a2e4841cfb4f356d936de2101aa5227762c
Victor Arruda <victor@portabilis.com.br>
2016-03-31 18:02:25 -0300

    WIP Refs #319

--- app/controllers/absence_justifications_controller.rb
+++ app/controllers/absence_justifications_controller.rb
@@ -3,7 +3,10 @@ class AbsenceJustificationsController < ApplicationController
   has_scope :per, default: 10
 
   def index
-    @absence_justifications = apply_scopes(AbsenceJustification.by_author(current_user.id).includes(:student).ordered)
+    @classrooms = Classroom.by_unity_and_teacher(current_user_unity.id, current_teacher.id)
+    @absence_justifications = apply_scopes(AbsenceJustification.by_author(current_user.id).where(unity_id: current_user_unity)
+                                                                .filter(filtering_params(params[:search]))
+                                                                .includes(:student).ordered)
 
     authorize @absence_justifications
   end
@@ -12,6 +15,9 @@ class AbsenceJustificationsController < ApplicationController
     @absence_justification = AbsenceJustification.new.localized
     @absence_justification.absence_date = Time.zone.today
     @absence_justification.author = current_user
+    @absence_justification.unity = current_user_unity
+    @classrooms = Classroom.by_unity_and_teacher(current_user_unity.id, current_teacher.id)
+    @teacher_id = current_teacher.id
 
     authorize @absence_justification
   end
@@ -19,18 +25,22 @@ class AbsenceJustificationsController < ApplicationController
   def create
     @absence_justification = AbsenceJustification.new(resource_params)
     @absence_justification.author = current_user
+    @absence_justification.unity = current_user_unity
 
     authorize @absence_justification
 
     if @absence_justification.save
       respond_with @absence_justification, location: absence_justifications_path
     else
+      @classrooms = Classroom.by_unity_and_teacher(current_user_unity, current_teacher.id)
       render :new
     end
   end
 
   def edit
     @absence_justification = AbsenceJustification.find(params[:id]).localized
+    @classrooms = Classroom.by_teacher_id(current_teacher.id)
+
     validate_current_user
 
     authorize @absence_justification
@@ -69,7 +79,8 @@ class AbsenceJustificationsController < ApplicationController
 
   def resource_params
     params.require(:absence_justification).permit(
-      :student_id, :absence_date, :justification, :absence_date_end
+      :student_id, :absence_date, :justification, :absence_date_end,
+      :unity_id, :classroom_id
     )
   end
 
@@ -80,5 +91,38 @@ class AbsenceJustificationsController < ApplicationController
       flash[:alert] = t('.current_user_not_allowed')
       redirect_to root_path
     end
+  end
+
+  def filtering_params(params)
+    if params
+      params.slice(:by_classroom, :by_student, :by_absence_date, :by_absence_date_end)
+    else
+      {}
+    end
+  end
+
+  protected
+
+  def fetch_students
+    begin
+      api = IeducarApi::Students.new(configuration.to_api)
+      result = api.fetch_for_daily(
+        {
+          classroom_api_code: @absence_justification.classroom.api_code,
+          discipline_api_code: @absence_justification.discipline.try(:api_code),
+          date: Time.zone.today
+        }
+      )
+
+      @api_students = result['alunos'].uniq
+    rescue IeducarApi::Base::ApiError => e
+      flash[:alert] = e.message
+      @api_students = []
+      redirect_to new_daily_frequency_path
+    end
+  end
+
+  def configuration
+    @configuration ||= IeducarApiConfiguration.current
   end
 end
--- app/models/absence_justification.rb
+++ app/models/absence_justification.rb
@@ -4,18 +4,32 @@ class AbsenceJustification < ActiveRecord::Base
   audited
 
   include Audit
+  include Filterable
 
   belongs_to :author, class_name: 'User'
   belongs_to :student
+  belongs_to :unity
+  belongs_to :classroom
+  belongs_to :discipline
 
-  validates :author, presence: true
-  validates :student, presence: true
+  validates :author,           presence: true
+  validates :student,          presence: true
+  validates :unity,            presence: true
+  validates :classroom_id,     presence: true
   validates :absence_date_end, presence: true,
                                uniqueness: { scope: :student_id }
-  validates :absence_date, presence: true,
-                           uniqueness: { scope: :student_id }
-  validates :justification, presence: true
+  validates :absence_date,     presence: true,
+                               uniqueness: { scope: :student_id }
+  validates :discipline,       presence: true,
+                               uniqueness: { scope: :student_id }
+  validates :justification,    presence: true
 
-  scope :by_author, lambda { |author_id| where(author_id: author_id).uniq }
   scope :ordered, -> { order(arel_table[:absence_date]) }
+
+  # search scopes
+  scope :by_author, lambda { |author_id| where(author_id: author_id).uniq }
+  scope :by_classroom, lambda { |classroom| where(classroom: classroom) }
+  scope :by_student, lambda { |student| joins(:student).where('students.name ILIKE ?', "%#{student}%") }
+  scope :by_absence_date, lambda { |date| where(absence_date: date) }
+  scope :by_absence_date_end, lambda { |date| where(absence_date_end: date) }
 end
