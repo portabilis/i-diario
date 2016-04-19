e15f2412977f3721c1ef92ed29658372e7d68a99
Victor Arruda <victor@portabilis.com.br>
2016-04-19 17:43:47 -0300

    Ajustes na implementação; Refs #319

---
+++ db/migrate/20160419140313_set_teacher_id_to_absence_justifications.rb
@@ -0,0 +1,10 @@
+class SetTeacherIdToAbsenceJustifications < ActiveRecord::Migration
+  def change
+    execute <<-SQL
+      update absence_justifications
+        set teacher_id = (select users.teacher_id
+                            from users
+                              where users.id = absence_justifications.author_id) 
+    SQL
+  end
+end
