36f5cca4235582226fa6cc2f6dc8844a1e664b51
Lucas Schmoeller da Silva <lucasschmoellerdasilva@gmail.com>
2016-05-11 14:57:38 -0300

    Corrigidos testes de aceitação do Cadastro de Eventos
    Refs #316

--- spec/support/turnip/school_calendar_events_steps.rb
+++ spec/support/turnip/school_calendar_events_steps.rb
@@ -16,6 +16,7 @@ module Turnip
     step 'poderei cadastrar um novo evento do calendário letivo' do
       fill_mask 'Data', with: '25/12/2015'
       fill_in 'Descrição', with: 'Natal'
+      fill_in 'Legenda', with: 'E'
       select 'Não letivo', :from => 'Tipo'
 
       page.execute_script %{
