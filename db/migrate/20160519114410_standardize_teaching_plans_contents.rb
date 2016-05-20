be3d109219b498ff400f582e76667e2036c3f036
Lucas Schmoeller da Silva <lucasschmoellerdasilva@gmail.com>
2016-05-20 17:23:09 -0300

    Corrigido migração

--- db/migrate/20160519114410_standardize_teaching_plans_contents.rb
+++ db/migrate/20160519114410_standardize_teaching_plans_contents.rb
@@ -2,7 +2,7 @@ class StandardizeTeachingPlansContents < ActiveRecord::Migration
   def change
     rename_column :teaching_plans, :content, :old_contents
     TeachingPlan.all.each do |teaching_plan|
-      teaching_plan.contents = teaching_plan.old_contents.split(/\s*[,;]\s* | [\r\n]+ /x).reject(&:empty?).map{|v| Content.find_or_create_by!(description: v)}
+      teaching_plan.contents = teaching_plan.old_contents.split(/\s*[,;]\s* | [\r\n]+ /x).map(&:strip!).reject(&:blank?).map{|v| Content.find_or_create_by!(description: v)}
       teaching_plan.save!
     end
   end
--- db/migrate/20160519184805_standardize_lesson_plans_contents.rb
+++ db/migrate/20160519184805_standardize_lesson_plans_contents.rb
@@ -2,7 +2,7 @@ class StandardizeLessonPlansContents < ActiveRecord::Migration
   def change
     rename_column :lesson_plans, :contents, :old_contents
     LessonPlan.all.each do |lesson_plan|
-      lesson_plan.contents = lesson_plan.old_contents.split(/\s*[,;]\s* | [\r\n]+ /x).reject(&:empty?).map{|v| Content.find_or_create_by!(description: v)}
+      lesson_plan.contents = lesson_plan.old_contents.split(/\s*[,;]\s* | [\r\n]+ /x).map(&:strip!).reject(&:blank?).map{|v| Content.find_or_create_by!(description: v)}
       lesson_plan.save!
     end
   end
