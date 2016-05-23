13653a5e95c33156d5f356333b466169ad3d0772
Lucas Schmoeller da Silva <lucasschmoellerdasilva@gmail.com>
2016-05-23 19:40:52 -0300

    Corrigido migração de conteúdos dos planos para novo componente
    Refs #466 #467

--- db/migrate/20160523221003_fix_standardize_contents_migrations.rb
+++ db/migrate/20160523221003_fix_standardize_contents_migrations.rb
@@ -1,11 +1,13 @@
 class FixStandardizeContentsMigrations < ActiveRecord::Migration
   def change
     LessonPlan.where("created_at::date < '2016-05-21'::date ").each do |lesson_plan|
+      lesson_plan.contents.destroy_all
       lesson_plan.contents = lesson_plan.old_contents.split(/\s*[,;]\s* | [\r\n]+ /x).map(&:strip).reject(&:blank?).map{|v| Content.find_or_create_by!(description: v)}
       lesson_plan.save!
     end
-    TeachingPlan.all.each do |teaching_plan|
-      teaching_plan.contents = teaching_plan.old_contents.split(/\s*[,;]\s* | [\r\n]+ /x).map(&:strip!).reject(&:blank?).map{|v| Content.find_or_create_by!(description: v)}
+    TeachingPlan.where("created_at::date < '2016-05-21'::date ").all.each do |teaching_plan|
+      teaching_plan.contents.destroy_all
+      teaching_plan.contents = teaching_plan.old_contents.split(/\s*[,;]\s* | [\r\n]+ /x).map(&:strip).reject(&:blank?).map{|v| Content.find_or_create_by!(description: v)}
       teaching_plan.save!
     end
   end
