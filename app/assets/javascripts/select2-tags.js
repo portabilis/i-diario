ec353d873d2ebbafbdc3cf227a442484c8cf1f46
Lucas Schmoeller da Silva <lucasschmoellerdasilva@gmail.com>
2016-05-24 13:17:53 -0300

    Implementado limite de exibição de resultados no select2-tags
    Refs #469

--- app/assets/javascripts/select2-tags.js
+++ app/assets/javascripts/select2-tags.js
@@ -10,7 +10,23 @@ $(function() {
               text: $.trim(term) + ' (Novo conteúdo)'
           };
       },
-      data: $(element).data('elements')
+      data: $(element).data('elements'),
+      formatLoadMore   : 'Carregando mais...',
+      query            : function (q) {
+      // pageSize is number of results to show in dropdown
+          var pageSize,
+              results;
+              pageSize = 20;
+              results  = _.filter(this.data, function (e) {
+                  return (q.term === "" || e.text.toUpperCase().indexOf(q.term.toUpperCase()) >= 0);
+           });
+          q.callback({
+              results: results.slice((q.page - 1) * pageSize, q.page * pageSize),
+              // retrieve more when user hits bottom
+              more   : results.length >= q.page * pageSize
+          });
+      }
+
     });
   });
 });
