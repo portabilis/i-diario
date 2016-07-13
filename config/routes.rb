541d424ab729442fd8ce620d0e4444f514840f20
Victor Arruda <victor@portabilis.com.br>
2016-07-13 15:23:40 -0300

    Cadastro de grupos de escolas; Refs #542

--- config/routes.rb
+++ config/routes.rb
@@ -123,6 +123,7 @@ Rails.application.routes.draw do
     resources :***REMOVED***, concerns: :history
     resources :***REMOVED***s, concerns: :history
     resources :***REMOVED***, concerns: :history
+    resources :***REMOVED***, concerns: :history
     resources :courses, only: [:index]
     resources :lectures, only: [:index]
 
---
+++ test/models/school_group_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class School***REMOVED***Test < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
