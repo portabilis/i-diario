4eaa24a905009c6964b1431872dbefa3b762a4b1
Victor Arruda <victor@portabilis.com.br>
2016-08-17 15:52:38 -0300

    Ajuste no teste; Refs #594

--- spec/factories/teaching_plans.rb
+++ spec/factories/teaching_plans.rb
@@ -8,7 +8,7 @@ FactoryGirl.define do
     grade
   end
 
-  factory :teaching_plan_without_contents do
+  factory :teaching_plan_without_contents, class: TeachingPlan do
     year 2015
     school_term_type SchoolTermTypes::SEMESTER
     school_term      Semesters::FIRST_SEMESTER
