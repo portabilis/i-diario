22439490dfd3dbd640477d6a16aea9992315aec9
Gabriel Souza <gabrielmatosdesouza@gmail.com>
2016-08-24 16:20:10 -0300

    Adicionado novo relatório registro de conteúdos por disciplina; Refs #602

--- app/controllers/discipline_lesson_plan_report_controller.rb
+++ app/controllers/discipline_lesson_plan_report_controller.rb
@@ -1,4 +1,6 @@
 class DisciplineLessonPlanReportController < ApplicationController
+  DISCIPLINE_LESSON_PLAN_REPORT = "1"
+  DISCIPLINE_CONTENT_RECORD = "2"
 
   def form
     @discipline_lesson_plan_report_form = DisciplineLessonPlanReportForm.new
@@ -9,15 +11,28 @@ class DisciplineLessonPlanReportController < ApplicationController
   def report
     @discipline_lesson_plan_report_form = DisciplineLessonPlanReportForm.new(resource_params)
     @discipline_lesson_plan_report_form.teacher_id = current_teacher_id
+    @discipline_lesson_plan_report_form.report_type = report_type
 
     if @discipline_lesson_plan_report_form.valid?
-      lesson_plan_report = DisciplineLessonPlanReport.build(current_entity_configuration,
-                                                              @discipline_lesson_plan_report_form.date_start,
-                                                              @discipline_lesson_plan_report_form.date_end,
-                                                              @discipline_lesson_plan_report_form.discipline_lesson_plan,
-                                                              current_teacher)
 
-      send_data(lesson_plan_report.render, filename: 'registro-de-conteudos-por-disciplinas.pdf', type: 'application/pdf', disposition: 'inline')
+      if report_type == DISCIPLINE_LESSON_PLAN_REPORT
+        lesson_plan_report = DisciplineLessonPlanReport.build(current_entity_configuration,
+                                                                @discipline_lesson_plan_report_form.date_start,
+                                                                @discipline_lesson_plan_report_form.date_end,
+                                                                @discipline_lesson_plan_report_form.discipline_lesson_plan,
+                                                                current_teacher)
+
+        send_data(lesson_plan_report.render, filename: 'planos-de-ensino-por-disciplina.pdf', type: 'application/pdf', disposition: 'inline')
+      elsif report_type == DISCIPLINE_CONTENT_RECORD
+        content_record_report = DisciplineContentRecordReport.build(current_entity_configuration,
+                                                                @discipline_lesson_plan_report_form.date_start,
+                                                                @discipline_lesson_plan_report_form.date_end,
+                                                                @discipline_lesson_plan_report_form.discipline_content_record,
+                                                                current_teacher)
+
+        send_data(content_record_report.render, filename: 'registro-de-conteudos-por-disciplinas.pdf', type: 'application/pdf', disposition: 'inline')
+      end
+
     else
       @discipline_lesson_plan_report_form
       fetch_collections
@@ -37,5 +52,9 @@ class DisciplineLessonPlanReportController < ApplicationController
                                                           :discipline_id,
                                                           :date_start,
                                                           :date_end)
+  end
+
+  def report_type
+    params[:report_type]
   end
 end
