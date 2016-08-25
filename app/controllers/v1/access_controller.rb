85467f318fc784ae6f489c33b074b4f329ab05af
Lucas Schmoeller da Silva <lucasschmoellerdasilva@gmail.com>
2016-08-25 11:11:26 -0300

    Validações adicionar no enviarfimacesso
    Refs #595

--- app/controllers/v1/access_controller.rb
+++ app/controllers/v1/access_controller.rb
@@ -38,22 +38,38 @@ class V1::AccessController < V1::BaseController
   def send_access
     code = 1
     msg = "OK"
-    if params[:TipoAcesso] != 3
-      unity = Unity.find_by(api_code: params[:IdUnidade])
-      student = Student.find_by(api_code: params[:IdAluno])
+    access_type = params[:TipoAcesso].to_i
 
-      access = ***REMOVED***.new(
-        transaction_date: Time.parse(params[:DataHora]),
-        student: student,
-        unity: unity,
-        unity_equipment: UnityEquipment.find_by(code: params[:IdEquipamento], unity_id: unity.id),
-        operation: params[:TipoAcesso] == 1 ? "entrance" : "exit"
-      )
+    if [1,2].include? access_type
 
-      if !access.save
+      begin
+
+        unity = Unity.find_by(api_code: params[:IdUnidade])
+        student = Student.find_by(api_code: params[:IdAluno])
+
+        access = ***REMOVED***.new(
+          transaction_date: Time.parse(params[:DataHora]),
+          student: student,
+          unity: unity,
+          unity_equipment: UnityEquipment.find_by(code: params[:IdEquipamento], unity_id: unity.try(:id)),
+          operation: access_type == 1 ? "entrance" : "exit"
+        )
+
+        if !access.save
+          code = 2
+          msg = access.errors.full_messages.join(", ")
+        end
+
+      rescue ArgumentError
         code = 2
-        msg = access.errors.full_messages.join(", ")
+        msg = "Data e hora inválida"
+      rescue
+        code = 2
+        msg = "Erro desconhecido"
       end
+    elsif access_type != 3
+      code = 2
+      msg = "Sentido inválido"
     end
 
     render json: {
