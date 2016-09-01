7d8140ce104534e416ca03b5784ba3769d210245
Lucas Schmoeller da Silva <lucasschmoellerdasilva@gmail.com>
2016-09-01 09:43:26 -0300

    Ajustes na API solicitaracesso
    Refs #626

--- app/services/request_access_verifier.rb
+++ app/services/request_access_verifier.rb
@@ -12,13 +12,13 @@ class RequestAccessVerifier
   def process!
     unless [1,2,3].include? @way
       self.response_msg = "Sentido inválido"
-      valid = false
+      self.valid = false
       return false
     end
 
     unless [1,2,3].include? @request_type
       self.response_msg = "Tipo de consulta inválida"
-      valid = false
+      self.valid = false
       return false
     end
 
@@ -26,28 +26,28 @@ class RequestAccessVerifier
       Time.parse @time
     rescue ArgumentError
       self.response_msg = "Data e hora inválida"
-      valid = false
+      self.valid = false
       return false
     end
 
     student = Student.find_by(api_code: @student_api_code)
     if student.blank?
       self.response_msg = "Aluno inválido"
-      valid = false
+      self.valid = false
       return false
     end
 
     unity = Unity.find_by(api_code: @unity_api_code)
     if unity.blank?
       self.response_msg = "Escola inválida"
-      valid = false
+      self.valid = false
       return false
     end
 
     unity_equipment = UnityEquipment.find_by(code: @unity_equipment_code, unity_id: unity.id)
     if unity_equipment.blank?
       self.response_msg = "Equipamento inválido"
-      valid = false
+      self.valid = false
       return false
     end
 
@@ -55,7 +55,7 @@ class RequestAccessVerifier
       student_biometric = StudentBiometric.find_by(biometric_type: unity_equipment.biometric_type, student_id: student.id)
       if student_biometric.blank?
         self.response_msg = "Biometria não cadastrada"
-        valid = false
+        self.valid = false
         return false
       end
       self.biometric = student_biometric.biometric
@@ -63,12 +63,12 @@ class RequestAccessVerifier
 
     if !StudentUnityChecker.new(student, unity).present?
       self.response_msg = "Acesso negado"
-      valid = false
+      self.valid = false
       return false
     end
 
     self.response_msg = "Acesso permitido"
-    valid = true
+    self.valid = true
   end
 
   attr_reader :response_msg, :valid, :biometric
