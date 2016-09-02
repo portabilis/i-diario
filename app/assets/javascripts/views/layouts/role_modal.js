db4f2699800ecfe7e2f62f5864dd32004e007bda
Gabriel Souza <gabrielmatosdesouza@gmail.com>
2016-09-02 16:33:04 -0300

    Ajustado comportamento da modal de perfil; Refs #612

--- app/assets/javascripts/views/layouts/role_modal.js
+++ app/assets/javascripts/views/layouts/role_modal.js
@@ -221,9 +221,11 @@
     fetchTeachers(unity_id);
     checkUnityType(unity_id);
 
-    $('#user_assumed_teacher_id').select2("data", []);
-    $('#user_current_classroom_id').select2("data", []);
-    $('#user_current_discipline_id').select2("data", []);
+    var emptyElements = insertEmptyElement([]);
+
+    $('#user_assumed_teacher_id').select2("data", emptyElements);
+    $('#user_current_classroom_id').select2("data", emptyElements);
+    $('#user_current_discipline_id').select2("data", emptyElements);
   });
 
   $('#user_assumed_teacher_id').on('change', function(){
