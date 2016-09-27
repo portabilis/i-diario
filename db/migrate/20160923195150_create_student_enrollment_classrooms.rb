763c5726b34bb44bdb9545b0969ed89f5bc560ca
Gabriel Souza <gabrielmatosdesouza@gmail.com>
2016-09-27 18:32:34 -0300

    Ajustada sincronização de movimentação de enturmação: * Criado tabela de sincronização de enturmação; * Modificado tipos de campos date para string; * Adicionado campo ativo; Refs #647

--- db/migrate/20160923195150_create_student_enrollment_classrooms.rb
+++ db/migrate/20160923195150_create_student_enrollment_classrooms.rb
@@ -2,9 +2,9 @@ class CreateStudentEnrollmentClassrooms < ActiveRecord::Migration
   def change
     create_table :student_enrollment_classrooms do |t|
       t.string :api_code
+      t.references :student_enrollment, index: true, foreign_key: true
       t.references :classroom, index: true, foreign_key: true
       t.string :classroom_code
-      t.references :student_enrollment, index: true, foreign_key: true
       t.date :joined_at
       t.date :left_at
       t.timestamp :updated_at
