6196e129fe22cef6fca7c0a5abbd41db686547f6
Gabriel Souza <gabrielmatosdesouza@gmail.com>
2016-10-06 14:06:47 -0300

    Ajustes na sincronização de matrículas; Refs #647

--- app/services/student_enrollment_synchronizer.rb
+++ app/services/student_enrollment_synchronizer.rb
@@ -80,20 +80,20 @@ class StudentEnrollmentSynchronizer
     end
 
     if record["enturmacoes"].present?
+      any_updated_or_new_record = false
       record["enturmacoes"].each do |record_classroom|
         if student_enrollment_classroom = student_enrollment.student_enrollment_classrooms.find_by(api_code: record_classroom["sequencial"])
-          if record_classroom["data_atualizacao"].to_s > student_enrollment_classroom.changed_at.to_s
-            student_enrollment_classroom.update(
-              api_code: record_classroom["sequencial"],
-              classroom_id: Classroom.find_by(api_code: record_classroom["turma_id"]).try(:id),
-              classroom_code: record_classroom["turma_id"],
-              joined_at: record_classroom["data_entrada"],
-              left_at: record_classroom["data_saida"],
-              changed_at: record_classroom["data_atualizacao"].to_s,
-              sequence: record_classroom["sequencial_fechamento"]
-            )
-          end
+          any_updated_or_new_record = record_classroom["data_atualizacao"].to_s > student_enrollment_classroom.changed_at.to_s
+          break if any_updated_or_new_record
         else
+          any_updated_or_new_record = true
+          break
+        end
+      end
+
+      if any_updated_or_new_record
+        student_enrollment.student_enrollment_classrooms.destroy_all
+        record["enturmacoes"].each do |record_classroom|
           student_enrollment.student_enrollment_classrooms.create(
             api_code: record_classroom["sequencial"],
             classroom_id: Classroom.find_by(api_code: record_classroom["turma_id"]).try(:id),
@@ -103,6 +103,20 @@ class StudentEnrollmentSynchronizer
             changed_at: record_classroom["data_atualizacao"].to_s,
             sequence: record_classroom["sequencial_fechamento"]
           )
+        end
+      else
+        record["enturmacoes"].each do |record_classroom|
+          if !student_enrollment.student_enrollment_classrooms.find_by(api_code: record_classroom["sequencial"])
+            student_enrollment.student_enrollment_classrooms.create(
+              api_code: record_classroom["sequencial"],
+              classroom_id: Classroom.find_by(api_code: record_classroom["turma_id"]).try(:id),
+              classroom_code: record_classroom["turma_id"],
+              joined_at: record_classroom["data_entrada"],
+              left_at: record_classroom["data_saida"],
+              changed_at: record_classroom["data_atualizacao"].to_s,
+              sequence: record_classroom["sequencial_fechamento"]
+            )
+          end
         end
       end
     end
