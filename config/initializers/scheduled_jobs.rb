966c2891236089f1d7fb796326b60141934512a9
Gabriel Souza <gabrielmatosdesouza@gmail.com>
2016-10-14 11:02:07 -0300

    Ajustado tempo de sincronizaçao em ambiente de testes para 5 minutos; Refs #651

--- config/initializers/scheduled_jobs.rb
+++ config/initializers/scheduled_jobs.rb
@@ -1,6 +1,6 @@
 Sidekiq::Cron::Job.destroy_all!
 if Rails.env.staging? || Rails.env.development?
-  Sidekiq::Cron::Job.create(name: "I-Educar Synchronization - every 2 min", cron: "*/2 * * * *", class: "IeducarSynchronizerWorker")
+  Sidekiq::Cron::Job.create(name: "I-Educar Synchronization - every 5 min", cron: "*/5 * * * *", class: "IeducarSynchronizerWorker")
 elsif Rails.env.production?
   Sidekiq::Cron::Job.create(name: "I-Educar Synchronization - every 10 min", cron: "*/10 * * * *", class: "IeducarSynchronizerWorker")
 end
