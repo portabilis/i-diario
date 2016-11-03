96c1df65aec6fa015fdf162c89978f57452e38c2
Lucas Schmoeller da Silva <lucasschmoellerdasilva@gmail.com>
2016-11-03 14:05:31 -0200

    Gravar auditoria de usuário lançamento frequência pelo app
    Refs #760

--- app/controllers/api/v1/daily_frequency_students_controller.rb
+++ app/controllers/api/v1/daily_frequency_students_controller.rb
@@ -4,13 +4,19 @@ class Api::V1::DailyFrequencyStudentsController < Api::V1::BaseController
   respond_to :json
 
   def update
-    user_id = params[:user_id] || 1
-
-    Audit.as_user(User.find(user_id)) do
-      daily_frequency_student = DailyFrequencyStudent.find(params[:id])
-      daily_frequency_student.update(present: params[:present])
-    end
+    daily_frequency_student = DailyFrequencyStudent.find(params[:id])
+    daily_frequency_student.update(present: params[:present])
 
     respond_with daily_frequency_student
+  end
+
+  def current_user
+    User.find(user_id)
+  end
+
+  protected
+
+  def user_id
+    @user_id ||= params[:user_id] || 1
   end
 end
