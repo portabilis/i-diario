ae9b50de688e6efdd5e41a25a1cdf81e5c05a5fe
Gabriel Souza <gabrielmatosdesouza@gmail.com>
2016-11-07 13:59:28 -0200

    Ajustado query de busca de enturmações de alunos; Refs #805

--- app/models/student_enrollment_classroom.rb
+++ app/models/student_enrollment_classroom.rb
@@ -3,6 +3,6 @@ class StudentEnrollmentClassroom < ActiveRecord::Base
   belongs_to :student_enrollment
 
   scope :by_classroom, lambda { |classroom_id| where(classroom_id: classroom_id) }
-  scope :by_date, lambda { |date| where("(? >= joined_at AND ? <= left_at) OR (? >= joined_at AND coalesce(left_at) = '')", date, date, date) }
+  scope :by_date, lambda { |date| where("(? >= joined_at AND ? < left_at) OR (? >= joined_at AND coalesce(left_at) = '')", date, date, date) }
   scope :by_date_not_before, lambda { |date| where.not('joined_at < ?', date) }
 end
