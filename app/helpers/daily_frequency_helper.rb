dc7df38315d21654cb95aa8efbcfe25e08983d31
Eliezer Salvato <eliezer@portabilis.com.br>
2017-01-31 11:52:11 -0200

    Ajustado erro ao lançar frequência * Ajustado para verificar se possuí calendario de classe antes de buscar a data, para não ocorrer erro de undefined method se retornar nulo Refs #1009

--- app/helpers/daily_frequency_helper.rb
+++ app/helpers/daily_frequency_helper.rb
@@ -15,9 +15,8 @@ module DailyFrequencyHelper
 
   def params_for_print_step daily_frequencies
     daily_frequency = daily_frequencies[0]
-    start_at = daily_frequency.school_calendar.classrooms.by_classroom(daily_frequency.classroom_id).first.classroom_step(daily_frequency.frequency_date).start_at || daily_frequency.school_calendar.step(daily_frequency.frequency_date).start_at
-    end_at = daily_frequency.school_calendar.classrooms.by_classroom(daily_frequency.classroom_id).first.classroom_step(daily_frequency.frequency_date).end_at || daily_frequency.school_calendar.step(daily_frequency.frequency_date).end_at
-
+    start_at = get_start_at(daily_frequency)
+    end_at = get_end_at(daily_frequency)
     {
       unity_id: daily_frequency.unity_id,
       classroom_id: daily_frequency.classroom_id,
@@ -28,6 +27,30 @@ module DailyFrequencyHelper
       school_calendar_year: daily_frequency.frequency_date.year,
       current_teacher_id: current_teacher.id
     }
+  end
+
+  def get_start_at(daily_frequency)
+    if daily_frequency.classroom.calendar
+      get_school_calendar_classroom_step(daily_frequency).start_at
+    else
+      get_school_calendar_step(daily_frequency).start_at
+    end
+  end
+
+  def get_end_at(daily_frequency)
+    if daily_frequency.classroom.calendar
+      get_school_calendar_classroom_step(daily_frequency).end_at
+    else
+      get_school_calendar_step(daily_frequency).end_at
+    end
+  end
+
+  def get_school_calendar_step(daily_frequency)
+    daily_frequency.school_calendar.step(daily_frequency.frequency_date)
+  end
+
+  def get_school_calendar_classroom_step(daily_frequency)
+    daily_frequency.school_calendar.classrooms.by_classroom(daily_frequency.classroom_id).first.classroom_step(daily_frequency.frequency_date)
   end
 
   def frequency_student_name_class(dependence, active)
