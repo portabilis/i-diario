7419ca2fc0844a31e3e2fa7d2076f12036fd0ac5
Victor Arruda <victor@portabilis.com.br>
2017-04-12 17:09:09 -0300

    Teste de configuração do paperclip;

--- app/models/lesson_plan_attachment.rb
+++ app/models/lesson_plan_attachment.rb
@@ -1,8 +1,6 @@
 class LessonPlanAttachment < ActiveRecord::Base
   belongs_to :lesson_plan
 
-  attr_accessor :entity_name
-
   has_attached_file :attachment,
                     url: "/:class/#{Entity.current.try(:name)}/:id/:basename.:extension",
                     path: "/:class/#{Entity.current.try(:name)}/:id/:basename.:extension"
@@ -10,4 +8,6 @@ class LessonPlanAttachment < ActiveRecord::Base
                                                         /doc\z/, /docx\z/, /ods\z/, /xls\z/, /xlsx\z/, /odp\z/,
                                                         /ppt\z/, /pptx\z/, /odg\z/, /xml\z/, /csv\z/]
   validates_with AttachmentSizeValidator, attributes: :attachment, less_than: 512.kilobytes
+
+  do_not_validate_attachment_file_type :attachment
 end
