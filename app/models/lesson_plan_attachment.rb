d5d4c18e5d88d7fa15ccf19e0a9121e1a6d0ff6d
Victor Arruda <victor@portabilis.com.br>
2017-04-12 16:09:08 -0300

    Ajuste na busca de entidade atual nos anexos do plano de aula;
    Refs #1166

--- app/models/lesson_plan_attachment.rb
+++ app/models/lesson_plan_attachment.rb
@@ -4,8 +4,8 @@ class LessonPlanAttachment < ActiveRecord::Base
   attr_accessor :entity_name
 
   has_attached_file :attachment,
-                    url: "/:class/#{Entity.current.name}/:id/:basename.:extension",
-                    path: "/:class/#{Entity.current.name}/:id/:basename.:extension"
+                    url: "/:class/#{Entity.current.try(:name)}/:id/:basename.:extension",
+                    path: "/:class/#{Entity.current.try(:name)}/:id/:basename.:extension"
   validates_attachment_file_name :attachment, matches: [/png\z/, /jpeg\z/, /jpg\z/, /gif\z/, /pdf\z/, /odt\z/,
                                                         /doc\z/, /docx\z/, /ods\z/, /xls\z/, /xlsx\z/, /odp\z/,
                                                         /ppt\z/, /pptx\z/, /odg\z/, /xml\z/, /csv\z/]
