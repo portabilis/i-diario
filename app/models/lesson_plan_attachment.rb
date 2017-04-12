ce2742a3d3abf5c0689eae794ed99bdd34a63ecf
Victor Arruda <victor@portabilis.com.br>
2017-04-12 17:41:09 -0300

    Ajustado mime type;

--- app/models/lesson_plan_attachment.rb
+++ app/models/lesson_plan_attachment.rb
@@ -9,5 +9,5 @@ class LessonPlanAttachment < ActiveRecord::Base
                                                         /ppt\z/, /pptx\z/, /odg\z/, /xml\z/, /csv\z/]
   validates_with AttachmentSizeValidator, attributes: :attachment, less_than: 512.kilobytes
 
-  validate_attachment :attachment, content_type: { content_type: "application/pdf" }
+  validates_attachment :attachment, content_type: { content_type: "application/pdf" }
 end
