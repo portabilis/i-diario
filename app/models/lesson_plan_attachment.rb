bc63002fbb561b2a5d70ab67582d758283f92254
Victor Arruda <victor@portabilis.com.br>
2017-04-12 18:47:09 -0300

    Removido validações;

--- app/models/lesson_plan_attachment.rb
+++ app/models/lesson_plan_attachment.rb
@@ -4,10 +4,10 @@ class LessonPlanAttachment < ActiveRecord::Base
   has_attached_file :attachment,
                     url: "/:class/#{Entity.current.try(:name)}/:id/:basename.:extension",
                     path: "/:class/#{Entity.current.try(:name)}/:id/:basename.:extension"
-  validates_attachment_file_name :attachment, matches: [/png\z/, /jpeg\z/, /jpg\z/, /gif\z/, /pdf\z/, /odt\z/,
-                                                        /doc\z/, /docx\z/, /ods\z/, /xls\z/, /xlsx\z/, /odp\z/,
-                                                        /ppt\z/, /pptx\z/, /odg\z/, /xml\z/, /csv\z/]
-  validates_with AttachmentSizeValidator, attributes: :attachment, less_than: 512.kilobytes
+  # validates_attachment_file_name :attachment, matches: [/png\z/, /jpeg\z/, /jpg\z/, /gif\z/, /pdf\z/, /odt\z/,
+  #                                                       /doc\z/, /docx\z/, /ods\z/, /xls\z/, /xlsx\z/, /odp\z/,
+  #                                                       /ppt\z/, /pptx\z/, /odg\z/, /xml\z/, /csv\z/]
+  # validates_with AttachmentSizeValidator, attributes: :attachment, less_than: 512.kilobytes
 
-  validates_attachment :attachment, content_type: { content_type: "application/pdf" }
+  # validates_attachment :attachment, content_type: { content_type: "application/pdf" }
 end
