1e361b89a2e8d0d2674526ceef1d332ac1a6106c
Victor Arruda <victor@portabilis.com.br>
2017-04-13 10:09:41 -0300

    Ajuste na criação de pastas do bucket na AWS;
    Refs #1166

--- app/models/lesson_plan_attachment.rb
+++ app/models/lesson_plan_attachment.rb
@@ -3,8 +3,8 @@ class LessonPlanAttachment < ActiveRecord::Base
 
   has_attached_file :attachment,
                     validate_media_type: false,
-                    url: "/:class/#{Entity.current.try(:name)}/:id/:basename.:extension",
-                    path: "/:class/#{Entity.current.try(:name)}/:id/:basename.:extension"
+                    url: "/#{Rails.env}/:class/:id/:basename-#{SecureRandom.hex(10)}.:extension",
+                    path: "/#{Rails.env}/:class/:id/:basename-#{SecureRandom.hex(10)}.:extension"
   validates_attachment_file_name :attachment, matches: [/png\z/, /jpeg\z/, /jpg\z/, /gif\z/, /pdf\z/, /odt\z/,
                                                         /doc\z/, /docx\z/, /ods\z/, /xls\z/, /xlsx\z/, /odp\z/,
                                                         /ppt\z/, /pptx\z/, /odg\z/, /xml\z/, /csv\z/]
