42e1017dabe67a2680e19774f2daf4714275b411
Gabriel Souza <gabrielmatosdesouza@gmail.com>
2017-04-14 09:35:49 -0300

    Ajustado tamanho do campo data; Refs #1161

--- app/controllers/daily_notes_controller.rb
+++ app/controllers/daily_notes_controller.rb
@@ -3,7 +3,7 @@ class DailyNotesController < ApplicationController
   has_scope :per, default: 10
 
   before_action :require_teacher
-  before_action :require_current_school_calendar, only: [:index, :new]
+  before_action :require_current_school_calendar
 
   def index
     @daily_notes = apply_scopes(DailyNote)
