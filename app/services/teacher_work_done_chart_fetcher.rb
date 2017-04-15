ac7db282a1921d4259b1e64e868fb71c966f775c
Gabriel Souza <gabrielmatosdesouza@gmail.com>
2017-04-15 11:08:11 -0300

    Modificado forma de busca de informação no gráfico: * Ajustado para buscar informação dos lançamentos realizados; Refs #1191

--- app/services/teacher_work_done_chart_fetcher.rb
+++ app/services/teacher_work_done_chart_fetcher.rb
@@ -12,22 +12,23 @@ class TeacherWorkDoneChartFetcher
                           .by_discipline_id(discipline)
                           .by_school_calendar_step(school_calendar_step)
 
-    student_enrollments_count = 0
-    teacher_avaliations.each do |avaliation|
-      student_enrollments_count += StudentEnrollmentsList.new(classroom: avaliation.classroom,
-                                                           discipline: avaliation.discipline,
-                                                           date: avaliation.test_date,
-                                                           show_inactive: false).student_enrollments.count
-    end
-
     daily_notes_ids = DailyNote.by_avaliation_id(teacher_avaliations).pluck(:id)
-    daily_note_students = DailyNoteStudent.where(daily_note_id: daily_notes_ids, active: true).where.not(note: nil).reject(&:exempted?)
+    completed_daily_note_students = DailyNoteStudent
+                                    .where(daily_note_id: daily_notes_ids,
+                                           active: true)
+                                    .where.not(note: nil)
+                                    .reject(&:exempted?)
 
-    completed_notes_count = daily_note_students.count
+    all_daily_note_students = DailyNoteStudent
+                              .where(daily_note_id: daily_notes_ids, active: true)
+                              .reject(&:exempted?)
 
-    pending_notes_count = student_enrollments_count - completed_notes_count
+    completed_daily_note_students_count = completed_daily_note_students.count
+    all_daily_note_students_count = all_daily_note_students.count
 
-    { pending_notes_count: pending_notes_count, completed_notes_count: completed_notes_count }
+    pending_notes_count = all_daily_note_students_count - completed_daily_note_students_count
+
+    { pending_notes_count: pending_notes_count, completed_notes_count: completed_daily_note_students_count }
   end
 
   private
