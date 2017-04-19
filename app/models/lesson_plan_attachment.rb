c49e277ee4ac3774e660de271accd090b076b035
Victor Arruda <victor@portabilis.com.br>
2017-04-19 18:06:27 -0300

    Ajuste no download dos anexos;

--- app/models/lesson_plan_attachment.rb
+++ app/models/lesson_plan_attachment.rb
@@ -5,7 +5,7 @@ class LessonPlanAttachment < ActiveRecord::Base
                     validate_media_type: false,
                     url: "/#{Rails.env}/:class/:id/:basename-:hash.:extension",
                     path: "/#{Rails.env}/:class/:id/:basename-:hash.:extension",
-                    hash_secret: "#{SecureRandom.hex(10)}"
+                    hash_secret: "#{Rails.application.secrets.secret_key_base}"
   validates_attachment_file_name :attachment, matches: [/png\z/, /jpeg\z/, /jpg\z/, /gif\z/, /pdf\z/, /odt\z/,
                                                         /doc\z/, /docx\z/, /ods\z/, /xls\z/, /xlsx\z/, /odp\z/,
                                                         /ppt\z/, /pptx\z/, /odg\z/, /xml\z/, /csv\z/]
