aa65f939435dccf8ab22f8e6a1089b726bbf318c
Caroline Salib <carolinesalibc@gmail.com>
2017-05-02 18:22:07 -0300

    Ajustado para trazer descrição correta das avaliações na dashboard; Refs #1216

--- app/serializers/avaliation_serializer.rb
+++ app/serializers/avaliation_serializer.rb
@@ -1,4 +1,8 @@
 class AvaliationSerializer < ActiveModel::Serializer
   attributes :id, :description_to_teacher, :test_date, :description, :classroom,
-             :discipline, :test_date_humanized, :test_date_today
+             :discipline, :test_date_humanized, :test_date_today, :to_s
+
+   def to_s
+     object.to_s
+   end
 end
