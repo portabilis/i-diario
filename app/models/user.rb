7299fb767e6f99b74632cd0cdb977f713a355b6b
Caroline Salib <carolinesalibc@gmail.com>
2017-05-05 14:50:26 -0300

    Ajustado para só atualizar lead em produção; Refs #1249

--- app/models/user.rb
+++ app/models/user.rb
@@ -257,7 +257,7 @@ class User < ActiveRecord::Base
 
   def update_rd_lead
     return unless GeneralConfiguration.current.allows_after_sales_relationship?
-    return if Rails.env.test?
+    return unless Rails.env.production?
     rdstation_client = RDStation::Client.new('***REMOVED***', '***REMOVED***', 'Usuário no produto Educar+')
 
     response = rdstation_client.create_lead({
