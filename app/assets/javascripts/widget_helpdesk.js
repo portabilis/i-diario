5c3ef93cc629bba796f43caec0f48029ddae5c63
Caroline Salib <carolinesalibc@gmail.com>
2017-05-15 10:00:08 -0300

    Ajustado widget do helpdesk para carregar no application; Refs #1277

---
+++ app/assets/javascripts/widget_helpdesk.js
@@ -0,0 +1,13 @@
+FreshWidget.init("", {
+    "queryString": "&widgetType=popup",
+    "utf8": "✓",
+    "widgetType": "popup",
+    "buttonType": "text",
+    "buttonText": "Suporte",
+    "buttonColor": "white",
+    "buttonBg": "#004c93",
+    "alignment": "4",
+    "offset": "235px",
+    "formHeight": "500px",
+    "url": "http://suporte.portabilis.com.br"
+});
