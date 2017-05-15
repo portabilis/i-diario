b76d53356f8f45a4d1a42e77665c09559ac978b4
Caroline Salib <carolinesalibc@gmail.com>
2017-05-15 14:14:55 -0300

    Ajustado posição do widget de suporte; Refs #1279

--- app/assets/javascripts/freshwidget_call.js
+++ app/assets/javascripts/freshwidget_call.js
@@ -6,7 +6,7 @@ FreshWidget.init("", {
     "buttonText": "Suporte",
     "buttonColor": "white",
     "buttonBg": "#004c93",
-    "alignment": "2",
+    "alignment": "3",
     "offset": "235px",
     "formHeight": "500px",
     "url": "http://suporte.portabilis.com.br"
