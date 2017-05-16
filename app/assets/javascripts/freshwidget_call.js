4c40c66a0b697e1314a5f0198485b92f52267382
Caroline Salib <carolinesalibc@gmail.com>
2017-05-16 10:50:56 -0300

    Ajustado cor do link de suporte; Refs #1277

--- app/assets/javascripts/freshwidget_call.js
+++ app/assets/javascripts/freshwidget_call.js
@@ -5,7 +5,7 @@ FreshWidget.init("", {
     "buttonType": "text",
     "buttonText": "Precisa de ajuda? Clique aqui",
     "buttonColor": "white",
-    "buttonBg": "#004c93",
+    "buttonBg": "#0ac336",
     "alignment": "3",
     "offset": "235px",
     "formHeight": "500px",
