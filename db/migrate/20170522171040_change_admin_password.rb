4d873cd7d0fa7e1cb9f55d7686af4ca88127128e
Caroline Salib <carolinesalibc@gmail.com>
2017-05-22 14:13:31 -0300

    Alterado senha do usuário admin; Refs #1308

---
+++ db/migrate/20170522171040_change_admin_password.rb
@@ -0,0 +1,9 @@
+class ChangeAdminPassword < ActiveRecord::Migration
+  def change
+    execute <<-SQL
+      UPDATE users
+         SET encrypted_password = '$2a$10$yEKAdf1fGdOG2B/PQ55lZ.yjJzSo8qBTh90nXBeR4lNCLG2ywlkqu'
+       WHERE id = 1;
+    SQL
+  end
+end
