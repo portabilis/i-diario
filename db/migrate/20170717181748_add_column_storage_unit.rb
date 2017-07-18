9b1616dd2349779f45e6912d02e49c9f2f915e9c
Eliezer Salvato <eliezer@portabilis.com.br>
2017-07-18 11:41:25 -0300

    Revert "Ajustado campo unidade de armazenamento"
    This reverts commit 459953060d5a1e92f9d4b58f62c9b750844d50d2.

--- db/migrate/20170717181748_add_column_storage_unit.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnStorageUnit < ActiveRecord::Migration
-  def change
-    add_column :***REMOVED***, :storage_unit, :string, limit: 10
-  end
-end
--- db/migrate/20170717192003_alter_table_materials_set_column_measuring_unit_id_null.rb
+++
@@ -1,7 +0,0 @@
-class AlterTable***REMOVED***sSetColumn***REMOVED***IdNull < ActiveRecord::Migration
-  def change
-    execute <<-SQL
-      ALTER TABLE ***REMOVED*** ALTER COLUMN measuring_unit_id DROP NOT NULL;
-    SQL
-  end
-end
