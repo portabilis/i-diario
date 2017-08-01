204d4a9b358d5c09c127d078d0641bdbf8aeb940
Caroline Salib <carolinesalibc@gmail.com>
2017-08-01 16:36:22 -0300

    Adicionado campo para retornar nome da unidade e turma; Refs #1490

--- app/serializers/daily_frequency_serializer.rb
+++ app/serializers/daily_frequency_serializer.rb
@@ -1,5 +1,14 @@
 class DailyFrequencySerializer < ActiveModel::Serializer
-  attributes :id, :unity_id, :classroom_id, :frequency_date, :discipline_id, :class_number
+  attributes :id, :unity_id, :unity_name, :classroom_id, :classroom_name,
+             :frequency_date, :discipline_id, :class_number
 
   has_many :students
+
+  def unity_name
+    object.unity.name
+  end
+
+  def classroom_name
+    object.classroom.description
+  end
 end
