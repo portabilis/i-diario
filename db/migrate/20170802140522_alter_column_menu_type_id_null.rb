4df90f9549d442beb9ba5df6878fb94c5334a739
Eliezer Salvato <eliezer@portabilis.com.br>
2017-08-02 14:51:04 -0300

    Ajustado de outra forma, pois estava perdendo os dados antigos Refs #1486

--- db/migrate/20170802140522_alter_column_menu_type_id_null.rb
+++
@@ -1,6 +0,0 @@
-class AlterColumn***REMOVED***IdNull < ActiveRecord::Migration
-  def change
-    execute "ALTER TABLE ***REMOVED***s DROP COLUMN ***REMOVED***_type_id"
-    execute "ALTER TABLE ***REMOVED***s ADD COLUMN ***REMOVED***_type_id int"
-  end
-end
