64be4e15bf482cdd9fa4bf045922e66a3dbc6c1d
Gabriel Souza <gabrielmatosdesouza@gmail.com>
2017-09-06 17:05:30 -0300

    Ajustado erro ao sincronizar vinculos de professores; Refs #1559

--- app/services/teachers_synchronizer.rb
+++ app/services/teachers_synchronizer.rb
@@ -27,58 +27,54 @@ class TeachersSynchronizer
 
   def update_records(collection, year)
     ActiveRecord::Base.transaction do
-      collection.each do |record|
-        teacher = update_or_create_teacher(record)
-        record_id = record['id']
-        update_discipline_classrooms(record['disciplinas_turmas'], teacher, year, record_id)
-      end
-      inactivate_all_inexisting_allocations(collection, year)
+      update_discipline_classrooms(collection, year)
     end
   end
 
-  def update_discipline_classrooms(collection, teacher, year, record_id)
-    return unless teacher
-
-    # inactivate all records and activate when find
-    # teacher.teacher_discipline_classrooms.update_all(active: false)
-
+  def update_discipline_classrooms(collection, year)
+    existing_ids = []
     collection.each do |record|
-      if discipline_classroom = discipline_classrooms.unscoped.where(api_code: record_id,
-                                                                    teacher_api_code: teacher.api_code,
-                                                                    discipline_api_code: record['disciplina_id'],
-                                                                    classroom_api_code: record['turma_id'],
-                                                                    year: year,
-                                                                    allow_absence_by_discipline: record['permite_lancar_faltas_componente']).first
-        discipline_classroom.update_attributes(
-          active: true,
-          discipline_id: Discipline.find_by(api_code: discipline_classroom.discipline_api_code).try(:id),
-          classroom_id: Classroom.find_by(api_code: discipline_classroom.classroom_api_code).try(:id),
-          allow_absence_by_discipline: record['permite_lancar_faltas_componente']
-        )
-      else
-        discipline_classrooms.create!(
-          api_code: record_id,
-          year: year,
-          active: true,
-          teacher_id: teacher.id,
-          teacher_api_code: teacher.api_code,
-          discipline_id: Discipline.find_by(api_code: record['disciplina_id']).try(:id),
-          discipline_api_code: record['disciplina_id'],
-          classroom_id: Classroom.find_by(api_code: record['turma_id']).try(:id),
-          classroom_api_code: record['turma_id'],
-          allow_absence_by_discipline: record['permite_lancar_faltas_componente']
-        )
+      existing_ids << record['id']
+      teacher = update_or_create_teacher(record)
+      next unless teacher
+
+      teacher_discipline_classrooms = discipline_classrooms.unscoped.where(
+        api_code: record['id']
+      )
+
+      max_changed_at = teacher_discipline_classrooms.maximum(:changed_at)
+
+      if !max_changed_at || record['updated_at'] > max_changed_at
+        teacher_discipline_classrooms.destroy_all
+        create_discipline_classrooms(record, year, teacher)
       end
     end
+
+    destroy_inexisting_teacher_discipline_classrooms(existing_ids)
   end
 
-  def inactivate_all_inexisting_allocations(collection, year)
-    allocation_ids = collection.map{ |value| value['id'] }
+  private
 
-    allocations = discipline_classrooms.where(year: year)
-                                       .where.not(api_code: allocation_ids)
+  def destroy_inexisting_teacher_discipline_classrooms(existing_ids)
+    discipline_classrooms.where.not(api_code: existing_ids).destroy_all
+  end
 
-    allocations.update_all(active: false)
+  def create_discipline_classrooms(collection, year, teacher)
+    collection['disciplinas_turmas'].each do |record|
+      discipline_classrooms.create!(
+        api_code: collection['id'],
+        year: year,
+        active: true,
+        teacher_id: teacher.id,
+        teacher_api_code: teacher.api_code,
+        discipline_id: Discipline.find_by(api_code: record['disciplina_id']).try(:id),
+        discipline_api_code: record['disciplina_id'],
+        classroom_id: Classroom.find_by(api_code: record['turma_id']).try(:id),
+        classroom_api_code: record['turma_id'],
+        allow_absence_by_discipline: record['permite_lancar_faltas_componente'],
+        changed_at: collection['updated_at']
+      )
+    end
   end
 
   def teachers(klass = Teacher)
@@ -88,8 +84,6 @@ class TeachersSynchronizer
   def discipline_classrooms(klass = TeacherDisciplineClassroom)
     klass
   end
-
-  private
 
   def update_or_create_teacher(record)
     teacher = teachers.find_by(api_code: record['servidor_id'])
@@ -104,11 +98,7 @@ class TeachersSynchronizer
     teacher
   end
 
-  def inactivate_all_allocations_for(year)
-    discipline_classrooms.where(year: year).update_all(active: false)
-  end
-
   def inactive_all_alocations_prior_to(year)
-    discipline_classrooms.where('year < ?', year).update_all(active: false)
+    discipline_classrooms.unscoped.where('year < ?', year).destroy_all
   end
 end
