c8017e246b5b07c00329cdcbfd600c0f1af5ae7c
Caroline Salib <carolinesalibc@gmail.com>
2017-10-16 17:51:58 -0200

    Adicionado campo de sequencia na api de alunos enturmados; Refs #1664

--- app/serializers/student_enrollment_serializer.rb
+++ app/serializers/student_enrollment_serializer.rb
@@ -1,5 +1,9 @@
 class StudentEnrollmentSerializer < ActiveModel::Serializer
-  attributes :id, :student_id, :status, :active
+  attributes :id, :student_id, :status, :active, :sequence
 
   has_one :student
+
+  def sequence
+    object.student_enrollment_classrooms.first.try(:sequence)
+  end
 end
