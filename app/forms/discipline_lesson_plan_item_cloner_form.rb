5182dc44c801ffc80f73ab92f2162299167e55e7
Wilkinson Souza <wilkinson@outlook.com.br>
2018-01-25 14:50:10 -0200

    Corrige observações efetuadas no code review.
    Refs #732

--- app/forms/discipline_lesson_plan_item_cloner_form.rb
+++ app/forms/discipline_lesson_plan_item_cloner_form.rb
@@ -3,7 +3,5 @@ class DisciplineLessonPlanItemClonerForm < ActiveRecord::Base
 
   attr_accessor :discipline_lesson_plan_cloner_form_id, :classroom_id, :start_at, :end_at
 
-  validates :classroom_id, presence: true
-  validates :start_at, presence: true
-  validates :end_at, presence: true
+  validates :classroom_id, :start_at, :end_at, presence: true
 end
