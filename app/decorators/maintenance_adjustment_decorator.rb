d71df04dbbd190b19c986f771ed09efbfe0afb28
Eliezer Salvato <eliezer@portabilis.com.br>
2018-01-31 10:39:38 -0200

    Implementa tela para ajustes e ajuste de faltas * Implementa tela para ajustes com os campos ano, tipo, escolas, observação e status * Implementa worker para chamar os serviços de acordo com o ajuste a ser feito * Implementa mensagem ao terminar os ajustes * Implementa serviço de ajuste de faltas
    Refs #740

---
+++ app/decorators/maintenance_adjustment_decorator.rb
@@ -0,0 +1,33 @@
+class MaintenanceAdjustmentDecorator
+  include Decore
+  include Decore::Proxy
+  include ActionView::Helpers::TagHelper
+
+  def unities_labels
+    return unless component.unities
+
+    component.unities.map do |u|
+      content_tag(:p, content_tag(:span, u, class: 'label label-info label-list'))
+    end.join.html_safe
+  end
+
+  def status_labels
+    return unless component.status
+
+    spin = case component.status
+      when MaintenanceAdjustmentStatus::IN_PROGRESS then
+        content_tag(:span, content_tag(:i, '', class: 'fa fa-cog fa-spin', style: 'margin-right: 3px'))
+      else ''
+    end
+
+    status_class = case component.status
+      when MaintenanceAdjustmentStatus::PENDING then 'info'
+      when MaintenanceAdjustmentStatus::IN_PROGRESS then 'warning'
+      when MaintenanceAdjustmentStatus::COMPLETED then 'success'
+      when MaintenanceAdjustmentStatus::ERROR then 'danger'
+    end
+
+    content_tag(:p, spin << MaintenanceAdjustmentStatus.t(component.status),
+      class: 'label label-list label-' << status_class).html_safe
+  end
+end
---
+++ app/services/absence_adjustments_service.rb
@@ -0,0 +1,73 @@
+class AbsenceAdjustmentsService
+  def self.adjust(unities, year)
+    new(unities, year).adjust
+  end
+
+  def initialize(unities, year)
+    @unities = unities
+    @year = year
+  end
+
+  def adjust
+    adjust_by_discipline_to_general
+    adjust_general_to_by_discipline
+  end
+
+  private
+
+  def adjust_by_discipline_to_general
+    daily_frequencies_to_destroy = []
+    daily_frequencies = DailyFrequency.joins(:classroom).merge(Classroom.joins(:exam_rule)
+                                      .merge(ExamRule.where(frequency_type: FrequencyTypes::GENERAL)))
+                                      .where('extract(year from frequency_date) = ?', @year)
+                                      .where(unity_id: @unities)
+                                      .where.not(discipline_id: nil)
+
+    classroom_id = 0
+    discipline_id = 0
+    absence_type_definer = nil
+
+    daily_frequencies.each do |daily_frequency|
+      if classroom_id != daily_frequency.classroom_id && discipline_id != daily_frequency.discipline_id
+        teacher = TeacherDisciplineClassroom.where(classroom_id: daily_frequency.classroom_id,
+                                                   discipline_id: daily_frequency.discipline_id).first.teacher
+        absence_type_definer = FrequencyTypeDefiner.new(daily_frequency.classroom, teacher)
+        absence_type_definer.define!
+
+        classroom_id = daily_frequency.classroom_id
+        discipline_id = daily_frequency.discipline_id
+
+        unless (absence_type_definer.frequency_type == FrequencyTypes::BY_DISCIPLINE)
+          daily_frequency.update_columns(
+            discipline_id: nil,
+            class_number: nil
+          )
+        end
+      else
+        daily_frequencies_to_destroy << daily_frequency unless (absence_type_definer.frequency_type == FrequencyTypes::BY_DISCIPLINE)
+      end
+    end
+
+    daily_frequencies_to_destroy.destroy_all
+  end
+
+  def adjust_general_to_by_discipline
+    daily_frequencies = DailyFrequency.joins(:classroom).merge(Classroom.joins(:exam_rule)
+                                      .merge(ExamRule.where(frequency_type: FrequencyTypes::BY_DISCIPLINE)))
+                                      .where('extract(year from frequency_date) = ?', @year)
+                                      .where(unity_id: @unities)
+                                      .where(discipline_id: nil)
+
+    daily_frequencies.each do |daily_frequency|
+      teacher_user_id = Audited::Adapters::ActiveRecord::Audit.where(auditable_type: 'DailyFrequency',
+                                                                     auditable_id: daily_frequency.id).first.user_id
+      teacher_id = Teacher.find(User.find(teacher_user_id).teacher_id)
+      discipline_id = daily_frequency.classroom.teacher_discipline_classrooms.where(teacher_id: teacher_id).first.discipline_id
+
+      daily_frequency.update_columns(
+        discipline_id: discipline_id,
+        class_number: 1
+      )
+    end
+  end
+end
