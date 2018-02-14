31c31570d8df2b4f2fa262dd23f29b3349679fe3
Wilkinson Souza <wilkinson@outlook.com.br>
2018-02-14 10:49:32 -0200

    Adiciona ordenação no método que busca as avaliações descritivas a serem enviadas, a fim de garantir que sempre será enviado o registro mais atualizado.
    Refs #846

--- app/services/exam_poster/descriptive_exam_poster.rb
+++ app/services/exam_poster/descriptive_exam_poster.rb
@@ -7,20 +7,24 @@ module ExamPoster
     def post!
       post_by_step.each do |classroom_id, classroom_descriptive_exam|
         classroom_descriptive_exam.each do |student_id, descriptive_exam|
-          api.send_post(pareceres: { classroom_id => { student_id => descriptive_exam } }, etapa: @post_data.step.to_number, resource: 'pareceres-por-etapa-geral')
+          api.send_post(pareceres: { classroom_id => { student_id => descriptive_exam } },
+                        etapa: @post_data.step.to_number,
+                        resource: 'pareceres-por-etapa-geral')
         end
       end
 
       post_by_year.each do |classroom_id, classroom_descriptive_exam|
         classroom_descriptive_exam.each do |student_id, descriptive_exam|
-          api.send_post(pareceres: { classroom_id => { student_id => descriptive_exam } }, resource: 'pareceres-anual-geral')
+          api.send_post(pareceres: { classroom_id => { student_id => descriptive_exam } },
+                        resource: 'pareceres-anual-geral')
         end
       end
 
       post_by_year_and_discipline.each do |classroom_id, classroom_descriptive_exam|
         classroom_descriptive_exam.each do |student_id, student_descriptive_exam|
           student_descriptive_exam.each do |discipline_id, discipline_descriptive_exam|
-            api.send_post(pareceres: { classroom_id => { student_id => { discipline_id => discipline_descriptive_exam } } }, resource: 'pareceres-anual-por-componente')
+            api.send_post(pareceres: { classroom_id => { student_id => { discipline_id => discipline_descriptive_exam } } },
+                          resource: 'pareceres-anual-por-componente')
           end
         end
       end
@@ -28,7 +32,9 @@ module ExamPoster
       post_by_step_and_discipline.each do |classroom_id, classroom_descriptive_exam|
         classroom_descriptive_exam.each do |student_id, student_descriptive_exam|
           student_descriptive_exam.each do |discipline_id, discipline_descriptive_exam|
-            api.send_post(pareceres: { classroom_id => { student_id => { discipline_id => discipline_descriptive_exam } } }, etapa: @post_data.step.to_number, resource: 'pareceres-por-etapa-e-componente')
+            api.send_post(pareceres: { classroom_id => { student_id => { discipline_id => discipline_descriptive_exam } } },
+                          etapa: @post_data.step.to_number,
+                          resource: 'pareceres-por-etapa-e-componente')
           end
         end
       end
@@ -51,9 +57,9 @@ module ExamPoster
         next unless step_exists_for_classroom?(classroom)
 
         if has_classroom_steps(classroom)
-          exams = DescriptiveExamStudent.by_classroom_and_classroom_step(classroom, get_step(classroom).id)
+          exams = DescriptiveExamStudent.by_classroom_and_classroom_step(classroom, get_step(classroom).id).order(updated_at: :asc)
         else
-          exams = DescriptiveExamStudent.by_classroom_and_step(classroom, get_step(classroom).id)
+          exams = DescriptiveExamStudent.by_classroom_and_step(classroom, get_step(classroom).id).order(updated_at: :asc)
         end
 
         exams.each do |exam|
@@ -71,7 +77,7 @@ module ExamPoster
         next if classroom.unity_id != @post_data.step.school_calendar.unity_id
         next if classroom.exam_rule.opinion_type != OpinionTypes::BY_YEAR
 
-        exams = DescriptiveExamStudent.by_classroom(classroom)
+        exams = DescriptiveExamStudent.by_classroom(classroom).order(updated_at: :asc)
         exams.each do |exam|
           descriptive_exams[classroom.api_code][exam.student.api_code]["valor"] = exam.value
         end
@@ -90,7 +96,7 @@ module ExamPoster
         next if classroom.unity_id != @post_data.step.school_calendar.unity_id
         next if classroom.exam_rule.opinion_type != OpinionTypes::BY_YEAR_AND_DISCIPLINE
 
-        exams = DescriptiveExamStudent.by_classroom_and_discipline(classroom, discipline)
+        exams = DescriptiveExamStudent.by_classroom_and_discipline(classroom, discipline).order(updated_at: :asc)
         exams.each do |exam|
           descriptive_exams[classroom.api_code][exam.student.api_code][discipline.api_code]["valor"] = exam.value
         end
@@ -111,9 +117,9 @@ module ExamPoster
         next unless step_exists_for_classroom?(classroom)
 
         if has_classroom_steps(classroom)
-          exams = DescriptiveExamStudent.by_classroom_discipline_and_classroom_step(classroom, discipline, get_step(classroom).id)
+          exams = DescriptiveExamStudent.by_classroom_discipline_and_classroom_step(classroom, discipline, get_step(classroom).id).order(updated_at: :asc)
         else
-          exams = DescriptiveExamStudent.by_classroom_discipline_and_step(classroom, discipline, get_step(classroom).id)
+          exams = DescriptiveExamStudent.by_classroom_discipline_and_step(classroom, discipline, get_step(classroom).id).order(updated_at: :asc)
         end
 
         exams.each do |exam|
