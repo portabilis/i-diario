03a23ba261fbddf65da6a82b7c2e3293a61dd0aa
Eliezer Salvato <eliezer@portabilis.com.br>
2018-03-08 14:19:02 -0300

    Ajusta para a sincronização de etapas especificas receber o campo updated_at e filtrar por ele
    Refs #918

--- app/services/ieducar_synchronizers/specific_steps_synchronizer.rb
+++ app/services/ieducar_synchronizers/specific_steps_synchronizer.rb
@@ -12,7 +12,7 @@ class SpecificStepsSynchronizer < BaseSynchronizer
 
   def synchronize!
     specific_steps_api.fetch(turma_id: api_classroom_id)['etapas'].each do |specific_step|
-      update_or_create_specific_step(classroom_id, specific_step['disciplina_id'], specific_step['etapas_utilizadas'])
+      update_or_create_specific_step(classroom_id, specific_step['disciplina_id'], specific_step['etapas_utilizadas'], Time.parse(specific_step['updated_at']))
     end
 
     finish_worker('SpecificStepsSynchronizer-' << api_classroom_id << '-' << specific_step['disciplina_id'])
@@ -26,18 +26,24 @@ class SpecificStepsSynchronizer < BaseSynchronizer
     @specific_steps ||= IeducarApi::SpecificSteps.new(synchronization.to_api)
   end
 
-  def update_or_create_specific_step(classroom_id, discipline_api_id, used_steps)
+  def update_or_create_specific_step(classroom_id, discipline_api_id, used_steps, ieducar_updated_at)
     ActiveRecord::Base.transaction do
       discipline_id = Discipline.find_by_api_code(discipline_api_id).try(:id)
 
       if discipline_id
         if specific_step = SpecificStep.find_by(classroom_id: classroom_id, discipline_id: discipline_id)
-          specific_step.update_column(:used_steps, used_steps)
+          if specific_step.ieducar_updated_at != ieducar_updated_at
+            specific_step.update_columns(
+              used_steps: used_steps,
+              ieducar_updated_at: ieducar_updated_at
+            )
+          end
         else
           SpecificStep.create!(
             classroom_id: classroom_id,
             discipline_id: discipline_id,
-            used_steps: used_steps
+            used_steps: used_steps,
+            ieducar_updated_at: ieducar_updated_at
           )
         end
       end
