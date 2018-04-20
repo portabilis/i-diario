f726a4a9c46da8d25a7e2021b72abfa1e2681d13
Lucas Schmoeller da Silva <lucasschmoellerdasilva@gmail.com>
2018-04-20 11:13:15 -0300

    Refatorado api do diário de frequencias
    Foi centralizado a lógica de criar ou achar um diário em um service, e adicionado uma transação

---
+++ app/services/daily_frequencies_creator.rb
@@ -0,0 +1,67 @@
+class DailyFrequenciesCreator
+  attr_reader :daily_frequencies
+
+  def initialize(params, class_numbers = nil)
+    @params = params
+    @class_numbers = class_numbers
+  end
+
+  def self.find_or_create!(params, class_numbers)
+    new(params, class_numbers).find_or_create!
+  end
+
+  def find_or_create!
+    ActiveRecord::Base.transaction do
+      find_or_create_daily_frequencies
+      find_or_create_daily_frequency_students
+    end
+    true
+  end
+
+  private
+
+  def find_or_create_daily_frequencies
+    @daily_frequencies = []
+    if @class_numbers.present?
+      @class_numbers.each do |class_number|
+        @daily_frequencies << DailyFrequency.find_or_create_by(@params.merge({class_number: class_number}))
+      end
+    else
+      @daily_frequencies << DailyFrequency.find_or_create_by(@params)
+    end
+  end
+
+  def find_or_create_daily_frequency_students
+    fetch_student_enrollments
+
+    @student_enrollments.each do |student_enrollment|
+      student = student_enrollment.student
+      dependence = student_has_dependence?(student_enrollment.id, @daily_frequencies[0].discipline_id)
+      @daily_frequencies.map do |daily_frequency|
+        (daily_frequency.students.where(student_id: student.id).first ||
+         daily_frequency.students.create(student_id: student.id,
+                                         dependence: dependence,
+                                         present: true,
+                                         active: true))
+      end
+    end
+  end
+
+  def fetch_student_enrollments
+    frequency_date = @params[:frequency_date] || Time.zone.today
+    @student_enrollments = StudentEnrollment
+      .includes(:student)
+      .by_classroom(@daily_frequencies[0].classroom)
+      .by_discipline(@daily_frequencies[0].discipline)
+      .by_date(frequency_date)
+      .active
+      .ordered
+  end
+
+  def student_has_dependence?(student_enrollment_id, discipline_id)
+    StudentEnrollmentDependence
+      .by_student_enrollment(student_enrollment_id)
+      .by_discipline(discipline_id)
+      .any?
+  end
+end
