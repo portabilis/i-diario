79c979e18eb8f2ca662a1c0692d297c526429a7b
Marcelo Cajueiro <marcelocajueiro@gmail.com>
2018-04-26 16:07:52 -0300

    Arruma erro na interface do sidekiq
    https://github.com/mperham/sidekiq/wiki/Monitoring#forbidden

--- config/initializers/sidekiq_web.rb
+++ config/initializers/sidekiq_web.rb
@@ -4,3 +4,4 @@ require 'sidekiq/web'
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
   [user, password] == ["admin", (Rails.application.secrets[:sidekiq_password] || "Sidekiq_123")]
 end
+Sidekiq::Web.set :session_secret, Rails.application.secrets[:secret_key_base]
