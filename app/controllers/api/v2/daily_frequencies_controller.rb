58b9d83d2e90065952d25a96febee277da096b91
Marcelo Cajueiro <marcelocajueiro@gmail.com>
2018-05-07 15:35:30 -0300

    Arruma Api::V2::DailyFrequenciesController
    - Remove duplicação de código
    - Força sempre passar uma lista de class_numbers para evitar problemas

--- app/controllers/api/v2/daily_frequencies_controller.rb
+++ app/controllers/api/v2/daily_frequencies_controller.rb
@@ -25,10 +25,30 @@ class Api::V2::DailyFrequenciesController < Api::V2::BaseController
   end
 
   def create
-    if params[:class_numbers].present?
-      process_multiple
+    @class_numbers = Array(params[:class_number] || params[:class_numbers].split(","))
+
+    frequency_params = {
+      unity_id: unity.id,
+      classroom_id: classroom.id,
+      discipline_id: params[:discipline_id],
+      frequency_date: params[:frequency_date],
+      class_number: @class_numbers.first,
+      school_calendar: current_school_calendar
+    }
+
+    @daily_frequency = DailyFrequency.new(frequency_params)
+
+    if @daily_frequency.valid?
+      creator = DailyFrequenciesCreator.new(frequency_params, @class_numbers)
+      creator.find_or_create!
+
+      if params[:class_numbers].present?
+        render json: creator.daily_frequencies
+      else
+        render json: creator.daily_frequencies[0]
+      end
     else
-      process_one
+      render json: @daily_frequency.errors.full_messages, status: 422
     end
   end
 
@@ -40,50 +60,6 @@ class Api::V2::DailyFrequenciesController < Api::V2::BaseController
 
   def teacher
     @teacher ||= Teacher.find_by_id(params[:teacher_id])
-  end
-
-  def process_one
-    frequency_params = {
-      unity_id: unity.id,
-      classroom_id: classroom.id,
-      discipline_id: params[:discipline_id],
-      frequency_date: params[:frequency_date],
-      class_number: params[:class_number],
-      school_calendar: current_school_calendar
-    }
-    @daily_frequency = DailyFrequency.new(frequency_params)
-
-    if @daily_frequency.valid?
-      creator = DailyFrequenciesCreator.new(frequency_params)
-      creator.find_or_create!
-      render json: creator.daily_frequencies[0]
-    else
-      render json: @daily_frequency.errors.full_messages, status: 422
-    end
-
-  end
-
-  def process_multiple
-    frequency_params = {
-      unity_id: unity.id,
-      classroom_id: classroom.id,
-      discipline_id: params[:discipline_id],
-      frequency_date: params[:frequency_date],
-      class_number: params[:class_numbers].first,
-      school_calendar: current_school_calendar
-    }
-
-    @class_numbers = params[:class_numbers].split(",")
-
-    @daily_frequency = DailyFrequency.new(frequency_params)
-
-    unless @daily_frequency.valid?
-      render json: @daily_frequency.errors.full_messages, status: 422
-    else
-      creator = DailyFrequenciesCreator.new(frequency_params, @class_numbers)
-      creator.find_or_create!
-      render json: creator.daily_frequencies
-    end
   end
 
   def configuration
