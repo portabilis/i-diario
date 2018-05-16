d9fae6bb75df215d953d6299c0ae9b96eb4d6c5f
Eliezer Salvato <eliezer@portabilis.com.br>
2018-05-16 20:10:39 -0300

    Ajusta para usar um enumerator ao invés de usar o número 1
    Refs #1081

--- app/models/student_enrollment_classroom.rb
+++ app/models/student_enrollment_classroom.rb
@@ -9,7 +9,7 @@ class StudentEnrollmentClassroom < ActiveRecord::Base
   scope :show_as_inactive, lambda { where(show_as_inactive_when_not_in_date: 't') }
   scope :by_grade, lambda { |grade_id| joins(:classroom).where(classrooms: { grade_id: grade_id })   }
   scope :by_student, lambda { |student_id| joins(student_enrollment: :student).where(students: { id: student_id }) }
-  scope :active, -> { joins(:student_enrollment).where(student_enrollments: { active: 1 }) }
+  scope :active, -> { joins(:student_enrollment).where(student_enrollments: { active: IeducarBooleanState::ACTIVE }) }
 
   def delete_invalid_presence_record
     if left_classroom? && classroom
