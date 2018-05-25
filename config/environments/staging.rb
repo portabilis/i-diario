3c791a8733bd68d3deb8a07c1ede50490603c115
Marcelo Cajueiro <marcelocajueiro@gmail.com>
2018-05-25 09:39:37 -0300

    Melhora configuração do staging
    - Remove configurações duplicadas com production;
    - liga cache para ter o mesmo comportamento de produção.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,15 +1,8 @@
 require Rails.root.join('config/environments/production')
 
 Rails.application.configure do
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both threaded web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
-  config.eager_load = true
-
-  # Full error reports are enabled and caching is turned of
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  # Full error reports are enabled
+  config.consider_all_requests_local = true
 
   # See everything in the log (default is :info)
   config.log_level = :debug
@@ -17,14 +10,6 @@ Rails.application.configure do
   config.active_support.deprecation = :log
 
   config.action_mailer.default_url_options = { :host => 'clientetest.portabilis.com.br' }
-
-  # Paperclip configuration
-  config.paperclip_defaults = {
-    storage: :s3,
-    s3_region: 'us-east-1',
-    bucket: '***REMOVED***',
-    s3_credentials: "#{Rails.root}/config/aws.yml"
-  }
 
   config.logger = GELF::Logger.new("***REMOVED***", 12203, "WAN", facility: "novo-educacao-staging")
 end
