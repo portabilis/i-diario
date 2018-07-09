71e23c78b60c21a00dbab1e8e175076c73a77c9d
Marcelo Cajueiro <marcelocajueiro@gmail.com>
2018-07-09 16:30:43 -0300

    Revert "Revert "Marca warning antes de enviar requets de notas""
    This reverts commit db4c64460b838d4df2e476b7f2555bbaadb2e35b.

--- app/services/exam_poster/base.rb
+++ app/services/exam_poster/base.rb
@@ -19,12 +19,12 @@ module ExamPoster
     def post!
       generate_requests
 
+      @post_data.mark_as_warning!(@warning_messages) if @warning_messages.present?
+
       worker_batch.update_attributes!(total_workers: requests.count)
       requests.each do |request|
         Ieducar::SendPostWorker.perform_async(entity_id, @post_data.id, request)
       end
-
-      @post_data.mark_as_warning!(@warning_messages) if @warning_messages.present?
     end
 
     private
