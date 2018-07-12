0937e03db7a178f1e7732874d7184790d70cd16c
kellyn <kellyn@portabilis.com.br>
2018-07-12 19:45:07 -0300

    Refatora código conforme code review
    Refs #763

--- app/assets/javascripts/resources/school_calendar_events.js
+++ app/assets/javascripts/resources/school_calendar_events.js
@@ -129,15 +129,31 @@ $(function () {
     }
   });
 
-  var togleLegendContainerVisibility = function() {
-    var isNotExtraSchool = $eventType.val() != 'extra_school';
-    var isNotNoSchoolWithFrequency = $eventType.val() != 'no_school_with_frequency';
-    var isNotBlank = $eventType.val() != '';
-    
-    if (isNotExtraSchool && isNotNoSchoolWithFrequency && isNotBlank) {
-      $legendContainer.removeClass('hidden');
-    } else {
+  var isEventTypeEqualTo = function(type) {
+    return $eventType.val() === type;
+  }
+
+  var eventTypeIsBlank = function() {
+    return isEventTypeEqualTo('');
+  }
+
+  var eventTypeIsExtraSchool = function() {
+    return isEventTypeEqualTo('extra_school');
+  }
+
+  var eventTypeIsNoSchoolWithFrequency = function() {
+    return isEventTypeEqualTo('no_school_with_frequency');
+  }
+
+  var shouldHideLegend = function() {
+    return eventTypeIsBlank() || eventTypeIsExtraSchool() || eventTypeIsNoSchoolWithFrequency();
+  }
+
+   var togleLegendContainerVisibility = function() {
+    if (shouldHideLegend()) {
       $legendContainer.addClass('hidden');
+    } else {
+      $legendContainer.removeClass('hidden');
     }
   }
 
