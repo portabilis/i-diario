b5b4dcb971ec2d9a4c430d47cfb33e70ff81f2ae
Eliezer Salvato <eliezer@portabilis.com.br>
2018-08-03 15:24:01 -0300

    Ajusta para executar o worker em uma fila separada e apenas até a data que existe problema.

--- app/workers/fix_daily_frequency_missing_students_worker.rb
+++ app/workers/fix_daily_frequency_missing_students_worker.rb
@@ -7,7 +7,7 @@ class FixDailyFrequencyMissingStudentsWorker
 
     entities.each do |entity|
       entity.using_connection do
-        frequencies = DailyFrequency.by_frequency_date_between('2018-01-01'.to_date, '2018-12-31'.to_date)
+        frequencies = DailyFrequency.by_frequency_date_between('2018-01-01'.to_date, '2018-07-17'.to_date)
 
         worker_batch = WorkerBatch.create!(
           main_job_class: 'FixDailyFrequencyMissingStudentsWorker',
@@ -17,7 +17,7 @@ class FixDailyFrequencyMissingStudentsWorker
         )
 
         frequencies.each do |daily_frequency|
-          DailyFrequencyCreatorWorker.perform_async(entity.id, daily_frequency.id, worker_batch.id)
+          DailyFrequencyCreatorWorker.set(queue: 'daily_frequency_creator').perform_async(entity.id, daily_frequency.id, worker_batch.id)
         end
       end
     end
