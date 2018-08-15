c0c55c6b8d2848edfa0af9220ea203d8c3cfece0
Eliezer Salvato <eliezer@portabilis.com.br>
2018-08-15 14:27:18 -0300

    Ajusta sql que busca as frequencias a serem removidas para usar left e ficar mais performatico;
    Refs #1333

--- app/services/delete_invalid_presence_record_service.rb
+++ app/services/delete_invalid_presence_record_service.rb
@@ -4,14 +4,23 @@ class DeleteInvalidPresenceRecordService
   end
 
   def run!
-    student_enrollment_classrooms = StudentEnrollmentClassroom.by_classroom(@student_enrollment_classroom.classroom_id)
-                                                              .by_student_enrollment(@student_enrollment_classroom.student_enrollment_id)
-
-    daily_frequency_students = DailyFrequencyStudent.by_classroom_id(@student_enrollment_classroom.classroom_id)
-                                                    .by_student_id(@student_enrollment_classroom.student_enrollment.student_id)
+    daily_frequency_students =
+      DailyFrequencyStudent.joins(:daily_frequency).joins(
+        <<-SQL
+          LEFT JOIN student_enrollment_classrooms AS sec
+                 ON sec.classroom_id = daily_frequencies.classroom_id
+                AND sec.student_enrollment_id = #{@student_enrollment_classroom.student_enrollment_id}
+                AND daily_frequencies.frequency_date >= CAST(sec.joined_at AS DATE)
+                AND (COALESCE(sec.left_at, '') = '' OR
+                     daily_frequencies.frequency_date < CAST(sec.left_at AS DATE))
+        SQL
+      )
+      .by_classroom_id(@student_enrollment_classroom.classroom_id)
+      .by_student_id(@student_enrollment_classroom.student_enrollment.student_id)
+      .where('sec.id IS NULL')
 
     daily_frequency_students.each do |daily_frequency_student|
-      daily_frequency_student.destroy unless student_enrollment_classrooms.by_date(daily_frequency_student.frequency_date).exists?
+      daily_frequency_student.destroy
     end
   end
 end
