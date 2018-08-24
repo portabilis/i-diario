f2d74f54ca945d7635f0815a2aacc2ed4ffbf324
Wilkinson Souza <wilkinson@outlook.com.br>
2018-08-24 13:33:10 -0300

    Corrige indice do teacher_discipline_classrooms

---
+++ db/migrate/20180824162827_fix_teacher_discipline_classrooms_indexes.rb
@@ -0,0 +1,15 @@
+class FixTeacherDisciplineClassroomsIndexes < ActiveRecord::Migration
+  def change
+    remove_index :teacher_discipline_classrooms, name: :idx_teacher_discipline_classrooms_all_fks
+    remove_index :teacher_discipline_classrooms, name: :idx_teacher_discipline_classrooms_two_fks
+    remove_index :teacher_discipline_classrooms, :discipline_id
+    remove_index :teacher_discipline_classrooms, :classroom_id
+    remove_index :teacher_discipline_classrooms, :teacher_id
+
+    add_index :teacher_discipline_classrooms, [:discipline_id, :classroom_id, :teacher_id], where: "deleted_at IS NULL", name: :idx_teacher_discipline_classrooms_all_fks
+    add_index :teacher_discipline_classrooms, [:discipline_id, :teacher_id], where: "deleted_at IS NULL", name: :idx_teacher_discipline_classrooms_two_fks
+    add_index :teacher_discipline_classrooms, :discipline_id, where: "deleted_at IS NULL"
+    add_index :teacher_discipline_classrooms, :classroom_id, where: "deleted_at IS NULL"
+    add_index :teacher_discipline_classrooms, :teacher_id, where: "deleted_at IS NULL"
+  end
+end
