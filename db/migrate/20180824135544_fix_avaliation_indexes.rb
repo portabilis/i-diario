c5bc1617b024c79ea0abb4a5f013428e3e9a7d67
Wilkinson Souza <wilkinson@outlook.com.br>
2018-08-24 11:06:43 -0300

    Corrige indices nas avaliações

---
+++ db/migrate/20180824135544_fix_avaliation_indexes.rb
@@ -0,0 +1,13 @@
+class FixAvaliationIndexes < ActiveRecord::Migration
+  def change
+    remove_index :avaliations, :classroom_id
+    remove_index :avaliations, :discipline_id
+    remove_index :avaliations, :school_calendar_id
+    remove_index :avaliations, :test_setting_id
+
+    add_index :avaliations, :classroom_id, where: "deleted_at IS NULL"
+    add_index :avaliations, :discipline_id, where: "deleted_at IS NULL"
+    add_index :avaliations, :school_calendar_id, where: "deleted_at IS NULL"
+    add_index :avaliations, :test_setting_id, where: "deleted_at IS NULL"
+  end
+end
