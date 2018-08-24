61e409797d3469815feef8fa87035e9b13d07ffa
Wilkinson Souza <wilkinson@outlook.com.br>
2018-08-24 12:48:28 -0300

    Fix migration

--- db/migrate/20180824144313_fix_daily_frequencies_indexes.rb
+++ db/migrate/20180824144313_fix_daily_frequencies_indexes.rb
@@ -1,11 +1,9 @@
 class FixDailyFrequenciesIndexes < ActiveRecord::Migration
   def change
-    remove_index :daily_frequencies, name: :daily_frequencies_unique_idx if index_exists?(:daily_frequencies, [:unity_id, :classroom_id, :frequency_date, :discipline_id, :class_number, :school_calendar_id], name: :daily_frequencies_unique_idx)
     remove_index :daily_frequencies, :classroom_id
     remove_index :daily_frequencies, :discipline_id
     remove_index :daily_frequencies, :unity_id
 
-    add_index :daily_frequencies, [:unity_id, :classroom_id, :frequency_date, :discipline_id, :class_number, :school_calendar_id], where: "deleted_at IS NULL", name: :daily_frequencies_unique_idx, unique: true
     add_index :daily_frequencies, :classroom_id, where: "deleted_at IS NULL"
     add_index :daily_frequencies, :discipline_id, where: "deleted_at IS NULL"
     add_index :daily_frequencies, :unity_id, where: "deleted_at IS NULL"
