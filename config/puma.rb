08d4a0cd6a11f726c4e81fa73dd14de128093c68
Marcelo Cajueiro <marcelocajueiro@gmail.com>
2018-09-03 17:54:10 -0300

    Refatora config do puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,43 +1,28 @@
 #!/usr/bin/env puma
 
-if ENV['RAILS_ENV'] != 'development'
-  deploy_to = '/var/www/novo-educacao'
+workers ENV['RAILS_ENV'] == 'production' ? 16 : 2
 
-  bind "unix://#{deploy_to}/shared/tmp/sockets/puma.sock"
-  directory "#{deploy_to}/current"
-  rackup "#{deploy_to}/current/config.ru"
-  pidfile "#{deploy_to}/shared/tmp/pids/puma.pid"
-  state_path "#{deploy_to}/shared/tmp/sockets/puma.state"
-  activate_control_app "unix://#{deploy_to}/shared/tmp/sockets/pumactl.sock"
+# Min and Max threads per worker
+threads 1, 1
 
-  stdout_redirect "#{deploy_to}/current/log/puma.stdout.log",
-    "#{deploy_to}/current/log/puma.stderr.log",
-    true
-
-  on_restart do
-    puts 'Refreshing Gemfile'
-    ENV["BUNDLE_GEMFILE"] = "#{deploy_to}/current/Gemfile"
-  end
-end
-
-if ENV['RAILS_ENV'] == 'development'
-  worker_timeout 10000
-end
+app_dir = "/var/www/novo-educacao"
+shared_dir = "#{app_dir}/shared"
 
 environment ENV['RAILS_ENV']
-threads 1,1
-workers ENV.fetch('PUMA_WORKERS', 4)
 
-preload_app!
-prune_bundler
+# Set up socket location
+bind "unix://#{shared_dir}/sockets/pumactl.sock"
 
-# Required for preload_app!
+# Logging
+stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+
+# Set master PID and state locations
+pidfile "#{shared_dir}/pids/puma.pid"
+state_path "#{shared_dir}/pids/puma.state"
+activate_control_app
+
 on_worker_boot do
-  ActiveSupport.on_load(:active_record) do
-    ActiveRecord::Base.establish_connection
-  end
-end
-
-before_fork do
-  ActiveRecord::Base.connection_pool.disconnect!
+  require "active_record"
+  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+  ActiveRecord::Base.establish_connection(YAML.load_file("#{shared_dir}/config/database.yml")[ENV['RAILS_ENV']])
 end
