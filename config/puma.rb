c8fc930eff139f48f8baef4fc44960dc9a7639f1
Marcelo Cajueiro <marcelocajueiro@gmail.com>
2018-09-03 18:14:28 -0300

    Arruma config do puma para padrões do mina

--- config/puma.rb
+++ config/puma.rb
@@ -11,15 +11,15 @@ shared_dir = "#{app_dir}/shared"
 environment ENV['RAILS_ENV']
 
 # Set up socket location
-bind "unix://#{shared_dir}/sockets/pumactl.sock"
+bind "unix://#{app_dir}/shared/tmp/sockets/puma.sock"
 
 # Logging
 stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 
 # Set master PID and state locations
-pidfile "#{shared_dir}/pids/puma.pid"
-state_path "#{shared_dir}/pids/puma.state"
-activate_control_app
+pidfile "#{app_dir}/shared/tmp/pids/puma.pid"
+state_path "#{app_dir}/shared/tmp/sockets/puma.state"
+activate_control_app "unix://#{app_dir}/shared/tmp/sockets/pumactl.sock"
 
 on_worker_boot do
   require "active_record"
