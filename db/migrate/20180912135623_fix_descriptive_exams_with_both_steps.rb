2797140df4764082309ddd18f5edb898bae7fe6c
Eliezer Salvato <eliezer@portabilis.com.br>
2018-09-12 16:59:08 -0300

    Ajusta as avaliações descritivas que possuem informadas etapas de turma e geral;
    refs #1424

---
+++ db/migrate/20180912135623_fix_descriptive_exams_with_both_steps.rb
@@ -0,0 +1,23 @@
+class FixDescriptiveExamsWithBothSteps < ActiveRecord::Migration
+  def change
+    execute <<-SQL
+      UPDATE descriptive_exams
+         SET school_calendar_step_id = NULL
+       WHERE school_calendar_step_id IS NOT NULL
+         AND school_calendar_classroom_step_id IS NOT NULL
+         AND EXISTS(SELECT 1
+                      FROM school_calendar_classrooms AS scc
+                     WHERE scc.classroom_id = descriptive_exams.classroom_id
+             );
+
+      UPDATE descriptive_exams
+         SET school_calendar_classroom_step_id = NULL
+       WHERE school_calendar_step_id IS NOT NULL
+         AND school_calendar_classroom_step_id IS NOT NULL
+         AND NOT EXISTS(SELECT 1
+                          FROM school_calendar_classrooms AS scc
+                         WHERE scc.classroom_id = descriptive_exams.classroom_id
+             );
+    SQL
+  end
+end
