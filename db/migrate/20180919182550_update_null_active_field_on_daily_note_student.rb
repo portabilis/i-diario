044f3b6bf4c3071fa1879f638d784c0fd132d3ac
Wilkinson Souza <wilkinson@outlook.com.br>
2018-09-19 17:10:20 -0300

    Altera para que somente seja ativo quando possuir nota de transferência

--- db/migrate/20180919182550_update_null_active_field_on_daily_note_student.rb
+++ db/migrate/20180919182550_update_null_active_field_on_daily_note_student.rb
@@ -2,7 +2,7 @@ class UpdateNullActiveFieldOnDailyNoteStudent < ActiveRecord::Migration
   def change
     execute <<-SQL
       UPDATE daily_note_students
-         SET active = true
+         SET active = transfer_note_id is not null
        WHERE active is null
     SQL
   end
