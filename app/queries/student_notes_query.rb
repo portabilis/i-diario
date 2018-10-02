1c68e2e558bf9263b67e4223136dbdcf863ed476
Joel <ayatamail@gmail.com>
2018-10-02 14:15:42 -0300

    Ajusta para não ter em conta avaliaçoes antes de ser enturmado

--- app/queries/student_notes_query.rb
+++ app/queries/student_notes_query.rb
@@ -5,13 +5,16 @@ class StudentNotesQuery
     @classroom = classroom
     @start_at = start_at.to_date
     @end_at = end_at.to_date
+    @joined_at = student_enrollment.joined_at
   end
 
   def daily_note_students
     DailyNoteStudent.by_student_id(student)
-                    .by_discipline_id(discipline)
-                    .by_classroom_id(classroom)
-                    .by_test_date_between(start_at, end_at)
+                                           .by_discipline_id(discipline)
+                                           .by_classroom_id(classroom)
+                                           .by_test_date_between(start_at, end_at)
+                                           .joins(daily_note: :avaliation)
+                                           .merge(Avaliation.by_test_date_between(joined_at, end_at))
   end
 
   def recovery_diary_records
@@ -20,14 +23,21 @@ class StudentNotesQuery
     RecoveryDiaryRecord.by_student_id(student)
                        .by_discipline_id(discipline)
                        .by_classroom_id(classroom)
-                       .joins(:avaliation_recovery_diary_record)
+                       .joins(avaliation_recovery_diary_record: :avaliation)
                        .merge(
                          AvaliationRecoveryDiaryRecord.by_test_date_between(start_at, end_at)
-                                                      .where.not(avaliation_id: avaliation_ids)
-                       )
+                                                      .where.not(avaliation_id: avaliation_ids))
+                       .merge(Avaliation.by_test_date_between(joined_at, end_at))
   end
 
   private
 
-  attr_accessor :student, :discipline, :classroom, :start_at, :end_at
+  attr_accessor :student, :discipline, :classroom, :start_at, :end_at, :joined_at
+
+  def student_enrollment
+    StudentEnrollmentClassroom.by_student(student)
+                              .by_classroom(classroom)
+                              .active
+                              .first
+  end
 end
