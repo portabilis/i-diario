5ceaf9c9ccb788a6c7a94383aa8dfab5cf6d3494
Joel <ayatamail@gmail.com>
2018-10-18 15:28:14 -0300

    Muda critério de seleção por data

--- db/migrate/20181018115803_create_function_students_available_by_date_range.rb
+++ db/migrate/20181018115803_create_function_students_available_by_date_range.rb
@@ -2,11 +2,11 @@ class CreateFunctionStudentsAvailableByDateRange < ActiveRecord::Migration
   def change
     execute <<-SQL
       CREATE OR REPLACE FUNCTION students_available_by_date_range(
-        l_classroom_id INT,
-        l_discipline_id INT,
-        l_start_at DATE,
-        l_end_at DATE,
-        l_step_number INT
+        classroom_id INT,
+        discipline_id INT,
+        start_at DATE,
+        end_at DATE,
+        step_number INT
       )
       RETURNS TABLE (
         student_id INT
@@ -17,26 +17,35 @@ class CreateFunctionStudentsAvailableByDateRange < ActiveRecord::Migration
         SELECT seed.student_enrollment_id AS id
           INTO exempted_student_enrollments
           FROM student_enrollment_exempted_disciplines AS seed
-         WHERE seed.discipline_id = l_discipline_id
-           AND l_step_number = ANY(string_to_array(steps, ',')::integer[]);
+         WHERE seed.discipline_id = discipline_id
+           AND step_number = ANY(string_to_array(steps, ',')::integer[]);
 
         RETURN QUERY (
           SELECT DISTINCT se.student_id
             FROM student_enrollments AS se
             JOIN student_enrollment_classrooms AS sec
               ON sec.student_enrollment_id = se.id
-           WHERE sec.classroom_id = l_classroom_id
-             AND CAST(sec.joined_at AS DATE) BETWEEN l_start_at AND l_end_at
+           WHERE sec.classroom_id = classroom_id
+             AND (
+              CASE
+                WHEN COALESCE(sec.left_at) = '' THEN
+                  CAST(sec.joined_at AS DATE) <= end_at
+                ELSE
+                  CAST(sec.joined_at AS DATE) <= end_at AND
+                  CAST(sec.left_at AS DATE) >= start_at AND
+                  CAST(sec.joined_at AS DATE) <> CAST(sec.left_at AS DATE)
+              END
+             )
              AND se.active = 1
              AND (exempted_student_enrollments.id IS NULL OR se.id NOT IN (exempted_student_enrollments.id))
-             AND (l_discipline_id IS NULL OR
+             AND (discipline_id IS NULL OR
                   NOT EXISTS(SELECT 1
                                FROM student_enrollment_dependences
                               WHERE student_enrollment_dependences.student_enrollment_id = se.id) OR
                   EXISTS(SELECT 1
                            FROM student_enrollment_dependences
                           WHERE student_enrollment_dependences.student_enrollment_id = se.id
-                            AND student_enrollment_dependences.discipline_id = l_discipline_id)
+                            AND student_enrollment_dependences.discipline_id = discipline_id)
                 )
         );
 
