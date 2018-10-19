7eedf374b2055d89290b35c1896bc022cb8f6869
Wilkinson Souza <wilki.darolt@gmail.com>
2018-10-19 09:26:43 -0300

    Remove espaços
    Co-Authored-By: MarceloCajueiro <marcelocajueiro@gmail.com>

--- app/services/ieducar_synchronizers/knowledge_areas_synchronizer.rb
+++ app/services/ieducar_synchronizers/knowledge_areas_synchronizer.rb
@@ -15,7 +15,7 @@ class KnowledgeAreasSynchronizer < BaseSynchronizer
     collection.each do |record|
       KnowledgeArea.find_or_initialize_by(api_code: record["id"]).tap do |knowledge_area|
         knowledge_area.description = record["nome"]
-        knowledge_area.sequence    = record["ordenamento_ac"]
+        knowledge_area.sequence = record["ordenamento_ac"]
         knowledge_area.save! if knowledge_area.changed?
       end
     end
