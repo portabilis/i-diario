e6a91f431a450e3e50245122dc559966d5c0a4f2
Marcelo Cajueiro <marcelocajueiro@gmail.com>
2018-10-23 09:43:33 -0300

    Dropa index apenas se existir
    Há inconsistência em bancos. Nem todos havia o index.

--- db/migrate/20181022174613_add_api_code_index_to_disciplines.rb
+++ db/migrate/20181022174613_add_api_code_index_to_disciplines.rb
@@ -1,6 +1,6 @@
 class AddApiCodeIndexToDisciplines < ActiveRecord::Migration
   def change
-    remove_index :disciplines, :api_code
+    execute "DROP INDEX index_disciplines_on_api_code IF EXISTS"
     add_index :disciplines, :api_code, unique: true
   end
 end
