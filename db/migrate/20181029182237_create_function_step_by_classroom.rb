2fcd752e8551cd9e6b81ee1a2493016e0fe2d030
Eliezer Salvato <eliezer@portabilis.com.br>
2018-10-29 22:33:25 -0300

    Cria a função step_by_classroom para buscar a etapa específica;
    Refs #1565

---
+++ db/migrate/20181029182237_create_function_step_by_classroom.rb
@@ -0,0 +1,32 @@
+class CreateFunctionStepByClassroom < ActiveRecord::Migration
+  def change
+    execute <<-SQL
+      CREATE OR REPLACE FUNCTION step_by_classroom(
+        l_classroom_id INT,
+        l_recorded_at DATE
+      )
+      RETURNS TABLE (
+        step_number BIGINT,
+        step_id INT,
+        start_at DATE,
+        end_at DATE,
+        start_date_for_posting DATE,
+        end_date_for_posting DATE,
+        created_at TIMESTAMP,
+        updated_at TIMESTAMP,
+        type TEXT
+      ) AS $$
+      BEGIN
+        RETURN QUERY (
+          SELECT *
+            FROM steps_by_classroom(l_classroom_id) AS steps
+           WHERE l_recorded_at BETWEEN steps.start_at AND steps.end_at
+           LIMIT 1
+        );
+
+        RETURN;
+      END; $$
+      LANGUAGE 'plpgsql';
+    SQL
+  end
+end
