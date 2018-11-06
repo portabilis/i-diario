05d8877993a5d930fff9d0311c5b33bf6b1e4416
Eliezer Salvato <eliezer@portabilis.com.br>
2018-11-06 14:20:03 -0200

    Ajusta para buscar turma da variavel local;
    * Foi verificado que foi refatorado e foi esquecido de mudar a variavel de instancia pela local.
    
    Refs #1609

--- app/services/exempted_disciplines_in_step.rb
+++ app/services/exempted_disciplines_in_step.rb
@@ -4,7 +4,7 @@ class ExemptedDisciplinesInStep
   end
 
   def discipline_ids(classroom_id, step_number)
-    SpecificStep.where(classroom_id: @classroom_id)
+    SpecificStep.where(classroom_id: classroom_id)
                 .where("not (? = ANY(string_to_array(used_steps, ',')::integer[]))", step_number)
                 .where.not(used_steps: '')
                 .pluck(:discipline_id)
