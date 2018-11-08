ee5ddbebaacfdc049c1e28d84a0b6f9441b99d89
Lucas Schmoeller da Silva <lucasschmoellerdasilva@gmail.com>
2018-11-08 18:06:02 -0200

    Atualizado Deploy e Puma conforme development

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env puma
 
 workers ENV['RAILS_ENV'] == 'production' ? 16 : 2
-preload_app!
 
 # Min and Max threads per worker
 threads 1, 1
@@ -17,16 +16,18 @@ if ENV['RAILS_ENV'] != 'development'
   environment ENV['RAILS_ENV']
 
   # Set up socket location
-  bind "unix://#{app_dir}/shared/tmp/sockets/puma.sock"
+  bind "unix://#{shared_dir}/sockets/puma.sock"
 
   # Logging
   stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 
   # Set master PID and state locations
-  pidfile "#{app_dir}/shared/tmp/pids/puma.pid"
-  state_path "#{app_dir}/shared/tmp/sockets/puma.state"
-  activate_control_app "unix://#{app_dir}/shared/tmp/sockets/pumactl.sock"
+  pidfile "#{shared_dir}/pids/puma.pid"
+  state_path "#{shared_dir}/sockets/puma.state"
+  activate_control_app "unix://#{shared_dir}/sockets/pumactl.sock"
 end
+
+preload_app!
 
 on_worker_boot do
   ActiveSupport.on_load(:active_record) do
