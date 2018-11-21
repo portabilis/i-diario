a4f529231ba109fbeca92107d3133d1b3fe9ab8d
Eliezer Salvato <eliezer@portabilis.com.br>
2018-11-21 16:28:41 -0200

    Ajusta para ter um unique index para as colunas recovery_diary_record_id e student_id;
    Refs #1640

---
+++ db/migrate/20181121180851_add_unique_index_to_recovery_diary_record_students.rb
@@ -0,0 +1,7 @@
+class AddUniqueIndexToRecoveryDiaryRecordStudents < ActiveRecord::Migration
+  def change
+    execute 'DROP INDEX IF EXISTS index_on_recovery_diary_record_id_and_student_id'
+    add_index :recovery_diary_record_students, [:recovery_diary_record_id, :student_id], unique: true,
+      name: 'index_unique_on_recovery_diary_record_students'
+  end
+end
