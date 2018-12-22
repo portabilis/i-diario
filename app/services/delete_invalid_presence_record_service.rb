d1d043ab3356344cc96d28e75ae4a5b29f39a189
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2018-12-22 11:56:48 -0200

    Refatora código. Aplica o principio de responsábilidade única para o método

--- app/services/delete_invalid_presence_record_service.rb
+++ app/services/delete_invalid_presence_record_service.rb
@@ -5,26 +5,31 @@ class DeleteInvalidPresenceRecordService
   end
 
   def run!
-    daily_frequency_students = DailyFrequencyStudent
-      .joins(:daily_frequency)
-      .by_classroom_id(@classroom_id)
-      .by_student_id(@student_id)
-      .where(
-        <<-SQL
-          NOT EXISTS(
-            SELECT 1
-              FROM student_enrollments AS se
-              JOIN student_enrollment_classrooms AS sec
-                ON sec.classroom_id = daily_frequencies.classroom_id
-              AND sec.student_enrollment_id = se.id
-              AND daily_frequencies.frequency_date >= CAST(sec.joined_at AS DATE)
-              AND (COALESCE(sec.left_at, '') = '' OR
-                    daily_frequencies.frequency_date < CAST(sec.left_at AS DATE))
-            WHERE se.student_id = #{@student_id}
-          )
-        SQL
-      )
+    daily_frequency_students_list = daily_frequency_students_list
+    destroy_list_daily_frequency_students(daily_frequency_students_list)
+  end
 
-    daily_frequency_students.destroy_all
+  def daily_frequency_students_list
+    DailyFrequencyStudent.joins(:daily_frequency)
+                         .by_classroom_id(@classroom_id)
+                         .by_student_id(@student_id)
+                         .where(<<-SQL
+                            NOT EXISTS(
+                              SELECT 1
+                                FROM student_enrollments AS se
+                                JOIN student_enrollment_classrooms AS sec
+                                  ON sec.classroom_id = daily_frequencies.classroom_id
+                                AND sec.student_enrollment_id = se.id
+                                AND daily_frequencies.frequency_date >= CAST(sec.joined_at AS DATE)
+                                AND (COALESCE(sec.left_at, '') = '' OR
+                                      daily_frequencies.frequency_date < CAST(sec.left_at AS DATE))
+                              WHERE se.student_id = #{@student_id}
+                            )
+                         SQL
+                               )
+  end
+
+  def destroy_list_daily_frequency_students(daily_frequency_list)
+    daily_frequency_list.destroy_all
   end
 end
