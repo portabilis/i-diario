b5455ceb4070f489818b5c6a0148043574aaddb8
Wilkinson Souza <wilkinson@outlook.com.br>
2018-12-27 19:37:43 -0200

    Rubocop

--- app/helpers/daily_frequency_helper.rb
+++ app/helpers/daily_frequency_helper.rb
@@ -1,11 +1,11 @@
 module DailyFrequencyHelper
-  def params_for_print_month daily_frequencies
+  def params_for_print_month(daily_frequencies)
     daily_frequency = daily_frequencies[0]
     {
       unity_id: daily_frequency.unity_id,
       classroom_id: daily_frequency.classroom_id,
       discipline_id: daily_frequency.discipline_id,
-      class_numbers: (1..@number_of_classes).to_a.join(","),
+      class_numbers: (1..@number_of_classes).to_a.join(','),
       start_at: l(daily_frequency.frequency_date.at_beginning_of_month),
       end_at: l(daily_frequency.frequency_date.end_of_month),
       school_calendar_year: daily_frequency.frequency_date.year,
@@ -13,7 +13,7 @@ module DailyFrequencyHelper
     }
   end
 
-  def params_for_print_step daily_frequencies
+  def params_for_print_step(daily_frequencies)
     daily_frequency = daily_frequencies[0]
     start_at = get_start_at(daily_frequency)
     end_at = get_end_at(daily_frequency)
@@ -21,7 +21,7 @@ module DailyFrequencyHelper
       unity_id: daily_frequency.unity_id,
       classroom_id: daily_frequency.classroom_id,
       discipline_id: daily_frequency.discipline_id,
-      class_numbers: (1..@number_of_classes).to_a.join(","),
+      class_numbers: (1..@number_of_classes).to_a.join(','),
       start_at: l(start_at),
       end_at: l(end_at),
       school_calendar_year: daily_frequency.frequency_date.year,
@@ -50,7 +50,11 @@ module DailyFrequencyHelper
   end
 
   def get_school_calendar_classroom_step(daily_frequency)
-    daily_frequency.school_calendar.classrooms.by_classroom(daily_frequency.classroom_id).first.classroom_step(daily_frequency.frequency_date)
+    daily_frequency.school_calendar
+                   .classrooms
+                   .by_classroom(daily_frequency.classroom_id)
+                   .first
+                   .classroom_step(daily_frequency.frequency_date)
   end
 
   def frequency_student_name_class(dependence, active, exempted_from_discipline)
@@ -67,13 +71,13 @@ module DailyFrequencyHelper
 
   def frequency_student_name(student, dependence, active, exempted_from_discipline)
     if !active
-      "**#{student.to_s}"
+      "**#{student}"
     elsif dependence
-      "*#{student.to_s}"
+      "*#{student}"
     elsif exempted_from_discipline
-      "****#{student.to_s}"
+      "****#{student}"
     else
-      "#{student.to_s}"
+      student.to_s
     end
   end
 end
