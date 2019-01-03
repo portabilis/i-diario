b7e060347fec2796ef4b58b60e69da51e5ff1211
Eliezer Salvato <eliezer@portabilis.com.br>
2019-01-03 11:46:28 -0200

    Ajusta para não verificar as avaliações já duplicadas, apenas impede novos casos;
    refs #1562

--- db/migrate/20181206195322_create_check_to_keep_unique_conceptual_exams.rb
+++ db/migrate/20181206195322_create_check_to_keep_unique_conceptual_exams.rb
@@ -3,7 +3,7 @@ class CreateCheckToKeepUniqueConceptualExams < ActiveRecord::Migration
     execute <<-SQL
       ALTER TABLE conceptual_exams
       ADD CONSTRAINT check_conceptual_exam_is_unique
-      CHECK (check_conceptual_exam_is_unique(id, classroom_id, student_id, recorded_at));
+      CHECK (check_conceptual_exam_is_unique(id, classroom_id, student_id, recorded_at)) NOT VALID;
     SQL
   end
 end
--- db/migrate/20181207112600_create_check_to_keep_unique_descriptive_exams.rb
+++ db/migrate/20181207112600_create_check_to_keep_unique_descriptive_exams.rb
@@ -3,7 +3,7 @@ class CreateCheckToKeepUniqueDescriptiveExams < ActiveRecord::Migration
     execute <<-SQL
       ALTER TABLE descriptive_exams
       ADD CONSTRAINT check_descriptive_exam_is_unique
-      CHECK (check_descriptive_exam_is_unique(id, classroom_id, discipline_id, recorded_at));
+      CHECK (check_descriptive_exam_is_unique(id, classroom_id, discipline_id, recorded_at)) NOT VALID;
     SQL
   end
 end
