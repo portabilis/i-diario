3b457208ef036d9a7b9f9b6970a81f38f52667d9
Joel <ayatamail@gmail.com>
2019-01-28 09:15:47 -0200

    Ajusta segun CR
    * Altea para usar HTTP codes sybols from Rake::Utils
    * Localiza message para pt-br

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -4,10 +4,10 @@ class ErrorsController < ApplicationController
   def show
     status_code = params[:code] || 500
 
-    text = case status_code
-           when HttpCodes::NOT_FOUND
+    text = case status_code.to_i
+           when Rack::Utils::SYMBOL_TO_STATUS_CODE[:not_found]
              t('http_codes.not_found')
-           when HttpCodes::UNPROCESSABLE_ENTITY
+           when Rack::Utils::SYMBOL_TO_STATUS_CODE[:unprocessable_entity]
              t('http_codes.unprocessable_entity')
            else
              t('http_codes.internal_server_error')
--- app/enumerations/http_codes.rb
+++
@@ -1,7 +0,0 @@
-class HttpCodes < EnumerateIt::Base
-  associate_values not_found: '404',
-                   unprocessable_entity: '422',
-                   internal_server_error: '500'
-
-  sort_by :none
-end
