1442c1c6d262c4215b7eceea779966fc964acb79
Wilkinson Souza <wilkinson@outlook.com.br>
2019-01-30 11:19:39 -0200

    Adiciona erro 1005 do i-educar as enumerações
    1005 - A nota <valorNota> está acima da configurada para nota máxima para exame que é <notaMaxima>.

--- app/enumerations/ieducar_error_messages.rb
+++ app/enumerations/ieducar_error_messages.rb
@@ -3,6 +3,6 @@ class IeducarErrorMessages < EnumerateIt::Base
                    teacher_must_have_scores_on_previous_steps: 1001,
                    coordinator_must_have_scores_on_previous_steps: 1002,
                    score_higher_than_allowed: 1003,
-                   score_lower_than_allowed: 1004
-
+                   score_lower_than_allowed: 1004,
+                   exam_score_higher_than_allowed: 1005
 end
