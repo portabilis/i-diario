9cf071b90967894b5c9cbadd07a86e55a24c3d48
Joel <ayatamail@gmail.com>
2019-02-06 17:26:14 -0200

    Ajuste na geração do relatório de frequência

--- app/enumerations/periods.rb
+++ app/enumerations/periods.rb
@@ -1,4 +1,13 @@
 class Periods < EnumerateIt::Base
+  class AsObject
+    attr_accessor :id, :to_s
+
+    def initialize(id, to_s)
+      @id = id
+      @to_s = to_s
+    end
+  end
+
   associate_values matutinal: '1',
                    vespertine: '2',
                    nightly: '3',
@@ -6,4 +15,8 @@ class Periods < EnumerateIt::Base
                    intermediate: '5'
 
   sort_by :none
+
+  def self.all
+    to_a.map { |name, id| AsObject.new(id, name) }
+  end
 end
---
+++ app/inputs/select2_period_input.rb
@@ -0,0 +1,29 @@
+class Select2PeriodInput < Select2Input
+  def input(wrapper_options)
+    raise 'User must be passed' unless options[:user].is_a? User
+
+    if (classroom = options[:user].current_classroom)
+      if classroom.period != Periods::FULL.to_s
+        input_html_options[:readonly] = 'readonly'
+        input_html_options[:value] = classroom.period
+      end
+    end
+
+    super(wrapper_options)
+  end
+
+  def parse_collection
+    classroom_period = options[:user].current_classroom.period
+    periods = Periods.all
+
+    options[:elements] = if classroom_period == Periods::FULL
+                           periods.keep_if do |period|
+                             [Periods::FULL, Periods::MATUTINAL, Periods::VESPERTINE].include?(period.id)
+                           end
+                         else
+                           periods.keep_if { |period| period.id == classroom_period }
+                         end
+
+    super
+  end
+end
