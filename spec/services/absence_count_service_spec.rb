149cddc4059dc0f681dc9d1da30f7063d5e70d4e
Joel <ayatamail@gmail.com>
2019-02-19 15:35:39 -0300

    Ajusta testes

--- spec/services/absence_count_service_spec.rb
+++ spec/services/absence_count_service_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe AbsenceCountService, type: :service do
-  let(:classroom) { create(:classroom, :current) }
+  let(:student) { create(:student) }
+  let(:classroom) { create(:classroom, :current, period: '4') }
+  let(:discipline) { create(:discipline) }
   let(:school_calendar) { create(:school_calendar_with_one_step, :current, unity: classroom.unity) }
-
-  let!(:daily_frequency_1) do
+  let(:start_date) { school_calendar.steps.first.start_at }
+  let(:end_date) { school_calendar.steps.first.end_at }
+  let(:daily_frequency_1) do
     create(
       :daily_frequency,
       frequency_date: "04/01/#{school_calendar.year}",
@@ -13,7 +16,23 @@ RSpec.describe AbsenceCountService, type: :service do
       period: 1
     )
   end
-  let!(:daily_frequency_2) do
+  let(:daily_frequency_student_present_1) do
+    create(
+      :daily_frequency_student,
+      student: student,
+      daily_frequency: daily_frequency_1,
+      present: true
+    )
+  end
+  let(:daily_frequency_student_absent_1) do
+    create(
+      :daily_frequency_student,
+      student: student,
+      daily_frequency: daily_frequency_1,
+      present: false
+    )
+  end
+  let(:daily_frequency_2) do
     create(
       :daily_frequency,
       frequency_date: "04/01/#{school_calendar.year}",
@@ -22,93 +41,147 @@ RSpec.describe AbsenceCountService, type: :service do
       period: 2
     )
   end
-
-  describe '#count_absences' do
-    context 'when a student is absent in both periods' do
-      let!(:daily_frequency_student_1) do
-        create(
-          :daily_frequency_student,
-          daily_frequency: daily_frequency_1,
-          present: false
-        )
-      end
-      let!(:daily_frequency_student_2) do
-        create(
-          :daily_frequency_student,
-          daily_frequency: daily_frequency_2,
-          present: false
-        )
-      end
-      let!(:daily_frequency_students) do
-        [daily_frequency_student_1, daily_frequency_student_2]
-      end
-
-      subject do
-        AbsenceCountService.new(daily_frequency_students)
-      end
-
-      it 'count as only one absence' do
-        expect(subject.count_absences).to eq(1)
-      end
-    end
+  let(:daily_frequency_student_present_2) do
+    create(
+      :daily_frequency_student,
+      student: student,
+      daily_frequency: daily_frequency_2,
+      present: true
+    )
+  end
+  let(:daily_frequency_student_absent_2) do
+    create(
+      :daily_frequency_student,
+      student: student,
+      daily_frequency: daily_frequency_2,
+      present: false
+    )
+  end
+  let(:daily_frequency_3) do
+    create(
+      :daily_frequency,
+      frequency_date: "04/01/#{school_calendar.year}",
+      classroom: classroom,
+      discipline: discipline,
+      class_number: 1,
+      school_calendar: school_calendar,
+      period: 1
+    )
+  end
+  let(:daily_frequency_student_present_3) do
+    create(
+      :daily_frequency_student,
+      student: student,
+      daily_frequency: daily_frequency_3,
+      present: true
+    )
+  end
+  let(:daily_frequency_student_absent_3) do
+    create(
+      :daily_frequency_student,
+      student: student,
+      daily_frequency: daily_frequency_3,
+      present: false
+    )
+  end
+  let(:daily_frequency_4) do
+    create(
+      :daily_frequency,
+      frequency_date: "04/01/#{school_calendar.year}",
+      classroom: classroom,
+      discipline: discipline,
+      class_number: 1,
+      school_calendar: school_calendar,
+      period: 2
+    )
+  end
+  let(:daily_frequency_student_present_4) do
+    create(
+      :daily_frequency_student,
+      student: student,
+      daily_frequency: daily_frequency_4,
+      present: true
+    )
+  end
+  let(:daily_frequency_student_absent_4) do
+    create(
+      :daily_frequency_student,
+      student: student,
+      daily_frequency: daily_frequency_4,
+      present: false
+    )
   end
 
-  describe '#count_absences' do
-    context 'when a student is present in at least on period' do
-      let!(:daily_frequency_student_1) do
-        create(
-          :daily_frequency_student,
-          daily_frequency: daily_frequency_1,
-          present: true
-        )
-      end
-      let!(:daily_frequency_student_2) do
-        create(
-          :daily_frequency_student,
-          daily_frequency: daily_frequency_2,
-          present: false
-        )
-      end
-      let!(:daily_frequency_students) do
-        [daily_frequency_student_1, daily_frequency_student_2]
-      end
-
+  describe '#count' do
+    context 'with general presence' do
       subject do
-        AbsenceCountService.new(daily_frequency_students)
+        AbsenceCountService.new(student, classroom, start_date, end_date)
       end
 
-      it 'does not count any absence' do
-        expect(subject.count_absences).to eq(0)
+      context 'when a student is absent in both periods' do
+        it 'count as only one absence' do
+          allow(subject).to receive(:student_frequencies_in_date_range).and_return(
+            [daily_frequency_student_absent_1, daily_frequency_student_absent_2]
+          )
+
+          expect(subject.count).to eq(1)
+        end
+      end
+
+      context 'when a student is present in at least one period' do
+        it 'does not count any absence' do
+          allow(subject).to receive(:student_frequencies_in_date_range).and_return(
+            [daily_frequency_student_present_1, daily_frequency_student_absent_2]
+          )
+
+          expect(subject.count).to eq(0)
+        end
+      end
+
+      context 'when a student is present in both periods' do
+        it 'does not count any absence' do
+          allow(subject).to receive(:student_frequencies_in_date_range).and_return(
+            [daily_frequency_student_present_1, daily_frequency_student_present_2]
+          )
+
+          expect(subject.count).to eq(0)
+        end
       end
     end
-  end
 
-  describe '#count_absences' do
-    context 'when a student is present in both periods' do
-      let!(:daily_frequency_student_1) do
-        create(
-          :daily_frequency_student,
-          daily_frequency: daily_frequency_1,
-          present: true
-        )
-      end
-      let!(:daily_frequency_student_2) do
-        create(
-          :daily_frequency_student,
-          daily_frequency: daily_frequency_2,
-          present: true
-        )
-      end
-      let!(:daily_frequency_students) do
-        [daily_frequency_student_1, daily_frequency_student_2]
-      end
-
+    context 'with presence by components' do
       subject do
-        AbsenceCountService.new(daily_frequency_students)
+        AbsenceCountService.new(student, classroom, start_date, end_date, discipline)
       end
 
-      it 'does not count any absence' do
-        expect(subject.count_absences).to eq(0)
+      context 'when a student is absent in both periods for the same class number' do
+        it 'does count only one absence' do
+          allow(subject).to receive(:student_frequencies_in_date_range).and_return(
+            [daily_frequency_student_absent_3, daily_frequency_student_absent_4]
+          )
+
+          expect(subject.count).to eq(1)
+        end
+      end
+
+      context 'when a student is present in at least one period for the same class number' do
+        it 'does not count any absence' do
+          allow(subject).to receive(:student_frequencies_in_date_range).and_return(
+            [daily_frequency_student_present_3, daily_frequency_student_absent_4]
+          )
+
+          expect(subject.count).to eq(0)
+        end
+      end
+
+      context 'when a student is present in btoth periods for the same class number' do
+        it 'does not count any absence' do
+          allow(subject).to receive(:student_frequencies_in_date_range).and_return(
+            [daily_frequency_student_present_3, daily_frequency_student_present_4]
+          )
+
+          expect(subject.count).to eq(0)
+        end
       end
     end
   end
