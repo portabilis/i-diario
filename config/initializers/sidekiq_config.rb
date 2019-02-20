e78d2c154334ae27a6fe78a567a84c2a718ada17
Marcelo Cajueiro <marcelocajueiro@gmail.com>
2019-02-20 08:49:41 -0300

    Altera expiração do sidekiq-status
    Para ver se fica mais consistente

--- config/initializers/sidekiq_config.rb
+++ config/initializers/sidekiq_config.rb
@@ -1,8 +1,9 @@
 Sidekiq.configure_server do |config|
-  Sidekiq::Status.configure_server_middleware config, expiration: 24.hours
-  Sidekiq::Status.configure_client_middleware config, expiration: 24.hours
+  Sidekiq::Status.configure_server_middleware config, expiration: 7.days
+  Sidekiq::Status.configure_client_middleware config, expiration: 7.days
 end
 
 Sidekiq.configure_client do |config|
-  Sidekiq::Status.configure_client_middleware config, expiration: 24.hours
+  Sidekiq::Status.configure_server_middleware config, expiration: 7.days
+  Sidekiq::Status.configure_client_middleware config, expiration: 7.days
 end
