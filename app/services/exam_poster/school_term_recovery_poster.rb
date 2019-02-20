f6019db25d5923a78e3fa1a41e0462201bdbb664
Eliezer Salvato <eliezer@portabilis.com.br>
2019-02-20 14:37:26 -0300

    Adiciona configuração para escolher a avaliação que vai usar a tabela de arredondamento;
    * Cria enumeration com as avaliações para arredondamento;
    * Cria decorator para ajustar os dados a serem mostrados no select2 e na index;
    * Cria campo array na tabela custom_rounding_tables para receber as avaliações;
    * Adiciona select2 multiplo para escolha das avaliações;
    * Ajusta service ScoreRounder e StudentAverageCalculator para verificar se pode fazer o arredondamento;
    
    refs #1960

--- app/services/exam_poster/school_term_recovery_poster.rb
+++ app/services/exam_poster/school_term_recovery_poster.rb
@@ -36,6 +36,7 @@ module ExamPoster
 
       classroom_ids.each do |classroom|
         teacher_discipline_classrooms = teacher.teacher_discipline_classrooms.where(classroom_id: classroom)
+        score_rounder = ScoreRounder.new(classroom, RoundedAvaliations::SCHOOL_TERM_RECOVERY)
 
         teacher_discipline_classrooms.each do |teacher_discipline_classroom|
           classroom = teacher_discipline_classroom.classroom
@@ -62,7 +63,7 @@ module ExamPoster
 
             value = StudentAverageCalculator.new(student_score)
                                             .calculate(classroom, discipline, get_step(classroom))
-            recovery_value = ScoreRounder.new(classroom).round(school_term_recovery)
+            recovery_value = score_rounder.round(school_term_recovery)
             scores[classroom.api_code][student_score.api_code][discipline.api_code]['nota'] = value
             scores[classroom.api_code][student_score.api_code][discipline.api_code]['recuperacao'] = recovery_value
           end
@@ -83,10 +84,10 @@ module ExamPoster
 
             score = student_recovery.try(:score)
 
-            if score
-              value = ScoreRounder.new(classroom).round(score)
-              scores[classroom.api_code][student.api_code][discipline.api_code]['recuperacao'] = value
-            end
+            next if score.blank?
+
+            value = score_rounder.round(score)
+            scores[classroom.api_code][student.api_code][discipline.api_code]['recuperacao'] = value
           end
         end
       end
