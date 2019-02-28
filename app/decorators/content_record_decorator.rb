3e1a083f3c48b6c89cd5677ad202af4ccca2db65
Eliezer Salvato <eliezer@portabilis.com.br>
2019-02-28 15:48:11 -0300

    Ajusta conforme novo escopo;
    * Ajusta para quando for acesso via professor, ser possívels apenas verificar e editar os planos de outros professores;
    * Ajusta para quando for acesso via servidor/administrador porder verificar, editar e excluir os planos de outros
    professores;
    * Ajusta os relatórios para que seja possível escolher os planos a serem demonstrados(do professor ou de outros);
    
    refs #1892

--- app/decorators/content_record_decorator.rb
+++ app/decorators/content_record_decorator.rb
@@ -3,7 +3,7 @@ class ContentRecordDecorator
   include Decore::Proxy
 
   def author(current_teacher)
-    return I18n.t('enumerations.plans_authors.my_plans') if component.teacher_id == current_teacher.id
+    return I18n.t('enumerations.plans_authors.my_plans') if component.teacher_id == current_teacher.try(:id)
 
     I18n.t('enumerations.plans_authors.others')
   end
--- app/forms/discipline_lesson_plan_report_form.rb
+++ app/forms/discipline_lesson_plan_report_form.rb
@@ -6,51 +6,56 @@ class DisciplineLessonPlanReportForm
                 :discipline_id,
                 :date_start,
                 :date_end,
-                :discipline_lesson_plan,
                 :teacher_id,
-                :report_type
+                :report_type,
+                :author
 
-  validates :date_start, presence: true, date: true, timeliness: { on_or_before: :date_end, type: :date, on_or_before_message: 'não pode ser maior que a Data final' }
-  validates :date_end, presence: true, date: true, timeliness: { on_or_after: :date_start, type: :date, on_or_after_message: 'deve ser maior ou igual a Data inicial' }
+  validates :date_start, presence: true, date: true, timeliness: {
+    on_or_before: :date_end,
+    type: :date,
+    on_or_before_message: 'não pode ser maior que a Data final'
+  }
+  validates :date_end, presence: true, date: true, timeliness: {
+    on_or_after: :date_start,
+    type: :date,
+    on_or_after_message: 'deve ser maior ou igual a Data inicial'
+  }
   validates :unity_id,
-    :classroom_id,
-    :discipline_id,
-    :teacher_id,
-    presence: true
+            :classroom_id,
+            :discipline_id,
+            :teacher_id,
+            :author,
+            presence: true
 
   validate :must_have_records
 
   def discipline_lesson_plan
     DisciplineLessonPlan.by_unity_id(unity_id)
-      .by_teacher_id(teacher_id)
-      .by_classroom_id(classroom_id)
-      .by_discipline_id(discipline_id)
-      .by_date_range(date_start.to_date, date_end.to_date)
-      .order_by_lesson_plan_date
+                        .by_author(author, teacher_id)
+                        .by_classroom_id(classroom_id)
+                        .by_discipline_id(discipline_id)
+                        .by_date_range(date_start.to_date, date_end.to_date)
+                        .order_by_lesson_plan_date
   end
 
   def discipline_content_record
     DisciplineContentRecord.by_unity_id(unity_id)
-      .by_teacher_id(teacher_id)
-      .by_classroom_id(classroom_id)
-      .by_discipline_id(discipline_id)
-      .by_date_range(date_start.to_date, date_end.to_date)
-      .order_by_content_record_date
+                           .by_author(author, teacher_id)
+                           .by_classroom_id(classroom_id)
+                           .by_discipline_id(discipline_id)
+                           .by_date_range(date_start.to_date, date_end.to_date)
+                           .order_by_content_record_date
   end
 
   private
 
   def must_have_records
-    return unless errors.blank?
+    return if errors.present?
 
-    if report_type == "1"
-      if discipline_lesson_plan.count == 0
-        errors.add(:discipline_lesson_plan, :must_have_discipline_lesson_plan)
-      end
-    else
-      if discipline_content_record.count == 0
-        errors.add(:discipline_lesson_plan, :must_have_discipline_lesson_plan)
-      end
+    if report_type == '1'
+      errors.add(:discipline_lesson_plan, :must_have_discipline_lesson_plan) if discipline_lesson_plan.count.zero?
+    elsif discipline_content_record.count.zero?
+      errors.add(:discipline_lesson_plan, :must_have_discipline_lesson_plan)
     end
   end
 end
