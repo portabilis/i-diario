b65428287cb7c68b9361f0bf8f85e0ad1f821284
Marcelo Cajueiro <marcelocajueiro@gmail.com>
2019-03-11 14:54:13 -0300

    Permite buscar período sem passar disciplina
    Quando o professor avalia uma turma de forma descritiva, o aplicativo
    não passa a disciplina.

--- app/services/teacher_period_fetcher.rb
+++ app/services/teacher_period_fetcher.rb
@@ -6,14 +6,17 @@ class TeacherPeriodFetcher
   end
 
   def teacher_period
-    period = if @teacher_id
-               TeacherDisciplineClassroom.find_by(
-                 teacher_id: @teacher_id,
-                 classroom_id: @classroom_id,
-                 discipline_id: @discipline_id
-               ).period
-             end
+    return Classroom.find(@classroom_id).period.to_i if @teacher_id.blank?
 
-    period || Classroom.find(@classroom_id).period.to_i
+    teacher_discipline_classrooms = TeacherDisciplineClassroom.where(
+      teacher_id: @teacher_id,
+      classroom_id: @classroom_id
+    )
+
+    if @discipline_id
+      teacher_discipline_classrooms = teacher_discipline_classrooms.where(discipline_id: @discipline_id)
+    end
+
+    teacher_discipline_classrooms.first.try(:period)
   end
 end
