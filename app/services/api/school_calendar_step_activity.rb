6efb0d5d709a0cf4aaa99b83164a9c102b94d3ea
Joel Ayata <joel@portabilis.com.br>
2019-03-29 17:19:04 -0300

    Adiciona validação para step que não existe

--- app/services/api/school_calendar_step_activity.rb
+++ app/services/api/school_calendar_step_activity.rb
@@ -7,6 +7,8 @@ module Api
     end
 
     def any_activity?
+      render_invalid_step if @calendar_step.nil?
+
       return true if frequencies_in_step(
         @calendar_step.school_calendar_id,
         @calendar_step.start_at,
