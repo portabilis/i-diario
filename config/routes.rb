6698c25dac53357e757146c454597deeb71a0b33
Marcelo Cajueiro <marcelocajueiro@gmail.com>
2019-04-05 10:12:45 -0300

    Configura google tag manager
    Coloca o código do GTM nas páginas. Para isso foi necessário mudar o
    approach da geração das páginas de erro. Como o gtm code vai estar nas
    nossas secret, precisa colocar nas páginas de erro de forma estática
    para continuar imprimindo lá. Assim, foi colocado para compilar as
    páginas.
    
    Para configurar, basta colocar o gtm_code no secrets.yml.

--- config/routes.rb
+++ config/routes.rb
@@ -320,8 +320,4 @@ Rails.application.routes.draw do
 
     resources :data_exportations, only: [:index, :create]
   end
-
-  match '/404', to: 'errors#not_found', via: :all
-  match '/422', to: 'errors#unprocessable_entity', via: :all
-  match '/500', to: 'errors#internal_server_error', via: :all
 end
