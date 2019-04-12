6595c4368716edcc9b59f7d7a6e52c662fe23774
Jeferson <jeferson.hsa@gmail.com>
2019-04-12 16:17:18 -0300

    refatoração de legibilidade em frequências

--- app/models/daily_frequency.rb
+++ app/models/daily_frequency.rb
@@ -27,26 +27,36 @@ class DailyFrequency < ActiveRecord::Base
   validate :ensure_belongs_to_step
 
   scope :by_unity_classroom_discipline_class_number_and_frequency_date_between,
-        lambda { |unity_id, classroom_id, discipline_id, class_number, start_at, end_at=Time.zone.now| where(unity_id: unity_id,
-                                                                                                             classroom_id: classroom_id,
-                                                                                                             discipline_id: discipline_id,
-                                                                                                             class_number: class_number,
-                                                                                                             frequency_date: start_at.to_date..end_at.to_date).includes(students: :student) }
-  scope :by_unity_classroom_and_frequency_date_between,
-        lambda { |unity_id, classroom_id, start_at, end_at = Time.zone.now| where(unity_id: unity_id,
-                                                                                  classroom_id: classroom_id,
-                                                                                  frequency_date: start_at.to_date..end_at.to_date).includes(students: :student) }
+        lambda { |unity_id, classroom_id, discipline_id, class_number, start_at, end_at = Time.zone.now|
+          where(
+            unity_id: unity_id,
+            classroom_id: classroom_id,
+            discipline_id: discipline_id,
+            class_number: class_number,
+            frequency_date: start_at.to_date..end_at.to_date
+          ).includes(students: :student)
+        }
 
-  scope :by_unity_id, lambda { |unity_id| where(unity_id: unity_id) }
-  scope :by_classroom_id, lambda { |classroom_id| where(classroom_id: classroom_id) }
+  scope :by_unity_classroom_and_frequency_date_between,
+        lambda { |unity_id, classroom_id, start_at, end_at = Time.zone.now|
+          where(
+            unity_id: unity_id,
+            classroom_id: classroom_id,
+            frequency_date: start_at.to_date..end_at.to_date
+          ).includes(students: :student)
+        }
+
+
+  scope :by_unity_id, ->(unity_id) { where(unity_id: unity_id) }
+  scope :by_classroom_id, ->(classroom_id) { where(classroom_id: classroom_id) }
   scope :by_period, ->(period) { where(period: period) }
-  scope :by_discipline_id, lambda { |discipline_id| where(discipline_id: discipline_id) }
-  scope :by_frequency_date, lambda { |frequency_date| where(frequency_date: frequency_date.to_date) }
-  scope :by_frequency_date_between, lambda { |start_at, end_at| where(frequency_date: start_at.to_date..end_at.to_date) }
-  scope :by_class_number, lambda { |class_number| where(class_number: class_number) }
+  scope :by_discipline_id, ->(discipline_id) { where(discipline_id: discipline_id) }
+  scope :by_frequency_date, ->(frequency_date) { where(frequency_date: frequency_date.to_date) }
+  scope :by_frequency_date_between, ->(start_at, end_at){ where(frequency_date: start_at.to_date..end_at.to_date) }
+  scope :by_class_number, ->(class_number) { where(class_number: class_number) }
   scope :by_school_calendar_id, ->(school_calendar_id) { where(school_calendar_id: school_calendar_id) }
-  scope :general_frequency, lambda { where(discipline_id: nil, class_number: nil) }
-  scope :has_frequency_for_student, lambda{ |student_id| joins(:students).merge(DailyFrequencyStudent.by_student_id(student_id)) }
+  scope :general_frequency, -> { where(discipline_id: nil, class_number: nil) }
+  scope :has_frequency_for_student, ->(student_id) { joins(:students).merge(DailyFrequencyStudent.by_student_id(student_id)) }
   scope :order_by_student_name, -> { order('students.name') }
   scope :order_by_frequency_date, -> { order(:frequency_date) }
   scope :order_by_frequency_date_desc, -> { order(frequency_date: :desc) }
@@ -55,11 +65,11 @@ class DailyFrequency < ActiveRecord::Base
   scope :order_by_classroom, -> { order(:classroom_id) }
 
   def find_by_student(student_id)
-    students.find_by_student_id(student_id)
+    students.find_by(student_id: student_id)
   end
 
-  def build_or_find_by_student student
-    students.where(student_id: student.id).first || students.build(student_id: student.id, present: 1)
+  def build_or_find_by_student(student)
+    find_by_student(student.id) || students.build(student_id: student.id, present: 1)
   end
 
   def origin=(value)
@@ -81,9 +91,7 @@ class DailyFrequency < ActiveRecord::Base
   def frequency_date_must_be_less_than_or_equal_to_today
     return unless frequency_date
 
-    if frequency_date > Time.zone.today
-      errors.add(:frequency_date, :must_be_less_than_or_equal_to_today)
-    end
+    errors.add(:frequency_date, :must_be_less_than_or_equal_to_today) if frequency_date > Time.zone.today
   end
 
   def frequency_must_be_global_or_discipline
