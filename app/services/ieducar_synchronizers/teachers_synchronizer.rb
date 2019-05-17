7a926724ac4459a0677dcb04f09d35d515fb0004
Eliezer Salvato <eliezer@portabilis.com.br>
2019-05-17 17:32:45 -0300

    Ajusta para quando o professor vier sem nome do ieducar seguir para o proximo registro
    refs #2018

--- app/services/ieducar_synchronizers/teachers_synchronizer.rb
+++ app/services/ieducar_synchronizers/teachers_synchronizer.rb
@@ -17,6 +17,8 @@ class TeachersSynchronizer < BaseSynchronizer
 
   def update_teachers(teachers)
     teachers.each do |teacher_record|
+      next if teacher_record.nome.blank?
+
       Teacher.find_or_initialize_by(api_code: teacher_record.servidor_id).tap do |teacher|
         teacher.name = teacher_record.nome
         teacher.active = teacher_record.ativo == IeducarBooleanState::ACTIVE
