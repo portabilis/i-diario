de78b93b24cb30c935564502d69bcc1764594948
Eliezer Salvato <eliezer@portabilis.com.br>
2019-06-10 17:03:29 -0300

    Implementa testes para o DeleteDispensedExamsAndFrequenciesService
    refs #2127

---
+++ spec/services/delete_dispensed_exams_and_frequencies_service_spec.rb
@@ -0,0 +1,205 @@
+require 'rails_helper'
+
+RSpec.describe DeleteDispensedExamsAndFrequenciesService, type: :service do
+  describe '#run!' do
+    let!(:classroom) { create(:classroom_numeric_and_concept) }
+    let!(:discipline) { create(:discipline) }
+    let!(:other_discipline) { create(:discipline) }
+    let(:unity) { create(:unity) }
+    let!(:student_enrollment) { create(:student_enrollment) }
+    let!(:school_calendar) {
+      create(
+        :school_calendar,
+        :school_calendar_with_semester_steps,
+        :current,
+        unity: classroom.unity
+      )
+    }
+    let!(:student_enrollment_classroom) {
+      create(
+        :student_enrollment_classroom,
+        classroom: classroom,
+        student_enrollment: student_enrollment
+      )
+    }
+    let(:first_semester) { 1 }
+    let(:second_semester) { 2 }
+    let(:inexisting_step) { 3 }
+
+    subject do
+      DeleteDispensedExamsAndFrequenciesService.new(
+        student_enrollment.id,
+        discipline.id,
+        [first_semester]
+      )
+    end
+
+    context 'when there are invalid daily note students' do
+      let(:avaliation1) {
+        create(
+          :current_avaliation,
+          classroom: classroom,
+          discipline: discipline,
+          school_calendar: school_calendar
+        )
+      }
+      let(:avaliation2) {
+        create(
+          :current_avaliation,
+          classroom: classroom,
+          school_calendar: school_calendar
+        )
+      }
+      let(:daily_note1) {
+        create(:current_daily_note, avaliation: avaliation1)
+      }
+      let(:daily_note2) {
+        create(:current_daily_note, avaliation: avaliation2)
+      }
+      let!(:daily_note_student1) {
+        create(
+          :current_daily_note_student,
+          daily_note: daily_note1,
+          student: student_enrollment.student
+        )
+      }
+      let!(:daily_note_student2) {
+        create(
+          :current_daily_note_student,
+          daily_note: daily_note2,
+          student: student_enrollment.student
+        )
+      }
+
+      it 'destroys invalid daily note students' do
+        expect { subject.run! }.to change { DailyNoteStudent.count }.from(2).to(1)
+      end
+
+      context 'when student_enrollment_classroom does not have classroom' do
+        it 'does not destroy invalid daily note students' do
+          student_enrollment_classroom.update(classroom_id: nil)
+
+          expect { subject.run! }.not_to(change { DailyNoteStudent.count })
+        end
+      end
+
+      context 'when steps_fetcher does not find the school_calendar' do
+        it 'does not destroy invalid daily note students' do
+          school_calendar.update(unity: unity)
+
+          expect { subject.run! }.not_to(change { DailyNoteStudent.count })
+        end
+      end
+
+      context 'when step does not exists' do
+        subject do
+          DeleteDispensedExamsAndFrequenciesService.new(
+            student_enrollment.id,
+            discipline.id,
+            [inexisting_step]
+          )
+        end
+
+        it 'does not destroy invalid daily note students' do
+          expect { subject.run! }.not_to(change { DailyNoteStudent.count })
+        end
+      end
+    end
+
+    context 'when there are invalid conceptual exam values' do
+      let!(:conceptual_exam) {
+        create(
+          :conceptual_exam_with_one_value,
+          classroom: classroom,
+          step_id: school_calendar.steps.first.id,
+          student: student_enrollment.student
+        )
+      }
+      let!(:conceptual_exam_value) {
+        create(
+          :conceptual_exam_value,
+          conceptual_exam: conceptual_exam,
+          discipline: discipline
+        )
+      }
+
+      it 'destroys invalid conceptual exam values' do
+        expect { subject.run! }.to change { ConceptualExamValue.count }.from(2).to(1)
+      end
+    end
+
+    context 'when there are invalid descriptive exam students' do
+      let(:descriptive_exam1) {
+        create(
+          :descriptive_exam,
+          :current,
+          classroom: classroom,
+          discipline: discipline,
+          step_id: school_calendar.steps.first.id
+        )
+      }
+      let(:descriptive_exam2) {
+        create(
+          :descriptive_exam,
+          :current,
+          classroom: classroom,
+          step_id: school_calendar.steps.last.id
+        )
+      }
+      let!(:descriptive_exam_student1) {
+        create(
+          :descriptive_exam_student,
+          descriptive_exam: descriptive_exam1,
+          student: student_enrollment.student
+        )
+      }
+      let!(:descriptive_exam_student2) {
+        create(
+          :descriptive_exam_student,
+          descriptive_exam: descriptive_exam2,
+          student: student_enrollment.student
+        )
+      }
+
+      it 'destroys invalid descriptive exam students' do
+        expect { subject.run! }.to change { DescriptiveExamStudent.count }.from(2).to(1)
+      end
+    end
+
+    context 'when there are invalid daily frequency students' do
+      let(:daily_frequency1) {
+        create(
+          :daily_frequency,
+          :current,
+          classroom: classroom,
+          discipline: discipline
+        )
+      }
+      let(:daily_frequency2) {
+        create(
+          :daily_frequency,
+          :current,
+          classroom: classroom
+        )
+      }
+      let!(:daily_frequency_student1) {
+        create(
+          :daily_frequency_student,
+          daily_frequency: daily_frequency1,
+          student: student_enrollment.student
+        )
+      }
+      let!(:daily_frequency_student2) {
+        create(
+          :daily_frequency_student,
+          daily_frequency: daily_frequency2,
+          student: student_enrollment.student
+        )
+      }
+
+      it 'destroys invalid daily frequency students' do
+        expect { subject.run! }.to change { DailyFrequencyStudent.count }.from(2).to(1)
+      end
+    end
+  end
+end
