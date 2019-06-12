18d0e89befc92d0fe8752d42b7c374df5679e06d
Joel Ayata <joel@portabilis.com.br>
2019-06-12 17:32:16 -0300

    Ajustes de estilo do codigo

--- spec/support/controller_helpers.rb
+++ spec/support/controller_helpers.rb
@@ -2,9 +2,9 @@ module ControllerHelpers
   def sign_in(user = double('user', id: rand(1..100)))
     if user.nil?
       allow(request.env['warden']).to receive(:authenticate!).and_throw(:warden, scope: :user)
-      allow(controller).to receive_messages current_user: nil
+      allow(controller).to receive_messages(current_user: nil)
     else
-      allow(request.env['warden']).to receive_messages authenticate!: user
+      allow(request.env['warden']).to receive_messages(authenticate!: user)
       allow(controller).to receive_messages(current_user: user)
     end
   end
