528de64e45be6b8716ff12f6d988f5b59514f568
Joel Ayata <joel@portabilis.com.br>
2019-07-15 18:01:03 -0300

    Adiciona DescriptiveExamStudents para usar Discardable na sinc

---
+++ db/migrate/20190712210522_add_discarded_at_to_descriptive_exam_student.rb
@@ -0,0 +1,10 @@
+class AddDiscardedAtToDescriptiveExamStudent < ActiveRecord::Migration
+  def up
+    add_column :descriptive_exam_students, :discarded_at, :datetime
+    add_index :descriptive_exam_students, :discarded_at
+  end
+
+  def down
+    remove_column :descriptive_exam_students, :discarded_at
+  end
+end
