039a0a707800ee8865d5eaccb2602721308d70b9
Eliezer Salvato <eliezer@portabilis.com.br>
2019-07-19 17:33:05 -0300

    Ajusta factories para serem mais dinâmicas utilizando dados variáveis
    * Ajusta para usar Date.current como padrão de data atual;
    * Ajusta para usar a gem faker sempre que o dados ser dinâmico;
    * Ajusta code style das factories;
    * Ajusta factories com herança errada;
    
    refs #2286

--- spec/factories/complementary_exams.rb
+++ spec/factories/complementary_exams.rb
@@ -1,10 +1,11 @@
 FactoryGirl.define do
   factory :complementary_exam do
-    unity
-    classroom
     discipline
-    complementary_exam_setting
-    recorded_at { Time.zone.today }
+    classroom
+    unity { classroom.unity }
+    association :complementary_exam_setting, factory: :complementary_exam_setting_with_two_grades
+
+    recorded_at { Date.current }
 
     after(:build) do |complementary_exam|
       complementary_exam.students << build(:complementary_exam_student, complementary_exam: complementary_exam)
