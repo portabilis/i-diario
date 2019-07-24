a3b0f0687855ed140c1b983f8014ab51b4782441
Eliezer Salvato <eliezer@portabilis.com.br>
2019-07-24 17:43:33 -0300

    Ajusta users sem relacionamento com user_roles
    * No database_cleaner não estava setando o usuário com user_roles, gerando erro quand precisava buscar alguma permissão;
    * Ajusta a factory users para setar o user_roles corretamente;
    
    refs #2286

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -11,12 +11,11 @@ FactoryGirl.define do
     admin true
 
     factory :user_with_user_role do
-      after(:build) do |user|
-        user.user_roles.build(
-          user: user,
-          role: create(:role),
-          unity: create(:unity)
-        )
+      after(:create) do |user|
+        user_role = create(:user_role)
+        user.user_roles << user_role
+        user.current_user_role = user_role
+        user.save!
       end
     end
   end
