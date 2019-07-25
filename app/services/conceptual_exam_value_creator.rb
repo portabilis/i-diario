e2512a95517698bd5b79c2c1e394ffad3c2b97a4
Joel Ayata <joel@portabilis.com.br>
2019-07-25 10:54:43 -0300

    Ajusta para remover loop aninhado

--- app/services/conceptual_exam_value_creator.rb
+++ app/services/conceptual_exam_value_creator.rb
@@ -11,26 +11,36 @@ class ConceptualExamValueCreator
   end
 
   def create_empty
-    ConceptualExam.where(classroom_id: @classroom.id).each do |conceptual_exam|
-      TeacherDisciplineClassroom.by_teacher_id(@teacher_id)
-                                .by_classroom(@classroom)
-                                .joins(join_conceptual_exam_value(conceptual_exam.id))
-                                .where('conceptual_exam_values.id IS NULL').each do |teacher_discipline_classroom|
-        ConceptualExamValue.create!(
-          conceptual_exam_id: conceptual_exam.id,
-          discipline_id: teacher_discipline_classroom.discipline_id,
-          value: nil,
-          exempted_discipline: false
-        )
-      end
+    TeacherDisciplineClassroom.joins(join_conceptual_exam_value)
+                              .joins(join_conceptual_exam)
+                              .select(
+                                'conceptual_exams.id AS conceptual_exam_id,
+                                teacher_discipline_classrooms.discipline_id AS discipline_id'
+                              )
+                              .by_teacher_id(@teacher_id)
+                              .by_classroom(@classroom)
+                              .where('conceptual_exams.discarded_at IS NULL')
+                              .where('conceptual_exam_values.id IS NULL').each do |record|
+      ConceptualExamValue.create!(
+        conceptual_exam_id: record.conceptual_exam_id,
+        discipline_id: record.discipline_id,
+        value: nil,
+        exempted_discipline: false
+      )
     end
   end
 
-  def join_conceptual_exam_value(conceptual_exam_id)
+  def join_conceptual_exam
+    <<-SQL
+    LEFT JOIN conceptual_exams
+           ON conceptual_exams.classroom_id = teacher_discipline_classrooms.classroom_id
+    SQL
+  end
+
+  def join_conceptual_exam_value
     <<-SQL
       LEFT JOIN conceptual_exam_values
              ON conceptual_exam_values.discipline_id = teacher_discipline_classrooms.discipline_id
-            AND conceptual_exam_values.conceptual_exam_id = #{conceptual_exam_id}
     SQL
   end
 end
