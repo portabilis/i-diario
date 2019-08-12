da1b8b389d72598f8530c9ec4346715d9aeb2047
Eliezer Salvato <eliezer@portabilis.com.br>
2019-08-12 10:34:43 -0300

    Ajusta factory discipline
    * Ajusta a factory para possuir a trait :with_teacher_discipline_classroom, que utiliza da trait de classroom e pode receber o argumeto teacher;
    
    refs #2348

--- spec/factories/disciplines.rb
+++ spec/factories/disciplines.rb
@@ -4,5 +4,22 @@ FactoryGirl.define do
 
     sequence(:api_code, &:to_s)
     description { Faker::Lorem.unique.sentence }
+
+    transient do
+      teacher nil
+    end
+
+    trait :with_teacher_discipline_classroom do
+      after(:create) do |discipline, evaluator|
+        teacher = evaluator.teacher || create(:teacher)
+
+        create(
+          :classroom,
+          :with_teacher_discipline_classroom,
+          discipline: discipline,
+          teacher: teacher
+        )
+      end
+    end
   end
 end
