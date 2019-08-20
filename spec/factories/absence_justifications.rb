ada24ecba3c3dcd496331452e504b017fe8062a0
Joel Ayata <joel@portabilis.com.br>
2019-08-20 17:46:24 -0300

    Revert "Ajusta testes"
    This reverts commit 6f79e66c3bf8eb18c44844e808041b79ed2fa593.

--- spec/factories/absence_justifications.rb
+++ spec/factories/absence_justifications.rb
@@ -1,13 +1,8 @@
 FactoryGirl.define do
   factory :absence_justification do
     teacher
-    students do
-      create_list(:student, 1)
-    end
+    student
 
-    disciplines do
-      create_list(:discipline, 1)
-    end
     teacher_id { teacher.id }
     absence_date { Date.current }
     absence_date_end { Date.current + 1 }
