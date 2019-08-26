56257c9cab91c2c8184bb0bb0cf2c7ebd5feb47b
Eliezer Salvato <eliezer@portabilis.com.br>
2019-08-26 16:16:14 -0300

    Ajusta as factories descriptive_exam e descriptive_exam_student
    * Ajusta para usar traits;
    * Remove trait current;
    * Ajusta lugares que estavam usando as factories;
    
    refs #2358

--- spec/factories/descriptive_exams.rb
+++ spec/factories/descriptive_exams.rb
@@ -1,35 +1,53 @@
 FactoryGirl.define do
   factory :descriptive_exam do
-    classroom
     discipline
+
+    association :classroom, factory: [:classroom, :score_type_concept, :with_classroom_semester_steps]
 
     opinion_type OpinionTypes::BY_YEAR_AND_DISCIPLINE
 
-    trait :current do
-      classroom
+    transient do
+      step nil
+      teacher nil
+      student nil
     end
 
-    after(:build) do |descriptive_exam|
-      teacher_discipline_classroom = create(
-        :teacher_discipline_classroom,
-        classroom: descriptive_exam.classroom,
-        discipline: descriptive_exam.discipline
-      )
-
-      descriptive_exam.teacher_id = teacher_discipline_classroom.teacher.id
-
-      if descriptive_exam.step_id.blank?
-        school_calendar = create(
-          :school_calendar,
-          :with_trimester_steps,
-          year: descriptive_exam.classroom.year,
-          unity: descriptive_exam.classroom.unity
-        )
-        descriptive_exam.step_id = school_calendar.step_by_number(1).id
+    before(:create) do |descriptive_exam, evaluator|
+      if descriptive_exam.classroom.calendar.present?
+        step = evaluator.step || descriptive_exam.classroom.calendar.classroom_steps.first
       end
 
-      descriptive_exam.recorded_at ||= descriptive_exam.step.start_at
-      descriptive_exam.step_number = descriptive_exam.step.step_number if descriptive_exam.step_number.zero?
+      if descriptive_exam.recorded_at.blank?
+        descriptive_exam.recorded_at = step.try(:first_school_calendar_date) || Date.current
+      end
+
+      descriptive_exam.step_number = step.try(:step_number) || 1 if descriptive_exam.step_number.zero?
+      descriptive_exam.step_id ||= step.try(:id)
+    end
+
+    trait :with_teacher_discipline_classroom do
+      after(:build) do |descriptive_exam, evaluator|
+        teacher = Teacher.find(descriptive_exam.teacher_id) if descriptive_exam.teacher_id.present?
+        teacher ||= evaluator.teacher || create(:teacher)
+        descriptive_exam.teacher_id = teacher.id if descriptive_exam.teacher_id.blank?
+
+        create(
+          :teacher_discipline_classroom,
+          classroom: descriptive_exam.classroom,
+          discipline: descriptive_exam.discipline,
+          teacher: teacher
+        )
+      end
+    end
+
+    trait :with_one_student do
+      after(:build) do |descriptive_exam, evaluator|
+        student = evaluator.student || create(:student)
+
+        descriptive_exam.descriptive_exam_students.build(
+          attributes_for(:descriptive_exam_student, student_id: student.id)
+        )
+      end
     end
   end
 end
