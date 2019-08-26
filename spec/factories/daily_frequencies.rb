dd8b02aba97d9e5237f915eae5b2906f901db480
Eliezer Salvato <eliezer@portabilis.com.br>
2019-08-26 13:51:32 -0300

    Ajusta a factory daily_frequency
    * Ajusta para usar traits;
    * Remove trait current;
    * Ajusta lugares que usavam a factory;
    
    refs #2356

--- spec/factories/daily_frequencies.rb
+++ spec/factories/daily_frequencies.rb
@@ -1,18 +1,17 @@
 FactoryGirl.define do
   factory :daily_frequency do
     discipline
-    association :classroom, factory: [:classroom, :with_classroom_semester_steps]
+    classroom
 
     unity { classroom.unity }
-    school_calendar { classroom.calendar.try(:school_calendar) || create(:school_calendar, :current) }
+    school_calendar { classroom.calendar.try(:school_calendar) || create(:school_calendar) }
 
     frequency_date { Date.current }
     class_number { rand(1..5) }
     period { Periods.to_a.sample[1] }
 
-    trait :current do
-      classroom
-      association :school_calendar, factory: [:school_calendar, :with_one_step]
+    transient do
+      teacher nil
     end
 
     trait :without_discipline do
@@ -22,6 +21,21 @@ FactoryGirl.define do
 
     trait :by_discipline do
       association :classroom, factory: [:classroom, :by_discipline]
+    end
+
+    trait :with_teacher_discipline_classroom do
+      after(:create) do |daily_frequency, evaluator|
+        teacher = Teacher.find(daily_frequency.teacher_id) if daily_frequency.teacher_id.present?
+        teacher ||= evaluator.teacher || create(:teacher)
+        daily_frequency.teacher_id = teacher.id if daily_frequency.teacher_id.blank?
+
+        create(
+          :teacher_discipline_classroom,
+          classroom: daily_frequency.classroom,
+          discipline: daily_frequency.discipline,
+          teacher: teacher
+        )
+      end
     end
   end
 end
