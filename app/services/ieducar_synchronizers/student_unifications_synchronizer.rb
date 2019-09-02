348495ec53fa77c077221065facf055881d50eb3
Joel Ayata <joel@portabilis.com.br>
2019-09-02 10:09:44 -0300

    Usa metodo student e refatora codigo

--- app/services/ieducar_synchronizers/student_unifications_synchronizer.rb
+++ app/services/ieducar_synchronizers/student_unifications_synchronizer.rb
@@ -19,46 +19,45 @@ class StudentUnificationsSynchronizer < BaseSynchronizer
     unifications.each do |unification|
       next if unification.main_id.blank?
 
-      student_id = Student.with_discarded.find_by(api_code: unification.main_id).try(:id)
-      next if student_id.blank?
+      student = student(unification.main_id)
+
+      next if student.blank?
 
       StudentUnification.find_or_initialize_by(
-        student_id: student_id
+        student_id: student.id
       ).tap do |student_unification|
         student_unification.unified_at = unification.created_at
         student_unification.active = unification.active
 
-        new_record = false
-
         if student_unification.changed?
-          new_record = true if student_unification.new_record?
+          new_record = student_unification.new_record?
 
           student_unification.save!
 
+          secondary_students = []
+
+          unification.duplicates_id.each do |api_code|
+            secondary_students << student(api_code)
+          end
+
           if new_record
-            unification.duplicates_id.each do |api_code|
+            secondary_students.each do |secondary_student|
               student_unification.unified_students.create!(
-                student: Student.with_discarded.find_by(api_code: api_code)
+                student: secondary_student
               )
             end
           end
 
           next if !unification.active && new_record
 
-          unify_or_revert(unification.active, student_id, unification.duplicates_id)
+          unify_or_revert(unification.active, student, secondary_students)
         end
       end
     end
   end
 
-  def unify_or_revert(unify, student_id, duplicates_id)
-    main_student = Student.with_discarded.find(student_id)
-    secondary_students = Student.with_discarded.where(id: duplicates_id)
-
-    if unify
-      StudentUnificationService.new(main_student, secondary_students).run!
-    else
-      StudentUnificationReverterService.new(main_student, secondary_students).run!
-    end
+  def unify_or_revert(unify, main_student, secondary_students)
+    StudentUnificationService.new(main_student, secondary_students).run! if unify
+    StudentUnificationReverterService.new(main_student, secondary_students).run! unless unify
   end
 end
