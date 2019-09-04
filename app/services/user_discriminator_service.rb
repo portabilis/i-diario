fd32d94ec72b52abec48c180ae7ba57b4c76c62c
Joel Ayata <joel@portabilis.com.br>
2019-09-04 10:39:48 -0300

    Ajusta segun CR

--- app/services/user_discriminator_service.rb
+++ app/services/user_discriminator_service.rb
@@ -5,14 +5,12 @@ class UserDiscriminatorService
   end
 
   def user_id
-    @user_id ||= begin
-                    teacher_id = @user.try(:assumed_teacher_id)
+    teacher_id = @user.try(:assumed_teacher_id)
 
-                    if @employee_or_admin && teacher_id
-                      User.find_by(teacher_id: teacher_id).try(:id)
-                    else
-                      @user.try(:id)
-                    end
-                  end
+    @user_id ||= if @employee_or_admin && teacher_id
+                   User.find_by(teacher_id: teacher_id).try(:id)
+                 else
+                   @user.try(:id)
+                 end
   end
 end
