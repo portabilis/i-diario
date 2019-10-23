93fdf3904ecaf7124c4d0615cef3b0801e878e9b
Eliezer Salvato <eliezer@portabilis.com.br>
2019-10-23 15:02:33 -0300

    Adiciona teste de exclusão de frequências
    * Ajusta a factory daily_frequency para possuir uma trait com alunos e a turma vir com etapa;
    * Cria teste de exclusão para validar se está permitindo excluir as frequências;
    
    refs #2555

---
+++ spec/controllers/daily_frequencies_controller_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+RSpec.describe DailyFrequenciesController, type: :controller do
+  let(:entity) { Entity.find_by(domain: 'test.host') }
+  let(:user) { create(:user_with_user_role, admin: false) }
+  let(:user_role) { user.user_roles.first }
+  let(:unity) { user_role.unity }
+  let(:current_teacher) { create(:teacher) }
+  let(:other_teacher) { create(:teacher) }
+  let(:classroom) { create(:classroom) }
+  let(:discipline) { create(:discipline) }
+
+  around(:each) do |example|
+    entity.using_connection do
+      example.run
+    end
+  end
+
+  before do
+    sign_in(user)
+    allow(controller).to receive(:authorize).and_return(true)
+    allow(controller).to receive(:current_user_is_employee_or_administrator?).and_return(false)
+    allow(controller).to receive(:current_teacher).and_return(current_teacher)
+    allow(controller).to receive(:current_user_unity).and_return(unity)
+    allow(controller).to receive(:current_user_school_year).and_return(classroom.year)
+    allow(controller).to receive(:current_user_classroom).and_return(classroom)
+    allow(controller).to receive(:current_user_discipline).and_return(discipline)
+  end
+
+  describe 'DELETE #destroy_multiple' do
+    let!(:daily_frequency_1) {
+      create(
+        :daily_frequency,
+        :with_students,
+        students_count: 3
+      )
+    }
+    let!(:daily_frequency_2) {
+      create(
+        :daily_frequency,
+        :with_students,
+        students_count: 3
+      )
+    }
+
+    it 'deletes all daily_frequencies and daily_frequency_students' do
+      daily_frequencies_ids = [daily_frequency_1.id, daily_frequency_2.id]
+
+      expect {
+        delete :destroy_multiple, locale: 'pt-BR', daily_frequencies_ids: daily_frequencies_ids
+      }.to change(DailyFrequency, :count).by(-2)
+    end
+  end
+end
--- spec/factories/daily_frequencies.rb
+++ spec/factories/daily_frequencies.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :daily_frequency do
     discipline
-    classroom
 
     unity { classroom.unity }
     school_calendar { classroom.calendar.try(:school_calendar) || create(:school_calendar) }
+    association :classroom, factory: [:classroom, :with_classroom_semester_steps]
 
     frequency_date { Date.current }
     class_number { rand(1..5) }
@@ -21,6 +21,16 @@ FactoryGirl.define do
 
     trait :by_discipline do
       association :classroom, factory: [:classroom, :by_discipline]
+    end
+
+    trait :with_students do
+      transient do
+        students_count 1
+      end
+
+      after(:create) do |daily_frequency, evaluator|
+        create_list(:daily_frequency_student, evaluator.students_count, daily_frequency: daily_frequency)
+      end
     end
 
     trait :with_teacher_discipline_classroom do
