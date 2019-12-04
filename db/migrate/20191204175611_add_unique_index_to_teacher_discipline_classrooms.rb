66dba7111d449fb1e843cccdbb4aafd255677346
Joel Ayata <joel@portabilis.com.br>
2019-12-04 15:14:25 -0300

    Remove duplicados e adiciona indice unico

---
+++ db/migrate/20191204175611_add_unique_index_to_teacher_discipline_classrooms.rb
@@ -0,0 +1,6 @@
+class AddUniqueIndexToTeacherDisciplineClassrooms < ActiveRecord::Migration
+  def change
+    add_index :teacher_discipline_classrooms, [:teacher_id, :discipline_id, :classroom_id, :year, :period],
+              name: 'idx_unique_teacher_discipline_classrooms', unique: true
+  end
+end
