adf5198fa585fbc94f0c118dfea080e89efa582d
Marcelo Cajueiro <marcelocajueiro@gmail.com>
2019-12-05 17:46:30 -0300

    Remove índices que já são cobertos por outros
    Usado relatório do pghero

---
+++ db/migrate/20191205203551_remove_unneeded_index_avaliation_exemptions_on_avaliation_id.rb
@@ -0,0 +1,11 @@
+class RemoveUnneededIndexAvaliationExemptionsOnAvaliationId < ActiveRecord::Migration
+  def up
+    remove_index :avaliation_exemptions, name: "index_avaliation_exemptions_on_avaliation_id"
+  end
+
+  def down
+    execute %{
+      CREATE INDEX index_avaliation_exemptions_on_avaliation_id ON public.avaliation_exemptions USING btree (avaliation_id);
+    }
+  end
+end
