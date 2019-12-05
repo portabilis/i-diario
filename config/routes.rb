b1e72c2ffe9d6d2e14ab82db2676f08af1ffaf60
Marcelo Cajueiro <marcelocajueiro@gmail.com>
2019-12-05 10:39:36 -0300

    Adiciona pghero
    Para nos dar insights sobre o banco.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
+  mount PgHero::Engine, at: 'pghero'
 
   get 'worker-processses-status', to: 'sidekiq_monitor#processes_status'
 
