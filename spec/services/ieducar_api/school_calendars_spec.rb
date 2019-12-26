c893fc332321ab71bfe679d7603c84e969bc527e
Joel Ayata <joel@portabilis.com.br>
2019-12-26 09:03:39 -0300

    Ajusta testes

--- spec/services/ieducar_api/school_calendars_spec.rb
+++ spec/services/ieducar_api/school_calendars_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe IeducarApi::SchoolCalendars, type: :service do
   let(:access_key) { '8IOwGIjiHvbeTklgwo10yVLgwDhhvs' }
   let(:secret_key) { '5y8cfq31oGvFdAlGMCLIeSKdfc8pUC' }
   let(:unity_id) { 1 }
+  let(:year) { Time.zone.today.year }
 
   subject {
     IeducarApi::SchoolCalendars.new(
@@ -17,7 +18,7 @@ RSpec.describe IeducarApi::SchoolCalendars, type: :service do
   describe '#fetch' do
     it 'should return all school calendars' do
       VCR.use_cassette('school_calendars') do
-        result = subject.fetch
+        result = subject.fetch(ano: year, escola: unity_id)
 
         expect(result.keys).to include('escolas')
         expect(result['escolas'].first.keys).to include('escola_id')
