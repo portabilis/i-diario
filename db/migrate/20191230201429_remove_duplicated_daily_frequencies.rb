65140be78c850e03cdf653ea8d6ed8225c4ab352
Joel Ayata <joel@portabilis.com.br>
2020-01-06 09:41:39 -0300

    Add migration para remover duplicados

---
+++ db/migrate/20191230201429_remove_duplicated_daily_frequencies.rb
@@ -0,0 +1,23 @@
+class RemoveDuplicatedDailyFrequencies < ActiveRecord::Migration
+  def change
+    daily_frequencies = DailyFrequency.joins(:classroom).group(
+      :classroom_id, :frequency_date, :discipline_id, :class_number
+    ).where.not(
+      classrooms: { period: Periods::FULL }
+    ).having(
+      'COUNT(1) > 1'
+    ).pluck(
+      'MAX(daily_frequencies.id)', :classroom_id, :frequency_date, :discipline_id, :class_number
+    )
+
+    daily_frequencies.each do |correct_id, classroom_id, frequency_date, discipline_id, class_number|
+      DailyFrequency.where(classroom_id: classroom_id, frequency_date: frequency_date)
+                    .where(<<-SQL, discipline_id, class_number)
+                        COALESCE(discipline_id, 0) = COALESCE(?, 0) AND
+                        COALESCE(class_number, 0) = COALESCE(?, 0)
+                    SQL
+                    .where.not(id: correct_id)
+                    .destroy_all
+    end
+  end
+end
