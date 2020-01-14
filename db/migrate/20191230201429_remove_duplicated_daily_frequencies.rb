511d8cf1cc642a9f0727c3049028cb9764ee0665
Joel Ayata <joel@portabilis.com.br>
2020-01-14 10:59:24 -0300

    Remove as frequencias certas

--- db/migrate/20191230201429_remove_duplicated_daily_frequencies.rb
+++ db/migrate/20191230201429_remove_duplicated_daily_frequencies.rb
@@ -11,15 +11,16 @@ class RemoveDuplicatedDailyFrequencies < ActiveRecord::Migration
     )
 
     daily_frequencies.each do |correct_id, classroom_id, frequency_date, discipline_id, class_number|
-      DailyFrequencyStudent.where.not(daily_frequency_id: correct_id).delete_all
+      daily_frequencies = DailyFrequency.where(classroom_id: classroom_id, frequency_date: frequency_date)
+                                        .where(<<-SQL, discipline_id, class_number)
+                                            COALESCE(discipline_id, 0) = COALESCE(?, 0) AND
+                                            COALESCE(class_number, 0) = COALESCE(?, 0)
+                                        SQL
+                                        .where.not(id: correct_id)
 
-      DailyFrequency.where(classroom_id: classroom_id, frequency_date: frequency_date)
-                    .where(<<-SQL, discipline_id, class_number)
-                        COALESCE(discipline_id, 0) = COALESCE(?, 0) AND
-                        COALESCE(class_number, 0) = COALESCE(?, 0)
-                    SQL
-                    .where.not(id: correct_id)
-                    .delete_all
+      DailyFrequencyStudent.where(daily_frequency_id: daily_frequencies.pluck(:id)).delete_all
+
+      daily_frequencies.delete_all
     end
   end
 end
