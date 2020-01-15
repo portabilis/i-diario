f9957a49739565e39633d3a01f6481a7fd7973dd
Joel Ayata <joel@portabilis.com.br>
2020-01-15 10:24:50 -0300

    Altera para Raw sql

--- db/migrate/20191230201429_remove_duplicated_daily_frequencies.rb
+++ db/migrate/20191230201429_remove_duplicated_daily_frequencies.rb
@@ -1,26 +1,45 @@
 class RemoveDuplicatedDailyFrequencies < ActiveRecord::Migration
   def change
-    daily_frequencies = DailyFrequency.joins(:classroom).group(
-      :classroom_id, :frequency_date, :discipline_id, :class_number
-    ).where.not(
-      classrooms: { period: Periods::FULL }
-    ).having(
-      'COUNT(1) > 1'
-    ).pluck(
-      'MAX(daily_frequencies.id)', :classroom_id, :frequency_date, :discipline_id, :class_number
-    )
+    execute <<-SQL
+      DO $$
+        DECLARE _daily_frequency RECORD;
+        DECLARE _repeated_daily_frequency_ids INTEGER[];
+      BEGIN
+        FOR _daily_frequency IN (
+          SELECT MAX(daily_frequencies.id) AS correct_id,
+                 daily_frequencies.classroom_id AS classroom_id,
+                 daily_frequencies.frequency_date AS frequency_date,
+                 daily_frequencies.discipline_id AS discipline_id,
+                 daily_frequencies.class_number AS class_number
+            FROM daily_frequencies
+            JOIN classrooms
+              ON classrooms.id = daily_frequencies.classroom_id
+             AND classrooms.discarded_at IS NULL
+           WHERE (classrooms.period <> '4')
+        GROUP BY daily_frequencies.classroom_id,
+                 daily_frequencies.frequency_date,
+                 daily_frequencies.discipline_id,
+                 daily_frequencies.class_number
+          HAVING COUNT(1) > 1
+        ) LOOP
+        _repeated_daily_frequency_ids := ARRAY[]::INTEGER[];
+        _repeated_daily_frequency_ids := ARRAY(
+          SELECT id
+            FROM daily_frequencies
+           WHERE classroom_id = _daily_frequency.classroom_id
+             AND frequency_date = _daily_frequency.frequency_date
+             AND COALESCE(discipline_id, 0) = COALESCE(_daily_frequency.discipline_id, 0)
+             AND COALESCE(class_number, 0) = COALESCE(_daily_frequency.class_number, 0)
+             AND daily_frequencies.id <> _daily_frequency.correct_id
+        );
 
-    daily_frequencies.each do |correct_id, classroom_id, frequency_date, discipline_id, class_number|
-      daily_frequencies = DailyFrequency.where(classroom_id: classroom_id, frequency_date: frequency_date)
-                                        .where(<<-SQL, discipline_id, class_number)
-                                            COALESCE(discipline_id, 0) = COALESCE(?, 0) AND
-                                            COALESCE(class_number, 0) = COALESCE(?, 0)
-                                        SQL
-                                        .where.not(id: correct_id)
+          DELETE FROM daily_frequency_students
+           WHERE daily_frequency_id = ANY (_repeated_daily_frequency_ids);
 
-      DailyFrequencyStudent.where(daily_frequency_id: daily_frequencies.pluck(:id)).delete_all
-
-      daily_frequencies.delete_all
-    end
+          DELETE FROM daily_frequencies
+           WHERE id = ANY ( _repeated_daily_frequency_ids);
+        END LOOP;
+      END$$;
+    SQL
   end
 end
