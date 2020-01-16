0862475517c655cfec8111f447481ec37bd6a366
Joel Ayata <joel@portabilis.com.br>
2020-01-16 17:24:56 -0300

    Add migration para remover casos duplicados

---
+++ db/migrate/20200116202250_remove_duplicated_api_code_teacher_discipline_classrooms.rb
@@ -0,0 +1,20 @@
+class RemoveDuplicatedApiCodeTeacherDisciplineClassrooms < ActiveRecord::Migration
+  def change
+    teacher_discipline_classrooms = TeacherDisciplineClassroom.unscoped.group(
+      :api_code, :teacher_id, :year, :discipline_id
+    ).having(
+      'COUNT(1) > 1'
+    ).pluck(
+      'MAX(id)', :api_code, :teacher_id, :year, :discipline_id
+    )
+
+    teacher_discipline_classrooms.each do |correct_id, api_code, teacher_id, year, discipline_id|
+      TeacherDisciplineClassroom.unscoped.where(
+        api_code: api_code,
+        teacher_id: teacher_id,
+        year: year,
+        discipline_id: discipline_id
+      ).where.not(id: correct_id).delete_all
+    end
+  end
+end
