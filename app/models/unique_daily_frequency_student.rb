ef5283d7a1cd29b4d6a1e51e05a5429b26046eaa
Eliezer Salvato <eliezer@portabilis.com.br>
2020-02-28 15:04:33 -0300

    Ajusta scope by_teacher_id para não sofrer com SQL injection
    refs #2815

--- app/models/unique_daily_frequency_student.rb
+++ app/models/unique_daily_frequency_student.rb
@@ -13,7 +13,7 @@ class UniqueDailyFrequencyStudent < ActiveRecord::Base
   scope :frequency_date, ->(frequency_date) { where(frequency_date: frequency_date) }
   scope :frequency_date_between, ->(start_at, end_at) { where(frequency_date: start_at.to_date..end_at.to_date) }
   scope :by_teacher_id, lambda { |teacher_id|
-    where("'{#{teacher_id}}' && unique_daily_frequency_students.absences_by")
+    where("'{?}' && unique_daily_frequency_students.absences_by", teacher_id.to_i)
   }
   scope :ordered, -> { order(frequency_date: :desc) }
 end
