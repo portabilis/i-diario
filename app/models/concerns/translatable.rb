4952f65289b746500dc0daba19c2afa06665db0a
Joel Ayata <joel@portabilis.com.br>
2020-03-03 16:42:14 -0300

    Adiciona param `option`

--- app/models/concerns/translatable.rb
+++ app/models/concerns/translatable.rb
@@ -2,7 +2,7 @@ module Translatable
   extend ActiveSupport::Concern
 
   module ClassMethods
-    def human_attribute_name(column_name)
+    def human_attribute_name(column_name, options = {})
       Translator.t("activerecord.attributes.#{model_name.param_key}.#{column_name}")
     end
   end
