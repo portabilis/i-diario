d83f076f40a3d3ed6bedde164aa69efd9a834dee
Joel Ayata <joel@portabilis.com.br>
2020-03-09 14:55:09 -0300

    Add offline js

--- app/assets/javascripts/educacao.js
+++ app/assets/javascripts/educacao.js
@@ -89,4 +89,20 @@ $(function () {
 
     checkboxes.prop("checked", $(this).prop('checked'));
   });
+
+  var
+      $online = $('.online'),
+      $offline = $('.offline');
+
+  Offline.on('confirmed-down', function () {
+      $online.fadeOut(function () {
+          $offline.fadeIn();
+      });
+  });
+
+  Offline.on('confirmed-up', function () {
+      $offline.fadeOut(function () {
+          $online.fadeIn();
+      });
+  });
 });
