8fe19511fbfd3cd74199828f3b2e44e7f984dd51
Joel Ayata <joel@portabilis.com.br>
2020-03-27 09:16:23 -0300

    Ajusta link para modal

--- app/helpers/pedagogical_tracking_helper.rb
+++ app/helpers/pedagogical_tracking_helper.rb
@@ -1,12 +1,10 @@
 module PedagogicalTrackingHelper
   def link_params(record)
-    params = if record.classroom_id.present?
-               { 'search[classroom_id]': record.classroom_id }
-             else
-               { 'search[unity_id]': record.unity_id }
-             end
-
-    params.merge!('search[start_date]': format(record.start_date), 'search[end_date]': format(record.end_date))
+    {
+      'search[unity_id]': record.unity_id,
+      'search[start_date]': format(record.start_date),
+      'search[end_date]': format(record.end_date)
+    }
   end
 
   def format(date)
