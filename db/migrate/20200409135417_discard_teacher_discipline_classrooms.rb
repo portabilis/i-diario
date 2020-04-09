e3f7b945a06f3c0c24fb8dca8dd3853e5e40089a
Marcelo Cajueiro <marcelocajueiro@gmail.com>
2020-04-09 13:30:47 -0300

    Remove migration lenta
    Iremos rodar na mão

--- db/migrate/20200409135417_discard_teacher_discipline_classrooms.rb
+++
@@ -1,7 +0,0 @@
-class DiscardTeacherDisciplineClassrooms < ActiveRecord::Migration
-  def change
-    Classroom.unscoped.discarded.each do |c|
-      c.teacher_discipline_classrooms.each { |tdc| tdc.delay(:discard) }
-    end
-  end
-end
