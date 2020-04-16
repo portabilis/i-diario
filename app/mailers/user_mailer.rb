6d7d3b0986477dadd79df47d6caabb7bfd0eb9fe
Marcelo Cajueiro <marcelocajueiro@gmail.com>
2020-04-16 11:32:12 -0300

    Não envia e-mail quando e-mail é nulo
    Evita erro.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,8 @@ class UserMailer < ActionMailer::Base
   default from: 'Notificação i-Diário <no@reply.com.br>'
 
   def notify_activation(user_email, user_name, user_logged_as, domain)
+    return unless user_email
+
     @user_name = user_name
     @user_logged_as = user_logged_as
     @domain = domain
