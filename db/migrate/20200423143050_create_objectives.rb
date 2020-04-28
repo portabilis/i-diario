dacadcbdb509c7453e4e0905112078a47b2904c0
Eliezer Salvato <eliezer@portabilis.com.br>
2020-04-28 11:33:44 -0300

    Remove criação de indice na tabela
    * Coluna com indice contém muitos dados e quebra o indice e nesse momento não tem necessidade de indice;
    
    refs #3011

--- db/migrate/20200423143050_create_objectives.rb
+++ db/migrate/20200423143050_create_objectives.rb
@@ -1,7 +1,7 @@
 class CreateObjectives < ActiveRecord::Migration
   def change
     create_table :objectives do |t|
-      t.text :description, null: false, index: true
+      t.text :description, null: false
 
       t.timestamps null: false
     end
