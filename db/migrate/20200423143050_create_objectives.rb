c952cd06d7c1f481b0bfe9875a31d531c8b0e430
Eliezer Salvato <eliezer@portabilis.com.br>
2020-04-28 11:23:41 -0300

    Altera tipo da coluna de descrição de objetivos para text
    refs #3011

--- db/migrate/20200423143050_create_objectives.rb
+++ db/migrate/20200423143050_create_objectives.rb
@@ -1,7 +1,7 @@
 class CreateObjectives < ActiveRecord::Migration
   def change
     create_table :objectives do |t|
-      t.string :description, null: false, index: true
+      t.text :description, null: false, index: true
 
       t.timestamps null: false
     end
