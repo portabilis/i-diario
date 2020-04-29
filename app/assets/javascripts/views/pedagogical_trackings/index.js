eaa571b73282f0f17e2cb1965787e95a5e582746
Joel Ayata <joel@portabilis.com.br>
2020-04-29 14:11:12 -0300

    Ajusta estilo do gráfico

--- app/assets/javascripts/views/pedagogical_trackings/index.js
+++ app/assets/javascripts/views/pedagogical_trackings/index.js
@@ -5,6 +5,92 @@ var done_frequencies_percentage = $('#done_frequencies_percentage').val();
 var done_content_records_percentage = $('#done_content_records_percentage').val();
 var unknown_teachers = $('#unknown_teachers').val();
 
+function textToCenter() {
+  Chart.pluginService.register({
+    beforeDraw: function(chart) {
+      if (chart.config.options.elements.center) {
+        // Get ctx from string
+        var ctx = chart.chart.ctx;
+
+        // Get options from the center object in options
+        var centerConfig = chart.config.options.elements.center;
+        var fontStyle = centerConfig.fontStyle || 'Arial';
+        var txt = centerConfig.text;
+        var color = centerConfig.color || '#000';
+        var maxFontSize = centerConfig.maxFontSize || 75;
+        var sidePadding = centerConfig.sidePadding || 20;
+        var sidePaddingCalculated = (sidePadding / 100) * (chart.innerRadius * 2)
+        // Start with a base font of 30px
+        ctx.font = "30px " + fontStyle;
+
+        // Get the width of the string and also the width of the element minus 10 to give it 5px side padding
+        var stringWidth = ctx.measureText(txt).width;
+        var elementWidth = (chart.innerRadius * 2) - sidePaddingCalculated;
+
+        // Find out how much the font can grow in width.
+        var widthRatio = elementWidth / stringWidth;
+        var newFontSize = Math.floor(30 * widthRatio);
+        var elementHeight = (chart.innerRadius * 2);
+
+        // Pick a new font size so it will not be larger than the height of label.
+        var fontSizeToUse = Math.min(newFontSize, elementHeight, maxFontSize);
+        var minFontSize = centerConfig.minFontSize;
+        var lineHeight = centerConfig.lineHeight || 25;
+        var wrapText = false;
+
+        if (minFontSize === undefined) {
+          minFontSize = 20;
+        }
+
+        if (minFontSize && fontSizeToUse < minFontSize) {
+          fontSizeToUse = minFontSize;
+          wrapText = true;
+        }
+
+        // Set font settings to draw it correctly.
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        var centerX = ((chart.chartArea.left + chart.chartArea.right) / 2);
+        var centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 2);
+        ctx.font = fontSizeToUse + "px " + fontStyle;
+        ctx.fillStyle = color;
+
+        if (!wrapText) {
+          ctx.fillText(txt, centerX, centerY);
+          return;
+        }
+
+        var words = txt.split(' ');
+        var line = '';
+        var lines = [];
+
+        // Break words up into multiple lines if necessary
+        for (var n = 0; n < words.length; n++) {
+          var testLine = line + words[n] + ' ';
+          var metrics = ctx.measureText(testLine);
+          var testWidth = metrics.width;
+          if (testWidth > elementWidth && n > 0) {
+            lines.push(line);
+            line = words[n] + ' ';
+          } else {
+            line = testLine;
+          }
+        }
+
+        // Move the center up depending on line height and number of lines
+        centerY -= (lines.length / 2) * lineHeight;
+
+        for (var n = 0; n < lines.length; n++) {
+          ctx.fillText(lines[n], centerX, centerY);
+          centerY += lineHeight;
+        }
+        //Draw text in center
+        ctx.fillText(line, centerX, centerY);
+      }
+    }
+  });
+}
+
 function clear_empty(element) {
   if (element.val === "empty") {
     $(element.target).select2("val", "");
@@ -18,24 +104,37 @@ function build_pie_chart(ctx, done_percentage, unknown_teachers = null){
   var borderColor = ['rgba(191, 74, 74, 1)', 'rgba(133, 191, 74, 1)']
 
   if (unknown_teachers) {
-    labels.push('% Lançamentos desconhecidos');
+    labels.push('% Período não mapeado');
     data = [(100 - (parseFloat(done_percentage) + parseFloat(unknown_teachers))).toFixed(2), done_percentage, unknown_teachers]
     backgroundColor.push('rgba(229, 201, 98, 1)');
     borderColor.push('rgba(229, 201, 98, 1)');
   }
 
+  textToCenter()
+
   new Chart(ctx, {
-      type: 'pie',
+      type: 'doughnut',
       data: {
           labels: labels,
           datasets: [{
               data: data,
               backgroundColor: backgroundColor,
               borderColor: borderColor,
-              borderWidth: 1
+              borderWidth: 0.5
           }]
       },
       options: {
+        cutoutPercentage: 70,
+        elements: {
+          center: {
+            text: 'Você selecionou um total de ' + $('#school_days').val() + ' dias letivos',
+            color: '#888888', // Default is #000000
+            fontStyle: 'Arial', // Default is Arial
+            sidePadding: 20, // Default is 20 (as a percentage)
+            minFontSize: 15, // Default is 20 (in px), set to false and text will not wrap.
+            lineHeight: 15 // Default is 25 (in px), used for when text wraps
+          }
+        }
       }
   });
 }
