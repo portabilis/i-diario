cf3e965d384d466de42cec8afdef44a02cdac1df
Marcelo Cajueiro <marcelocajueiro@gmail.com>
2020-04-30 14:37:02 -0300

    Muda para salvar session no redis
    Assim diminui io no postgres além de ocupar menos espaço.

---
+++ db/migrate/20200430173523_drop_table_sessions.rb
@@ -0,0 +1,16 @@
+class DropTableSessions < ActiveRecord::Migration
+  def up
+    drop_table :sessions
+  end
+
+  def down
+    create_table :sessions do |t|
+      t.string :session_id, null: false
+      t.text :data
+      t.timestamps
+    end
+
+    add_index :sessions, :session_id, unique: true
+    add_index :sessions, :updated_at
+  end
+end
