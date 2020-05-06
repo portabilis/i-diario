e4f60bdcdc18e998b9a6199f82b8fe229ab51398
Eliezer Salvato <eliezer@portabilis.com.br>
2020-05-06 15:49:19 -0300

    Ajusta para filtrar os campos do select de acordo com o typo
    refs #3011

--- app/models/learning_objectives_and_skill.rb
+++ app/models/learning_objectives_and_skill.rb
@@ -11,6 +11,8 @@ class LearningObjectivesAndSkill < ActiveRecord::Base
   scope :by_code, ->(code) { where('unaccent(code) ILIKE unaccent(?)', "%#{code}%") }
   scope :by_description, ->(description) { where('unaccent(description) ILIKE unaccent(?)', "%#{description}%") }
   scope :by_step, ->(step) { where(step: step) }
+  scope :by_discipline, ->(discipline) { where(discipline: discipline) }
+  scope :by_field_of_experience, ->(field_of_experience) { where(field_of_experience: field_of_experience) }
   scope :by_grade, ->(grade) { where('?  = ANY(grades)', grade) }
   scope :ordered, -> { order(:code) }
 
