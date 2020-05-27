5cc57df974fef5a5170482a2944ceabd9aa0b0c2
Joel Ayata <joel@portabilis.com.br>
2020-05-27 14:14:08 -0300

    Ajusta aba de permissões quando não tem aba de usuarios

--- app/assets/javascripts/views/roles/form.js
+++ app/assets/javascripts/views/roles/form.js
@@ -15,65 +15,67 @@ $(function() {
     createSelect2Remote(true, 'tbody#user-roles tr:last input.select2_remote', '');
   });
 
-  if ($('#user_name').val().length == 0 && $('#unity_name').val().length == 0) {
-    $('#filter_user_roles').attr('disabled', 'disabled');
-  }
-
-  $('#user_name').on('keyup', function() {
-    var empty = $(this).val().length == 0;
-
-    if (empty && $('#unity_name').val().length != 0) {
-      empty = false;
-    }
-
-    enableDisableButton(empty)
-  });
-
-  $('#unity_name').on('keyup', function() {
-    var empty = $(this).val().length == 0;
-
-    if (empty && $('#user_name').val().length != 0) {
-      empty = false;
-    }
-
-    enableDisableButton(empty)
-  });
-
-  function enableDisableButton(empty) {
-    if (empty) {
+  if ($('#li-users').length) {
+    if ($('#user_name').val().length == 0 && $('#unity_name').val().length == 0) {
       $('#filter_user_roles').attr('disabled', 'disabled');
-    } else {
-      $('#filter_user_roles').attr('disabled', false);
-    }
-  }
-
-  function goToEditPath() {
-    params = {
-      user_name: $('#user_name').val(),
-      unity_name: $('#unity_name').val(),
-      feature_filter: $('#feature_filter').val(),
-      active_permissions_tab: $('#li-general').hasClass('active'),
-      active_users_tab: $('#li-users').hasClass('active'),
-      permissions_page: $('#permissions_page').val()
     }
 
-    edit_path = Routes.edit_role_pt_br_path($('#role_id').val()) + '?';
+    $('#user_name').on('keyup', function() {
+      var empty = $(this).val().length == 0;
 
-    $.each(params, function(param, value){
-      edit_path += param + '=' + value + '&';
+      if (empty && $('#unity_name').val().length != 0) {
+        empty = false;
+      }
+
+      enableDisableButton(empty)
     });
 
-    window.location.href = edit_path.slice(0,-1);
+    $('#unity_name').on('keyup', function() {
+      var empty = $(this).val().length == 0;
+
+      if (empty && $('#user_name').val().length != 0) {
+        empty = false;
+      }
+
+      enableDisableButton(empty)
+    });
+
+    function enableDisableButton(empty) {
+      if (empty) {
+        $('#filter_user_roles').attr('disabled', 'disabled');
+      } else {
+        $('#filter_user_roles').attr('disabled', false);
+      }
+    }
+
+    function goToEditPath() {
+      params = {
+        user_name: $('#user_name').val(),
+        unity_name: $('#unity_name').val(),
+        feature_filter: $('#feature_filter').val(),
+        active_permissions_tab: $('#li-general').hasClass('active'),
+        active_users_tab: $('#li-users').hasClass('active'),
+        permissions_page: $('#permissions_page').val()
+      }
+
+      edit_path = Routes.edit_role_pt_br_path($('#role_id').val()) + '?';
+
+      $.each(params, function(param, value){
+        edit_path += param + '=' + value + '&';
+      });
+
+      window.location.href = edit_path.slice(0,-1);
+    }
+
+    $('#filter_user_roles').click(function() {
+      goToEditPath();
+    });
+
+    $('#clean_filter').click(function() {
+      $('#user_name').val('');
+      $('#unity_name').val('');
+
+      goToEditPath();
+    });
   }
-
-  $('#filter_user_roles').click(function() {
-    goToEditPath();
-  });
-
-  $('#clean_filter').click(function() {
-    $('#user_name').val('');
-    $('#unity_name').val('');
-
-    goToEditPath();
-  });
 });
