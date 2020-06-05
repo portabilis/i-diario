fbf1f506672e53348e73b0d36857b208fbe289be
Joel Ayata <joel@portabilis.com.br>
2020-06-05 10:26:20 -0300

    Remove paginacao e filtro na aba de permissoes

--- config/routes.rb
+++ config/routes.rb
@@ -127,7 +127,6 @@ Rails.application.routes.draw do
     resources :roles do
       member do
         get :history
-        get :fetch_features
       end
     end
     resources  :user_roles, only: [:show]
