abc50c42c20e05e0450f746ddefcc9297105883d
Marcelo Cajueiro <marcelocajueiro@gmail.com>
2020-07-06 16:45:27 -0300

    Revert "Merge pull request #3110 from portabilis/issue-2943"
    This reverts commit 487801460a39f768a351c083777e515edb357162, reversing
    changes made to 032d9e24168c1bf6c38aba47923cd5d4ca6b7653.
    
    Tem bug em produção.
    
    https://app.honeybadger.io/projects/54397/faults/65437201/fc8a7686-bfc0-11ea-9ec3-3e6be2d737da

--- app/models/teacher_unification.rb
+++
@@ -1,6 +0,0 @@
-class TeacherUnification < ActiveRecord::Base
-  audited
-
-  belongs_to :teacher
-  has_many :unified_teachers, class_name: 'TeacherUnificationTeacher', dependent: :restrict_with_error
-end
--- app/models/teacher_unification_teacher.rb
+++
@@ -1,6 +0,0 @@
-class TeacherUnificationTeacher < ActiveRecord::Base
-  audited associated_with: :teacher_unification
-
-  belongs_to :teacher_unification
-  belongs_to :teacher
-end
