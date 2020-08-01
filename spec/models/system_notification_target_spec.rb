543b2cf84a73465c1ac2ec6ee0c14121d6b33b52
ayata <jayata@uci.cu>
2020-08-01 16:59:11 -0300

    Ajusta testes

--- spec/models/system_notification_target_spec.rb
+++ spec/models/system_notification_target_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe SystemNotificationTarget, :type => :model do
+  let(:unread_notifications) { double('unread_notifications') }
+
   context "Validations" do
     it { should validate_presence_of :system_notification }
     it { should validate_presence_of :user }
@@ -8,7 +10,8 @@ RSpec.describe SystemNotificationTarget, :type => :model do
 
   describe ".read!" do
     it "should update all records as readed" do
-      expect(SystemNotificationTarget).to receive(:update_all).with(read: true)
+      allow(SystemNotificationTarget).to receive(:where).with(read: false).and_return(unread_notifications)
+      expect(unread_notifications).to receive(:update_all).with(read: true)
 
       SystemNotificationTarget.read!
     end
