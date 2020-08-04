ca4558ce43e12c9dc3254d2e3eb3df8262105b08
Marcelo Griggio Cajueiro <marcelocajueiro@gmail.com>
2020-08-04 12:18:05 -0300

    Apaga arquivo  errado

--- foo.rb
+++
@@ -1,8 +0,0 @@
-
-
-t = StudentEnrollmentClassroom.where(api_code: '248039'); nil
-t = t.where(api_code: '27908'); nil
-StudentEnrollmentClassroom.where(t.where_values.join(' OR '))
-
-StudentEnrollmentClassroom.where(t.where_values.inject(:or))
-
