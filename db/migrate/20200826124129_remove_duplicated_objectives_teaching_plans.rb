91c607db6176c133a6abf5574263e2bf4580924a
Joel Ayata <joel@portabilis.com.br>
2020-08-26 11:36:56 -0300

    Remove conteudos/objetivos duplicados e adiciona indice unico

---
+++ db/migrate/20200826124129_remove_duplicated_objectives_teaching_plans.rb
@@ -0,0 +1,18 @@
+class RemoveDuplicatedObjectivesTeachingPlans < ActiveRecord::Migration
+  def change
+    objectives_teaching_plans = ObjectivesTeachingPlan.group(
+      :teaching_plan_id, :objective_id
+    ).having(
+      'COUNT(1) > 1'
+    ).pluck(
+      'MAX(id)', :teaching_plan_id, :objective_id
+    )
+
+    objectives_teaching_plans.each do |correct_id, teaching_plan_id, objective_id|
+      ObjectivesTeachingPlan.where(
+        teaching_plan_id: teaching_plan_id,
+        objective_id: objective_id
+      ).where.not(id: correct_id).each(&:destroy)
+    end
+  end
+end
