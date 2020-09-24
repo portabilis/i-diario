6ddc7ce18579d1cf185e1e160d40e331d2cf349f
Marcelo Cajueiro <marcelocajueiro@gmail.com>
2020-09-24 10:16:19 -0300

    Remove migrations desnecessárias

--- db/migrate/20200910190125_update_knowledge_area_with_grup_descriptors_false.rb
+++
@@ -1,8 +0,0 @@
-class UpdateKnowledgeAreaWithGrupDescriptorsFalse < ActiveRecord::Migration
-  def change
-    execute <<-SQL
-      update knowledge_areas
-         set group_descriptors = false
-    SQL
-  end
-end
--- db/migrate/20200910190313_add_knowledge_area_with_group_descriptors_default.rb
+++
@@ -1,5 +0,0 @@
-class AddKnowledgeAreaWithGroupDescriptorsDefault < ActiveRecord::Migration
-  def change
-    change_column :knowledge_areas, :group_descriptors, :boolean, default: false
-  end
-end
