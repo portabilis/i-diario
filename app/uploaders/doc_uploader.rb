fb8dfd93bf8b1f812635982970b4c2b2cc406f3d
Marcelo Cajueiro <marcelocajueiro@gmail.com>
2020-10-06 18:13:42 -0300

    Adiciona configuração para o carrierwave
    Pode ser diferente o padrão e do doc uploader.
    
    Em breve documentação.

--- app/uploaders/doc_uploader.rb
+++ app/uploaders/doc_uploader.rb
@@ -1,4 +1,20 @@
 class DocUploader < CarrierWave::Uploader::Base
+  FOG_CONFIG =
+    begin
+      config_file =
+        if File.exist? Rails.root.join('config', "doc_uploader_fog_#{Rails.env}.yml")
+          Rails.root.join('config', "doc_uploader_fog_#{Rails.env}.yml")
+        elsif File.exist? Rails.root.join('config', "fog_#{Rails.env}.yml")
+          Rails.root.join('config', "fog_#{Rails.env}.yml")
+        else
+          Rails.root.join('config', 'fog.yml')
+        end
+
+      YAML.safe_load(
+        File.open(config_file)
+      ).with_indifferent_access
+    end
+
   def store_dir
     "#{Rails.env}/#{model.class.to_s.underscore}/#{model.id}"
   end
@@ -12,14 +28,18 @@ class DocUploader < CarrierWave::Uploader::Base
     "#{original} - #{secure_token}.#{file.extension}" if original_filename.present?
   end
 
+  def fog_directory
+    Rails.application.secrets[:DOC_UPLOADER_FOG_DIRECTORY]
+  end
+
+  def fog_credentials
+    FOG_CONFIG
+  end
+
   protected
 
   def secure_token
     var = :"@#{mounted_as}_secure_token"
     model.instance_variable_get(var) || model.instance_variable_set(var, SecureRandom.uuid)
-  end
-
-  def fog_directory
-    Rails.application.secrets[:BUCKET_NAME]
   end
 end
