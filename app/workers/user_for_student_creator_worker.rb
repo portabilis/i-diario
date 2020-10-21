cde8069e55c18d9a5a74f0b0bf5b0c745a9bef33
Joel Ayata <joel@portabilis.com.br>
2020-10-21 08:44:56 -0300

    Move worker para fila low e seta como unique

--- app/workers/user_for_student_creator_worker.rb
+++ app/workers/user_for_student_creator_worker.rb
@@ -1,6 +1,8 @@
 class UserForStudentCreatorWorker
   include Sidekiq::Worker
 
+  sidekiq_options unique: :until_and_while_executing, queue: :low
+
   def perform(entity_id)
     Entity.find(entity_id).using_connection do
       UserForStudentCreator.create!
