5c7596b482c4f2b2a681a0c64a68f0e81dd6233d
Joel Ayata <joel@portabilis.com.br>
2020-11-09 16:07:35 -0300

    Refatora envio de emails
    - Cria BaseMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,17 +1,13 @@
-class UserMailer < ActionMailer::Base
-  default from: "Notificação i-Diário <#{Rails.application.secrets.NO_REPLY_ADDRESS}>"
-
+class UserMailer < BaseMailer
   def notify_activation(user_email, user_name, user_logged_as, domain)
     return unless user_email
 
-    skip_domains = Rails.application.secrets.EMAIL_SKIP_DOMAINS.split(',')
-
-    return if skip_domains.any? { |skip_domain| user_email.ends_with?(skip_domain) }
+    skip_domains([user_email])
 
     @user_name = user_name
     @user_logged_as = user_logged_as
     @domain = domain
 
-    mail to: user_email, subject: 'Conta de acesso ativada'
+    mail(to: @recipient, subject: 'Conta de acesso ativada') if @recipient.present?
   end
 end
