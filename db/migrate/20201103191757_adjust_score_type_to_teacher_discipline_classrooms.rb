68bf12330d8cfe9411be5ce880da4e36de802848
Joel Ayata <joel@portabilis.com.br>
2020-11-10 14:54:54 -0300

    Ajusta para nao usar muita memoria armazenando muitos ids

--- db/migrate/20201103191757_adjust_score_type_to_teacher_discipline_classrooms.rb
+++ db/migrate/20201103191757_adjust_score_type_to_teacher_discipline_classrooms.rb
@@ -1,30 +1,17 @@
 class AdjustScoreTypeToTeacherDisciplineClassrooms < ActiveRecord::Migration
   def change
     execute <<-SQL
-      DO $$
-        DECLARE numeric_score_relation_ids INTEGER[];
-        DECLARE conceptual_score_relation_ids INTEGER[];
-      BEGIN
-        numeric_score_relation_ids := ARRAY(
-          SELECT id
-            FROM teacher_discipline_classrooms
-           WHERE score_type = '1'
-        );
+      UPDATE teacher_discipline_classrooms
+         SET score_type = 'X'
+       WHERE score_type = '2';
 
-        conceptual_score_relation_ids := ARRAY(
-          SELECT id
-            FROM teacher_discipline_classrooms
-           WHERE score_type = '2'
-        );
+      UPDATE teacher_discipline_classrooms
+         SET score_type = '2'
+       WHERE score_type = '1';
 
-        UPDATE teacher_discipline_classrooms
-           SET score_type = '1'
-         WHERE id = ANY(conceptual_score_relation_ids);
-
-        UPDATE teacher_discipline_classrooms
-           SET score_type = '2'
-         WHERE id = ANY(numeric_score_relation_ids);
-      END$$;
+      UPDATE teacher_discipline_classrooms
+         SET score_type = '1'
+       WHERE score_type = 'X';
     SQL
   end
 end
