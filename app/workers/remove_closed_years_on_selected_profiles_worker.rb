f4995ba1b0f06ef32d0589eb67ed74ddf81b85bf
Joel Ayata <joel@portabilis.com.br>
2020-12-10 11:24:31 -0300

    Revert "Adiciona `Entity.current` na execução do worker"
    This reverts commit cd20504473c973d89e67e995251d9585ccf45ab1.

--- app/workers/remove_closed_years_on_selected_profiles_worker.rb
+++ app/workers/remove_closed_years_on_selected_profiles_worker.rb
@@ -4,11 +4,7 @@ class RemoveClosedYearsOnSelectedProfilesWorker
   sidekiq_options unique: :until_and_while_executing, queue: :low
 
   def perform(entity_id, unity_id, year)
-    entity = Entity.find(entity_id)
-
-    entity.using_connection do
-      Entity.current = entity
-
+    Entity.find(entity_id).using_connection do
       users = User.by_current_unity_id(unity_id)
                   .by_current_school_year(year)
                   .where.not(teacher_id: nil)
