db913b0e0d8f0e7e72b74b9c4666ac27c7f0c50d
Joel Ayata <joel@portabilis.com.br>
2021-01-20 14:47:48 -0300

    Ajusta query para copiar objetivos/conteudos

--- app/services/teaching_plan_objectives_fetcher.rb
+++ app/services/teaching_plan_objectives_fetcher.rb
@@ -36,8 +36,7 @@ class TeachingPlanObjectivesFetcher
     steps_numbers = steps_fetcher.steps_by_date_range(@start_date.to_date, @end_date.to_date).map(&:step_number)
 
     school_term_type_steps_ids = SchoolTermTypeStep.joins(:school_term_type)
-                                                   .where(step_number: step.step_number)
-                                                   .where(school_term_type: { steps_number: steps_numbers })
+                                                   .where(school_term_types: { steps_number: steps_numbers })
                                                    .pluck(:id)
     school_term_type_steps_ids << YEARLY_SCHOOL_TERM_TYPE_STEP_ID
   end
