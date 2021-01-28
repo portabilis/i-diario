f9d308bc1b27b59e967fed9977bdf2438e4a7b00
Joel Ayata <joel@portabilis.com.br>
2021-01-28 14:13:49 -0300

    Ajusta query para usar o numero de etapas e nao a descrição e refatora a classe

--- app/services/test_setting_fetcher.rb
+++ app/services/test_setting_fetcher.rb
@@ -11,28 +11,19 @@ class TestSettingFetcher
   def current
     raise ArgumentError if @classroom.blank?
 
-    year = @step.try(:school_calendar).try(:year) || @classroom.year
+    @year = @step.try(:school_calendar).try(:year) || @classroom.year
 
-    general_by_school_test_setting = general_by_school_test_setting(year)
-
-    return general_by_school_test_setting if general_by_school_test_setting.present?
-
-    general_test_setting = general_test_setting(year)
-
-    return general_test_setting if general_test_setting.present?
-
-    TestSetting.find_by(
-      year: year,
-      school_term_type_step: school_term_type_step
-    )
+    general_by_school_test_setting.presence ||
+      general_test_setting.presence ||
+      by_school_term_test_setting.presence
   end
 
   private
 
-  def general_test_setting(year)
+  def general_test_setting
     TestSetting.find_by(
       exam_setting_type: ExamSettingTypes::GENERAL,
-      year: year
+      year: @year
     )
   end
 
@@ -53,18 +44,22 @@ class TestSettingFetcher
   end
 
   def step_school_term_type_step
-    description = @step.school_calendar_parent.step_type_description
+    steps_number = @step.school_calendar_parent.steps.count
     step_number = @step.step_number
 
     SchoolTermTypeStep.joins(:school_term_type)
-                      .where(school_term_types: { description: description })
+                      .where(school_term_types: { steps_number: steps_number })
                       .find_by(step_number: step_number)
   end
 
-  def general_by_school_test_setting(year)
-    TestSetting.where(year: year, exam_setting_type: ExamSettingTypes::GENERAL_BY_SCHOOL)
+  def general_by_school_test_setting
+    TestSetting.where(year: @year, exam_setting_type: ExamSettingTypes::GENERAL_BY_SCHOOL)
                .by_unities(@classroom.unity)
                .where("grades @> ARRAY[?]::integer[] OR grades = '{}'", @classroom.grade)
                .first
+  end
+
+  def by_school_term_test_setting
+    TestSetting.find_by(year: @year, school_term_type_step: school_term_type_step)
   end
 end
