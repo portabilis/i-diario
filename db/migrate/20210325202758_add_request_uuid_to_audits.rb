2f46cf7a13bf61db830e6a8926ef0449389c55e3
jayata <ayatamail@gmail.com>
2021-03-26 14:16:19 -0300

    Remove transaction nas migrations do upgrade da gem audits

--- db/migrate/20210325202758_add_request_uuid_to_audits.rb
+++ db/migrate/20210325202758_add_request_uuid_to_audits.rb
@@ -1,7 +1,9 @@
 class AddRequestUuidToAudits < ActiveRecord::Migration
+  disable_ddl_transaction!
+
   def self.up
     add_column :audits, :request_uuid, :string
-    add_index :audits, :request_uuid
+    add_index :audits, :request_uuid, algorithm: :concurrently
   end
 
   def self.down
