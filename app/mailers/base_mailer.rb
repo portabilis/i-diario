0da682e98140bfa5774e907554ab7c5b7e1b0ad8
Gustavo Kureck Mendes <gustavo@portabilis.com.br>
2021-03-31 16:19:14 -0300

    Corrige problema quando nao tinha EMAIL_SKIP_DOMAINS informado no secrets.yml

--- app/mailers/base_mailer.rb
+++ app/mailers/base_mailer.rb
@@ -2,7 +2,7 @@
 
 class BaseMailer < ActionMailer::Base
   default from: "Notificação i-Diário <#{Rails.application.secrets.NO_REPLY_ADDRESS}>".freeze
-  SKIP_DOMAINS = (Rails.application.secrets.EMAIL_SKIP_DOMAINS || []).split(',').freeze
+  SKIP_DOMAINS = (Rails.application.secrets.EMAIL_SKIP_DOMAINS || []).split(',').flatten.freeze
 
   def skip_domains(emails)
     @recipient = emails.delete_if { |email|
