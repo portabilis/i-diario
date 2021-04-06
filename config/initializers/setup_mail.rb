e722f6163f878c2846d012f599471f512d05c7ec
Gustavo Kureck Mendes <gustavo@portabilis.com.br>
2021-04-06 12:17:18 -0300

    Retira ambiente de desenvolvimento que foi utilizado para testes

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,4 +1,4 @@
-if Rails.env.production? || Rails.env.staging? || Rails.env.development?
+if Rails.env.production? || Rails.env.staging?
   ActionMailer::Base.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
