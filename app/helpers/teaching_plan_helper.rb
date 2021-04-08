3949a27a480ad55eea3e7dd96df79494c0958097
jayata <ayatamail@gmail.com>
2021-04-08 09:38:10 -0300

    Troca a ordem e adiciona safe navigation operator

--- app/helpers/teaching_plan_helper.rb
+++ app/helpers/teaching_plan_helper.rb
@@ -1,5 +1,5 @@
 module TeachingPlanHelper
   def teaching_plan_destroy?(teaching_plan)
-    teaching_plan.teacher.id == current_teacher.try(:id) || current_user.current_role_is_admin_or_employee?
+    current_user.current_role_is_admin_or_employee? || teaching_plan&.teacher&.id == current_teacher.try(:id)
   end
 end
