a64d2df82d0884968d59599e288346a40defae98
jayata <ayatamail@gmail.com>
2021-04-13 14:04:51 -0300

    Remove o atributo estilo

--- db/migrate/20210412205044_remove_font_family_from_descriptive_exam_student_values.rb
+++ db/migrate/20210412205044_remove_font_family_from_descriptive_exam_student_values.rb
@@ -1,7 +1,7 @@
 class RemoveFontFamilyFromDescriptiveExamStudentValues < ActiveRecord::Migration
   def change
     DescriptiveExamStudent.where("value ILIKE '%font-family:%'").each do |descriptive_exam_student|
-      new_value = descriptive_exam_student.value.gsub(/(?<=[(\s*)|;|'|"])font-family:([^;>]*)(;|(?:(?!>).)*)/, '')
+      new_value = descriptive_exam_student.value.gsub(/style=('|"|\\"|\\').+('|")/, '')
 
       descriptive_exam_student.value = new_value
       descriptive_exam_student.save!
