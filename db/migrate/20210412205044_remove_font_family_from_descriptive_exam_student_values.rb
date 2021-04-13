9bf86287c3ef65427a11673051599ef064385d65
jayata <ayatamail@gmail.com>
2021-04-13 11:27:40 -0300

    Melhora legibilidade da migration

--- db/migrate/20210412205044_remove_font_family_from_descriptive_exam_student_values.rb
+++ db/migrate/20210412205044_remove_font_family_from_descriptive_exam_student_values.rb
@@ -1,8 +1,10 @@
 class RemoveFontFamilyFromDescriptiveExamStudentValues < ActiveRecord::Migration
   def change
-    DescriptiveExamStudent.where("value ILIKE '%font-family:%'").each do |des|
-      des.value = des.value.gsub(/(?<=[(\s*)|;|'|"])font-family:([^;>]*)(;|(?:(?!>).)*)/, '')
-      des.save!
+    DescriptiveExamStudent.where("value ILIKE '%font-family:%'").each do |descriptive_exam_student|
+      new_value = descriptive_exam_student.value.gsub(/(?<=[(\s*)|;|'|"])font-family:([^;>]*)(;|(?:(?!>).)*)/, '')
+
+      descriptive_exam_student.value = new_value
+      descriptive_exam_student.save!
     end
   end
 end
