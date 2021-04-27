dda59904e1d4a3c79ded7adeba24316ccb8344e5
jayata <ayatamail@gmail.com>
2021-04-27 16:08:09 -0300

    Ajusta summernote para deixar apenas as tags html
    - Limpa os estilos, comentarios alem de tagos como
    'style', 'script','applet','embed','noframes','noscript'

--- app/assets/javascripts/views/descriptive_exams/student_fields.js
+++ app/assets/javascripts/views/descriptive_exams/student_fields.js
@@ -9,19 +9,47 @@ $(function () {
     disableDragAndDrop : true,
     callbacks : {
       onPaste : function (event) {
-        event.preventDefault();
+        var thisNote = $(this);
+        var updatePastedText = function(someNote){
+          var original = someNote.summernote('code');
+          var cleaned = CleanPastedHTML(original);
 
-        let text = null;
+          someNote.summernote('reset');
+          someNote.summernote('pasteHTML', cleaned);
 
-        if (window.clipboardData){
-          text = window.clipboardData.getData("Text");
-        } else if (event.originalEvent && event.originalEvent.clipboardData){
-          text = event.originalEvent.clipboardData.getData("Text");
-        }
+        };
 
-        $(this).summernote('insertText', text);
-        $(this).val(text);
+        setTimeout(function () {
+          //the function is called after the text is really pasted.
+          updatePastedText(thisNote);
+      }, 10);
       }
     }
   });
+
+  function CleanPastedHTML(input) {
+    // 1. remove line breaks / Mso classes
+    var stringStripper = /(\n|\r| class=(")?Mso[a-zA-Z]+(")?)/g;
+    var output = input.replace(stringStripper, ' ');
+    // 2. strip Word generated HTML comments
+    var commentSripper = new RegExp('<!--(.*?)-->','g');
+    var output = output.replace(commentSripper, '');
+    var tagStripper = new RegExp('<(/)*(meta|link|span|\\?xml:|st1:|o:|font)(.*?)>','gi');
+    // 3. remove tags leave content if any
+    output = output.replace(tagStripper, '');
+    // 4. Remove everything in between and including tags '<style(.)style(.)>'
+    var badTags = ['style', 'script','applet','embed','noframes','noscript'];
+
+    for (var i=0; i< badTags.length; i++) {
+      tagStripper = new RegExp('<'+badTags[i]+'.*?'+badTags[i]+'(.*?)>', 'gi');
+      output = output.replace(tagStripper, '');
+    }
+    // 5. remove attributes ' style="..."'
+    var badAttributes = ['style', 'start'];
+    for (var i=0; i< badAttributes.length; i++) {
+      var attributeStripper = new RegExp(' ' + badAttributes[i] + '="(.*?)"','gi');
+      output = output.replace(attributeStripper, '');
+    }
+    return output;
+  }
 });
