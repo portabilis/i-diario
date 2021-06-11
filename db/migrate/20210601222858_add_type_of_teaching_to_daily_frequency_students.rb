8115bcc9b841e78b771429ec0ee3bbd14da241cc
gustavomendess <gustavocod13@outlook.com>
2021-06-10 21:46:56 -0300

    Remove drop de colunas da migration

--- db/migrate/20210601222858_add_type_of_teaching_to_daily_frequency_students.rb
+++ db/migrate/20210601222858_add_type_of_teaching_to_daily_frequency_students.rb
@@ -1,6 +1,5 @@
 class AddTypeOfTeachingToDailyFrequencyStudents < ActiveRecord::Migration
   def change
-    remove_column :daily_frequency_students, :type_of_teaching
     add_column :daily_frequency_students, :type_of_teaching, :integer, default: 1
   end
 end
--- db/migrate/20210601232758_add_type_of_teaching_to_general_configurations.rb
+++ db/migrate/20210601232758_add_type_of_teaching_to_general_configurations.rb
@@ -1,7 +1,5 @@
 class AddTypeOfTeachingToGeneralConfigurations < ActiveRecord::Migration
   def change
-    remove_column :general_configurations, :type_of_teaching
-    remove_column :general_configurations, :types_of_teaching
     add_column :general_configurations, :type_of_teaching, :boolean, default: false
     add_column :general_configurations, :types_of_teaching, :integer, array: true, default: [1]
   end
