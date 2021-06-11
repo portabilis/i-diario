4a244ac4b31effd4c2fbbb961153809542ac4c36
jayata <ayatamail@gmail.com>
2021-06-11 15:32:48 -0300

    Remove relacionamento desnecessario

--- app/models/classrooms_grade.rb
+++ app/models/classrooms_grade.rb
@@ -2,7 +2,7 @@ class ClassroomsGrade < ActiveRecord::Base
   belongs_to :classroom
   belongs_to :grade
   belongs_to :exam_rule
-  has_and_belongs_to_many :avaliations
+
   has_many :student_enrollment_classrooms
   has_many :student_enrollments, through: :student_enrollment_classrooms
 
