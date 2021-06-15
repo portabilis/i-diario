b542a90bff048ee840e34481c1f68c1173ed4529
Bruno Carvalho <bruno.carvalho310303@gmail.com>
2021-06-15 00:40:40 -0300

    Move método para o modelo de busca ativa e recebe data ao invés de objeto nos parâmetros

--- app/models/active_search.rb
+++ app/models/active_search.rb
@@ -2,4 +2,16 @@ class ActiveSearch < ActiveRecord::Base
   belongs_to :student_enrollment
 
   has_enumeration_for :status, with: ActiveSearchStatus, create_helpers: true
+
+  def in_active_search?(student_enrollment_id, date)
+    student_active_search = ActiveSearch.where(student_enrollment_id: student_enrollment_id)
+    not_in_progress = student_active_search.where.not(status: ActiveSearchStatus::IN_PROGRESS)
+                                           .where('? between start_date and end_date', date)
+                                           .exists?
+    return not_in_progress if not_in_progress
+
+    student_active_search.where(status: ActiveSearchStatus::IN_PROGRESS)
+                         .where('start_date <= ?', date)
+                         .exists?
+  end
 end
