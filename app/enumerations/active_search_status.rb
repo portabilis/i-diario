25b0a3f56f6ef990832cdc2b024ed9572ed2b3a0
Bruno Carvalho <bruno.carvalho310303@gmail.com>
2021-06-15 16:39:25 -0300

    Corrige ordem de status de acordo com tarefa do ieducar

--- app/enumerations/active_search_status.rb
+++ app/enumerations/active_search_status.rb
@@ -1,8 +1,8 @@
 class ActiveSearchStatus < EnumerateIt::Base
   associate_values abandonment: 1,
                    in_progress: 2,
-                   return: 3,
-                   return_with_justification: 4
+                   return_with_justification: 3,
+                   return: 4
 
   sort_by :none
 end
