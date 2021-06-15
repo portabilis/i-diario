a4afc2101304acf2a85544ec20ebc7ace392649a
Bruno Carvalho <bruno.carvalho310303@gmail.com>
2021-06-15 16:23:30 -0300

    Adiciona retorno com justificativa

--- app/enumerations/active_search_status.rb
+++ app/enumerations/active_search_status.rb
@@ -1,7 +1,8 @@
 class ActiveSearchStatus < EnumerateIt::Base
   associate_values abandonment: 1,
                    in_progress: 2,
-                   return: 3
+                   return: 3,
+                   return_with_justification: 4
 
   sort_by :none
 end
