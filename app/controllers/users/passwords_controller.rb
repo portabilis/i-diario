8b2d06f5af1fbd1b7021006ea44bd5342c1628bc
gustavomendess <gustavocod13@outlook.com>
2021-06-24 22:40:00 -0300

    Seta rota novamente para reset de password e faz overwrite em método para validar o password

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,2 +1,13 @@
 class Users::PasswordsController < Devise::PasswordsController
+  def update
+    password = params[:user][:password]
+    token = params[:user][:reset_password_token]
+    if weak_password?(password)
+      flash[:error] = I18n.t('errors.general.weak_password')
+      redirect_to edit_user_password_path(reset_password_token: token)
+    else
+      flash.clear
+      super
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
 
   localized do
     devise_for :users, controllers: {
-      sessions: 'users/sessions'
+      sessions: 'users/sessions',
+      passwords: 'users/passwords'
     }
 
     namespace :api do
