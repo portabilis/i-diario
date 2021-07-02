493c63164d63419f31ae9e6cc20fd5f3bb8f00c0
gustavomendess <gustavocod13@outlook.com>
2021-07-02 15:54:36 -0300

    Melhora nome da classe e refatora

--- app/services/ieducar_synchronizers/active_search_synchronizer.rb
+++ app/services/ieducar_synchronizers/active_search_synchronizer.rb
@@ -1,6 +1,8 @@
-class ActiveSearcheSynchronizer < BaseSynchronizer
-  def create_or_update_active_search(student_enrollment)
-    ActiveSearch.find_or_initialize_by(student_enrollment_id: student_enrollment.id).tap do |active_search|
+class ActiveSearchSynchronizer
+  include Sidekiq::Worker
+
+  def perform(student_enrollment_id)
+    ActiveSearch.find_or_initialize_by(student_enrollment_id: student_enrollment_id).tap do |active_search|
       active_search.start_date = active_search_record.data_inicio
       active_search.end_date = active_search_record.data_fim
       active_search.status = active_search_record.resultado_busca_ativa
