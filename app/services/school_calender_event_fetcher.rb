8bc68682300faeb582ca6126893fb3bc1d9836e5
Marco Castro <marcodotcastro@gmail.com>
2021-08-12 17:50:19 -0300

    tarefa(calendario_letivo): adicionado validação na entidade

--- app/services/school_calender_event_fetcher.rb
+++ app/services/school_calender_event_fetcher.rb
@@ -6,12 +6,11 @@ class SchoolCalenderEventFetcher
   def uniqueness_start_at_and_end_at?
     attributes = {
       school_calendar_id: @resource.school_calendar_id, coverage: @resource.coverage,
-      start_date: @resource.start_date, end_date: @resource.end_date,
       grade_id: @resource.grade_id, course_id: @resource.course_id , discipline_id: @resource.discipline_id , classroom_id: @resource.classroom_id
     }
     events = SchoolCalendarEvent.where(attributes)
-    events = events.where("(start_date <= ?) and (end_date >= ?)", @resource.end_date, @resource.start_date)
-    events = events.where("id != :id", id: @resource.id) if @resource.id
+    events = events.where("(? <= end_date and ? >= start_date)", @resource.start_date, @resource.end_date)
+    events = events.where("id != :id", id: @resource.id) if @resource.persisted?
     events.any?
   end
 
