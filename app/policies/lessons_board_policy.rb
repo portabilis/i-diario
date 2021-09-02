24e80eef89414463b11bf99ddaa23a1a5f581310
gustavomendess <gustavocod13@outlook.com>
2021-09-02 15:55:29 -0300

    Corrige nome da permissão no policy

--- app/policies/lessons_board_policy.rb
+++ app/policies/lessons_board_policy.rb
@@ -1,18 +1,18 @@
 class LessonsBoardPolicy < ApplicationPolicy
   def new?
-    return super if @user.admin? || @user.employee?
+    return super if @user.admin? || @user.employee? || @user.can_change?(:lessons_boards)
 
     false
   end
 
   def edit?
-    return super if @user.admin? || @user.employee?
+    return super if @user.admin? || @user.employee? || @user.can_change?(:lessons_boards)
 
     false
   end
 
   def show?
-    return super if @user.admin? || @user.employee?
+    return super if @user.admin? || @user.employee? || @user.can_show?(:lessons_boards)
 
     false
   end
