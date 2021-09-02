007369afc52f07037cb8540424619b754c4d0f5d
gustavomendess <gustavocod13@outlook.com>
2021-09-02 14:22:08 -0300

    Remove obrigatoriedade de informar professor

--- app/models/lessons_board_lesson_weekday.rb
+++ app/models/lessons_board_lesson_weekday.rb
@@ -3,8 +3,6 @@ class LessonsBoardLessonWeekday < ActiveRecord::Base
 
   audited
 
-  validates :teacher_discipline_classroom_id, presence: true
-
   belongs_to :teacher_discipline_classroom
   belongs_to :lessons_board_lesson
 
