445d50cf2124d8a7a1b34264c5d99e424772b690
gustavomendess <gustavocod13@outlook.com>
2021-09-01 21:29:32 -0300

    Melhora nome do método

--- app/helpers/lessons_board_helper.rb
+++ app/helpers/lessons_board_helper.rb
@@ -1,5 +1,5 @@
 module LessonsBoardHelper
-  def teacher_name_by_teacher_discipline_classroom(teacher_discipline_classroom_id)
+  def teacher_name_and_discipline(teacher_discipline_classroom_id)
     teacher_discipline = TeacherDisciplineClassroom.find(teacher_discipline_classroom_id)
 
     teacher_discipline.teacher.name.try(:strip).upcase + ' - ' +
