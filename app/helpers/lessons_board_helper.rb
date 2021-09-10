176d1965786632febd84fa48535b810c2a0f40a6
gustavomendess <gustavocod13@outlook.com>
2021-09-10 16:04:35 -0300

    Remove helper que não é mais necessário

--- app/helpers/lessons_board_helper.rb
+++
@@ -1,8 +0,0 @@
-module LessonsBoardHelper
-  def teacher_name_and_discipline(teacher_discipline_classroom_id)
-    teacher_discipline = TeacherDisciplineClassroom.find(teacher_discipline_classroom_id)
-
-    teacher_discipline.teacher.name.try(:strip).upcase + ' - ' +
-    teacher_discipline.discipline.description.try(:strip).upcase
-  end
-end
