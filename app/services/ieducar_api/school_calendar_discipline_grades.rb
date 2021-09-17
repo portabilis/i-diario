32277d80ad4e915c4bfaf48d3e64008cbff3b8f8
Eder Soares <edersoares@me.com>
2021-09-17 17:13:04 -0300

    Altera endpoint

--- app/services/ieducar_api/school_calendar_discipline_grades.rb
+++ app/services/ieducar_api/school_calendar_discipline_grades.rb
@@ -2,7 +2,7 @@ module IeducarApi
   class SchoolCalendarDisciplineGrades < Base
     def fetch(params = {})
       params.reverse_merge!(
-        path: 'module/Api/ComponenteCurricular',
+        path: 'module/Api/Escola',
         resource: 'escola-serie-disciplinas-anos-letivos'
       )
       raise ApiError, 'É necessário informar pelo menos uma escola' if params[:escola].blank?
