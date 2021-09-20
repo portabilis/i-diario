8bd2baad704d10462a731fc950c9311a63ef8569
gustavomendess <gustavocod13@outlook.com>
2021-09-20 00:17:50 -0300

    Revert "Revert "Limpa erros ao alterar escola, série, turma e período""
    This reverts commit ad874109bfd22cd8fd3d4b8f5f8ee3dca86a2369.

--- app/assets/javascripts/views/lessons_boards/new.js
+++ app/assets/javascripts/views/lessons_boards/new.js
@@ -16,22 +16,27 @@ $(function () {
   });
 
   $('#lessons_board_unity').on('change', async function () {
+    errors = {};
     flashMessages.pop('');
     clearFields();
     clearClassroomsAndGrades();
     period_div.hide();
+    $('#btn-submit').attr("disabled", true);
     await updateGrades();
   })
 
   $('#lessons_board_grade').on('change', async function () {
+    errors = {};
     flashMessages.pop('');
     clearFields();
     $('#lessons_board_classroom_id').select2('val', '');
     period_div.hide();
+    $('#btn-submit').attr("disabled", true);
     await updateClassrooms();
   })
 
   $('#lessons_board_classroom_id').on('change', async function () {
+    errors = {};
     flashMessages.pop('');
     $('#lessons_board_period').select2('val', '');
     await getPeriod();
@@ -45,6 +50,7 @@ $(function () {
   })
 
   $('#lessons_board_period').on('change', function() {
+    errors = {};
     let period = $('#lessons_number_classroom_id').val();
 
     if (period == PERIOD_FULL) {
