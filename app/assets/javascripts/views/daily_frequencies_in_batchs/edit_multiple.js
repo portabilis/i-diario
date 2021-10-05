736b662ccf243475a8a50b1b5dc619fc6c3a5537
gustavomendess <gustavocod13@outlook.com>
2021-10-05 16:47:46 -0300

    Altera cores dos checks e coloca js no lugar correto

--- app/assets/javascripts/views/daily_frequencies_in_batchs/edit_multiple.js
+++ app/assets/javascripts/views/daily_frequencies_in_batchs/edit_multiple.js
@@ -31,10 +31,12 @@ $(document).ready( function() {
       checkbox.prop('disabled', true)
     }
   })
+  $('.class-number-checkbox').each( function () {
+    markGeneralCheckbox($(this).closest('td'))
+  });
 })
 
 $(function () {
-
   var showConfirmation = $('#new_record').val() == 'true';
 
   // fix to checkboxes work correctly
@@ -84,37 +86,54 @@ $(function () {
   });
 
   $('.alert-success, .alert-danger').fadeTo(700, 0.1).fadeTo(700, 1.0);
+});
 
-  $('.general-checkbox').on('change', function() {
-    let checked = $(this).prop('checked')
-    $(this).closest('td').find('.class-number-checkbox').prop('checked', checked)
-    studentAbsencesCount($(this).closest('tr'))
-  })
+$('.general-checkbox').on('change', function() {
+  $(this).closest('td').find('.general-checkbox-icon').removeClass('half-checked')
+  let checked = $(this).prop('checked')
+  $(this).closest('td').find('.class-number-checkbox').prop('checked', checked)
+  studentAbsencesCount($(this).closest('tr'))
+})
 
-  $('.class-number-checkbox').on('change', function() {
-    $(this).closest('td').find('.general-checkbox').prop('checked', false)
-    studentAbsencesCount($(this).closest('tr'))
-  });
+$('.class-number-checkbox').on('change', function() {
+  markGeneralCheckbox($(this).closest('td'))
+  studentAbsencesCount($(this).closest('tr'))
+});
 
-  function studentAbsencesCount(tr) {
-    let count = tr.find('.class-number-checkbox:not(:checked)').length
-    tr.find('.student-absences-count').text(count)
+function studentAbsencesCount(tr) {
+  let count = tr.find('.class-number-checkbox:not(:checked)').length
+  tr.find('.student-absences-count').text(count)
+}
+
+$('.date-collapse').on('click', function () {
+  let index = $(this).index() + 1
+  if ($(this).closest('table').find('tbody tr td:nth-child(' + index + ') .class-number-collapse').hasClass('hidden')) {
+    $(this).closest('table').find('tbody tr td:nth-child(' + index + ') .class-number-collapse').removeClass('hidden')
+    $(this).closest('table').find('tbody tr td:nth-child(' + index + ') .class-number-collapse').removeClass('collapsed')
+    $(this).find('#icon-remove').removeClass('hidden')
+    $(this).find('#icon-add').addClass('hidden')
+    $(this).removeClass('collapsed')
+  } else {
+    $(this).closest('table').find('tbody tr td:nth-child(' + index + ') .class-number-collapse').addClass('hidden')
+    $(this).closest('table').find('tbody tr td:nth-child(' + index + ') .class-number-collapse').addClass('collapsed')
+    $(this).find('#icon-add').removeClass('hidden')
+    $(this).find('#icon-remove').addClass('hidden')
+    $(this).addClass('collapsed')
+  }
+});
+
+function markGeneralCheckbox(td) {
+  td.find('.general-checkbox').prop('checked', true)
+  let all_checked = td.find('.class-number-checkbox:not(:checked)').length == 0
+  let all_not_checked = td.find('.class-number-checkbox:is(:checked)').length == 0
+
+  if (all_checked) {
+    td.find('.general-checkbox-icon').removeClass('half-checked')
+  } else {
+    td.find('.general-checkbox-icon').addClass('half-checked')
   }
 
-  $('.date-collapse').on('click', function () {
-    let index = $(this).index() + 1
-    if ($(this).closest('table').find('tbody tr td:nth-child(' + index + ') .class-number-collapse').hasClass('hidden')) {
-      $(this).closest('table').find('tbody tr td:nth-child(' + index + ') .class-number-collapse').removeClass('hidden')
-      $(this).closest('table').find('tbody tr td:nth-child(' + index + ') .class-number-collapse').removeClass('collapsed')
-      $(this).find('#icon-remove').removeClass('hidden')
-      $(this).find('#icon-add').addClass('hidden')
-      $(this).removeClass('collapsed')
-    } else {
-      $(this).closest('table').find('tbody tr td:nth-child(' + index + ') .class-number-collapse').addClass('hidden')
-      $(this).closest('table').find('tbody tr td:nth-child(' + index + ') .class-number-collapse').addClass('collapsed')
-      $(this).find('#icon-add').removeClass('hidden')
-      $(this).find('#icon-remove').addClass('hidden')
-      $(this).addClass('collapsed')
-    }
-  });
-});
+  if (all_not_checked) {
+    td.find('.general-checkbox').prop('checked', false)
+  }
+}
