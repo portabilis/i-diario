c16dfa13f976ab36376ad34a2aad3727f0090853
Bruno Carvalho <bruno.carvalho310303@gmail.com>
2021-10-15 18:36:21 -0300

    Restaura funcionalidade do campo data

--- app/assets/javascripts/views/pedagogical_trackings/index.js
+++ app/assets/javascripts/views/pedagogical_trackings/index.js
@@ -155,6 +155,24 @@ $('#search_end_date').on('change', function(e){
   $('form.filter_tracking_search_form').trigger("submit");
 });
 
+// $('#search_start_date').blur(function(e){
+//   const value = $('#search_start_date').val()
+//   console.log(e);
+//   if (value.length > 0){
+//     $('form.filter_tracking_search_form').trigger("submit");
+//   }
+// });
+// $('#search_end_date').blur(function(e){
+//   const value = $('#search_end_date').val()
+//   clear_empty(e);
+//   if (value.length > 0){
+//     console.log(value);
+//     $('form.filter_tracking_search_form').trigger("submit");
+//     console.log(value);
+//   }
+// });
+
+
 var unity_id = $('#unity_id').val();
 var start_date = $('#start_date').val();
 var end_date = $('#end_date').val();
