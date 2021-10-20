a897be12e120c7fc7ddf3b5567286bbce47cc0b1
gustavomendess <gustavocod13@outlook.com>
2021-10-20 15:12:23 -0300

    Corrige arquivo que foi mexido sem querer

--- lib/portabilis/inputs/boolean_input.rb
+++ lib/portabilis/inputs/boolean_input.rb
@@ -1,6 +1,5 @@
 module Portabilis
-  module
-  Inputs
+  module Inputs
     class BooleanInput < SimpleForm::Inputs::BooleanInput
       def input(wrapper_options)
         merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
