d6884d4c0888f3a8f4fd724b79aa588f32d58451
gustavomendess <gustavocod13@outlook.com>
2021-10-29 09:42:40 -0300

    Lança flash na view e remove do js

--- app/assets/javascripts/views/daily_frequencies_in_batchs/new.js
+++
@@ -1,4 +0,0 @@
-$(document).ready( function() {
-  let days_limit_message = 'É possível registrar frequência de no máximo 15 dias por lançamento, independente da quantidade de dias informados na seleção inicial.';
-  $('#flash-messages').html('<div class="alert alert-info fade in"><i class="fa-fw fa fa-info"></i> '  + days_limit_message + ' </div>');
-})
