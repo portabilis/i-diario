4d5a8a3e5603452f868a345b866e120ff5dc1e7e
Bruno Carvalho <bruno.carvalho310303@gmail.com>
2021-11-16 15:29:07 -0300

    Pega src da image beta

--- app/assets/javascripts/views/pedagogical_trackings/index.js
+++ app/assets/javascripts/views/pedagogical_trackings/index.js
@@ -141,7 +141,8 @@ function build_pie_chart(ctx, done_percentage, unknown_teachers = null){
 
 $(document).ready( function() {
   let beta_title = 'Este recurso ainda está em processo de desenvolvimento e pode apresentar problemas'
-  $('.fa-check-square-o').closest('h2').after(`<img src="/assets/beta.png" class="beta-badge" style="margin-bottom: 9px; margin-left: 5px" title="${beta_title}">`);
+  let img_src = $('#image-beta').attr('src');
+  $('.fa-check-square-o').closest('h2').after(`<img src="${img_src}" class="beta-badge" style="margin-bottom: 9px; margin-left: 5px" title="${beta_title}">`);
 })
 
 build_pie_chart(frequency_chart_ctx, done_frequencies_percentage, unknown_teachers);
