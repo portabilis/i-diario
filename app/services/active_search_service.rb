296360b12d6e20308e07a0cf7aad2fa54120596d
Marco Castro <marcodotcastro@gmail.com>
2021-12-06 10:22:01 -0300

    resolver(sincronização): regrar limpar avaliação numérica em busca ativa

---
+++ app/services/active_search_service.rb
@@ -0,0 +1,19 @@
+class ActiveSearchService
+
+  def initialize(active_search, options = { note: nil })
+    @active_search = active_search
+    @options = options
+  end
+
+  def daily_note
+    student = @active_search.student_enrollment.student
+    start_date = @active_search.start_date
+    end_date = @active_search.end_date.nil? ? Date.today : @active_search.end_date
+
+    daily_note_students = DailyNoteStudent.by_test_date_between(start_date, end_date)
+                                          .by_student_id(student)
+
+    daily_note_students.update_all(note: @options[:note])
+  end
+
+end
