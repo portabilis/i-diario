518807a58d23a888515224c7b2ac540c1fc75850
gustavomendess <gustavocod13@outlook.com>
2021-12-14 16:29:58 -0300

    Corrige mensagem de erro ao tentar enviar instruções de desbloqueio de conta

--- app/controllers/users/unlocks_controller.rb
+++ app/controllers/users/unlocks_controller.rb
@@ -1,5 +1,15 @@
 class Users::UnlocksController < Devise::UnlocksController
+  def create
+    self.resource = resource_class.send_unlock_instructions(resource_params)
+    yield resource if block_given?
 
+    if successfully_sent?(resource)
+      respond_with({}, location: after_sending_unlock_instructions_path_for(resource))
+    else
+      flash[:error] = I18n.t('devise.unlocks.not_send_instructions')
+      redirect_to new_user_unlock_path
+    end
+  end
   def show
     super
     self.resource.update_columns(status: "active") if self.resource.persisted?
