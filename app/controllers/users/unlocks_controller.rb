b0a42c6bab70c5c95e15c1e9ca5e96dd32ce086d
gustavomendess <gustavocod13@outlook.com>
2021-12-14 17:17:37 -0300

    Trata melhor as mensagens de erro

--- app/controllers/users/unlocks_controller.rb
+++ app/controllers/users/unlocks_controller.rb
@@ -6,12 +6,29 @@ class Users::UnlocksController < Devise::UnlocksController
     if successfully_sent?(resource)
       respond_with({}, location: after_sending_unlock_instructions_path_for(resource))
     else
-      flash[:error] = I18n.t('devise.unlocks.unsent_instructions')
+      if resource.errors.any?
+        set_errors(resource.errors.messages)
+      end
+
       redirect_to new_user_unlock_path
     end
+    #super
   end
+
   def show
     super
     self.resource.update_columns(status: "active") if self.resource.persisted?
+  end
+
+  private
+
+  def set_errors(error)
+    not_bloqued = error[:email].first == "não estava bloqueado"
+    not_found = error[:email].first == "não encontrado"
+
+    return flash[:error] = I18n.t('devise.unlocks.not_bloqued') if not_bloqued
+    return flash[:error] = I18n.t('devise.unlocks.user_not_found') if not_found
+
+    flash[:error] = I18n.t('devise.unlocks.unsent_instructions')
   end
 end
