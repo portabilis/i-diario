53368836a20c2f958ae80ffc622bdf45deb66d01
gustavomendess <gustavocod13@outlook.com>
2022-01-19 17:56:24 -0300

    Prioriza busca ativa na listagem

--- app/presenters/daily_note_student_presenter.rb
+++ app/presenters/daily_note_student_presenter.rb
@@ -2,32 +2,32 @@ class DailyNoteStudentPresenter < BasePresenter
   def student_name_class
     name_class = 'multiline '
 
-    if exempted_from_discipline
-      name_class += 'exempted-student-from-discipline'
+    if in_active_search
+      name_class += 'in-active-search'
     elsif !active
       name_class += 'inactive-student'
     elsif exempted
       name_class += 'exempted-student'
     elsif dependence
       name_class += 'dependence-student'
-    elsif in_active_search
-      name_class += 'in-active-search'
+    elsif exempted_from_discipline
+      name_class += 'exempted-student-from-discipline'
     end
 
     name_class
   end
 
   def student_name
-    if exempted_from_discipline
-      "****#{student}"
+    if in_active_search
+      "*****#{student}"
     elsif !active
       "***#{student}"
     elsif exempted
       "**#{student}"
     elsif dependence
       "*#{student}"
-    elsif in_active_search
-      "*****#{student}"
+    elsif exempted_from_discipline
+      "****#{student}"
     else
       student.to_s
     end
