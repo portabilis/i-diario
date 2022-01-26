6bae7b691e18c5634fd4861458be7474ac56bcaa
gustavomendess <gustavocod13@outlook.com>
2022-01-26 19:07:11 -0300

    Corrige service que busca anos que aluno tem enturmação

--- app/services/years_from_student_fetcher.rb
+++ app/services/years_from_student_fetcher.rb
@@ -3,11 +3,13 @@ class YearsFromStudentFetcher
     student_enrollment_classrooms = StudentEnrollmentClassroom.by_student(student_id).includes(:classroom)
     return if student_enrollment_classrooms.nil?
 
-    years = student_enrollment_classrooms.map { |student_enrollment_classroom|
-      next if student_enrollment_classroom.classroom.nil?
+    years = student_enrollment_classrooms.map do |student_enrollment_classroom|
+      next if student_enrollment_classroom.classrooms_grades.nil?
 
-      student_enrollment_classroom.classroom.year
-    }
+      student_enrollment_classroom.classrooms_grades.map do |classroom_grade|
+        classroom_grade.classroom.year
+      end
+    end
     years.compact.uniq.sort.reverse
   end
 
