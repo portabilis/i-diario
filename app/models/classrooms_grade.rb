c46a5cc737c2034b095fe8d06788ddddd7167e68
gustavomendess <gustavocod13@outlook.com>
2022-02-22 19:41:13 -0300

    Descarta enturmações caso série seja apagada

--- app/models/classrooms_grade.rb
+++ app/models/classrooms_grade.rb
@@ -22,4 +22,12 @@ class ClassroomsGrade < ActiveRecord::Base
   scope :by_student_id, lambda { |student_id|
     joins(:student_enrollments).where(student_enrollments: { student_id: student_id })
   }
+
+  after_discard do
+    student_enrollment_classrooms.discard_all
+  end
+
+  after_undiscard do
+    student_enrollment_classrooms.undiscard_all
+  end
 end
