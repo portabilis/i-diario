907b51ed1f715463b0c3576ecea965d4f57f05b0
gustavomendess <gustavocod13@outlook.com>
2022-03-22 14:18:46 -0300

    Seta como disabled caso venha null

--- app/assets/javascripts/views/descriptive_exams/form.js
+++ app/assets/javascripts/views/descriptive_exams/form.js
@@ -69,6 +69,8 @@ $(function () {
       }),
       success: function(descriptive_exam_id) {
         if (descriptive_exam_id === null || !$.isNumeric(descriptive_exam_id)) {
+          view_btn.addClass('disabled');
+          view_btn.attr('href', '')
           return;
         }
 
