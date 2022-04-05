fa6ad115f250430aad94579bd9509737576a4edd
gustavomendess <gustavocod13@outlook.com>
2022-04-05 19:07:41 -0300

    Ajusta view, js e query para novo formato em lote

--- app/queries/observation_record_report_query.rb
+++ app/queries/observation_record_report_query.rb
@@ -1,17 +1,19 @@
 class ObservationRecordReportQuery
-  def initialize(unity_id, teacher_id, classroom_id, discipline_id, start_at, end_at)
+  def initialize(unity_id, teacher_id, classroom_id, discipline_id, start_at, end_at, current_user_id)
     @unity_id = unity_id
     @teacher_id = teacher_id
     @classroom_id = classroom_id
     @discipline_id = discipline_id
     @start_at = start_at.to_date
     @end_at = end_at.to_date
+    @current_user_id = current_user_id
   end
 
   def observation_diary_records
     if @classroom_id.eql?('all')
-      classrooms_id = if current_user.teacher?
-                        Classroom.by_unity_and_teacher(unity_id, current_teacher.id).pluck(:id)
+      user = User.find(current_user_id)
+      classrooms_id = if user.teacher?
+                        Classroom.by_unity_and_teacher(unity_id, user.id).pluck(:id)
                       else
                         Classroom.by_unity(unity_id).pluck(:id)
                       end
@@ -34,5 +36,5 @@ class ObservationRecordReportQuery
 
   private
 
-  attr_accessor :unity_id, :teacher_id, :classroom_id, :discipline_id, :start_at, :end_at
+  attr_accessor :unity_id, :teacher_id, :classroom_id, :discipline_id, :start_at, :end_at, :current_user_id
 end
