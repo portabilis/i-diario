47d8ba6d24c0078db0d90dfdd33e20f47a56912e
gustavomendess <gustavocod13@outlook.com>
2022-04-05 18:28:09 -0300

    Ajusta query do relatório para emitir em lote

--- app/queries/observation_record_report_query.rb
+++ app/queries/observation_record_report_query.rb
@@ -1,5 +1,6 @@
 class ObservationRecordReportQuery
-  def initialize(teacher_id, classroom_id, discipline_id, start_at, end_at)
+  def initialize(unity_id, teacher_id, classroom_id, discipline_id, start_at, end_at)
+    @unity_id = unity_id
     @teacher_id = teacher_id
     @classroom_id = classroom_id
     @discipline_id = discipline_id
@@ -8,18 +9,30 @@ class ObservationRecordReportQuery
   end
 
   def observation_diary_records
-    relation = ObservationDiaryRecord.includes(notes: :students)
-      .by_teacher(teacher_id)
-      .by_classroom(classroom_id)
-      .where(date: start_at..end_at)
-      .order(:date)
+    if @classroom_id.eql?('all')
+      classrooms_id = if current_user.teacher?
+                        Classroom.by_unity_and_teacher(unity_id, current_teacher.id).pluck(:id)
+                      else
+                        Classroom.by_unity(unity_id).pluck(:id)
+                      end
+      relation = ObservationDiaryRecord.includes(notes: :students)
+                                       .by_classroom(classrooms_id)
+                                       .where(date: start_at..end_at)
+                                       .order(:date)
+    else
+      relation = ObservationDiaryRecord.includes(notes: :students)
+                                       .by_teacher(teacher_id)
+                                       .by_classroom(classroom_id)
+                                       .where(date: start_at..end_at)
+                                       .order(:date)
 
-    relation = relation.by_discipline(discipline_id) if discipline_id.present?
+      relation = relation.by_discipline(discipline_id) if discipline_id.present?
+    end
 
     relation
   end
 
   private
 
-  attr_accessor :teacher_id, :classroom_id, :discipline_id, :start_at, :end_at
+  attr_accessor :unity_id, :teacher_id, :classroom_id, :discipline_id, :start_at, :end_at
 end
