3783934c840108db4cfd4faab94acd73a5a16fc8
brunoocarvalhoo <bruno.carvalho310303@gmail.com>
2022-04-05 16:29:42 -0300

    Altera nome do método da migration para rodar em staging

--- db/migrate/20220404145952_add_minimum_score_to_test_settings.rb
+++ db/migrate/20220404145952_add_minimum_score_to_test_settings.rb
@@ -1,5 +1,5 @@
 class AddMinimumScoreToTestSettings < ActiveRecord::Migration
-  def create
+  def change
     add_column :test_settings, :minimum_score, :integer, default: 0, null: false
   end
 end
