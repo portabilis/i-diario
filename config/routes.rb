75bbc271e68cd8497a8383065292abbe112aee1b
gustavomendess <gustavocod13@outlook.com>
2022-04-19 16:58:08 -0300

    Permite visualizar histórico

--- config/routes.rb
+++ config/routes.rb
@@ -275,7 +275,7 @@ Rails.application.routes.draw do
     end
     resources :final_recovery_diary_records, concerns: :history
     resources :avaliation_recovery_diary_records, concerns: :history
-    resources :avaliation_recovery_lowest_notes
+    resources :avaliation_recovery_lowest_notes, concerns: :history
     resources :conceptual_exams, concerns: :history do
       collection do
         get :exempted_disciplines
