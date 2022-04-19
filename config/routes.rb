13c2ac2d3a55585c14dcecfc51cf4ddabc8b78fa
gustavomendess <gustavocod13@outlook.com>
2022-04-19 16:26:16 -0300

    Cria rota

--- config/routes.rb
+++ config/routes.rb
@@ -275,6 +275,7 @@ Rails.application.routes.draw do
     end
     resources :final_recovery_diary_records, concerns: :history
     resources :avaliation_recovery_diary_records, concerns: :history
+    resources :avaliation_recovery_lowest_notes
     resources :conceptual_exams, concerns: :history do
       collection do
         get :exempted_disciplines
