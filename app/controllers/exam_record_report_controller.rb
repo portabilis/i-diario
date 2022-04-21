292acf00727607bcce4641fc3c2d98c2f6ef6da8
gustavomendess <gustavocod13@outlook.com>
2022-04-21 03:50:01 -0300

    Passa método por parâmetro

--- app/controllers/exam_record_report_controller.rb
+++ app/controllers/exam_record_report_controller.rb
@@ -32,7 +32,8 @@ class ExamRecordReportController < ApplicationController
       @exam_record_report_form.daily_notes,
       @exam_record_report_form.students_enrollments,
       @exam_record_report_form.complementary_exams,
-      @exam_record_report_form.school_term_recoveries
+      @exam_record_report_form.school_term_recoveries,
+      @exam_record_report_form.recovery_lowest_notes?
     )
   end
 
