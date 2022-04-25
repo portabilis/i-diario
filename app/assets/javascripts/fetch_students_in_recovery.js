6e66048a1a94936b803168222b1033ddc2d9cfd9
gustavomendess <gustavocod13@outlook.com>
2022-04-25 18:27:13 -0300

    Usa moment para formatar datas

--- app/assets/javascripts/fetch_students_in_recovery.js
+++ app/assets/javascripts/fetch_students_in_recovery.js
@@ -1,7 +1,5 @@
-let dateRegex = '^(?:(?:31(\\/)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/)(?:0?[1,3-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$';
-
 function fetchStudentsInRecovery(classroom, discipline, exam_rule, step_id, recorded_at, success_callback) {
-  if (_.isEmpty(step_id) || _.isEmpty(recorded_at.match(dateRegex)) || exam_rule.recovery_type === 0) {
+  if (_.isEmpty(step_id) || _.isEmpty(moment(recorded_at)._i) || exam_rule.recovery_type === 0) {
     return;
   }
 
