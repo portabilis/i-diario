b93627bd30cc09e99cc0361ea0d440dda6464329
gustavomendess <gustavocod13@outlook.com>
2022-04-25 16:56:17 -0300

    Remove default

--- db/migrate/20220419184536_create_avaliation_recovery_lowest_notes.rb
+++ db/migrate/20220419184536_create_avaliation_recovery_lowest_notes.rb
@@ -4,7 +4,7 @@ class CreateAvaliationRecoveryLowestNotes < ActiveRecord::Migration
       t.references :recovery_diary_record, index: { name: 'idx_recovery_diary_record_id_on_recovery_lowest_note' },
                    null: false, foreign_key: true
       t.date :recorded_at, null: false
-      t.integer :step_number, null:false, default: 0
+      t.integer :step_number, null: false
       t.timestamps null: false
     end
   end
