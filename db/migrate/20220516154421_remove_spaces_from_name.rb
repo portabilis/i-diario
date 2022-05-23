a3d0a386a740dbdab94447d75b117433e59a6c80
gustavomendess <gustavocod13@outlook.com>
2022-05-23 15:35:05 -0300

    Remove elementos também do meio da string

--- db/migrate/20220516154421_remove_spaces_from_name.rb
+++ db/migrate/20220516154421_remove_spaces_from_name.rb
@@ -1,7 +1,8 @@
 class RemoveSpacesFromName < ActiveRecord::Migration
   def change
     execute <<-SQL
-      UPDATE users SET first_name = BTRIM(first_name), last_name = BTRIM(last_name);
+      UPDATE users SET first_name = trim(regexp_replace(first_name, '\s+', ' ', 'g')),
+                       last_name = trim(regexp_replace(last_name, '\s+', ' ', 'g'))
     SQL
   end
 end
