f99ef0e5311d0c6eb183a94cb078832b5d0d4f98
gustavomendess <gustavocod13@outlook.com>
2022-06-17 11:43:56 -0300

    Ajusta para utilizar range de datas da etapa

--- app/services/student_enrollments_list.rb
+++ app/services/student_enrollments_list.rb
@@ -190,7 +190,7 @@ class StudentEnrollmentsList
     student_ids = fetch_student_enrollments.map(&:student_id)
 
     StudentEnrollment
-      .by_date(@date)
+      .by_date_range(start_at, end_at)
       .where(
         student_id: student_ids,
         status: (StudentEnrollmentStatus::TRANSFERRED || StudentEnrollmentStatus::RECLASSIFIED)
