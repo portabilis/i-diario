22a3c33faa9bc04fe18d221e44483123edd8599c
gustavomendess <gustavocod13@outlook.com>
2022-07-20 01:11:28 -0300

    Ajusta lógica de criação e apaga disciplina caso seja alterado para não agrupar

--- app/services/ieducar_synchronizers/knowledge_areas_synchronizer.rb
+++ app/services/ieducar_synchronizers/knowledge_areas_synchronizer.rb
@@ -25,14 +25,21 @@ class KnowledgeAreasSynchronizer < BaseSynchronizer
 
         if knowledge_area.group_descriptors
           Discipline.find_or_initialize_by(
-            knowledge_area_id: knowledge_area.id
+            knowledge_area_id: knowledge_area.id,
+            grouper: true
           ).tap do |grouper_discipline|
             grouper_discipline.description = knowledge_area.description
             grouper_discipline.grouper = true
 
             grouper_discipline.save! if grouper_discipline.changed?
           end
+        else
+          Discipline.find_by(
+            knowledge_area_id: knowledge_area.id,
+            grouper: true
+          )&.destroy
         end
+
 
         knowledge_area.discard_or_undiscard(knowledge_area_record.deleted_at.present?)
       end
