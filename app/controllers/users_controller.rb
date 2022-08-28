9320619c1a9a175a5216a25083ac6d4cbfc86f35
gustavomendess <gustavocod13@outlook.com>
2022-08-28 11:17:30 -0300

    Ignora validação caso usuário não seja do email portabilis

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -128,7 +128,7 @@ class UsersController < ApplicationController
   end
 
   def not_allow_admin?
-    return false if user_params[:admin] == "0"
+    return false if user_params[:admin] == "0" || !current_user.is_admin_email?
 
     role_ids = user_params[:user_roles_attributes].values.map do |user_role|
       user_role[:role_id] if user_role[:_destroy] == "false"
