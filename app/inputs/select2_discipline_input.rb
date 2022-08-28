3b0ca14a87e2a908cfd4bff6a73027176e4eaa9c
gustavomendess <gustavocod13@outlook.com>
2022-08-28 12:05:06 -0300

    Revert input de disciplina

--- app/inputs/select2_discipline_input.rb
+++ app/inputs/select2_discipline_input.rb
@@ -3,9 +3,7 @@ class Select2DisciplineInput < Select2Input
   def input(wrapper_options)
     raise "User must be passed" unless options[:user].is_a? User
 
-    discipline = options[:user].current_discipline || Discipline.unscoped.find(options[:user].current_discipline_id)
-
-    if discipline.present?
+    if options[:user].current_discipline.present?
       input_html_options[:readonly] = 'readonly' unless options[:admin_or_employee].presence
       input_html_options[:value] = input_value if input_html_options[:value].blank?
     end
@@ -18,17 +16,17 @@ class Select2DisciplineInput < Select2Input
 
     disciplines =
       if options[:record]&.persisted? && options[:record]&.discipline
-        Discipline.unscoped.where(id: options[:record].discipline.id)
+        Discipline.where(id: options[:record].discipline.id)
       elsif options[:admin_or_employee].presence
-        Discipline.unscoped.by_classroom(user.current_classroom_id)
+        Discipline.by_classroom(user.current_classroom_id)
       elsif user.current_discipline_id?
-        Discipline.unscoped.where(id: user.current_discipline_id)
+        Discipline.where(id: user.current_discipline_id)
       elsif user.current_teacher.present? && options[:grade_id]
-        Discipline.unscoped.by_grade(options[:grade_id]).by_teacher_id(user.current_teacher.id)
+        Discipline.by_grade(options[:grade_id]).by_teacher_id(user.current_teacher.id)
       elsif user.current_teacher.present? && options[:classroom_id]
-        Discipline.unscoped.by_classroom(options[:classroom_id]).by_teacher_id(user.current_teacher.id)
+        Discipline.by_classroom(options[:classroom_id]).by_teacher_id(user.current_teacher.id)
       elsif user.current_unity.present? && options[:grade_id]
-        Discipline.unscoped.by_unity_id(user.current_unity.id).by_grade(options[:grade_id])
+        Discipline.by_unity_id(user.current_unity.id).by_grade(options[:grade_id])
       end
 
     options[:elements] = disciplines.present? ? disciplines.grouped_by_knowledge_area : []
@@ -41,6 +39,6 @@ class Select2DisciplineInput < Select2Input
   def input_value
     return options[:record].discipline_id if options[:record]&.persisted?
 
-    options[:user].current_discipline_id unless options[:admin_or_employee].presence
+    options[:user].current_discipline.id unless options[:admin_or_employee].presence
   end
 end
