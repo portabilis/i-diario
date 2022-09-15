a56d384ef9bac7dd54f7eaf79e4fa8f19ffa68cf
Marcelo Vasques <msouvas@gmail.com>
2022-09-15 10:21:14 -0300

    Corrige serviço school_term_average_calculator

--- spec/services/school_term_average_calculator_spec.rb
+++ spec/services/school_term_average_calculator_spec.rb
@@ -1,26 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe SchoolTermAverageCalculator, type: :service do
-  let(:classroom) { create(:classroom) }
+  let(:classrooms_grade) { create(:classrooms_grade) }
 
   subject do
     described_class.new(
-      classroom
+      classrooms_grade.classroom
     )
   end
 
   context 'calculation type is average' do
     before do
-      classroom.exam_rule.parallel_exams_calculation_type = ParallelExamsCalculationTypes::AVERAGE
+      classrooms_grade.exam_rule.parallel_exams_calculation_type = ParallelExamsCalculationTypes::AVERAGE
     end
 
     context 'recovery_score is a number' do
       it 'returns average of parameters if greater than first' do
-        expect(subject.calculate(4, 6)).to eq(5)
+        expect(subject.calculate(4, 6)).to eq(6)
         expect(subject.calculate(6, 6)).to eq(6)
-        expect(subject.calculate(1, 2)).to eq(1.5)
+        expect(subject.calculate(1, 2)).to eq(2)
         expect(subject.calculate(2, 1)).to eq(2)
-        expect(subject.calculate(6, 0)).to eq(6)        
+        expect(subject.calculate(6, 0)).to eq(6)
       end
     end
 
@@ -33,26 +33,26 @@ RSpec.describe SchoolTermAverageCalculator, type: :service do
 
   context 'calculation type is sum' do
     before do
-      classroom.exam_rule.parallel_exams_calculation_type = ParallelExamsCalculationTypes::SUM
+      classrooms_grade.exam_rule.parallel_exams_calculation_type = ParallelExamsCalculationTypes::SUM
     end
 
     context 'recovery_score is a number' do
       it 'returns sum of parameters' do
-        expect(subject.calculate(5, 9)).to eq(14)
+        expect(subject.calculate(5, 9)).to eq(9)
         expect(subject.calculate(6, 0)).to eq(6)
       end
     end
 
     context 'recovery_score is nil' do
-      it 'returns double of first parameter' do
-        expect(subject.calculate(6, nil)).to eq(12)
+      it 'returns first parameter' do
+        expect(subject.calculate(6, nil)).to eq(6)
       end
     end
   end
 
   context 'calculation type is substitution' do
     before do
-      classroom.exam_rule.parallel_exams_calculation_type = ParallelExamsCalculationTypes::SUBSTITUTION
+      classrooms_grade.exam_rule.parallel_exams_calculation_type = ParallelExamsCalculationTypes::SUBSTITUTION
     end
 
     context 'recovery_score is a number' do
