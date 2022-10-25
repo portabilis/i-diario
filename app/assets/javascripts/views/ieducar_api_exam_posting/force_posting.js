b4848d3d82b473610a4b215139de16a26582ffb9
brunoocarvalhoo <bruno.carvalho310303@gmail.com>
2022-10-25 14:44:13 -0300

    Altera nome das varíaveis de mapeamento para melhor entendimento

--- app/assets/javascripts/views/ieducar_api_exam_posting/force_posting.js
+++ app/assets/javascripts/views/ieducar_api_exam_posting/force_posting.js
@@ -31,14 +31,14 @@
       const button = document.getElementById(key);
       if ((clickTracking[key].clicked > clickTracking[key].clicked_old) && (clickTracking[key].paid < clickTracking[key].timeout)) {
         const interval = setInterval(() => {
-          let aux = JSON.parse(localStorage.getItem('click-tracking'));
-          if (aux[key].paid >= aux[key].timeout) {
-            aux[key] = {
-              ...aux[key],
-              clicked_old: aux[key].clicked,
+          let trackingFromStorage = JSON.parse(localStorage.getItem('click-tracking'));
+          if (trackingFromStorage[key].paid >= trackingFromStorage[key].timeout) {
+            trackingFromStorage[key] = {
+              ...trackingFromStorage[key],
+              clicked_old: trackingFromStorage[key].clicked,
             }
           } else {
-            aux[key].paid += 1000;
+            trackingFromStorage[key].paid += 1000;
           }
           localStorage.setItem('click-tracking', JSON.stringify(aux));
         }, 1000);
@@ -46,15 +46,15 @@
         if (button) button.style.pointerEvents = 'none';
         setTimeout(() => {
           if (button) button.style.pointerEvents = 'auto';
-          let aux1 = JSON.parse(localStorage.getItem('click-tracking'));
-          aux1[key] = {
-            ...aux1[key],
-            clicked_old: aux1[key].clicked,
+          let trackingFromStoragePaid = JSON.parse(localStorage.getItem('click-tracking'));
+          trackingFromStoragePaid[key] = {
+            ...trackingFromStoragePaid[key],
+            clicked_old: trackingFromStoragePaid[key].clicked,
             paid: 0
           }
 
           clearInterval(interval);
-          localStorage.setItem('click-tracking', JSON.stringify(aux1));
+          localStorage.setItem('click-tracking', JSON.stringify(trackingFromStoragePaid));
         }, clickTracking[key].timeout - clickTracking[key].paid);
       }
     });
