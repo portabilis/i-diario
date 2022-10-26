723ca072b24e00ca1709e9a5072b460a7611071c
gustavomendess <gustavocod13@outlook.com>
2022-10-25 23:04:29 -0300

    Cria traits

--- spec/factories/lessons_board_lessons.rb
+++ spec/factories/lessons_board_lessons.rb
@@ -1,6 +1,35 @@
 FactoryGirl.define do
   factory :lessons_board_lesson do
     lessons_board
-    lesson_number [1, 2, 3, 4, 5, 6].sample
+
+    trait :with_five_weekdays do
+      after(:build) do |lesson_board_lesson|
+        create(
+          :lessons_board_lesson_weekday,
+          lessons_board_lesson: lesson_board_lesson.lessons_board,
+          weekday: :monday
+        )
+        create(
+          :lessons_board_lesson_weekday,
+          lessons_board_lesson: lesson_board_lesson.lessons_board,
+          weekday: :tuesday
+        )
+        create(
+          :lessons_board_lesson_weekday,
+          lessons_board_lesson: lesson_board_lesson.lessons_board,
+          weekday: :wednesday
+        )
+        create(
+          :lessons_board_lesson_weekday,
+          lessons_board_lesson: lesson_board_lesson.lessons_board,
+          weekday: :thursday
+        )
+        create(
+          :lessons_board_lesson_weekday,
+          lessons_board_lesson: lesson_board_lesson.lessons_board,
+          weekday: :friday
+        )
+      end
+    end
   end
 end
