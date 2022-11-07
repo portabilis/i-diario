f602ed2dba930af6aaa3b5120ec61aa691937fac
Marcelo Vasques <msouvas@gmail.com>
2022-11-07 16:25:02 -0300

    Cria decorators para as views

---
+++ app/decorators/knowledge_area_content_record_decorator.rb
@@ -0,0 +1,12 @@
+class KnowledgeAreaContentRecordDecorator
+  include Decore
+
+  def daily_activities_required?
+    general_configuration = GeneralConfiguration.current
+    return false if general_configuration.require_daily_activities_record_does_not_require? ||
+                    general_configuration.require_daily_activities_record_require_on_discipline_content_records?
+
+    true
+  end
+
+end
