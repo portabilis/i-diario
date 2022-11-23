302a6270dfdf30577cc685776c4deb556ff69d36
gustavomendess <gustavocod13@outlook.com>
2022-11-22 22:33:52 -0300

    Remove validação sem sentido

--- app/models/lessons_board_lesson_weekday.rb
+++ app/models/lessons_board_lesson_weekday.rb
@@ -7,8 +7,6 @@ class LessonsBoardLessonWeekday < ActiveRecord::Base
   belongs_to :teacher_discipline_classroom
   belongs_to :lessons_board_lesson
 
-  validates :teacher_discipline_classroom_id, presence: true
-
   default_scope -> { kept }
 
   scope :by_classroom, ->(classroom_id) do
--- spec/models/lessons_board_lesson_weekday_spec.rb
+++ spec/models/lessons_board_lesson_weekday_spec.rb
@@ -11,8 +11,4 @@ RSpec.describe LessonsBoardLessonWeekday, type: :model do
     it { expect(subject).to belong_to(:teacher_discipline_classroom) }
     it { expect(subject).to belong_to(:lessons_board_lesson) }
   end
-
-  describe 'validations' do
-    it { expect(subject).to validate_presence_of(:teacher_discipline_classroom_id) }
-  end
 end
