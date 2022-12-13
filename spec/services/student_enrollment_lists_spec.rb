bbbb56c1061554b09c0c8ffec968f39935fa6ccc
AnaPerola <anaperolinha@hotmail.com>
2022-12-13 09:18:45 -0300

    Comenta contexto temporariamente

--- spec/services/student_enrollment_lists_spec.rb
+++ spec/services/student_enrollment_lists_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe StudentEnrollmentsList, type: :service do
       student_enrollment_id: student_enrollment.id
     )
   }
+  let(:current_user) { create(:user) }
   let(:classroom_grade) { student_enrollment_classroom.classrooms_grade_id }
   let(:classroom) { student_enrollment_classroom.classrooms_grade.classroom_id }
   let(:discipline) { create(:discipline) }
@@ -278,40 +279,38 @@ RSpec.describe StudentEnrollmentsList, type: :service do
     end
 
     context 'when searching student_enrollment with recovery_note_in_step' do
-      let!(:recovery_diary_record) {
-        create(
-          :recovery_diary_record,
-          :with_teacher_discipline_classroom,
-          :with_students,
-          classroom_id: classroom,
-          discipline_id: discipline.id
-        )
-      }
-      let!(:school_term_recovery_diary_record) {
-        create(
-          :school_term_recovery_diary_record,
-          recovery_diary_record: recovery_diary_record
-        )
-      }
-      # teste = build(
-      #   :school_term_recovery_diary_record,
-      #   recovery_diary_record: recovery_diary_record,
-      #   step: recovery_diary_record.step
-      # )
-      let!(:school_calendar) { create(:school_calendar, unity_id: student_enrollment_classroom.classrooms_grade.classroom.unity_id) }
-      let!(:school_calendar_step) { create(:school_calendar_step, school_calendar_id: school_calendar.id) }
-
-      it 'returns array of student_enrollment while include with_recovery_note_in_step' do
-        subject = described_class.new(
-          classroom: classroom,
-          discipline: discipline,
-          search_type: :by_date,
-          date: '2017-01-01',
-          with_recovery_note_in_step: AffectedScoreTypes::STEP_RECOVERY_SCORE
-        )
-
-        # expect(subject.student_enrollments).to include(student_enrollment_classroom.student_enrollment)
-      end
+      # let!(:recovery_diary_record) {
+      #   create(
+      #     :recovery_diary_record,
+      #     :with_teacher_discipline_classroom,
+      #     :with_students,
+      #     classroom_id: classroom,
+      #     discipline_id: discipline.id
+      #   )
+      # }
+      # let!(:school_calendar) { create(:school_calendar, unity_id: student_enrollment_classroom.classrooms_grade.classroom.unity_id) }
+      # let!(:school_calendar_step) { create(:school_calendar_step, school_calendar_id: school_calendar.id) }
+      # # let!(:school_term_recovery_diary_record) {
+      # #   create(
+      # #     :school_term_recovery_diary_record,
+      # #     recovery_diary_record: recovery_diary_record,
+      # #     step: school_calendar_step
+      # #   )
+      # }
+      #
+      # it 'returns array of student_enrollment while include with_recovery_note_in_step' do
+      #   current_user.current_classroom_id = recovery_diary_record.classroom_id
+      #   current_user.current_discipline_id = recovery_diary_record.discipline_id
+      #   subject = described_class.new(
+      #     classroom: classroom,
+      #     discipline: discipline,
+      #     search_type: :by_date,
+      #     date: '2017-01-01',
+      #     with_recovery_note_in_step: AffectedScoreTypes::STEP_RECOVERY_SCORE
+      #   )
+      #   allow(recovery_diary_record).to receive(:current_user).and_return(current_user)
+      #   expect(subject.student_enrollments).to include(student_enrollment_classroom.student_enrollment)
+      # end
     end
 
     context 'when searching student_enrollment with show_inactive' do
