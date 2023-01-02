683625282e0895c17460a89c130aaeea61ac12ad
AnaPerola <anaperolinha@hotmail.com>
2023-01-02 17:04:11 -0300

    Move lógica de alunos em dispensa para service

---
+++ app/services/students_exempt_from_discipline.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class StudentsExemptFromDiscipline
+  def self.call(params)
+    @student_enrollments = params.fetch(:student_enrollments)
+    @discipline = params.fetch(:discipline)
+    @steps = params.fetch(:steps)
+  end
+
+  def call
+    return {} if @discipline.blank?
+
+    student_enrollments_exempt = StudentEnrollmentExemptedDiscipline.by_discipline(@discipline.id)
+                                                                    .by_step_number(@steps)
+                                                                    .by_student_enrollment(@student_enrollments)
+                                                                    .includes(student_enrollment: [:student])
+
+    student_has_exempt_for_step(student_enrollments_exempt)
+  end
+
+
+  private
+
+  def student_has_exempt_for_step(student_enrollments_exempt)
+
+    exempts_from_discipline = {}
+
+    student_enrollments_exempt.each do |student_exempted|
+      exempts_from_discipline[student_exempted.student_enrollment_id] ||= @steps
+    end
+
+    exempts_from_discipline
+  end
+end
