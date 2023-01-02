dce9c22b4111ced1db11268f4a67c7cf88f527c3
AnaPerola <anaperolinha@hotmail.com>
2023-01-02 17:04:41 -0300

    Move lógica de alunos ativos na data para service

---
+++ app/services/active_students_on_date.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class ActiveStudentsOnDate
+  def self.call(params)
+    @student_enrollments = params.fetch(:student_enrollments)
+    @date = params.fetch(:date)
+  end
+
+  def call
+    enrollment_classrooms = StudentEnrollmentClassroom.by_student_enrollment(@student_enrollments)
+                                                      .by_date(@date)
+
+    student_active_on_date(enrollment_classrooms)
+  end
+
+  private
+
+  def student_active_on_date(enrollment_classrooms)
+    active_on_date = {}
+
+    enrollment_classrooms.each do |enrollment_classroom|
+      active_on_date[enrollment_classroom.id] ||= []
+      active_on_date[enrollment_classroom.id] << @date
+    end
+
+    active_on_date
+  end
+end
