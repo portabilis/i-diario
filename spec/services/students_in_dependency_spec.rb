5232ed9331b72f9f666b24d29d808a73a585085e
AnaPerola <anaperolinha@hotmail.com>
2023-01-03 12:47:37 -0300

    Cria casos de teste para validar o envio de duas ou mais disciplinas e matriculas por parametro

--- spec/services/students_in_dependency_spec.rb
+++ spec/services/students_in_dependency_spec.rb
@@ -13,7 +13,6 @@ RSpec.describe StudentsInDependency, type: :service do
     )
   }
 
-
   context '#call' do
     it 'should returns student_enrollment with student in dependency' do
       subject = described_class.call(
@@ -22,6 +21,25 @@ RSpec.describe StudentsInDependency, type: :service do
       )
 
       expect(subject).to include(student_enrollment_classroom.student_enrollment_id)
+    end
+
+    let!(:student_enrollment_classroom_3) { create(:student_enrollment_classroom) }
+    let!(:discipline_3) { create(:discipline) }
+    let!(:student_enrollment_dependence_3) {
+      create(
+        :student_enrollment_dependence,
+        student_enrollment_id: student_enrollment_classroom_3.student_enrollment_id,
+        discipline_id: discipline_3.id
+      )
+    }
+
+    it 'should returns student_enrollments with students in dependency in two disciplines' do
+      subject = described_class.call(
+        student_enrollments: [student_enrollment_classroom.student_enrollment, student_enrollment_classroom_3.student_enrollment],
+        discipline: [discipline, discipline_3]
+      )
+
+      expect(subject).to include(student_enrollment_classroom.student_enrollment_id, student_enrollment_classroom_3.student_enrollment_id)
     end
 
     it 'should returns hash empty for student without dependency' do
