571ec65632eef31fdcb2faf0c4fc037dad54818b
AnaPerola <anaperolinha@hotmail.com>
2023-01-03 11:40:25 -0300

    Cria factory e casos de teste para service StudentsInDependency

---
+++ spec/services/students_in_dependency_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe StudentsInDependency, type: :service do
+  let!(:student_enrollment_classroom) { create(:student_enrollment_classroom) }
+  let!(:student_enrollment_classroom_2) { create(:student_enrollment_classroom) }
+  let!(:discipline) { create(:discipline) }
+  let!(:student_enrollment_dependence) {
+    create(
+      :student_enrollment_dependence,
+      student_enrollment_id: student_enrollment_classroom.student_enrollment_id,
+      discipline_id: discipline.id
+    )
+  }
+
+
+  context '#call' do
+    it 'should returns student_enrollment with student in dependency' do
+      subject = described_class.call(
+        student_enrollments: student_enrollment_classroom.student_enrollment_id,
+        discipline: discipline
+      )
+
+      expect(subject).to include(student_enrollment_classroom.student_enrollment_id)
+    end
+
+    it 'should returns hash empty for student without dependency' do
+      subject = described_class.call(
+        student_enrollments: student_enrollment_classroom_2.student_enrollment_id,
+        discipline: discipline
+      )
+
+      expect(subject).to be_empty
+    end
+  end
+end
