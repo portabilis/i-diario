4890d3127c45373527ff76d9e3974da1d37bcf32
AnaPerola <anaperolinha@hotmail.com>
2023-01-04 18:11:04 -0300

    Ajusta teste para service StudentsExemptFromDiscipline

--- spec/services/students_exempt_from_discipline_spec.rb
+++ spec/services/students_exempt_from_discipline_spec.rb
@@ -1,38 +1,63 @@
 # frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe StudentsExemptFromDiscipline, type: :service do
-  let!(:student_enrollment_classroom_2) { create(:student_enrollment_classroom) }
-  let!(:student_enrollment_classroom) { create(:student_enrollment_classroom) }
-  let!(:discipline) { create(:discipline) }
-  let!(:student_enrollment_exempted_discipline) {
-    create(
-      :student_enrollment_exempted_discipline,
-      student_enrollment_id: student_enrollment_classroom.student_enrollment_id,
-      discipline_id: discipline.id
-    )
-  }
-
+  let(:discipline) { create(:discipline) }
+  let(:student_enrollments) { create_list(:student_enrollment, 3) }
+  before do
+    discipline
+    student_enrollments
+  end
 
   context '#call' do
-    it 'should returns student_enrollment with student exempt from discipline' do
-      subject = described_class.call(
-        student_enrollments: student_enrollment_classroom.student_enrollment_id,
+    it 'should returns student_enrollments with students exempt from discipline' do
+      create_enrollments_exempted(student_enrollments, discipline)
+      student_enrollment_ids = student_enrollments.map(&:id)
+
+      subject = StudentsExemptFromDiscipline.call(
+        student_enrollments: student_enrollments,
         discipline: discipline,
         steps: 1
       )
 
-      expect(subject).to include(student_enrollment_classroom.student_enrollment_id)
+      expect(subject).to include(
+        { student_enrollment_ids.first => 1 },
+        { student_enrollment_ids.second => 1 },
+        student_enrollment_ids.last => 1
+      )
+      expect(subject.size).to be(3)
     end
 
-    it 'should returns hash empty for student without exempt' do
-      subject = described_class.call(
-        student_enrollments: student_enrollment_classroom_2.student_enrollment_id,
+    it 'should not returns student_enrollments without exempt from discipline' do
+      student_enrollment_ids = student_enrollments.map(&:id)
+
+      subject = StudentsExemptFromDiscipline.call(
+        student_enrollments: student_enrollments,
         discipline: discipline,
         steps: 1
       )
 
-      expect(subject).to be_empty
+      expect(subject).not_to include(
+        { student_enrollment_ids.first => 1 },
+        { student_enrollment_ids.second => 1 },
+        student_enrollment_ids.last => 1
+      )
     end
   end
+end
+
+def create_enrollments_exempted(student_enrollments, discipline)
+  student_enrollment_exempted_disciplines = []
+
+  student_enrollments.each do |student_enrollment|
+    enrollment_exempted = create(
+      :student_enrollment_exempted_discipline,
+      student_enrollment: student_enrollment,
+      discipline: discipline
+    )
+    student_enrollment_exempted_disciplines << enrollment_exempted
+  end
+
+  student_enrollment_exempted_disciplines
 end
