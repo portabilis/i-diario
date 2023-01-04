5763bd9777c120a63825d4ae42cf3cfa7484debc
AnaPerola <anaperolinha@hotmail.com>
2023-01-04 15:45:35 -0300

    Altera variaveis let para variaveis comuns

--- spec/services/active_students_on_date_spec.rb
+++ spec/services/active_students_on_date_spec.rb
@@ -4,46 +4,35 @@ require 'rails_helper'
 RSpec.describe ActiveStudentsOnDate, type: :service do
   context '#call' do
     let(:student_enrollment) { create(:student_enrollment) }
-    let(:enrollment_classrooms_on_date) {
-      create_list(
+    before { student_enrollment }
+
+    it 'should returns enrollment classrooms on date' do
+      enrollment_classrooms_on_date = create_list(
         :student_enrollment_classroom,
         3,
         joined_at: '2017-01-01',
         student_enrollment: student_enrollment
       )
-    }
-    let(:enrollment_classrooms_out_date) {
-      create_list(
+
+      students = ActiveStudentsOnDate.call(student_enrollments: student_enrollment.id, date: '2017-05-05')
+
+      enrollment_classrooms_on_date.each do |enrollment_classroom|
+        expect(students).to include({ enrollment_classroom.id => ['2017-05-05'] })
+      end
+      expect(students.size).to be(3)
+    end
+
+    it 'should not returns enrollment classrooms out of date' do
+      enrollment_classrooms_out_date = create_list(
         :student_enrollment_classroom,
         3,
         joined_at: '2017-01-01',
         left_at: '2017-04-04',
         student_enrollment: student_enrollment
       )
-    }
 
-    subject do
-      ActiveStudentsOnDate.call(
-        student_enrollments: student_enrollment.id,
-        date: '2017-05-05'
-      )
-    end
+      students = ActiveStudentsOnDate.call(student_enrollments: student_enrollment.id, date: '2017-05-05')
 
-    before do
-      enrollment_classrooms_on_date
-      enrollment_classrooms_out_date
-    end
-
-    it 'should returns enrollment classrooms on date' do
-      students = subject
-      enrollment_classrooms_on_date.each do |enrollment_classroom|
-        expect(students).to include({ enrollment_classroom.id => ['2017-05-05'] })
-      end
-      expect(subject.size).to be(3)
-    end
-
-    it 'should not returns enrollment classrooms out of date' do
-      students = subject
       enrollment_classrooms_out_date.each do |enrollment_classroom|
         expect(students).to_not include({ enrollment_classroom.id => ['2017-05-05'] })
       end
