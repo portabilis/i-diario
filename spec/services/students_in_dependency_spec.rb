a6edff04372dd72ac07ebf60f2372cc45f9f8b65
AnaPerola <anaperolinha@hotmail.com>
2023-01-05 09:43:13 -0300

    Ajusta teste para service StudentsInDependency

--- spec/services/students_in_dependency_spec.rb
+++ spec/services/students_in_dependency_spec.rb
@@ -1,51 +1,44 @@
 # frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe StudentsInDependency, type: :service do
-  let!(:student_enrollment_classroom) { create(:student_enrollment_classroom) }
-  let!(:student_enrollment_classroom_2) { create(:student_enrollment_classroom) }
-  let!(:discipline) { create(:discipline) }
-  let!(:student_enrollment_dependence) {
-    create(
-      :student_enrollment_dependence,
-      student_enrollment_id: student_enrollment_classroom.student_enrollment_id,
-      discipline_id: discipline.id
-    )
-  }
-
   context '#call' do
-    it 'should returns student_enrollment with student in dependency' do
-      subject = described_class.call(
-        student_enrollments: student_enrollment_classroom.student_enrollment_id,
-        discipline: discipline
-      )
+    let(:student_enrollments) { create_list(:student_enrollment, 2) }
+    let(:disciplines) { create_list(:discipline, 2) }
 
-      expect(subject).to include(student_enrollment_classroom.student_enrollment_id)
+    before do
+      disciplines
+      student_enrollments
     end
 
-    let!(:student_enrollment_classroom_3) { create(:student_enrollment_classroom) }
-    let!(:discipline_3) { create(:discipline) }
-    let!(:student_enrollment_dependence_3) {
+    it 'should returns student_enrollments with student with dependency' do
       create(
         :student_enrollment_dependence,
-        student_enrollment_id: student_enrollment_classroom_3.student_enrollment_id,
-        discipline_id: discipline_3.id
+        student_enrollment: student_enrollments.first,
+        discipline: disciplines.first
       )
-    }
-
-    it 'should returns student_enrollments with students in dependency in two disciplines' do
-      subject = described_class.call(
-        student_enrollments: [student_enrollment_classroom.student_enrollment, student_enrollment_classroom_3.student_enrollment],
-        discipline: [discipline, discipline_3]
+      create(
+        :student_enrollment_dependence,
+        student_enrollment: student_enrollments.last,
+        discipline: disciplines.last
       )
 
-      expect(subject).to include(student_enrollment_classroom.student_enrollment_id, student_enrollment_classroom_3.student_enrollment_id)
+      subject = StudentsInDependency.call(
+        student_enrollments: student_enrollments,
+        discipline: disciplines
+      )
+
+      expect(subject).to include(
+        { student_enrollments.first.id => [disciplines.first.id] },
+        { student_enrollments.last.id => [disciplines.last.id] }
+      )
     end
 
-    it 'should returns hash empty for student without dependency' do
-      subject = described_class.call(
-        student_enrollments: student_enrollment_classroom_2.student_enrollment_id,
-        discipline: discipline
+    it 'should not returns student_enrollments with student without dependency' do
+      subject = StudentsInDependency.call(
+        student_enrollments: student_enrollments,
+        discipline: disciplines
       )
 
       expect(subject).to be_empty
