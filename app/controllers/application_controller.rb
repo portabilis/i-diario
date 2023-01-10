6f4d4c46b97cde847a49d37f25c18c7bbbafa6dc
Marcelo Vasques <msouvas@gmail.com>
2023-01-09 22:51:03 -0300

    Resolve bug do Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session, prepend: true
 
   before_action :check_entity_status
   before_action :authenticate_user!
