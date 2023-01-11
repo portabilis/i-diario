597ac2ffa5bc8112b4b9e8a329626a3ea3489e81
Marcelo Vasques <msouvas@gmail.com>
2023-01-11 11:39:56 -0300

    Mostra campo com nomes quando não há busca de alunos

--- app/decorators/observation_diary_record_decorator.rb
+++ app/decorators/observation_diary_record_decorator.rb
@@ -4,11 +4,12 @@ class ObservationDiaryRecordDecorator
   include ActionView::Context
   include ActionView::Helpers::UrlHelper
 
-  def students_labels(selected_id)
+  def students_labels(selected_id = nil)
     return unless component.students
 
     student_cell = if all_students.count == component.students.count
-                     content_tag(:span, 'Todos', class: 'student-name')
+                     student_class = 'student-name' if selected_id
+                     content_tag(:span, 'Todos', class: student_class)
                    else
                      student_label_for(component.students.ordered.distinct, selected_id)
                    end
@@ -18,7 +19,7 @@ class ObservationDiaryRecordDecorator
 
   def student_label_for(students, selected_id)
     students.map { |student|
-      student_class = 'student-name' if selected_id == student.id
+      student_class = 'student-name' if selected_id && selected_id == student.id
       content_tag(:span, student, class: student_class)
     }.join(', ').html_safe
   end
