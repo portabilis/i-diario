833b0245aa9043e9f55783a4881811ed74d2f7d1
AnaPerola <anaperolinha@hotmail.com>
2023-01-18 11:16:47 -0300

    Cria constante para tipo de busca e insere busca por range de datas

--- app/services/student_enrollments_retriever.rb
+++ app/services/student_enrollments_retriever.rb
@@ -1,4 +1,7 @@
 class StudentEnrollmentsRetriever
+  SEARCH_TYPES = [
+    :by_date, :by_date_range
+  ].freeze
 
   def self.call(params)
     new(params).call
@@ -9,12 +12,14 @@ class StudentEnrollmentsRetriever
     @classroom = params.fetch(:classroom)
     @discipline = params.fetch(:discipline)
     @date = params.fetch(:date, nil)
+    @start_at = params.fetch(:start_at, nil)
+    @end_at = params.fetch(:ent_at, nil)
 
     ensure_has_valid_params
   end
 
   def call
-    return if @classroom.blank? || @discipline.blank? || @search_type.blank?
+    return if @classroom.blank? || @discipline.blank?
 
     student_enrollments ||= StudentEnrollment.by_classroom(@classroom)
                                              .by_discipline(@discipline)
@@ -27,9 +32,12 @@ class StudentEnrollmentsRetriever
   end
 
   private
+
   def ensure_has_valid_params
     if @search_type.eql?(:by_date)
       raise ArgumentError, 'Should define date argument on search by date' unless @date
+    elsif @search_type.eql?(:by_date_range)
+      raise ArgumentError, 'Should define @start_at or @end_at argument on search by date_range' unless @start_at || @end_at
     end
   end
 
