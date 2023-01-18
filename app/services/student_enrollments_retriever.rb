cec7c49f790a01a0ab4b72beee7ef79c5e116666
AnaPerola <anaperolinha@hotmail.com>
2023-01-18 12:33:01 -0300

    Cria e valida tipos de busca para matriculas

--- app/services/student_enrollments_retriever.rb
+++ app/services/student_enrollments_retriever.rb
@@ -1,6 +1,6 @@
 class StudentEnrollmentsRetriever
   SEARCH_TYPES = [
-    :by_date, :by_date_range
+    :by_date, :by_date_range, :by_year
   ].freeze
 
   def self.call(params)
@@ -13,7 +13,8 @@ class StudentEnrollmentsRetriever
     @disciplines = params.fetch(:disciplines)
     @date = params.fetch(:date, nil)
     @start_at = params.fetch(:start_at, nil)
-    @end_at = params.fetch(:ent_at, nil)
+    @end_at = params.fetch(:end_at, nil)
+    @year = params.fetch(:year, nil)
 
     ensure_has_valid_search_params
   end
@@ -29,6 +30,7 @@ class StudentEnrollmentsRetriever
                                              .includes(:student_enrollment_classrooms)
                                              .active
 
+    search_by_search_type(student_enrollments)
   end
 
   private
@@ -38,7 +40,21 @@ class StudentEnrollmentsRetriever
       raise ArgumentError, 'Should define date argument on search by date' unless @date
     elsif @search_type.eql?(:by_date_range)
       raise ArgumentError, 'Should define @start_at or @end_at argument on search by date_range' unless @start_at || @end_at
+    elsif @search_type.eql?(:by_year)
+      raise ArgumentError, 'Should define @start_at or @end_at argument on search by date_range' unless @year
     end
+  end
+
+  def search_by_search_type(student_enrollments)
+    if @search_type.eql?(:by_date)
+      enrollments_on_period = student_enrollments.by_date(@date)
+    elsif  @search_type.eql?(:by_date_range)
+      enrollments_on_period = student_enrollments.by_date_range(@start_at, @end_at)
+    elsif  @search_type.eql?(:by_year)
+      enrollments_on_period = student_enrollments.by_year(@year)
+    end
+
+    enrollments_on_period
   end
 
 end
