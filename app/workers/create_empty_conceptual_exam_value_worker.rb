ba7c4b9aa3796ebf225d6100e3e9dcd65f4129e7
AnaPerola <anaperolinha@hotmail.com>
2023-01-19 12:07:40 -0300

    Insere novo parametro para enviar as series vinculadas nas turmas

--- app/workers/create_empty_conceptual_exam_value_worker.rb
+++ app/workers/create_empty_conceptual_exam_value_worker.rb
@@ -3,9 +3,9 @@ class CreateEmptyConceptualExamValueWorker
 
   sidekiq_options unique: :until_and_while_executing, queue: :low
 
-  def perform(entity_id, classroom_id, teacher_id)
+  def perform(entity_id, classroom_id, teacher_id, grade_id)
     Entity.find(entity_id).using_connection do
-      ConceptualExamValueCreator.create_empty_by(classroom_id, teacher_id)
+      ConceptualExamValueCreator.create_empty_by(classroom_id, teacher_id, grade_id)
     end
   end
 end
