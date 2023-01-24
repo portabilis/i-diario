2dc968ad84c3fe51f3df5bb079512abd0bc8120b
AnaPerola <anaperolinha@hotmail.com>
2023-01-24 14:51:16 -0300

    Descomenta método testado

--- app/services/student_enrollment_classrooms_retriever.rb
+++ app/services/student_enrollment_classrooms_retriever.rb
@@ -37,8 +37,7 @@ class StudentEnrollmentClassroomsRetriever
                                                         .active
 
     enrollment_classrooms = enrollment_classrooms.by_grade(grade) if grade
-    # enrollment_classrooms = enrollment_classrooms.by_period(period) if period
-    # enrollment_classrooms = enrollment_classrooms.by_opinion_type(opinion_type, classrooms) if opinion_type
+    enrollment_classrooms = enrollment_classrooms.by_period(period) if period
     # enrollment_classrooms = enrollment_classrooms.with_recovery_note_in_step(step, discipline) if with_recovery_note_in_step
     enrollment_classrooms = search_by_dates(enrollment_classrooms) if include_date_range
 
