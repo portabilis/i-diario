9f89a820e906e561938b5b248495958889dfe761
AnaPerola <anaperolinha@hotmail.com>
2023-01-25 09:17:46 -0300

    Corrige nome do parametro

--- app/workers/create_empty_conceptual_exam_value_worker.rb
+++ app/workers/create_empty_conceptual_exam_value_worker.rb
@@ -2,7 +2,14 @@ class CreateEmptyConceptualExamValueWorker
   include Sidekiq::Worker
 
   sidekiq_options unique: :until_and_while_executing, queue: :low
-
+  CreateEmptyConceptualExamValueWorker.perform_in(
+    1.second,
+    entity_id,
+    classroom_id,
+    teacher_id,
+    grades_in_disciplines
+  )
+end
   def perform(entity_id, classroom_id, teacher_id, grades_in_disciplines)
     Entity.find(entity_id).using_connection do
       ConceptualExamValueCreator.create_empty_by(classroom_id, teacher_id, grades_in_disciplines)
