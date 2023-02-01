ae838445a8c20d9132814bb6d4acf8509b2b753f
AnaPerola <anaperolinha@hotmail.com>
2023-02-01 10:49:24 -0300

    Cria novo scope para filtrar enturmacões por classroom_grades

--- app/models/student_enrollment.rb
+++ app/models/student_enrollment.rb
@@ -21,6 +21,9 @@ class StudentEnrollment < ActiveRecord::Base
   scope :by_grade, lambda { |grade_id|
     joins(:student_enrollment_classrooms).merge(StudentEnrollmentClassroom.by_grade(grade_id))
   }
+  scope :by_classroom_grades, lambda { |classrooms_grade_id|
+    joins(:student_enrollment_classrooms).merge(StudentEnrollmentClassroom.by_classroom_grade(classrooms_grade_id))
+  }
   scope :by_discipline, lambda {|discipline_id| by_discipline_query(discipline_id)}
   scope :by_score_type, lambda {|score_type, classroom_id| by_score_type_query(score_type, classroom_id)}
   scope :by_opinion_type, lambda {|opinion_type, classroom_id| by_opinion_type_query(opinion_type, classroom_id)}
