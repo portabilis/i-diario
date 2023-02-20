1005a88bc099b692e691f5d6affea3202f0d71c3
Eder Soares <edersoares@me.com>
2023-02-20 18:06:10 -0300

    Serviço para obter alunos com justificativa de faltas

---
+++ app/services/absence_justified_on_date.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class AbsenceJustifiedOnDate
+  def self.call(params)
+    new(params).call
+  end
+
+  def initialize(params)
+    @students = params.fetch(:students)
+    @date = params.fetch(:date)
+  end
+
+  def call
+    absence_justifications = AbsenceJustification.by_date(@date).by_student_id(@students)
+
+    absence_justified = {}
+
+    absence_justifications.each do |absence_justification|
+      absence_justification.students.each do |student|
+        absence_justified[student.id] ||= []
+        absence_justified[student.id] << @date
+      end
+    end
+
+    absence_justified
+  end
+end
