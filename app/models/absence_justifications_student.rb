1fb5d43011653e40a2e60e767a9c5f2d1aa5a6a9
Eder Soares <edersoares@me.com>
2023-02-28 17:12:18 -0300

    Ao lançar uma justificativa de falta para  um aluno, caso o mesmo tenha uma presença já lançada para o dia em questão, a presença deve ser marcada como falta e o lançamento deve ser vinculado a uma justificativa.

--- app/models/absence_justifications_student.rb
+++ app/models/absence_justifications_student.rb
@@ -6,5 +6,25 @@ class AbsenceJustificationsStudent < ActiveRecord::Base
   belongs_to :student
   belongs_to :absence_justification
 
+  after_save :justify_old_absences
+
   default_scope -> { kept }
+
+  private
+
+  def justify_old_absences
+    absence_date = absence_justification.absence_date
+    absence_date_end = absence_justification.absence_date_end
+    classroom_id = absence_justification.classroom_id
+
+    daily_frequency_students = DailyFrequencyStudent.by_classroom_id(classroom_id)
+                         .by_frequency_date_between(absence_date, absence_date_end)
+                         .by_student_id(student_id)
+
+    daily_frequency_students.each do |daily_frequency_student|
+      daily_frequency_student.present = false
+      daily_frequency_student.absence_justification_student_id = id
+      daily_frequency_student.save
+    end
+  end
 end
