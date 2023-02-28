17c3705b0d269092d930f382bcfc7ccdeb55fd60
Eder Soares <edersoares@me.com>
2023-02-28 17:20:48 -0300

    Ao excluir uma justificativa de falta, remove o vínculo com a frequência

--- app/models/absence_justifications_student.rb
+++ app/models/absence_justifications_student.rb
@@ -6,11 +6,21 @@ class AbsenceJustificationsStudent < ActiveRecord::Base
   belongs_to :student
   belongs_to :absence_justification
 
+  before_destroy :remove_justification_from_frequencies
   after_save :justify_old_absences
 
   default_scope -> { kept }
 
   private
+
+  def remove_justification_from_frequencies
+    daily_frequency_students = DailyFrequencyStudent.by_absence_justification_student_id(id)
+
+    daily_frequency_students.each do |daily_frequency_student|
+      daily_frequency_student.absence_justification_student_id = nil
+      daily_frequency_student.save
+    end
+  end
 
   def justify_old_absences
     absence_date = absence_justification.absence_date
