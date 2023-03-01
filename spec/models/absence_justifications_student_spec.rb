7d708553f857b4e51f7b14f0aece21d106c88474
Eder Soares <edersoares@me.com>
2023-03-01 16:14:17 -0300

    Refactor

--- spec/models/absence_justifications_student_spec.rb
+++ spec/models/absence_justifications_student_spec.rb
@@ -9,16 +9,14 @@ RSpec.describe AbsenceJustificationsStudent, type: :model do
   end
 
   describe 'callbacks' do
-    context 'should remove justification from student frequencies ' do
-      let!(:absence_justifications_student) { create(:absence_justifications_student, :with_daily_frequency) }
+    it 'should remove justification from student frequencies ' do
+      absence_justifications_student = create(:absence_justifications_student, :with_daily_frequency)
 
-      it 'ensures justification was removed' do
-        expect(DailyFrequencyStudent.first.absence_justification_student_id).to equal absence_justifications_student.id
+      expect(DailyFrequencyStudent.first.absence_justification_student_id).to equal absence_justifications_student.id
 
-        absence_justifications_student.absence_justification.destroy!
+      absence_justifications_student.absence_justification.destroy!
 
-        expect(DailyFrequencyStudent.first.absence_justification_student_id).to be_nil
-      end
+      expect(DailyFrequencyStudent.first.absence_justification_student_id).to be_nil
     end
 
     it 'should justify presence or absence already released' do
