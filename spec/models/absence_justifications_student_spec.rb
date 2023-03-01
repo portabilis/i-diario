8feab669b5e2f903cb48ac00408b4832deecd077
Eder Soares <edersoares@me.com>
2023-03-01 15:52:06 -0300

    Garante que ao excluir uma justificativa o vínculo com a frequência é removido

--- spec/models/absence_justifications_student_spec.rb
+++ spec/models/absence_justifications_student_spec.rb
@@ -1,10 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe AbsenceJustificationsStudent, type: :model do
-  subject(:absence_justifications_student) { create(:absence_justifications_student) }
-
   describe 'associations' do
+    subject(:absence_justifications_student) { create(:absence_justifications_student) }
+
     it { expect(subject).to belong_to(:absence_justification) }
     it { expect(subject).to belong_to(:student) }
+  end
+
+  describe 'callbacks' do
+    context  'should remove justification from student frequencies ' do
+      let!(:absence_justifications_student) { create(:absence_justifications_student, :with_daily_frequency) }
+
+      it 'ensures justification was removed' do
+        expect(DailyFrequencyStudent.first.absence_justification_student_id).to equal absence_justifications_student.id
+
+        absence_justifications_student.absence_justification.destroy!
+
+        expect(DailyFrequencyStudent.first.absence_justification_student_id).to be_nil
+      end
+    end
   end
 end
