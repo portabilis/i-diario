59c6a32f7d78a99c9548b67ab86e13bc65d7d4ee
gustavomendess <gustavocod13@outlook.com>
2023-03-04 00:37:22 -0300

    Ajusta envio

--- app/services/exam_poster/numerical_exam_poster.rb
+++ app/services/exam_poster/numerical_exam_poster.rb
@@ -52,7 +52,7 @@ module ExamPoster
         teacher_discipline_classrooms.each do |teacher_discipline_classroom|
           classroom = teacher_discipline_classroom.classroom
           discipline = teacher_discipline_classroom.discipline
-          score_rounder = ScoreRounder.new(classroom, RoundedAvaliations::SCHOOL_TERM_RECOVERY)
+          score_rounder = ScoreRounder.new(classroom, RoundedAvaliations::SCHOOL_TERM_RECOVERY, get_step(classroom))
 
           next unless can_post?(classroom)
 
