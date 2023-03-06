c24561d1c4c6409009a63b93ae2c8084977d38ca
AnaPerola <anaperolinha@hotmail.com>
2023-03-06 10:39:58 -0300

    Remove condicional para buscar vinculos utilizar o index tambem nos descartados

--- db/migrate/20230221202600_update_index_to_teacher_discipline_classroom.rb
+++ db/migrate/20230221202600_update_index_to_teacher_discipline_classroom.rb
@@ -6,6 +6,6 @@ class UpdateIndexToTeacherDisciplineClassroom < ActiveRecord::Migration
 
     add_index :teacher_discipline_classrooms, [:api_code, :teacher_id, :classroom_id, :discipline_id, :year, :grade_id],
               name: 'idx_unique_not_discarded_teacher_discipline_classrooms', unique: true,
-              algorithm: :concurrently, where: 'discarded_at IS NULL'
+              algorithm: :concurrently
   end
 end
