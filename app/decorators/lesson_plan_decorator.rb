e649dc7aa4f4fd15cdb66f19094babdeef387f50
AnaPerola <anaperolinha@hotmail.com>
2023-03-06 14:38:20 -0300

    Cria método para validar se a configuracao do cliente solicita a remocao do campo objetivo

--- app/decorators/lesson_plan_decorator.rb
+++ app/decorators/lesson_plan_decorator.rb
@@ -4,4 +4,11 @@ class LessonPlanDecorator
   def author(current_teacher)
     PlanAuthorFetcher.new(component, current_teacher).author
   end
+
+  def removed_objectives?
+    general_configuration = GeneralConfiguration.current
+    return false if general_configuration.remove_lesson_plan_objectives
+
+    true
+  end
 end
