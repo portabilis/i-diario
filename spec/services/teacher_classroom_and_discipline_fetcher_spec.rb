5e54d4f5c1c913b2ddce75bb038165920d8a8f33
AnaPerola <anaperolinha@hotmail.com>
2023-03-16 12:13:12 -0300

    Ajusta e cria cenarios de teste

--- spec/services/teacher_classroom_and_discipline_fetcher_spec.rb
+++ spec/services/teacher_classroom_and_discipline_fetcher_spec.rb
@@ -47,21 +47,78 @@ RSpec.describe TeacherClassroomAndDisciplineFetcher, type: :service do
         expect(fetch_classrooms_disciplines_grades[:classroom_grades]).to contain_exactly(classroom_grades)
       end
     end
+
     context 'when the required params are incorrect' do
-      it 'should return Error:Wrong number of arguments' do
-        expect { TeacherClassroomAndDisciplineFetcher.fetch!(unity, classroom.year) }.to
-        raise_error(ArgumentError, 'wrong number of arguments (given 2, expected 3)')
-
-        expect { TeacherClassroomAndDisciplineFetcher.fetch! }.to
-        raise_error(ArgumentError, 'wrong number of arguments (given 0, expected 3)')
-      end
-
-      it 'should return empty hashs' do
+      it 'should return empty hash if teacher teacher is invalid' do
         another_classroom = create(:classroom)
 
         expect(
           TeacherClassroomAndDisciplineFetcher.fetch!(1, another_classroom.unity, another_classroom.year)
         ).to eq(classrooms: [], disciplines: [], classroom_grades: [])
+      end
+
+      it 'should return empty hash if year is not equal to classroom' do
+        another_classroom = create(:classroom, year: 2015)
+        another_link = create(:teacher_discipline_classroom, year: 2015, classroom: classroom)
+
+        expect(
+          TeacherClassroomAndDisciplineFetcher.fetch!(another_link.teacher_id, another_classroom.unity, 2023)
+        ).to eq(classrooms: [], disciplines: [], classroom_grades: [])
+      end
+
+      it 'should return nil if teacher and/or unity is blank' do
+        another_classroom = create(:classroom, year: 2023)
+        another_link = create(:teacher_discipline_classroom, year: 2023, classroom: another_classroom)
+
+        expect(TeacherClassroomAndDisciplineFetcher.fetch!(nil, nil, 2023)).to be_nil
+        expect(TeacherClassroomAndDisciplineFetcher.fetch!(nil, another_classroom.unity, 2023)).to be_nil
+        expect(TeacherClassroomAndDisciplineFetcher.fetch!(another_link.teacher_id, nil, 2023)).to be_nil
+      end
+
+      it 'should return empty hash if unity has no link with teacher' do
+        another_classroom = create(:classroom, id: 22, year: 2023)
+        another_teacher = create(:teacher, id: 55)
+
+        create(:teacher_discipline_classroom, id: 104, year: 2023)
+
+        expect(
+          TeacherClassroomAndDisciplineFetcher.fetch!(another_teacher.id, another_classroom.unity, another_classroom.year)
+        ).to eq(classrooms: [], disciplines: [], classroom_grades: [])
+      end
+    end
+
+    context 'when teacher has no connection with the discipline' do
+      let(:another_classroom) { create(:classroom, id: 1577, year: 2023) }
+      let(:another_teacher) { create(:teacher, id: 1755) }
+      let(:another_discipline) { create(:discipline, id: 2566) }
+      let(:link_teacher) {
+        create(
+          :teacher_discipline_classroom,
+          id: 1245,
+          classroom: another_classroom,
+          teacher: another_teacher,
+          year: 2023
+        )
+      }
+
+      before do
+        link_teacher
+      end
+
+      subject(:fetch_classrooms_disciplines_grades) {
+        TeacherClassroomAndDisciplineFetcher.fetch!(
+          another_teacher.id,
+          another_classroom.unity,
+          another_classroom.year
+        )
+      }
+
+      it 'should return only hash with link of classroom' do
+        expect(fetch_classrooms_disciplines_grades[:classrooms]).to contain_exactly(another_classroom)
+      end
+
+      it 'should not return disciplines without linked' do
+        expect(fetch_classrooms_disciplines_grades[:disciplines]).not_to contain_exactly(another_discipline)
       end
     end
   end
