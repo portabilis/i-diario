fdf43eba640cf92234bab568a99e4b7a3915e2bc
AnaPerola <anaperolinha@hotmail.com>
2023-04-09 19:48:56 -0300

    Adiciona condicional alter para realizar o drop

--- db/migrate/20160510134619_remove_constraint_current_user_role_from_user.rb
+++ db/migrate/20160510134619_remove_constraint_current_user_role_from_user.rb
@@ -2,7 +2,7 @@ class RemoveConstraintCurrentUserRoleFromUser < ActiveRecord::Migration[4.2]
   def change
     execute <<-SQL
       DROP INDEX index_users_on_current_user_role_id;
-      ALTER TABLE users DROP CONSTRAINT users_current_user_role_id_fk;
+      ALTER TABLE users DROP CONSTRAINT IF EXISTS users_current_user_role_id_fk;
     SQL
   end
 end
