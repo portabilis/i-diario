791bcc14cc97a57387381f3e3a01e31fe6fbe31b
AnaPerola <anaperolinha@hotmail.com>
2023-04-11 09:12:30 -0300

    Permite a execução do worker apenas se há modificações

--- app/services/ieducar_synchronizers/students_synchronizer.rb
+++ app/services/ieducar_synchronizers/students_synchronizer.rb
@@ -3,7 +3,7 @@ class StudentsSynchronizer < BaseSynchronizer
     update_students(
       HashDecorator.new(
         api.fetch(
-          escola: unity_api_code
+          escola: '118'
         )['alunos']
       )
     )
@@ -16,6 +16,8 @@ class StudentsSynchronizer < BaseSynchronizer
   end
 
   def update_students(students)
+    allow_create_users_for_students = GeneralConfiguration.current.create_users_for_students_when_synchronize
+
     students.each do |student_record|
       next if student_record.nome_aluno.blank?
 
@@ -25,19 +27,20 @@ class StudentsSynchronizer < BaseSynchronizer
         student.avatar_url = student_record.foto_aluno
         student.birth_date = student_record.data_nascimento
         student.api = true
+
         student.uses_differentiated_exam_rule = false if student.uses_differentiated_exam_rule.nil?
         student.save! if student.changed?
+
+        create_users(student) if student.changed? && allow_create_users_for_students
 
         discarded = student_record.deleted_at.present?
 
         student.discard_or_undiscard(discarded)
       end
     end
-
-    create_users if GeneralConfiguration.current.create_users_for_students_when_synchronize
   end
 
-  def create_users
-    UserForStudentCreatorWorker.perform_in(1.second, entity_id)
+  def create_users(student)
+    UserForStudentCreatorWorker.perform_in(1.second, entity_id, student)
   end
 end
