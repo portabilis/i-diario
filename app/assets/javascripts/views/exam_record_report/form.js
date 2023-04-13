128e3e81ac9d68ad091be8f214416c3f7428b198
AnaPerola <anaperolinha@hotmail.com>
2023-04-13 18:08:31 -0300

    Cria funcoes para buscar a etapa da turma

--- app/assets/javascripts/views/exam_record_report/form.js
+++ app/assets/javascripts/views/exam_record_report/form.js
@@ -8,4 +8,35 @@ $(document).ready(function() {
       message = `O <b>Registros de avaliações numéricas</b> apresentará somente as notas lançadas nos diários de avaliação e recuperações numéricas. Para conferência de notas conceituais e/ou descritivas acessar, respectivamente, o <a href="${redirect_link_report_card}"><b>Boletim do professor</b></a> ou as telas de <a href="${redirect_link_conceptual_exams}"><b>Diário de avaliações conceituais</b></a> e <a href="${redirect_link_descriptive_exams}"><b>Avaliações descritivas</b></a>.`;
 
   flashMessages.info(message);
+
+  var $classroom = $('#exam_record_report_form_classroom_id');
+
+  $classroom.on('change', async function () {
+    await getStep();
+  });
+
+  async function getStep() {
+    let classroom_id = $classroom.select2('val');
+
+    if (!_.isEmpty(classroom_id)) {
+      return $.ajax({
+        url: Routes.fetch_step_exam_record_report_pt_br_path({
+          classroom_id: classroom_id,
+          format: 'json'
+        }),
+        success: handleFetchStepByClassroomSuccess,
+        error: handleFetchStepByClassroomError
+      });
+    }
+  }
+
+  function handleFetchStepByClassroomSuccess(data) {
+    let step = $('#exam_record_report_form_school_calendar_classroom_step_id');
+    console.log('data', data);
+    step.select2('val', data);
+  };
+
+  function handleFetchStepByClassroomError() {
+    flashMessages.error('Ocorreu um erro ao buscar a etapa da turma.');
+  };
 });
