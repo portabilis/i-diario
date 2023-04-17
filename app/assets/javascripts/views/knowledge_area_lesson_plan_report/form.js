9c6a1500e6246b87bbf31f64ea26d6891d32a70a
AnaPerola <anaperolinha@hotmail.com>
2023-04-16 21:52:41 -0300

    Corrige identação para melhor legibilidade

--- app/assets/javascripts/views/knowledge_area_lesson_plan_report/form.js
+++ app/assets/javascripts/views/knowledge_area_lesson_plan_report/form.js
@@ -3,13 +3,17 @@ $(function(){
 
   $('#lesson-plan-report').on('click', function(e){
     e.preventDefault();
-    $('#knowledge-area-lesson-plan-report-form').attr('action', Routes.knowledge_area_lesson_plan_report_pt_br_path());
+    $('#knowledge-area-lesson-plan-report-form').attr('action', 
+      Routes.knowledge_area_lesson_plan_report_pt_br_path()
+    );
     $('#knowledge-area-lesson-plan-report-form').submit();
   });
 
   $('#content-record-report').on('click', function(e){
     e.preventDefault();
-    $('#knowledge-area-lesson-plan-report-form').attr('action', Routes.knowledge_area_content_record_report_pt_br_path());
+    $('#knowledge-area-lesson-plan-report-form').attr('action', 
+      Routes.knowledge_area_content_record_report_pt_br_path()
+    );
     $('#knowledge-area-lesson-plan-report-form').submit();
   });
 });
