d097fd8021b72b81db20e628dc3c86d06b5d27d7
AnaPerola <anaperolinha@hotmail.com>
2023-04-24 14:40:28 -0300

    Ajusta ordem para realizar a migration sem os cascate

--- db/migrate/20220928111308_drop_pre_registrations_and_dependences.rb
+++ db/migrate/20220928111308_drop_pre_registrations_and_dependences.rb
@@ -1,9 +1,9 @@
 class DropPreRegistrationsAndDependences < ActiveRecord::Migration
-  # def update
-  #   drop_table :pre_registrations, { force: :cascade }
-  #   drop_table :pre_registration_unities, { force: :cascade }
-  #   drop_table :pre_registration_configs, { force: :cascade }
-  #   drop_table :pre_registration_availabilities, { force: :cascade }
-  #   drop_table :deficiencies_pre_registration, { force: :cascade }
-  # end
+  def change
+    drop_table :deficiencies_pre_registrations
+    drop_table :pre_registration_availabilities
+    drop_table :pre_registration_unities
+    drop_table :pre_registration_configs
+    drop_table :pre_registrations
+  end
 end
