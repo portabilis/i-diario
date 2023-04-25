9c6ad0b99a5406779831ed4f6a5ed0ca001436e3
AnaPerola <anaperolinha@hotmail.com>
2023-04-25 11:21:33 -0300

    Deleta migration que deve ser criada no i-Diário-plus

--- db/migrate/20220928111308_drop_pre_registrations_and_dependences.rb
+++
@@ -1,9 +0,0 @@
-class DropPreRegistrationsAndDependences < ActiveRecord::Migration
-  def change
-    drop_table :deficiencies_pre_registrations
-    drop_table :pre_registration_availabilities
-    drop_table :pre_registration_unities
-    drop_table :pre_registration_configs
-    drop_table :pre_registrations
-  end
-end
