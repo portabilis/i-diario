8690510d5805970cf182833bead4909f55381939
AnaPerola <anaperolinha@hotmail.com>
2023-05-08 10:20:00 -0300

    Cria função para buscar disciplinas de acordo com a turma

--- app/assets/javascripts/views/daily_frequencies_in_batchs/new.js
+++ app/assets/javascripts/views/daily_frequencies_in_batchs/new.js
@@ -2,4 +2,43 @@ $(document).ready( function() {
   let beta_title = 'Este recurso ainda está em processo de desenvolvimento e pode apresentar problemas'
   let img_src = $('#image-beta').attr('src');
   $('.fa-check-square-o').closest('h2').after(`<img src="${img_src}" class="beta-badge" style="margin-bottom: 9px; margin-left: 5px" title="${beta_title}">`);
+
+  window.disciplines = [];
+
+  var flashMessages = new FlashMessages(),
+      $classroom = $("#frequency_in_batch_form_classroom_id"),
+      $discipline = $("#frequency_in_batch_form_discipline_id");
+
+  var fetchDisciplines = function (params, callback) {
+    if (_.isEmpty(window.disciplines)) {
+      $.getJSON('/disciplinas?' + $.param(params)).always(function (data) {
+        window.disciplines = data;
+        callback(window.disciplines);
+      });
+    } else {
+      callback(window.disciplines);
+    }
+  };
+
+  $classroom.on('change', function (e) {
+    var params = {
+      classroom_id: e.val
+    };
+
+    window.disciplines = [];
+    $discipline.val('').select2({ data: [] });
+
+    if (!_.isEmpty(e.val)) {
+      fetchDisciplines(params, function (disciplines) {
+        var selectedDisciplines = _.map(disciplines, function (discipline) {
+          return { id:discipline['id'], text: discipline['description'] };
+        });
+
+        $discipline.select2({
+          data: selectedDisciplines
+        });
+      });
+    }
+  });
+
 })
