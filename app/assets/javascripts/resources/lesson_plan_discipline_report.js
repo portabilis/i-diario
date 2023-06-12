e5a64f3de84cee16e43ddfaa558361420b4a377b
AnaPerola <anaperolinha@hotmail.com>
2023-06-12 11:00:12 -0300

    Remove código não mais utilizado

--- app/assets/javascripts/resources/lesson_plan_discipline_report.js
+++ app/assets/javascripts/resources/lesson_plan_discipline_report.js
@@ -3,19 +3,8 @@ $(function () {
   window.disciplines = [];
 
   var $hideWhenGlobalAbsence = $(".hide-when-global-absence"),
-      $globalAbsence = $("#discipline_lesson_plan_report_form_global_absence"),
-      $examRuleNotFoundAlert = $('#exam-rule-not-found-alert')
-
-  var fetchClassrooms = function (params, callback) {
-    if (_.isEmpty(window.classrooms)) {
-      $.getJSON(Routes.classrooms_pt_br_path(params)).always(function (data) {
-        window.classrooms = data;
-        callback(window.classrooms);
-      });
-    } else {
-      callback(window.classrooms);
-    }
-  };
+    $globalAbsence = $("#discipline_lesson_plan_report_form_global_absence"),
+    $examRuleNotFoundAlert = $('#exam-rule-not-found-alert')
 
   var fetchDisciplines = function (params, callback) {
     if (_.isEmpty(window.disciplines)) {
@@ -34,52 +23,23 @@ $(function () {
     });
   };
 
-  var $unity = $('#discipline_lesson_plan_report_form_unity_id');
   var $classroom = $('#discipline_lesson_plan_report_form_classroom_id');
   var $discipline = $('#discipline_lesson_plan_report_form_discipline_id');
 
-  $unity.on('change', function (e) {
-    var params = {
-      filter: {
-        by_unity: e.val
-      },
-      find_by_current_teacher: true
-    };
-
-    window.classrooms = [];
-    window.disciplines = [];
-
-    $classroom.val('').select2({ data: [] });
-    $discipline.val('').select2({ data: [] });
-    $('#discipline_lesson_plan_report_form_class_numbers').select2("val", "")
-
-    if (!_.isEmpty(e.val)) {
-      fetchClassrooms(params, function (classrooms) {
-        var selectedClassrooms = _.map(classrooms, function (classroom) {
-          return { id:classroom['id'], text: classroom['description'] };
-        });
-
-        $classroom.select2({
-          data: selectedClassrooms
-        });
-      });
-    }
-  });
-
-  var checkExamRule = function(params){
-    fetchExamRule(params, function(exam_rule){
+  var checkExamRule = function (params) {
+    fetchExamRule(params, function (exam_rule) {
       $('form input[type=submit]').removeClass('disabled');
-      if(!$.isEmptyObject(exam_rule)){
+      if (!$.isEmptyObject(exam_rule)) {
         $examRuleNotFoundAlert.addClass('hidden');
 
-        if(exam_rule.frequency_type == 1){
+        if (exam_rule.frequency_type == 1) {
           $globalAbsence.val(1);
           $hideWhenGlobalAbsence.hide();
-        }else{
+        } else {
           $globalAbsence.val(0);
           $hideWhenGlobalAbsence.show();
         }
-      }else{
+      } else {
         $globalAbsence.val(0);
         $hideWhenGlobalAbsence.hide();
 
@@ -105,7 +65,7 @@ $(function () {
 
       fetchDisciplines(params, function (disciplines) {
         var selectedDisciplines = _.map(disciplines, function (discipline) {
-          return { id:discipline['id'], text: discipline['description'] };
+          return { id: discipline['id'], text: discipline['description'] };
         });
 
         $discipline.select2({
@@ -115,7 +75,7 @@ $(function () {
     }
   });
 
-  if ($classroom.length && $classroom.val().length){
-    checkExamRule({classroom_id: $classroom.val()});
+  if ($classroom.length && $classroom.val().length) {
+    checkExamRule({ classroom_id: $classroom.val() });
   }
 });
