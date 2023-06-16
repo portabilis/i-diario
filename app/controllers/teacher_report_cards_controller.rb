8b8540a20d6b73ddbe7b46a8c8f9197da6b203b5
AnaPerola <anaperolinha@hotmail.com>
2023-06-16 11:14:37 -0300

    Adiciona valores default nas variaveis para o form

--- app/controllers/teacher_report_cards_controller.rb
+++ app/controllers/teacher_report_cards_controller.rb
@@ -2,11 +2,18 @@ class TeacherReportCardsController < ApplicationController
   before_action :require_current_teacher
 
   def form
-    @teacher_report_card_form = TeacherReportCardForm.new(unity_id: current_unity.id)
+    @teacher_report_card_form = TeacherReportCardForm.new(
+      unity_id: current_unity.id,
+      classroom_id: current_user_classroom.id,
+      grade_id: current_user_classroom.classrooms_grades.pluck(:grade_id),
+      discipline_id: current_user_discipline.id
+    )
     @teacher_report_card_form.status = TeacherReportCardStatus::ALL
-    authorize(TeacherReportCard, :show?)
 
+    grades
     fetch_linked_by_teacher unless current_user.current_role_is_admin_or_employee?
+
+    authorize(TeacherReportCard, :show?)
   end
 
   def report
@@ -78,7 +85,6 @@ class TeacherReportCardsController < ApplicationController
   def grades
     @grades ||= current_user_classroom.classrooms_grades.map(&:grade)
   end
-  helper_method :grades
 
   def resource_params
     params.require(:teacher_report_card_form).permit(
@@ -90,6 +96,5 @@ class TeacherReportCardsController < ApplicationController
     @fetch_linked_by_teacher ||= TeacherClassroomAndDisciplineFetcher.fetch!(current_teacher.id, current_unity, current_school_year)
     @disciplines = @fetch_linked_by_teacher[:disciplines]
     @classrooms = @fetch_linked_by_teacher[:classrooms]
-    @grades = @fetch_linked_by_teacher[:classroom_grades].map(&:grade)
   end
 end
