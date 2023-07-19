42e4238852227c9eccd6b016a84f17704b757909
tiagocamargo <tiago.camargo@portabilis.com.br>
2023-07-19 18:23:53 -0300

    Trata redis quando não for sentinel

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,15 +1,21 @@
-config_redis = {
+if (Rails.application.secrets[:REDIS_MODE] == 'sentinel')
+  config_redis = {
     url: "#{Rails.application.secrets[:REDIS_URL]}#{Rails.application.secrets[:REDIS_DB_SIDEKIQ]}",
     role: "master",
     sentinels: Rails.application.secrets[:REDIS_SENTINELS].split(";").map { |host| { host: host,  port: 26379 }}
   }
+else
+  config_redis = {
+    url: Rails.application.secrets[:REDIS_URL])
+  }
+}
 
 $REDIS_DB = Redis.new(config_redis)
 
 Sidekiq.configure_server do |config|
-    config.redis = config_redis
+  config.redis = config_redis
 end
 
 Sidekiq.configure_client do |config|
-    config.redis = config_redis
+  config.redis = config_redis
 end
