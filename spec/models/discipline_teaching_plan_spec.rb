77d45d1630e2293a087250e489d395a81174793a
Greice Felipe <greice@portabilis.com.br>
2023-08-03 15:49:43 -0300

    Remove teste de Plano de Ensino no mesmo período.

--- spec/models/discipline_teaching_plan_spec.rb
+++ spec/models/discipline_teaching_plan_spec.rb
@@ -15,18 +15,5 @@ RSpec.describe DisciplineTeachingPlan, type: :model do
   describe 'validations' do
     it { expect(subject).to validate_presence_of(:teaching_plan) }
     it { expect(subject).to validate_presence_of(:discipline) }
-
-    it 'should validate uniqueness of discipline teaching plan' do
-      other_teaching_plan = create(:discipline_teaching_plan, :with_teacher_discipline_classroom)
-
-      subject = build(
-        :discipline_teaching_plan,
-        teaching_plan: other_teaching_plan.teaching_plan,
-        discipline: other_teaching_plan.discipline
-      )
-
-      expect(subject).to_not be_valid
-      expect(subject.errors.messages[:base]).to include('Já existe um plano de ensino para o período informado')
-    end
   end
 end
