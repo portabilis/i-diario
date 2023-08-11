92d6e0006a84c9f9a1f8fe16bbe129d66910625c
AnaPerola <anaperolinha@hotmail.com>
2023-08-11 12:07:09 -0300

    Filtra apenas turmas e disciplinas de acordo com a regra de avaliação

--- app/controllers/conceptual_exams_controller.rb
+++ app/controllers/conceptual_exams_controller.rb
@@ -521,9 +521,8 @@ class ConceptualExamsController < ApplicationController
 
   def fetch_linked_by_teacher
     @fetch_linked_by_teacher ||= TeacherClassroomAndDisciplineFetcher.fetch!(current_teacher.id, current_unity, current_school_year)
-    @classrooms = @fetch_linked_by_teacher[:classrooms]
-    @disciplines = @fetch_linked_by_teacher[:disciplines]
-    @classroom_grades = @fetch_linked_by_teacher[:classroom_grades]
+    @classrooms = @fetch_linked_by_teacher[:classrooms].by_score_type(ScoreTypes::CONCEPT)
+    @disciplines = @fetch_linked_by_teacher[:disciplines].by_score_type(ScoreTypes::CONCEPT)
   end
 
   def allow_teacher_modify_prev_years
