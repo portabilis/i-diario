378ca181587cbc16d4ff97d809545e9b387f04b2
AnaPerola <anaperolinha@hotmail.com>
2023-08-30 19:01:20 -0300

    Recebe alunos como parametro e evita chamar outro servico já utilizado

--- app/services/exam_poster/student_only_with_recovery_fetcher.rb
+++ app/services/exam_poster/student_only_with_recovery_fetcher.rb
@@ -3,13 +3,14 @@ module ExamPoster
     attr_reader :recoveries
     attr_reader :scores
 
-    def initialize(teacher, classroom, discipline, step, school_term_recovery_diary_record)
+    def initialize(teacher, classroom, discipline, step, students_with_daily_note, school_term_recovery_diary_record)
       @teacher = teacher
       @classroom = classroom
       @discipline = discipline
       @step = step
       @recoveries = []
       @scores = []
+      @students_with_daily_note = students_with_daily_note
       @school_term_recovery_diary_record = school_term_recovery_diary_record
     end
 
@@ -20,21 +21,14 @@ module ExamPoster
 
     private
 
-    def fetch_school_term_recovery_score(classroom, discipline, step)
+    def fetch_school_term_recovery_score
       return unless @school_term_recovery_diary_record
 
-      student_ids = students_with_daily_note.map(&:id)
+      student_ids = @students_with_daily_note.map(&:id)
       student_recoveries = RecoveryDiaryRecordStudent.by_recovery_diary_record_id(
-        school_term_recovery_diary_record.recovery_diary_record_id
+        @school_term_recovery_diary_record.recovery_diary_record_id
       )
       student_recoveries.where.not(student_id: student_ids)
-    end
-
-    def students_with_daily_note
-      teacher_score_fetcher = TeacherScoresFetcher.new(@teacher, @classroom, @discipline, @step)
-      teacher_score_fetcher.fetch!
-
-      teacher_score_fetcher.scores
     end
   end
 end
