e83292543ba3d045499c514f148cecb9143498cb
AnaPerola <anaperolinha@hotmail.com>
2023-09-25 10:56:19 -0300

    Remove shift que estava removendo a série do objeto

--- app/validators/school_calendar_day_validator.rb
+++ app/validators/school_calendar_day_validator.rb
@@ -8,13 +8,12 @@ class SchoolCalendarDayValidator < ActiveModel::EachValidator
     school_day = true
     message = ''
 
-    loop do
-      grade_id = grade_ids&.shift
+    grade_ids.each do |grade_id|
       school_day = false unless record.school_calendar.day_allows_entry?(value, grade_id, classroom_id, discipline_id)
       step = record.school_calendar.steps.posting_date_after_and_before(value).first
       message = step ? I18n.t('errors.messages.not_school_calendar_day') : I18n.t('errors.messages.is_not_between_steps')
 
-      break if grade_ids.blank? || !school_day
+      break if !school_day
     end
 
     record.errors.add(attribute, message) unless school_day
