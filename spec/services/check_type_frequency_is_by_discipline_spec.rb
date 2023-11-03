16820e55b8718f001f808fd83c16643c98710510
AnaPerola <anaperolinha@hotmail.com>
2023-11-03 11:30:16 -0300

    Ajustando cenário de teste

--- spec/services/check_type_frequency_is_by_discipline_spec.rb
+++ spec/services/check_type_frequency_is_by_discipline_spec.rb
@@ -1,19 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe CheckTypeFrequencyByDisciplineService, type: :service do
-  context 'when the params are correct' do
+  let(:exam_rule) { create(:exam_rule) }
+  let(:classroom) { create(:classroom, exam_rule: exam_rule) }
+  let(:teacher_discipline_classroom) {
+    create(
+      :teacher_discipline_classroom,
+      allow_absence_by_discipline: 0,
+      classroom: classroom
+    )
+  }
+
+  describe 'when the params are correct' do
     subject(:frequency_by_discipline) {
       CheckTypeFrequencyByDisciplineService.call(
-        class_numbers, params, uinty, teacher, school_calendar, user
+        teacher_discipline_classroom.classroom_id, teacher_discipline_classroom.teacher
       )
     }
 
-    it 'is_expected' do
-      expect(frequency_by_discipline.size).to eq(3)
+    context 'if frequencia do professor for por falta por disciplina' do
+      before do
+        teacher_discipline_classroom.update(allow_absence_by_discipline: 1)
+      end
+
+      # it 'is_expected to return true' do
+      #   expect(frequency_by_discipline).to be_truthy
+      # end
     end
 
-    it 'is_expected' do
-      expect(frequency_by_discipline).to be_truthy
+    context 'if frequencia do professor for por falta por aula' do
+      it 'is_expected to return false' do
+        expect(frequency_by_discipline).to be_falsey
+      end
+    end
+  end
+
+  describe 'when the params are incorrect' do
+    subject(:frequency_by_discipline) {
+      CheckTypeFrequencyByDisciplineService.call(
+        nil, teacher_discipline_classroom.teacher_id
+      )
+    }
+
+    it 'Is expected returns nil if any parameter is nil or invalid' do
+      expect{CheckTypeFrequencyByDisciplineService.call(
+        nil, teacher_discipline_classroom.teacher_id
+      )}.to nil
     end
   end
 end
