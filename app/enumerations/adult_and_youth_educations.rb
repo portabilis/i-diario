6d01abcaeebceb8200dafdb4c20304a3e505d813
AnaPerola <anaperolinha@hotmail.com>
2023-11-13 16:20:16 -0300

    Cria enumerator para representar o valor de séries

---
+++ app/enumerations/adult_and_youth_educations.rb
@@ -0,0 +1,11 @@
+class AdultAndYouthEducations < EnumerateIt::Base
+  associate_values :first_year_EJA,
+                   :second_year_EJA,
+                   :third_year_EJA,
+                   :fourth_year_EJA,
+                   :fifth_year_EJA,
+                   :sixth_year_EJA,
+                   :seventh_year_EJA,
+                   :eighth_year_EJA,
+                   :ninth_year_EJA
+end
