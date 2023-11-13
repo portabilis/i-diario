721b04b2128d4b5414c29f3b7a13cc9e8cdb0a15
AnaPerola <anaperolinha@hotmail.com>
2023-11-13 16:55:59 -0300

    Ajusta enumerator e traduções

--- app/enumerations/adult_and_youth_educations.rb
+++ app/enumerations/adult_and_youth_educations.rb
@@ -1,11 +1,11 @@
 class AdultAndYouthEducations < EnumerateIt::Base
-  associate_values :first_year_EJA,
-                   :second_year_EJA,
-                   :third_year_EJA,
-                   :fourth_year_EJA,
-                   :fifth_year_EJA,
-                   :sixth_year_EJA,
-                   :seventh_year_EJA,
-                   :eighth_year_EJA,
-                   :ninth_year_EJA
+  associate_values :first_year,
+                   :second_year,
+                   :third_year,
+                   :fourth_year,
+                   :fifth_year,
+                   :sixth_year,
+                   :seventh_year,
+                   :eighth_year,
+                   :ninth_year
 end
