b9498bf50b8c69c651cf363ff048f169a028a7ce
AnaPerola <anaperolinha@hotmail.com>
2023-11-14 16:31:21 -0300

    Ajusta nome do serviço e cria cenários de teste

---
+++ app/services/list_grades_by_step_service.rb
@@ -0,0 +1,33 @@
+class ListGradesByStepService
+  def initialize(step)
+    @step = step
+  end
+
+  def self.call(step, to_json = true)
+    new(step).call(to_json)
+  end
+
+  def call(to_json = true)
+    grades = fetch_grades(to_json)
+    grades || group_children_education(to_json)
+  end
+
+  private
+
+  def fetch_grades(to_json)
+    case @step
+    when 'adult_and_youth_education'
+      to_json ? AdultAndYouthEducations.to_select : AdultAndYouthEducations.to_select(false)
+    when 'elementary_school'
+      to_json ? ElementaryEducations.to_select : ElementaryEducations.to_select(false)
+    when 'child_school'
+      to_json ? ChildEducations.to_select : ChildEducations.to_select(false)
+    end
+  end
+
+  def group_children_education(to_json)
+    return nil unless GeneralConfiguration.current.group_children_education
+
+    to_json ? GroupChildEducations.to_select : GroupChildEducations.to_select(false)
+  end
+end
