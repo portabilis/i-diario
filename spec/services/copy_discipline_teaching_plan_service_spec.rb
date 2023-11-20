2dc60d29bde0be10d68281fd0983ea9be9a83354
AnaPerola <anaperolinha@hotmail.com>
2023-11-20 19:50:20 -0300

    Ajusta cenário de teste

--- spec/services/copy_discipline_teaching_plan_service_spec.rb
+++ spec/services/copy_discipline_teaching_plan_service_spec.rb
@@ -4,48 +4,23 @@ RSpec.describe CopyDisciplineTeachingPlanService, type: :service do
   let(:user) { create(:user, :with_user_role_administrator) }
   let(:unity) { create(:unity) }
   # vinculo para o professor turmas diferentes, mesma disciplina, escola e ano
-  let(:classroom_2) { create(:classroom, unity: unity) }
-  let(:classroom_grades_2) {
-    create(
-      :classrooms_grade,
-      classroom: classroom_2,
-      grade: classroom_grades.grade
-    )
-  }
-  let!(:teacher_discipline_classroom_2) {
-    create(
-      :teacher_discipline_classroom,
-      teacher: current_teacher,
-      classroom: classroom_2,
-      discipline: discipline,
-      grade: classroom_grades.grade
-    )
-  }
-  # vinculo outro professor com outra turma, mesma disciplina e ano
-  let(:other_teacher) { create(:teacher) }
-  let(:other_classroom) { create(:classroom, unity: unity) }
-  let!(:other_classroom_grades) { create(:classrooms_grade, classroom: other_classroom, grade: classroom_grades.grade) }
-  let!(:teacher_discipline_classroom) {
-    create(
-      :teacher_discipline_classroom,
-      teacher: other_teacher,
-      classroom: other_classroom,
-      discipline: discipline,
-      grade: classroom_grades.grade
-    )
-  }
-  # vinculo para o professor com outra turma, outra série mesma disciplina e ano
-  let(:classroom_3) { create(:classroom, unity: unity) }
-  let!(:classroom_grades_3) { create(:classrooms_grade, classroom: classroom_3) }
-  let!(:teacher_discipline_classroom_3) {
-    create(
-      :teacher_discipline_classroom,
-      teacher: current_teacher,
-      classroom: classroom_3,
-      discipline: discipline,
-      grade: classroom_grades_3.grade
-    )
-  }
+  # let(:classroom_2) { create(:classroom, unity: unity) }
+  # let(:classroom_grades_2) {
+  #   create(
+  #     :classrooms_grade,
+  #     classroom: classroom_2,
+  #     grade: classroom_grades.grade
+  #   )
+  # }
+  # let!(:teacher_discipline_classroom_2) {
+  #   create(
+  #     :teacher_discipline_classroom,
+  #     teacher: current_teacher,
+  #     classroom: classroom_2,
+  #     discipline: discipline,
+  #     grade: classroom_grades.grade
+  #   )
+  # }
 
   let(:discipline) { create(:discipline) }
   let(:classroom) { create(:classroom, unity: unity) }
@@ -58,6 +33,7 @@ RSpec.describe CopyDisciplineTeachingPlanService, type: :service do
       :teaching_plan,
       :with_teacher_discipline_classroom,
       teacher: current_teacher,
+      classroom: classroom,
       unity: unity,
       discipline: discipline,
       year: classroom.year,
@@ -119,7 +95,7 @@ RSpec.describe CopyDisciplineTeachingPlanService, type: :service do
   describe '#copy_discipline_teaching_plan_worker' do
     subject(:copy_discipline_teaching_plan) {
       CopyDisciplineTeachingPlanService.call(
-        discipline_teaching_plan.id, classroom.year, [classroom.unity_id], [classroom_grades.grade_id, classroom_grades_3.grade_id]
+        discipline_teaching_plan.id, classroom.year, [classroom.unity_id], [classroom_grades.grade_id]
       )
     }
 
@@ -134,4 +110,108 @@ RSpec.describe CopyDisciplineTeachingPlanService, type: :service do
       end
     end
   end
+
+  describe 'when the copy is to be made for same grade' do
+    context 'with 2 teachers in different classrooms at the same unity' do
+      subject(:copy_discipline_teaching_plan) {
+        CopyDisciplineTeachingPlanService.call(
+          discipline_teaching_plan.id, classroom.year, [classroom.unity_id], [classroom_grades.grade_id]
+        )
+      }
+
+      it 'is expected returns 2 copies' do
+        other_teacher = create_setup_other_teacher_same_grade
+        expect(copy_discipline_teaching_plan.count).to eq(2)
+        expect(copy_discipline_teaching_plan.map(&:teaching_plan).map(&:teacher_id)).to eq(
+          [other_teacher.id, current_teacher.id]
+        )
+      end
+    end
+
+    context 'with 1 teacher in different classrooms and unities' do
+      let!(:other_unity) { create_setup_other_classroom_and_unity }
+
+      subject(:copy_discipline_teaching_plan) {
+        CopyDisciplineTeachingPlanService.call(
+          discipline_teaching_plan.id, classroom.year, [classroom.unity_id, other_unity.id], [classroom_grades.grade_id]
+        )
+      }
+
+      it 'is expected returns 2 copies' do
+        expect(copy_discipline_teaching_plan.count).to eq(2)
+        expect(copy_discipline_teaching_plan.map(&:teaching_plan).map(&:unity_id)).to eq(
+          [classroom.unity_id, other_unity.id]
+        )
+      end
+    end
+  end
+
+  describe 'when the copy is to be made for many grades' do
+    context 'with 1 teacher in different classrooms' do
+      let!(:other_grade) { create_setup_only_teacher_other_grade }
+
+      subject(:copy_discipline_teaching_plan) {
+        CopyDisciplineTeachingPlanService.call(
+          discipline_teaching_plan.id,
+          classroom.year,
+          [classroom.unity_id],
+          [classroom_grades.grade_id, other_grade.id]
+        )
+      }
+
+      it 'is expected returns 2 copies' do
+        expect(copy_discipline_teaching_plan.count).to eq(2)
+        expect(copy_discipline_teaching_plan.map(&:teaching_plan).map(&:grade_id)).to eq(
+          [other_grade.id, classroom_grades.grade_id]
+        )
+      end
+    end
+  end
+end
+
+# Cria vinculo para outro professor com outra turma mesma disciplina, mesma serie e ano
+def create_setup_other_teacher_same_grade
+  other_teacher = create(:teacher)
+  other_classroom = create(:classroom, unity: unity)
+  classroom_grades_with_grade = create(
+    :classrooms_grade,
+    classroom: other_classroom,
+    grade: classroom_grades.grade
+  )
+  create(
+    :teacher_discipline_classroom,
+    teacher: other_teacher,
+    classroom: other_classroom,
+    discipline: discipline,
+    grade: classroom_grades.grade
+  )
+  other_teacher
+end
+
+# Cria vinculo para o outro professor com outra turma e escola, mesma disciplina, mesma serie e ano
+def create_setup_other_classroom_and_unity
+  other_classroom = create(:classroom)
+  other_classroom_grades = create(:classrooms_grade, classroom: other_classroom)
+  create(
+    :teacher_discipline_classroom,
+    teacher: current_teacher,
+    classroom: other_classroom,
+    discipline: discipline,
+    grade: other_classroom_grades.grade
+  )
+  other_classroom.unity
+end
+
+# Cria vinculo para o mesmo professor com outra turma, mesma disciplina, outra série e ano
+def create_setup_only_teacher_other_grade
+  other_classroom = create(:classroom, unity: unity)
+  other_classroom_grades = create(:classrooms_grade, classroom: other_classroom)
+  create(
+    :teacher_discipline_classroom,
+    teacher: current_teacher,
+    classroom: other_classroom,
+    discipline: discipline,
+    grade: other_classroom_grades.grade
+  )
+  other_classroom_grades.grade
 end
