9022c0d4a831d01f507e364bfcf05d7e8db7e2a2
AnaPerola <anaperolinha@hotmail.com>
2023-12-07 09:39:35 -0300

    Ajusta nome do controller na rota e cria cenário de teste

--- spec/controllers/attendance_record_report_by_students_controller_spec.rb
+++ spec/controllers/attendance_record_report_by_students_controller_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 RSpec.describe AttendanceRecordReportByStudentsController, type: :controller do
+  let(:current_teacher) { create(:teacher) }
+  let(:unity) { create(:unity) }
+  let(:classroom) {
+    create(
+      :classroom,
+      :with_teacher_discipline_classroom,
+      :with_classroom_trimester_steps,
+      :score_type_numeric,
+      unity: unity,
+      teacher: current_teacher
+    )
+  }
 
+  before do
+    allow(controller).to receive(:current_classroom).and_return(classroom)
+    allow(controller).to receive(:current_teacher).and_return(current_teacher)
+  end
+
+  describe 'GET #form' do
+    it 'returns http success' do
+      get :form
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
