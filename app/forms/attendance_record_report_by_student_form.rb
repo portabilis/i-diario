aab9eba8e69ffbd15cfa845217d9f145c53a718b
AnaPerola <anaperolinha@hotmail.com>
2023-12-08 10:23:18 -0300

    Ajusta nome da classe e aplica validações

--- app/forms/attendance_record_report_by_student_form.rb
+++ app/forms/attendance_record_report_by_student_form.rb
@@ -1,3 +1,20 @@
-class AttendanceRecordReportbyStudentForm
+class AttendanceRecordReportByStudentForm
   include ActiveModel::Model
+
+  attr_accessor :unity_id,
+                :classroom_id,
+                :period,
+                :start_at,
+                :end_at,
+                :school_calendar_year
+
+  validates :start_at, presence: true, date: true, timeliness: {
+    on_or_before: :end_at, type: :date, on_or_before_message: I18n.t('errors.messages.on_or_before_message')
+  }
+  validates :end_at, presence: true, date: true, timeliness: {
+    on_or_after: :start_at, type: :date, on_or_after_message: I18n.t('errors.messages.on_or_after_message')
+  }
+  validates :unity_id, presence: true
+  validates :classroom_id, presence: true
+  validates :period, presence: true
 end
