3324b9802b8acf2fff8c6863e5cbc013a5dad8e3
AnaPerola <anaperolinha@hotmail.com>
2023-12-10 15:16:48 -0300

    Cria action pra gerar relatorio

--- app/controllers/attendance_record_report_by_students_controller.rb
+++ app/controllers/attendance_record_report_by_students_controller.rb
@@ -15,7 +15,44 @@ class AttendanceRecordReportByStudentsController < ApplicationController
     set_options_by_user
   end
 
+  def report
+    @attendance_record_report_by_student_form = AttendanceRecordReportByStudentForm.new(resource_params)
+    @attendance_record_report_by_student_form.fetch_daily_frequencies
+
+    if @attendance_record_report_by_student_form.valid?
+      # attendance_record_report_by_student_form = AttendanceRecordReportByStudent.build(
+      #   current_entity_configuration,
+      #   current_teacher,
+      #   current_user_school_year,
+      #   @attendance_record_report_by_student_form.start_at,
+      #   @attendance_record_report_by_student_form.end_at,
+      #   @attendance_record_report_by_student_form.fetch_daily_frequencies,
+      #   @attendance_record_report_by_student_form.enrollment_classrooms_list,
+      #   @attendance_record_report_by_student_form.students_frequencies_percentage,
+      #   current_user,
+      #   resource_params[:classroom_id]
+      # )
+    else
+      @attendance_record_report_by_student_form.school_calendar_year = current_school_year
+
+      set_options_by_user
+      clear_invalid_dates
+      render :form
+    end
+  end
+
   private
+
+  def clear_invalid_dates
+    @attendance_record_report_form.start_at = parse_date(resource_params[:start_at])
+    @attendance_record_report_form.end_at = parse_date(resource_params[:end_at])
+  end
+
+  def parse_date(date_string)
+    Date.parse(date_string)
+  rescue ArgumentError
+    ''
+  end
 
   def set_options_by_user
     @admin_or_teacher ||= current_user.current_role_is_admin_or_employee?
@@ -35,5 +72,17 @@ class AttendanceRecordReportByStudentsController < ApplicationController
       current_user_classroom.id,
       nil
     ).teacher_period
+  end
+
+  def resource_params
+    params.require(:attendance_record_report_by_student_form).permit(
+      :unity_id,
+      :classroom_id,
+      :period,
+      :start_at,
+      :end_at,
+      :school_calendar_year,
+      :current_teacher_id
+    )
   end
 end
