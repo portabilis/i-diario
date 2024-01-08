17b41fd5be9176965894a7be49e2dc5e13a16300
AnaPerola <anaperolinha@hotmail.com>
2024-01-08 11:18:07 -0300

    Cria métodos para verificações

--- app/forms/attendance_record_report_by_student_form.rb
+++ app/forms/attendance_record_report_by_student_form.rb
@@ -69,6 +69,88 @@ class AttendanceRecordReportByStudentForm
       .by_period(adjusted_period)
       .distinct
       .order('classrooms_grades.classroom_id')
+    return unless unity_id.present?
+
+    unity ||= Unity.find(unity_id)
+  end
+
+  def current_user
+    return unless current_user_id.present?
+
+    user ||= User.find(current_user_id)
+  end
+
+  def set_grades
+    return unless classroom_id.eql?('all')
+
+    classroom_ids = set_all_classrooms
+    grades ||= ClassroomsGrade.includes(:grade)
+                              .by_classroom_id(classroom_ids)
+                              .map(&:grade)
+                              .uniq
+  end
+
+  def set_all_classrooms
+    return classroom_id unless classroom_id.eql?('all')
+    return Classroom.by_unity(unity_id).distinct unless current_user.teacher?
+
+    Classroom.by_unity_and_teacher(unity_id, current_user.teacher_id).distinct
+  end
+
+  def fetch_daily_frequencies
+    classroom_id = set_all_classrooms
+
+    @daily_frequencies ||= DailyFrequencyQuery.call(
+      classroom_id: classroom_id.map(&:id),
+      period: period,
+      frequency_date: start_at..end_at,
+      all_students_frequencies: true
+    ).order(:owner_teacher_id)
+  end
+
+  def enrollment_classrooms_list
+    adjusted_period = period != Periods::FULL ? period : nil
+    classroom_id = set_all_classrooms
+
+    @enrollment_classrooms_list ||= StudentEnrollmentClassroomsRetriever.call(
+      classrooms: classroom_id,
+      disciplines: nil,
+      start_at: start_at,
+      end_at: end_at,
+      search_type: :by_date_range,
+      show_inactive: false,
+      period: adjusted_period
+    )
+  end
+
+  def students_frequencies_percentage
+    percentage_by_student = Hash.new(0)
+    daily_frequency_by_student = Hash.new { |hash, key| hash[key] = Set.new }
+
+    fetch_daily_frequencies.flat_map(&:students).each do |daily_frequency_student|
+      student_id = daily_frequency_student.student_id
+      frequency_date = daily_frequency_student.daily_frequency.frequency_date
+
+      next if daily_frequency_by_student[student_id].include?(frequency_date)
+
+      percentage_by_student[student_id] ||= 0
+      percentage_by_student[student_id] += 1 unless daily_frequency_student.present?
+
+      daily_frequency_by_student[student_id].add(frequency_date)
+    end
+
+    total_school_days = UnitySchoolDay.by_unity_id(unity_id)
+                                      .by_year(school_calendar_year)
+                                      .by_date_between(start_at, end_at)
+                                      .count
+
+    percentage_by_student.each do |student_id, infrequency|
+      multiplication = (total_school_days - infrequency) * 100
+      result = multiplication / total_school_days
+      percentage_by_student[student_id] = result.negative? ? 0.to_s + '%' : result.to_s + '%'
+    end
+
+    percentage_by_student
   end
 
   # def students_frequencies_percentage
