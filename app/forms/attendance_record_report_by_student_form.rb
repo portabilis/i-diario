5cc1140f14ba9b26c39cea5269e1105d4645f502
AnaPerola <anaperolinha@hotmail.com>
2024-01-10 12:46:27 -0300

    Utiliza retorno do calulo para renderizar na view

--- app/forms/attendance_record_report_by_student_form.rb
+++ app/forms/attendance_record_report_by_student_form.rb
@@ -76,7 +76,9 @@ class AttendanceRecordReportByStudentForm
   def students_by_classrooms
     select_all_classrooms.map do |classroom|
       students = enrollment_classrooms_list.select{ |student| student[:classroom_id].eql?(classroom.id) }
-      # frequencies_by_classroom = calculate_percentage_of_presence.select{ |student| student[:classroom].eql?(classroom.id)}
+      frequencies_by_classroom = calculate_percentage_of_presence(classroom.id).select do |student|
+        student[:classroom].eql?(classroom.id)
+      end.first
 
       next if students.empty?
 
@@ -88,16 +90,19 @@ class AttendanceRecordReportByStudentForm
             {
               student_id: student[:student_id],
               student_name: student[:student_name],
-              sequence: student[:sequence]
-              # frequency:
+              sequence: student[:sequence],
+              frequency: frequencies_by_classroom[:students].select do |frequency_by_student|
+                frequency_by_student[:percentage_frequency] if frequency_by_student[:student_id] == student[:student_id]
+              end.first
             }
           end
         }
       }
     end.compact.reduce(&:merge)
+
   end
 
-  def fetch_daily_frequencies(student_id, classroom_id)
+  def fetch_daily_frequencies(classroom_id)
     @daily_frequencies_by_classroom ||= DailyFrequencyQuery.call(
       classroom_id: classroom_id,
       period: adjusted_period,
@@ -106,8 +111,8 @@ class AttendanceRecordReportByStudentForm
     ).order(:classroom_id).group_by(&:classroom_id)
   end
 
-  def calculate_percentage_of_presence
-    @daily_frequencies_by_classroom.map do |classroom_id, daily_frequencies|
+  def calculate_percentage_of_presence(classroom_id)
+    fetch_daily_frequencies(classroom_id).map do |classroom_id, daily_frequencies|
       {
         classroom: classroom_id,
         students: daily_frequencies.flat_map do |daily_frequency|
@@ -115,11 +120,11 @@ class AttendanceRecordReportByStudentForm
         end.group_by(&:student_id).map do |key, daily_frequency_student|
           total_daily_frequency_students = daily_frequency_student.count.to_f
           total_presence = daily_frequency_student.map { |dfs| dfs if dfs.present }.compact.count.to_f
-          frequency = ((total_presence / total_daily_frequency_students) * 100).round(2)
+          percentage_frequency = ((total_presence / total_daily_frequency_students) * 100).round(2)
 
           {
             student_id: daily_frequency_student.first.student_id,
-            frequency: frequency
+            percentage_frequency: percentage_frequency
           }
         end
       }
