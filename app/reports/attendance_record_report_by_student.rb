1ff4a700ff9f1def4ba1fc79fd09b7e147a4b64e
AnaPerola <anaperolinha@hotmail.com>
2024-01-12 10:51:40 -0300

    Prepara nova classe para criar e organizar o conteudo do relatorio

--- app/reports/attendance_record_report_by_student.rb
+++ app/reports/attendance_record_report_by_student.rb
@@ -1,57 +1,82 @@
 class AttendanceRecordReportByStudent < BaseReport
-
-  attr_accessor :start_at, :end_at, :daily_frequencies, :enrollment_classrooms_list,
-
   def self.build(
-    start_at,
-    end_at,
-    daily_frequencies,
-    enrollment_classrooms_list,
-    students_frequencies_percentage,
-    entity_configuration,
-    teacher,
-    current_user,
-    classroom_id
+    select_all_classrooms,
+    enrollment_classrooms_list
   )
     new(:landscape).build(
-      start_at,
-      end_at,
-      daily_frequencies,
-      enrollment_classrooms_list,
-      students_frequencies_percentage,
-      entity_configuration,
-      teacher,
-      current_user,
-      classroom_id
+      select_all_classrooms,
+      enrollment_classrooms_list
     )
   end
 
   def build(
-    start_at,
-    end_at,
-    daily_frequencies,
-    enrollment_classrooms_list,
-    students_frequencies_percentage,
-    entity_configuration,
-    teacher,
-    current_user,
-    classroom_id
+    select_all_classrooms,
+    enrollment_classrooms_list
   )
-    @start_at = start_at
-    @end_at = end_at
-    @daily_frequencies = daily_frequencies
-    @enrollment_classrooms_list = enrollment_classrooms_list
-    @students_frequencies_percentage = students_frequencies_percentage
-    @entity_configuration = entity_configuration
-    @teacher = set_teacher(teacher, classroom_id, current_user)
-  end
 
   private
 
-  def set_teacher(teacher, classroom_id, current_user)
-    return teacher unless current_user.current_role_is_admin_or_employee?
-    return teacher if teacher.daily_frequencies.where(classroom_id: classroom_id).any?
+  def students_by_classrooms
+    select_all_classrooms.map do |classroom|
+      students = enrollment_classrooms_list.select{ |student| student[:classroom_id].eql?(classroom.id) }
+      frequencies_by_classroom = calculate_percentage_of_presence.select do |student|
+        student[:classroom].eql?(classroom.id)
+      end.first
 
-    Classroom.find(classroom_id).teacher_discipline_classrooms.first.teacher
+      next if frequencies_by_classroom.blank?
+      next if students.empty?
+
+      {
+        classroom.id => {
+          classroom_name: classroom.description,
+          grade_name: classroom.grades.first.description,
+          students: students.map do |student|
+            {
+              student_id: student[:student_id],
+              student_name: student[:student_name],
+              sequence: student[:sequence],
+              frequency: frequencies_by_classroom[:students].select do |frequency_by_student|
+                frequency_by_student[:percentage_frequency] if frequency_by_student[:student_id] == student[:student_id]
+              end.first
+            }
+          end
+        }
+      }
+    end.compact.reduce(&:merge)
+  end
+
+  def fetch_daily_frequencies
+    classrooms = select_all_classrooms
+
+    @daily_frequencies_by_classroom ||= DailyFrequencyQuery.call(
+      classroom_id: classroom_id.eql?('all') ? classrooms.map(&:id) : classroom_id,
+      period: adjusted_period,
+      frequency_date: start_at..end_at,
+      all_students_frequencies: true
+    ).order(:classroom_id).group_by(&:classroom_id)
+  end
+
+  def calculate_percentage_of_presence
+    daily_frequencies = fetch_daily_frequencies
+
+    return if daily_frequencies.blank?
+
+    daily_frequencies.map do |classroom_id, daily_frequencies|
+      {
+        classroom: classroom_id,
+        students: daily_frequencies.flat_map do |daily_frequency|
+          daily_frequency.students
+        end.group_by(&:student_id).map do |key, daily_frequency_student|
+          total_daily_frequency_students = daily_frequency_student.count.to_f
+          total_presence = daily_frequency_student.map { |dfs| dfs if dfs.present }.compact.count.to_f
+          percentage_frequency = ((total_presence / total_daily_frequency_students) * 100).round(2)
+
+          {
+            student_id: daily_frequency_student.first.student_id,
+            percentage_frequency: percentage_frequency
+          }
+        end
+      }
+    end
   end
 end
