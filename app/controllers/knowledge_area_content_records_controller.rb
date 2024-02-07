461b9bcaad0ed8ef63baa334569a4e133bc7f171
AnaPerola <anaperolinha@hotmail.com>
2024-02-07 16:16:12 -0300

    Insere before_action para validar turma antes de acessar as actions

--- app/controllers/knowledge_area_content_records_controller.rb
+++ app/controllers/knowledge_area_content_records_controller.rb
@@ -3,6 +3,7 @@ class KnowledgeAreaContentRecordsController < ApplicationController
   has_scope :per, default: 10
 
   before_action :require_current_teacher
+  before_action :require_current_classroom
   before_action :require_allow_to_modify_prev_years, only: [:create, :update, :destroy, :clone]
 
   def index
