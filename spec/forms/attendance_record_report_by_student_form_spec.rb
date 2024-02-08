02dad5386c49e5289fe65d2d429f92833bd1205b
AnaPerola <anaperolinha@hotmail.com>
2024-02-08 15:01:08 -0300

    Corrige nomeclatura do matcher

--- spec/forms/attendance_record_report_by_student_form_spec.rb
+++ spec/forms/attendance_record_report_by_student_form_spec.rb
@@ -8,14 +8,14 @@ RSpec.describe AttendanceRecordReportByStudentForm, type: :model do
   let(:current_user_admin) { create(:user, :with_user_role_administrator) }
 
   describe 'validations' do
-    it { is_expect.to validate_presence_of(:school_calendar_year) }
-    it { is_expect.to validate_presence_of(:school_calendar) }
-    it { is_expect.to validate_presence_of(:current_user_id) }
-    it { is_expect.to validate_presence_of(:classroom_id) }
-    it { is_expect.to validate_presence_of(:unity_id) }
-    it { is_expect.to validate_presence_of(:period) }
-    it { is_expect.to validate_presence_of(:start_at) }
-    it { is_expect.to validate_presence_of(:end_at) }
+    it { is_expected.to validate_presence_of(:school_calendar_year) }
+    it { is_expected.to validate_presence_of(:school_calendar) }
+    it { is_expected.to validate_presence_of(:current_user_id) }
+    it { is_expected.to validate_presence_of(:classroom_id) }
+    it { is_expected.to validate_presence_of(:unity_id) }
+    it { is_expected.to validate_presence_of(:period) }
+    it { is_expected.to validate_presence_of(:start_at) }
+    it { is_expected.to validate_presence_of(:end_at) }
 
     context 'when end_at is lower than start_at' do
       subject(:attendance_record_report_by_student_form) {
@@ -33,7 +33,7 @@ RSpec.describe AttendanceRecordReportByStudentForm, type: :model do
         subject.start_at = '2023-02-02'
         subject.end_at = '2023-01-01'
 
-        is_expect.to_not be_valid
+        is_expected.to_not be_valid
         expect(subject.errors.messages[:start_at]).to include('não pode ser maior que a Data final')
         expect(subject.errors.messages[:end_at]).to include('deve ser maior ou igual a Data inicial')
       end
