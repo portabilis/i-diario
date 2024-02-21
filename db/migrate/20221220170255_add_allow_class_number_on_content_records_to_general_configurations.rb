f9cb4ecce42d5cc7113baadbb175a5f66c7d37e0
AnaPerola <anaperolinha@hotmail.com>
2024-02-21 15:05:36 -0300

    Refatoração das migrações para serem reversíveis adequadamente

--- db/migrate/20221220170255_add_allow_class_number_on_content_records_to_general_configurations.rb
+++ db/migrate/20221220170255_add_allow_class_number_on_content_records_to_general_configurations.rb
@@ -1,5 +1,9 @@
 class AddAllowClassNumberOnContentRecordsToGeneralConfigurations < ActiveRecord::Migration
-  def change
+  def up
     add_column :general_configurations, :allow_class_number_on_content_records, :boolean, default: false
+  end
+
+  def down
+    remove_column :general_configurations, :allow_class_number_on_content_records
   end
 end
--- db/migrate/20230503013503_add_class_number_to_discipline_content_record.rb
+++ db/migrate/20230503013503_add_class_number_to_discipline_content_record.rb
@@ -1,5 +1,9 @@
 class AddClassNumberToDisciplineContentRecord < ActiveRecord::Migration
-  def change
+  def up
     add_column :discipline_content_records, :class_number, :integer, default: 0
+  end
+
+  def down
+    remove_column :discipline_content_records, :class_number
   end
 end
