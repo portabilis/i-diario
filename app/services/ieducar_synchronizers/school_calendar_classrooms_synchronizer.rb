cc15f64ac9edf4731a8eae169ae46cd515a63610
AnaPerola <anaperolinha@hotmail.com>
2024-03-06 12:20:21 -0300

    Permite a alteração de calendario letivo na turma

--- app/services/ieducar_synchronizers/school_calendar_classrooms_synchronizer.rb
+++ app/services/ieducar_synchronizers/school_calendar_classrooms_synchronizer.rb
@@ -41,10 +41,10 @@ class SchoolCalendarClassroomsSynchronizer < BaseSynchronizer
 
         begin
           SchoolCalendarClassroom.find_or_initialize_by(
-            classroom_id: classroom_id,
-            school_calendar_id: school_calendar.id
+            classroom_id: classroom_id
           ).tap do |school_calendar_classroom|
             school_calendar_classroom.step_type_description = school_calendar_classroom_record.descricao
+            school_calendar_classroom.school_calendar_id = school_calendar.id
 
             school_calendar_classroom.save! if school_calendar_classroom.changed?
 
