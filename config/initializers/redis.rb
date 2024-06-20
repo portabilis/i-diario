4e601dfb8401cace3e6f2019e1237e97b60add0f
AnaPerola <anaperolinha@hotmail.com>
2024-06-20 17:35:27 -0300

    Evita configuração de redis para ambiente de desenvolvimento

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,21 +1,46 @@
-if (Rails.application.secrets[:REDIS_MODE] == 'sentinel')
-  config_redis = {
-    url: "#{Rails.application.secrets[:REDIS_URL]}#{Rails.application.secrets[:REDIS_DB_SIDEKIQ]}",
-    role: "master",
-    sentinels: Rails.application.secrets[:REDIS_SENTINELS].split(";").map { |host| { host: host,  port: 26379 }}
-  }
+def redis_dev
+  $REDIS_DB = nil
+
+  Sidekiq.configure_server do |config|
+    config.redis = nil
+  end
+end
+
+def configure_redis
+  redis_url = Rails.application.secrets[:REDIS_URL]
+  redis_db_sidekiq = Rails.application.secrets[:REDIS_DB_SIDEKIQ]
+
+  if redis_url.blank? || redis_db_sidekiq.blank?
+    raise "Redis URL or DB sidekiq is not set in secrets"
+  end
+
+  if Rails.application.secrets[:REDIS_MODE] == 'sentinel'
+    redis_sentinels = Rails.application.secrets[:REDIS_SENTINELS]
+
+    if redis_sentinels.blank?
+      raise "Redis sentinels are not set in secrets"
+    end
+
+    config_redis = {
+      url: "#{redis_url}#{redis_db_sidekiq}",
+      role: "master",
+      sentinels: redis_sentinels.split(";").map { |host| { host: host, port: 26379 }}
+    }
+  else
+    config_redis = {
+      url: "#{redis_url}#{redis_db_sidekiq}"
+    }
+  end
+
+  $REDIS_DB = Redis.new(config_redis)
+
+  Sidekiq.configure_server do |config|
+    config.redis = config_redis
+  end
+end
+
+if Rails.env.test? || Rails.env.development?
+  redis_dev
 else
-  config_redis = {
-    url: "#{Rails.application.secrets[:REDIS_URL]}#{Rails.application.secrets[:REDIS_DB_SIDEKIQ]}"
-  }
-end
-
-$REDIS_DB = Redis.new(config_redis)
-
-Sidekiq.configure_server do |config|
-  config.redis = config_redis
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = config_redis
+  configure_redis
 end
