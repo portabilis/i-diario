40c083a24bb7f21ae770b9fbe567a9d875ef550d
AnaPerola <anaperolinha@hotmail.com>
2024-06-26 11:37:48 -0300

    Filtra apenas as escolas que tiverem planos de ensino

--- app/controllers/api/v2/teaching_plans_controller.rb
+++ app/controllers/api/v2/teaching_plans_controller.rb
@@ -6,7 +6,10 @@ module Api
       def index
         return unless params[:teacher_id]
 
-        @unities = Unity.by_teacher(params[:teacher_id]).ordered.uniq
+        @unities = Unity.by_teacher(params[:teacher_id]).ordered.distinct
+        @unities = @unities.select do |unity|
+          TeachingPlan.by_unity_id(unity.id).exists?
+        end
         @teacher_id = params[:teacher_id]
       end
     end
