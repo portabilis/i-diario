cc541c30cf9539c4d57ebb32765e4da794a3bdc4
AnaPerola <anaperolinha@hotmail.com>
2024-07-01 15:04:19 -0300

    Aplica ajustes para impedir extensões invalidas ao fazer upload

--- app/models/teaching_plan_attachment.rb
+++ app/models/teaching_plan_attachment.rb
@@ -4,4 +4,14 @@ class TeachingPlanAttachment < ApplicationRecord
   mount_uploader :attachment, DocUploader
 
   delegate :filename, to: :attachment
+
+  validate :attachment_extension_whitelist
+
+  private
+
+  def attachment_extension_whitelist
+    if attachment.present? && !attachment.file.extension.match(/\A(doc|docx|pdf|txt)\z/)
+      errors.add(:attachment, "must be a JPEG, JPG, PNG, GIF, PDF, ODT, DOC, DOCX, ODS, XLS, XLSX, ODP, PPT, PPTX, ODG, XML, or CSV file")
+    end
+  end
 end
