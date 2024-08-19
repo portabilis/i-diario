0aac94bb337ec54726a534ba9bd4777566d98351
AnaPerola <anaperolinha@hotmail.com>
2024-08-19 19:22:27 -0300

    Insere query para buscar alunos e frequencias

--- app/services/api/list_student_attendances_by_classroom_service.rb
+++ app/services/api/list_student_attendances_by_classroom_service.rb
@@ -29,6 +29,24 @@ module Api
     end
 
     def process_daily_frequencies_by_classroom(classroom)
+      student_ids = Student.where(api_code: students_api_code).pluck(:id)
+      daily_frequencies = query_daily_frequencies(classroom.id, student_ids)
+    end
+
+    def query_daily_frequencies(classroom_id, student_ids)
+      DailyFrequency
+        .by_classroom_id(classroom_id)
+        .by_frequency_date_between(start_at, end_at)
+        .joins(:classroom, students: :student)
+        .where(daily_frequency_students: { student_id: student_ids })
+        .group('classrooms.api_code, students.api_code, students.name')
+        .select("
+          students.api_code AS student_api_code,
+          students.name AS student_name,
+          SUM(daily_frequency_students.present::int) AS presences,
+          SUM((NOT daily_frequency_students.present)::int) AS absences,
+          classrooms.api_code AS classroom_api_code
+        ")
     end
   end
 end
