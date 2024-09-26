4e190ae70cec7a69f92c344628af488e7f0c0125
AnaPerola <anaperolinha@hotmail.com>
2024-09-26 11:35:43 -0300

    Remove complexidade para helper

--- app/helpers/data_exportations_helper.rb
+++ app/helpers/data_exportations_helper.rb
@@ -9,4 +9,16 @@ module DataExportationsHelper
     return formatted_backup_link(backup) if backup.completed?
     return "Erro: #{backup.error_message}" if backup.error?
   end
+
+  def backup_button_or_status(form, backup, backup_type)
+    return exporting_button if backup.started?
+
+    form.submit 'Exportar', name: "backup_type[#{backup_type}]", class: 'btn bg-color-red txt-color-white'
+  end
+
+  def exporting_button
+    link_to '#', class: 'btn bg-color-red txt-color-white' do
+      content_tag(:i, '', class: 'fa fa-cog fa-spin') + ' Exportando...'
+    end
+  end
 end
