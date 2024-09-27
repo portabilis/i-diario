cde0ee24e5b5eace8071898d3fc884263c1f3db1
AnaPerola <anaperolinha@hotmail.com>
2024-09-27 11:27:23 -0300

    Ajusta condicional para melhorar legibilidade e tratar erro

--- app/controllers/descriptive_exams_controller.rb
+++ app/controllers/descriptive_exams_controller.rb
@@ -12,7 +12,7 @@ class DescriptiveExamsController < ApplicationController
     select_options_by_user
     select_opinion_types
 
-    unless @opinion_types.first.text.eql?('Avaliação padrão (regular)')
+    if @opinion_types&.first&.text != 'Avaliação padrão (regular)'
       @descriptive_exam.discipline_id = current_user_discipline.id
     end
 
@@ -179,7 +179,8 @@ class DescriptiveExamsController < ApplicationController
   end
 
   def opinion_type_by_year?(opinion_type = nil)
-    [OpinionTypes::BY_YEAR, OpinionTypes::BY_YEAR_AND_DISCIPLINE].include?(opinion_type || @descriptive_exam.opinion_type)
+    [OpinionTypes::BY_YEAR,
+     OpinionTypes::BY_YEAR_AND_DISCIPLINE].include?(opinion_type || @descriptive_exam.opinion_type)
   end
 
   def recorded_at_by_step
