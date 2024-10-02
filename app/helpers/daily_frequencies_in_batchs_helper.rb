93deb5ac386de55e44b21e27a284ede4f6d652b1
Leonardo <leonardo.oliveira.nascimento@gmail.com>
2024-10-02 21:09:41 +0100

    remoção do @additiona_data.any? pois a variavel sempre será um array

--- app/helpers/daily_frequencies_in_batchs_helper.rb
+++ app/helpers/daily_frequencies_in_batchs_helper.rb
@@ -3,12 +3,10 @@ module DailyFrequenciesInBatchsHelper
     additional_class = nil
     tooltip = nil
 
-    if @additional_data.any?
-      @additional_data.each do |additional_data|
-        if additional_data[:date] == date[:date] && additional_data[:student_id] == student[:student][:id] && !student[:active]
-          additional_class = additional_data[:additional_class]
-          tooltip = additional_data[:tooltip]
-        end
+    @additional_data.each do |additional_data|
+      if additional_data[:date] == date[:date] && additional_data[:student_id] == student[:student][:id] && !student[:active]
+        additional_class = additional_data[:additional_class]
+        tooltip = additional_data[:tooltip]
       end
     end
 
@@ -21,12 +19,11 @@ module DailyFrequenciesInBatchsHelper
   def custom_student_name(student)
     student_name = student[:student][:name]
     color = false
-    if @additional_data.any?
-      @additional_data.each do |additional_data|
-        if additional_data[:student_id] == student[:student][:id] && !student[:active]
-          student_name = '*' + student[:student][:name]
-          color = '#a90329'
-        end
+
+    @additional_data.each do |additional_data|
+      if additional_data[:student_id] == student[:student][:id] && !student[:active]
+        student_name = '*' + student[:student][:name]
+        color = '#a90329'
       end
     end
 
