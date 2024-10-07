7ca12018f65e11cce996e46ceb98384b5b944ad5
AnaPerola <anaperolinha@hotmail.com>
2024-10-07 16:13:08 -0300

    Melhora cenários de teste

--- spec/services/school_day_checker_spec.rb
+++ spec/services/school_day_checker_spec.rb
@@ -1,12 +1,13 @@
 require 'spec_helper'
 
 RSpec.describe SchoolDayChecker, type: :service do
-  let!(:classroom) { create(:classroom) }
-  let!(:classroom_grades) { create(:classrooms_grade, classroom: classroom) }
-  let!(:school_calendar) { create(:school_calendar, :with_one_step, unity: classroom.unity) }
+  let(:classroom) { create(:classroom) }
+  let(:classroom_grades) { create_list(:classrooms_grade, 3, classroom: classroom)}
+  let(:school_calendar) { create(:school_calendar, :with_one_step, unity: classroom.unity) }
 
   describe '#day_allows_entry?' do
     context 'when there is a non-school event and it does not allow entries directly linked to the classroom' do
+      let!(:date) { '2017-03-03'.to_date }
       let!(:school_calendar_event) {
         create(
           :school_calendar_event,
@@ -14,16 +15,15 @@ RSpec.describe SchoolDayChecker, type: :service do
           start_date: '2017-03-03',
           end_date: '2017-03-03',
           classroom: classroom,
-          course: classroom_grades.grade.course,
-          grade: classroom_grades.grade,
+          course: classroom_grades.first.grade.course,
+          grade: classroom_grades.first.grade,
           coverage: "by_classroom",
           event_type: EventTypes::NO_SCHOOL
         )
       }
 
       it 'returns false for a school day when grade_id and classroom_id are provided' do
-        date = '2017-03-03'.to_date
-        grade_id = classroom_grades.grade.id
+        grade_id = classroom_grades.first.grade.id
         subject = SchoolDayChecker.new(school_calendar, date, grade_id, classroom.id, nil).day_allows_entry?
 
         expect(subject).to be false
@@ -33,81 +33,109 @@ RSpec.describe SchoolDayChecker, type: :service do
         # Atenção! Quando enviamos apenas o parametro de turma
         # O serviço não está verificando corretamente se a turma tem vinculo com o evento,
         # Por isso temos esse retorno true (true é um valor errado, pois essa turma tem sim evento)
-        date = '2017-03-03'.to_date
-        grade_id = classroom_grades.grade.id
+        grade_id = classroom_grades.first.grade.id
         subject = SchoolDayChecker.new(school_calendar, date, nil, classroom.id, nil).day_allows_entry?
 
         expect(subject).to be true
       end
 
       it 'returns false for a school day when grade_id is provided' do
-        date = '2017-03-03'.to_date
-        grade_id = classroom_grades.grade.id
+        grade_id = classroom_grades.first.grade.id
         subject = SchoolDayChecker.new(school_calendar, date, grade_id, nil, nil).day_allows_entry?
 
         expect(subject).to be false
       end
 
       it 'returns true for a school day when course is provided' do
-        date = '2017-03-03'.to_date
-        course_id = classroom_grades.grade.course.id
+        course_id = classroom_grades.first.grade.course.id
         subject = SchoolDayChecker.new(school_calendar, date, nil, nil, course_id).day_allows_entry?
 
         expect(subject).to be false
       end
     end
 
-    # context 'when there is a non-school event and it does not allow entries directly linked to the course' do
-    #   subject do
-    #   SchoolDayChecker.new(
-    #     school_calendar, '2017-02-02', nil, classroom.id, nil
-    #   ).day_allows_entry?
-    #   end
+    context 'when there is a non-school event and it does not allow entries directly linked to the course' do
+      let(:date) { '2017-04-04'.to_date }
+      let(:grade_id) { classroom_grades.first.grade.id }
+      let!(:school_calendar_event) {
+        create(
+          :school_calendar_event,
+          school_calendar: school_calendar,
+          start_date: '2017-04-04',
+          end_date: '2017-04-04',
+          classroom: nil,
+          course: classroom_grades.first.grade.course,
+          grade: nil,
+          coverage: "by_course",
+          event_type: EventTypes::NO_SCHOOL
+        )
+      }
 
-    #   it 'returns false for school day when the classroom is linked to the course' do
-    #   expect(subject).to be true
-    #   end
+      it 'returns false for school day when only classroom_id is provided' do
+        subject = SchoolDayChecker.new(school_calendar, date, nil, classroom.id, nil).day_allows_entry?
+        expect(subject).to be false
+      end
 
-    #   it 'returns true for school day when the classroom is not linked to the course' do
-    #   expect(subject).to be true
-    #   end
+      it 'returns false for school day when the classroom_id and grade_id are provided' do
+        subject = SchoolDayChecker.new(school_calendar, date, grade_id, classroom.id, nil).day_allows_entry?
+        expect(subject).to be false
+      end
 
-    #   it 'returns false for school day when the grade is linked to the course' do
-    #   expect(subject).to be true
-    #   end
+      it 'returns false for school day when only grade is linked to the course' do
+        subject = SchoolDayChecker.new(school_calendar, date, grade_id, nil, nil).day_allows_entry?
+        expect(subject).to be false
+      end
 
-    #   it 'returns true for school day when the course is linked to the event' do
-    #   expect(subject).to be true
-    #   end
+      it 'returns false for school day when only course is linked to the event' do
+        course_id = classroom_grades.first.grade.course.id
+        subject = SchoolDayChecker.new(school_calendar, date, nil, nil, course_id).day_allows_entry?
+        expect(subject).to be false
+      end
+    end
 
-    #   it 'returns false for school day when the course is not linked to the event' do
-    #   expect(subject).to be true
-    #   end
-    # end
+    context 'when there is a non-school event and it does not allow entries directly linked to the grade' do
+      let(:date) { '2017-05-05'.to_date }
+      let(:grade_id) { classroom_grades.first.grade.id }
+      let!(:school_calendar_event) {
+        create(
+          :school_calendar_event,
+          school_calendar: school_calendar,
+          start_date: '2017-05-05',
+          end_date: '2017-05-05',
+          classroom: nil,
+          course: classroom_grades.first.grade.course,
+          grade: classroom_grades.first.grade,
+          coverage: "by_grade",
+          event_type: EventTypes::NO_SCHOOL
+        )
+      }
 
-    # context 'when there is a non-school event and it does not allow entries directly linked to the grade' do
-    #   subject do
-    #   SchoolDayChecker.new(
-    #     school_calendar, '2017-02-02', nil, classroom.id, nil
-    #   ).day_allows_entry?
-    #   end
+      it 'returns true for a school day when only classroom is linked to the grade' do
+        # Atenção! Quando enviamos apenas o parametro de turma
+        # O serviço não está verificando corretamente se a turma tem vinculo com o evento,
+        # Por isso temos esse retorno true (true é um valor errado, pois essa turma tem sim evento)
+        subject = SchoolDayChecker.new(school_calendar, date, nil, classroom.id, nil).day_allows_entry?
+        expect(subject).to be true
+      end
 
-    #   it 'returns false for a school day when the classroom is linked to the grade' do
-    #   expect(subject).to be true
-    #   end
+      it 'returns false for a school day when the classroom_id and grade_id are provided' do
+        subject = SchoolDayChecker.new(school_calendar, date, grade_id, classroom.id, nil).day_allows_entry?
+        expect(subject).to be false
+      end
 
-    #   it 'returns true for a school day when the classroom is not linked to the grade' do
-    #   expect(subject).to be true
-    #   end
+      # 'returns false for a school day when the classroom_id and grade_id is not linked to the grade'
 
-    #   it 'returns false for a school day when the grade is linked to the event' do
-    #   expect(subject).to be true
-    #   end
+      it 'returns false for a school day when only grade is provided' do
+        subject = SchoolDayChecker.new(school_calendar, date, grade_id, nil, nil).day_allows_entry?
+        expect(subject).to be false
+      end
 
-    #   it 'returns false for a school day when the grade is not linked to the event' do
-    #   expect(subject).to be true
-    #   end
-    # end
+      it 'returns true for a school day when the course is provided' do
+        course_id = classroom_grades.first.grade.course.id
+        subject = SchoolDayChecker.new(school_calendar, date, nil, nil, course_id).day_allows_entry?
+        # expect(subject).to be true
+      end
+    end
   end
 
 end
