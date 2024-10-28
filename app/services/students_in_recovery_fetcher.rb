d470480c8914b79a69bd8cc80c72402243f3c423
AnaPerola <anaperolinha@hotmail.com>
2024-10-28 18:17:06 -0300

    Ajusta localização de métodos para facilitar leitura do serviço

--- app/services/students_in_recovery_fetcher.rb
+++ app/services/students_in_recovery_fetcher.rb
@@ -49,47 +49,45 @@ class StudentsInRecoveryFetcher
       end
     end
 
-  def classroom
-    @classroom ||= Classroom.find(@classroom_id)
+    filter_differentiated_students(students, differentiated)
   end
 
-  def classroom_grades_with_recovery_rule
-    return @classroom_grade if @classroom_grade.present?
+  def filter_students_in_recovery
+    classroom_grade_ids = classroom_grades_with_recovery_rule.map(&:id)
+    fetch_student_enrollment_classroooms(classroom_grade_ids)
+  end
 
-    @classroom_grade = []
+  def fetch_students_in_specific_recovery(differentiated = nil)
+    students = []
 
     recovery_steps = RecoveryStepsFetcher.new(step, classroom).fetch
     recovery_exam_rule = exam_rule.recovery_exam_rules.find { |recovery_diary_record|
       recovery_diary_record.steps.last.eql?(@step.to_number)
     }
 
-    if @classroom_grade.empty?
-      classroom_grades
-    else
-      @classroom_grade
+    if recovery_exam_rule.present?
+      students = filter_students_in_recovery.select do |student|
+        sum_averages = recovery_steps.sum do |step|
+          student[:student].average(classroom, discipline, step) || 0
+        end
+        average = sum_averages / recovery_steps.count.to_f
+
+        average < recovery_exam_rule.average
+      end
     end
+
+    filter_differentiated_students(students, differentiated)
   end
 
-  def classroom_grades
-    classroom.classrooms_grades.includes(:exam_rule)
-  end
+  def filter_differentiated_students(students, differentiated)
+    if differentiated == !!differentiated
+      students = students.select do |student|
+        student = student[:student] if student[:student].present?
+        student.uses_differentiated_exam_rule == differentiated
+      end
+    end
 
-  def discipline
-    @discipline ||= Discipline.find(@discipline_id)
-  end
-
-  def steps_fetcher
-    @steps_fetcher ||= StepsFetcher.new(classroom)
-  end
-
-  def step
-    @step ||= steps_fetcher.step_by_id(@step_id)
-  end
-
-  def student_enrollments(classroom_grade_ids)
-    end_at = @date.to_date > step.end_at ? step.end_at : @date.to_date
-
-    @student_enrollments ||= fetch_student_enrollment_classroooms(classroom_grade_ids, discipline, classroom)
+    students
   end
 
   def fetch_student_enrollment_classroooms(classroom_grade_ids, discipline, classroom)
@@ -107,14 +105,12 @@ class StudentsInRecoveryFetcher
 
     @classroom_grade = []
 
-    if classroom_grades_with_recovery_rule.first.exam_rule.parallel_recovery_average
-      students = students.select do |student|
-        average = student[:student].average(classroom, discipline, step) || 0
-        average < classroom_grades_with_recovery_rule.first.exam_rule.parallel_recovery_average
-      end
+    classroom_grades&.each do |classroom_grade|
+      @classroom_grade << classroom_grade unless classroom_grade.exam_rule.recovery_type.eql?(0)
     end
 
-    filter_differentiated_students(students, differentiated)
+    @classroom_grade = classroom_grades if @classroom_grade.empty?
+    @classroom_grade
   end
 
   def classroom_grades
