d13cb28aac8c4231f21755b5893fa5db369d0931
AnaPerola <anaperolinha@hotmail.com>
2024-11-05 14:58:11 -0300

    Remove utilização de um método de discard para melhorar legibilidade

--- app/services/remove_daily_note_students.rb
+++ app/services/remove_daily_note_students.rb
@@ -26,13 +26,18 @@ class RemoveDailyNoteStudents
 
       next if daily_note_student.note.present? || daily_note_student.transfer_note_id.present?
 
-      daily_note_student.discard_or_undiscard(!student_inactive_in_date?)
-      daily_note_student.active = student_inactive_in_date?
+      if student_inactive_in_date?
+        daily_note_student.discard
+        daily_note_student.active = false
+      else
+        daily_note_student.undiscard
+        daily_note_student.active = true
+      end
     end
   end
 
   def student_inactive_in_date?
-    @date_avaliation < joined_at.to_date && @date_avaliation >= left_at.to_date
+    @date_avaliation < joined_at.to_date || @date_avaliation >= left_at.to_date
   end
 
   private
