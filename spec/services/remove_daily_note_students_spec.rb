539b60a8c1630911c0b120c6e2bfee95c694cf4d
AnaPerola <anaperolinha@hotmail.com>
2024-11-05 14:59:40 -0300

    Incrementa mais cenários de teste para enturmações diferentes

--- spec/services/remove_daily_note_students_spec.rb
+++ spec/services/remove_daily_note_students_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe RemoveDailyNoteStudents, type: :service do
     create(
       :avaliation,
       school_calendar: school_calendar,
-      test_date: Date.current,
+      test_date: '2017-03-01',
       classroom: classroom,
       discipline: teacher_discipline_classroom.discipline,
       teacher_id: teacher_discipline_classroom.teacher.id
@@ -32,7 +32,7 @@ RSpec.describe RemoveDailyNoteStudents, type: :service do
       :student_enrollment_classroom,
       student_enrollment: student_enrollment,
       classrooms_grade: classroom_grades,
-      joined_at: '2017-02-01', # data de entrada do aluno na turma
+      joined_at: '2017-02-02', # data de entrada do aluno na turma
       left_at: '2017-04-04' # data de saída do aluno na turma
     )
   }
@@ -92,7 +92,18 @@ RSpec.describe RemoveDailyNoteStudents, type: :service do
 
     context 'and is NOT in enrolled on the date' do
       it 'return an array with daily_note_student with discarded' do
+        # 01/03/2017 - avaliacao
+        # 01/02/2017 - entrada do aluno na turma
         student_enrollment_classroom.update(left_at: '2017-02-25')
+
+        expect(subject.map(&:discarded?)).to eq [true]
+        expect(subject.map(&:active)).to eq [false]
+      end
+
+      it 'return an array with daily_note_student with discarded' do
+        # 01/03/2017 - avaliacao
+        # 04/04/2017 - saida do aluno na turma
+        student_enrollment_classroom.update(joined_at: '2017-03-02')
 
         expect(subject.map(&:discarded?)).to eq [true]
         expect(subject.map(&:active)).to eq [false]
