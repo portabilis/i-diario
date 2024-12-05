4c83d44cd232c039457602e4a26137ce206db5ed
AnaPerola <anaperolinha@hotmail.com>
2024-12-05 18:06:40 -0300

    Padroniza nome da fila

--- app/workers/notify_by_email_daily_frequency_in_batch_worker.rb
+++ app/workers/notify_by_email_daily_frequency_in_batch_worker.rb
@@ -1,7 +1,7 @@
 class NotifyByEmailDailyFrequencyInBatchWorker
   include Sidekiq::Worker
 
-  sidekiq_options queue: :daily_frequency_in_batch, unique: :until_and_while_executing
+  sidekiq_options queue: :notify_daily_frequency_in_batch, unique: :until_and_while_executing
 
   def perform(name, email, url, dates, classroom, unity)
     ReceiptMailer.notify_daily_frequency_in_batch_success(name, email, url, dates, classroom, unity).deliver_now
