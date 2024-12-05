743c08e15b813d67a9d20eab54716fe330fc5003
AnaPerola <anaperolinha@hotmail.com>
2024-12-05 17:55:14 -0300

    Informa os devidos parametros para o worker

--- app/workers/notify_by_email_daily_frequency_in_batch_worker.rb
+++ app/workers/notify_by_email_daily_frequency_in_batch_worker.rb
@@ -3,7 +3,7 @@ class NotifyByEmailDailyFrequencyInBatchWorker
 
   sidekiq_options queue: :daily_frequency_in_batch, unique: :until_and_while_executing
 
-  def perform
+  def perform(name, email, url, dates, classroom, unity)
     ReceiptMailer.notify_daily_frequency_in_batch_success(name, email, url, dates, classroom, unity).deliver_now
   end
 end
--- app/workers/notify_by_email_daily_frequency_worker.rb
+++ app/workers/notify_by_email_daily_frequency_worker.rb
@@ -3,7 +3,7 @@ class NotifyByEmailDailyFrequencyWorker
 
   sidekiq_options queue: :notify_daily_frequencies, unique: :until_and_while_executing
 
-  def perform
+  def perform(name, email, url, dates, classroom, unity)
     ReceiptMailer.notify_daily_frequency_success(name, email, url, dates, classroom, unity).deliver_now
   end
 end
