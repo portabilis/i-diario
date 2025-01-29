8cd047ba00981240c8304dd57561ec52ba6b6143
AnaPerola <anaperolinha@hotmail.com>
2025-01-29 09:48:13 -0300

    Remove breakpoint e modifica recebimento de id para api_code da escola

--- app/controllers/api/v2/school_calendar_events_controller.rb
+++ app/controllers/api/v2/school_calendar_events_controller.rb
@@ -9,17 +9,17 @@ module Api
 
         return if missing_params
 
-        unity_id = params[:unity_id]
+        unity_api_code = params[:unity_id]
         year = params[:year]
         start_date = params[:start_date]
         end_date = params[:end_date]
 
-        unity = Unity.find_by(id: unity_id)
-        return render json: { error: "Unidade escolar não encontrada." }, status: :not_found unless unity
+        unity = Unity.find_by(api_code: unity_api_code)
+        return render json: { error: 'Unidade escolar não encontrada.' }, status: :not_found unless unity
 
-        events = validate_calendar_and_events(unity_id, year, start_date, end_date)
-        return if  events.nil?
-        binding.pry
+        events = validate_calendar_and_events(unity.id, year, start_date, end_date)
+        return if events.nil?
+
         render json: build_response(unity, year, events)
       end
 
@@ -31,7 +31,7 @@ module Api
         return false unless missing_params.any?
 
         render json: { error: "Os seguintes parâmetros são obrigatórios: #{missing_params.join(', ')}" },
-          status: :unprocessable_entity
+               status: :unprocessable_entity
       end
 
       def build_response(unity, year, events)
@@ -53,14 +53,14 @@ module Api
 
       def validate_calendar_and_events(unity_id, year, start_date = nil, end_date = nil)
         events = SchoolCalendarEvent.joins(:school_calendar)
-                  .where(school_calendars: { unity_id: unity_id, year: year })
+                                    .where(school_calendars: { unity_id: unity_id, year: year })
 
         if start_date.present? && end_date.present?
           events = events.where('start_date >= ? AND end_date <= ?', start_date, end_date)
         end
 
         if events.empty?
-          render json: { message: "Nenhum evento encontrado para os calendários letivos da unidade e ano especificados." },
+          render json: { message: 'Nenhum evento encontrado para os calendários letivos da unidade e ano especificados.' },
                  status: :not_found
           return [nil, nil]
         end
