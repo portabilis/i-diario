f56aff758e3385bffcc2ce4f0afb38d2baacb6d8
AnaPerola <anaperolinha@hotmail.com>
2025-02-14 15:42:00 -0300

    Melhora a funcionalidade de impressão do quadro de aulas, clonando a tabela e substituindo inputs por spans antes da impressão

--- app/assets/javascripts/views/lessons_boards/show.js
+++ app/assets/javascripts/views/lessons_boards/show.js
@@ -1,27 +1,32 @@
-// Ao remover esse arquivo remover tambem o plugin printThis
 $(function() {
-  $("#print-button ").on("click", function() {
-    $("#lessons-board-table").printThis({
+  $("#print-button").on("click", function() {
+    let clone = $("#lessons-board-table").clone();
+
+    clone.find("input:not([type='hidden'])").each(function() {
+      let value = $(this).val();
+      $(this).replaceWith(`<span>${value}</span>`);
+    });
+
+    clone.find("input[type='hidden']").remove();
+
+    clone.printThis({
       pageTitle: "Quadro de Aulas",
       header:
-        "<table class='table table-bordered'><thead<tr>" +
-        "<th style='width: 40px;'>ESCOLA</th>" +
-        "<th style='width: 40px;'>Série</th>" +
-        "<th style='width: 40px;'>Turma</th>" +
-        "<th style='width: 40px;'>Periodo</th></tr><tr>" +
-        "<th style='width: 40px;'>" +
+        "<table class='table table-bordered'><thead><tr>" +
+        "<th>ESCOLA</th><th>Série</th><th>Turma</th><th>Período</th></tr><tr>" +
+        "<th>" +
         $("#lessons_board_unity").val() +
         "</th>" +
-        "<th style='width: 40px;'>" +
+        "<th>" +
         $("#lessons_board_grade").val() +
         "</th>" +
-        "<th style='width: 40px;'>" +
+        "<th>" +
         $("#lessons_board_classroom_id").val() +
         "</th>" +
-        "<th style='width: 40px;'>" +
+        "<th>" +
         $("#lessons_board_period").val() +
         "</th></tr>" +
-        "<thead></table>"
+        "</thead></table>"
     });
   });
 });
