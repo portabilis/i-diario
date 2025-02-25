121a1753ca9bf5ffa71ec780c4bbed21f9f93556
Marcelo Cajueiro <marcelocajueiro@gmail.com>
2025-02-25 11:19:55 -0300

    Altera tempo de cache do perfil
    Já que ele irá expirar na sincronização, podemos ter um cache longo aqui

--- app/services/current_profile.rb
+++ app/services/current_profile.rb
@@ -177,7 +177,7 @@ class CurrentProfile
   end
 
   def cache(key_complements)
-    Rails.cache.fetch cache_key_to_user + [IeducarApiSynchronization.last] + Array(key_complements), expires_in: 10.minutes do
+    Rails.cache.fetch cache_key_to_user + [IeducarApiSynchronization.last] + Array(key_complements), expires_in: 8.hours do
       yield
     end
   end
