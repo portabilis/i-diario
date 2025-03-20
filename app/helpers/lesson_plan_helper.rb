d459a2390f5ad10187d00b0be2e40fc1e13cd75b
AnaPerola <anaperolinha@hotmail.com>
2025-03-20 16:09:45 -0300

    Adiciona novos métodos de helper para forms de planos de aula e ensino, melhorando a gestão de URLs e métodos de requisição com base na action atual.

--- app/helpers/lesson_plan_helper.rb
+++ app/helpers/lesson_plan_helper.rb
@@ -2,4 +2,37 @@ module LessonPlanHelper
   def lesson_plan_destroy?(lesson_plan)
     lesson_plan.teacher.id == current_teacher.try(:id) || current_user.current_role_is_admin_or_employee?
   end
+
+  def discipline_lesson_plan_form_url(discipline_lesson_plan, action_name)
+    case action_name
+    when 'new'
+      discipline_lesson_plans_path(locale: I18n.locale)
+    when 'edit'
+      discipline_lesson_plan_path(discipline_lesson_plan)
+    when 'show'
+      lesson_plan_opinion_path(discipline_lesson_plan.lesson_plan.id, locale: I18n.locale)
+    end
+  end
+
+  def lesson_plan_form_method(action_name)
+    case action_name
+    when 'new'
+      :post
+    when 'show'
+      :patch
+    else
+      :patch
+    end
+  end
+
+  def knowledge_area_lesson_plan_form_url(knowledge_area_lesson_plan, action_name)
+    case action_name
+    when 'new'
+      new_knowledge_area_lesson_plan_path(locale: I18n.locale)
+    when 'edit'
+      edit_knowledge_area_lesson_plan_path(knowledge_area_lesson_plan)
+    when 'show'
+      lesson_plan_opinion_path(knowledge_area_lesson_plan.lesson_plan.id, locale: I18n.locale)
+    end
+  end
 end
