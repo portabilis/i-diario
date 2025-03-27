a07b6ef8168e3c80119504480fb5efdea0c47f86
AnaPerola <anaperolinha@hotmail.com>
2025-03-27 11:19:55 -0300

    Adiciona tratamento de erro para entidade não encontrada no InfrequencyTrackingNotifierWorker

--- app/workers/infrequency_tracking_notifier_worker.rb
+++ app/workers/infrequency_tracking_notifier_worker.rb
@@ -1,11 +1,28 @@
+class EntityNotFoundError < StandardError; end
+
 class InfrequencyTrackingNotifierWorker
   include Sidekiq::Worker
 
   sidekiq_options unique: :until_and_while_executing, queue: :low
 
   def perform(entity_id)
-    Entity.find(entity_id).using_connection do
+    entity = Entity.find_by(id: entity_id)
+
+    unless entity
+      error = EntityNotFoundError.new("Entity com o id #{entity_id} não foi encontrado")
+      log_and_notify_error(error)
+      raise error
+    end
+
+    entity.using_connection do
       InfrequencyTrackingNotifier.notify!
     end
+  end
+
+  private
+
+  def log_and_notify_error(error)
+    Rails.logger.warn("Error occurred: #{error.class} - #{error.message}")
+    Honeybadger.notify(error)
   end
 end
