5ac06de3d9b181283f1b19c0ec7610b79416d5e1
AnaPerola <anaperolinha@hotmail.com>
2025-03-27 14:06:16 -0300

    Refatora método perform para reduzir complexidade e melhorar legibilidade
    - Move a lógica de busca da entidade para o método find_entity
    - Adiciona método notify_absences? para verificar a configuração global
    - Extrai lógica de notificação para o método notify_infrequency!
    - Centraliza o tratamento de erros no método log_and_notify_error
    
    Objetivo: Reduzir o número de linhas e melhorar a modularidade do código

--- app/workers/infrequency_tracking_notifier_worker.rb
+++ app/workers/infrequency_tracking_notifier_worker.rb
@@ -6,20 +6,34 @@ class InfrequencyTrackingNotifierWorker
   sidekiq_options unique: :until_and_while_executing, queue: :low
 
   def perform(entity_id)
-    entity = Entity.find_by(id: entity_id)
+    entity = find_entity(entity_id)
 
-    unless entity
-      error = EntityNotFoundError.new("Entity com o id #{entity_id} não foi encontrado")
-      log_and_notify_error(error)
-      raise error
-    end
+    return unless entity
 
     entity.using_connection do
-      InfrequencyTrackingNotifier.notify!
+      general_configuration = GeneralConfiguration.current
+
+      return unless notify_absences?(general_configuration)
+
+      notify_infrequency!
     end
   end
 
   private
+
+  def find_entity(entity_id)
+    entity = Entity.find_by(id: entity_id)
+    log_and_notify_error(EntityNotFoundError.new("Entity com o id #{entity_id} não foi encontrada")) unless entity
+    entity
+  end
+
+  def notify_absences?(general_configuration)
+    general_configuration.notify_consecutive_or_alternate_absences
+  end
+
+  def notify_infrequency!
+    InfrequencyTrackingNotifier.notify!
+  end
 
   def log_and_notify_error(error)
     Rails.logger.warn("Error occurred: #{error.class} - #{error.message}")
