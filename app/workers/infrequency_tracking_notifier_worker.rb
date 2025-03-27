0352c06a92c9e39047449b4074952e78dfb97444
AnaPerola <anaperolinha@hotmail.com>
2025-03-27 17:35:08 -0300

    Remove codigo não necessário e aplica logs para entender se os workers estao sendo acionados para cada cliente

--- app/workers/infrequency_tracking_notifier_worker.rb
+++ app/workers/infrequency_tracking_notifier_worker.rb
@@ -1,42 +1,11 @@
-class EntityNotFoundError < StandardError; end
-
 class InfrequencyTrackingNotifierWorker
   include Sidekiq::Worker
 
   sidekiq_options unique: :until_and_while_executing, queue: :low
 
   def perform(entity_id)
-    entity = find_entity(entity_id)
-
-    return unless entity
-
-    entity.using_connection do
-      general_configuration = GeneralConfiguration.current
-
-      return unless notify_absences?(general_configuration)
-
-      notify_infrequency!
+    Entity.find(entity_id).using_connection do
+      InfrequencyTrackingNotifier.notify!
     end
-  end
-
-  private
-
-  def find_entity(entity_id)
-    entity = Entity.find_by(id: entity_id)
-    log_and_notify_error(EntityNotFoundError.new("Entity com o id #{entity_id} não foi encontrada")) unless entity
-    entity
-  end
-
-  def notify_absences?(general_configuration)
-    general_configuration.notify_consecutive_or_alternate_absences
-  end
-
-  def notify_infrequency!
-    InfrequencyTrackingNotifier.notify!
-  end
-
-  def log_and_notify_error(error)
-    Rails.logger.warn("Error occurred: #{error.class} - #{error.message}")
-    Honeybadger.notify(error)
   end
 end
--- lib/tasks/send_notifications.rake
+++ lib/tasks/send_notifications.rake
@@ -2,10 +2,15 @@ namespace :send_notification do
   desc 'Send absence notification'
   task absences: :environment do
     Entity.active.each do |entity|
-      InfrequencyTrackingNotifierWorker.perform_in(1.second, entity.id)
+      Rails.logger.info "Enviando notificação para a entidade: #{entity.name} (ID: #{entity.id})"
 
-      puts "Notificação de infrequência foi disparada com sucesso para a entidade #{entity.name}"
-      puts 'Para mais detalhes acompanhe a execução do worker InfrequencyTrackingNotifierWorker no sidekiq'
+      jid = InfrequencyTrackingNotifierWorker.perform_in(1.second, entity.id)
+
+      if jid.present?
+        Rails.logger.info "Notificação agendada com sucesso para #{entity.name} (ID: #{entity.id}) - Job ID: #{jid}"
+      else
+        Rails.logger.error "Falha ao agendar notificação para #{entity.name} (ID: #{entity.id})"
+      end
     end
   end
 end
