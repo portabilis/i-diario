1771d82b9bcd2533846ce8b569e51911e4a6152f
mateusinacioo <mateus.inacio321@gmail.com>
2025-04-09 16:35:38 -0300

    Arruma controller para incluir o método filter_students_in_recovery listando alunos em recuperação

--- app/controllers/school_term_recovery_diary_records_controller.rb
+++ app/controllers/school_term_recovery_diary_records_controller.rb
@@ -52,8 +52,9 @@ class SchoolTermRecoveryDiaryRecordsController < ApplicationController
     @school_term_recovery_diary_record.assign_attributes(resource_params.to_h)
     @school_term_recovery_diary_record.step_number = @school_term_recovery_diary_record.step.try(:step_number)
     @school_term_recovery_diary_record.recovery_diary_record.teacher_id = current_teacher_id
+    @school_term_recovery_diary_record.recovery_diary_record.current_user = current_user
 
-    authorize @school_term_recovery_diary_record
+     authorize @school_term_recovery_diary_record
 
     if @school_term_recovery_diary_record.save
       respond_with @school_term_recovery_diary_record, location: school_term_recovery_diary_records_path
@@ -63,6 +64,7 @@ class SchoolTermRecoveryDiaryRecordsController < ApplicationController
       else
         fetch_linked_by_teacher
       end
+      set_options_by_user
       fetch_disciplines_by_classroom
 
       render :new
@@ -87,12 +89,8 @@ class SchoolTermRecoveryDiaryRecordsController < ApplicationController
     authorize @school_term_recovery_diary_record
 
     reload_students_list
+    filter_students_in_recovery
 
-    students_in_recovery = fetch_students_in_recovery
-    mark_students_not_in_recovery_for_destruction(students_in_recovery)
-    mark_exempted_disciplines(students_in_recovery)
-
-    @any_student_exempted_from_discipline = any_student_exempted_from_discipline?
     @number_of_decimal_places = current_test_setting&.number_of_decimal_places ||
                                 current_test_setting_step(step)&.number_of_decimal_places
   end
@@ -108,13 +106,17 @@ class SchoolTermRecoveryDiaryRecordsController < ApplicationController
     if @school_term_recovery_diary_record.save
       respond_with @school_term_recovery_diary_record, location: school_term_recovery_diary_records_path
     else
+      set_options_by_user
+
+      @students = @school_term_recovery_diary_record.recovery_diary_record.students
+
       if @admin_or_teacher
         @number_of_decimal_places = current_test_setting.number_of_decimal_places
       else
         fetch_linked_by_teacher
       end
-      reload_students_list
       fetch_disciplines_by_classroom
+      filter_students_in_recovery
 
       render :edit
     end
@@ -311,5 +313,13 @@ class SchoolTermRecoveryDiaryRecordsController < ApplicationController
 
     classroom = @school_term_recovery_diary_record.recovery_diary_record.classroom
     @disciplines = @disciplines.by_classroom(classroom).not_descriptor
+  end
+
+  def filter_students_in_recovery
+    students_in_recovery = fetch_students_in_recovery
+    mark_students_not_in_recovery_for_destruction(students_in_recovery)
+    mark_exempted_disciplines(students_in_recovery)
+
+    @any_student_exempted_from_discipline = any_student_exempted_from_discipline?
   end
 end
