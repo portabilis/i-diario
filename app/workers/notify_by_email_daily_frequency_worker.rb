2dddefc6684586ea013a35354f311cf2dd5e58f3
AnaPerola <anaperolinha@hotmail.com>
2025-05-26 14:55:17 -0300

    Altera a fila de opções do Sidekiq para 'send_emails' nas classes de notificação de frequência diária

--- app/workers/notify_by_email_daily_frequency_worker.rb
+++ app/workers/notify_by_email_daily_frequency_worker.rb
@@ -1,7 +1,7 @@
 class NotifyByEmailDailyFrequencyWorker
   include Sidekiq::Worker
 
-  sidekiq_options queue: :notify_daily_frequencies, unique: :until_and_while_executing
+  sidekiq_options queue: :send_emails, unique: :until_and_while_executing
 
   def perform(name, email, url, dates, classroom, unity)
     ReceiptMailer.notify_daily_frequency_success(name, email, url, dates, classroom, unity).deliver_now
