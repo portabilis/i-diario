8f34bf18136e70db93c1738b7610a76a701b6c52
Marcelo Griggio Cajueiro <marcelocajueiro@gmail.com>
2025-07-09 08:54:56 -0300

    Corrige pontos identificados no code review
    - Adiciona método down na migration para permitir rollback completo
    - Adiciona traduções para os novos botões de sincronização
    - Utiliza chaves de tradução i18n ao invés de strings hardcoded nos botões

--- db/migrate/20250708000000_add_period_to_ieducar_api_synchronizations.rb
+++ db/migrate/20250708000000_add_period_to_ieducar_api_synchronizations.rb
@@ -1,5 +1,9 @@
 class AddPeriodToIeducarApiSynchronizations < ActiveRecord::Migration[5.0]
-  def change
+  def up
     add_column :ieducar_api_synchronizations, :period, :string
+  end
+
+  def down
+    remove_column :ieducar_api_synchronizations, :period
   end
 end
