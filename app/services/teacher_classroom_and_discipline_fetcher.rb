3ecc96b3cd582c7139221b390bc9d9582891c1dd
“mateusinacioo” <mateus.inacio321@gmail.com>
2025-07-16 12:17:41 -0300

    Adiciona include evitando N+1

--- app/services/teacher_classroom_and_discipline_fetcher.rb
+++ app/services/teacher_classroom_and_discipline_fetcher.rb
@@ -28,7 +28,7 @@ class TeacherClassroomAndDisciplineFetcher
   def classrooms_fetch
     return [] if unity.nil?
 
-    Classroom.by_unity_and_teacher(unity.id, teacher_id).by_year(current_school_year).ordered.distinct
+    Classroom.includes(:unity).by_unity_and_teacher(unity.id, teacher_id).by_year(current_school_year).ordered.distinct
   end
 
   def disciplines_fetch
