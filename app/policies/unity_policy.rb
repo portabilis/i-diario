c4cb3c3a9d490afbceb808fa2e408e874280a30c
“mateusinacioo” <mateus.inacio321@gmail.com>
2025-07-24 11:06:32 -0300

    Adiciona permissão de visualização de anos letivos.

--- app/policies/unity_policy.rb
+++ app/policies/unity_policy.rb
@@ -1,4 +1,8 @@
 class UnityPolicy < ApplicationPolicy
+  def view_school_calendars?
+    user.can_show?(:manage_school_years_of_the_unit)
+  end
+
   def manage_school_calendars?
     user.can_change?(:manage_school_years_of_the_unit)
   end
