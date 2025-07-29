82ba79349d33feb9297b50654cbbcb33a9c256e9
“mateusinacioo” <mateus.inacio321@gmail.com>
2025-07-29 12:39:10 -0300

    Refarota set_filters do Diário de Avaliações Numéricas

--- app/controllers/daily_notes_controller.rb
+++ app/controllers/daily_notes_controller.rb
@@ -312,14 +312,9 @@ disciplines: @discipline)
   end
 
   def set_filters
-    unless params.key?(:filter) || params.key?(:page)
-      params[:filter] = {
-        by_classroom_id: current_user_classroom.id,
-        by_discipline_id: current_user_discipline.id
-      }
-    end
-
     params[:filter] ||= {}
+    params[:filter][:by_classroom_id] ||= current_user_classroom.id
+    params[:filter][:by_discipline_id] ||= current_user_discipline.id
 
     if params[:filter][:by_step_id].present?
       step_id = params[:filter].delete(:by_step_id)
