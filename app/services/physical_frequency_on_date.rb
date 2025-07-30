a3a91c56acdbbea60cb0a1cf454f0dbc14491a91
“mateusinacioo” <mateus.inacio321@gmail.com>
2025-07-30 16:12:52 -0300

    Arrumar serviço para considerar data inicia e final.

--- app/services/physical_frequency_on_date.rb
+++ app/services/physical_frequency_on_date.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PhysicalFrequencyOnDate
-  attr_reader :student_enrollment_ids, :date
+    attr_reader :student_enrollment_ids, :start_date, :end_date
 
   def self.call(params)
     new(params).call
@@ -9,19 +9,21 @@ class PhysicalFrequencyOnDate
 
   def initialize(params)
     @student_enrollment_ids = params.fetch(:student_enrollment_ids)
-    @date = params.fetch(:date)
+    @start_date = params.fetch(:start_date)
+    @end_date = params.fetch(:end_date, @start_date)
   end
 
   def call
     daily_physical_frequencies = DailyPhysicalFrequency.where(
       student_enrollment_id: student_enrollment_ids,
-      frequency_date: date
+      frequency_date: start_date..end_date
     )
 
     physical_frequencies = {}
 
     daily_physical_frequencies.each do |physical_frequency|
-      physical_frequencies[physical_frequency.student_enrollment_id] = physical_frequency.present
+      physical_frequencies[physical_frequency.student_enrollment_id] ||= {}
+      physical_frequencies[physical_frequency.student_enrollment_id][physical_frequency.frequency_date] = physical_frequency.present
     end
 
     physical_frequencies
