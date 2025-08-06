bcfae3f19e7eaffe85763c6c423aa309c38fd530
“mateusinacioo” <mateus.inacio321@gmail.com>
2025-08-06 09:25:11 -0300

    Altera var por let no form da recuperação da menor nota

--- app/assets/javascripts/views/avaliation_recovery_lowest_notes/form.js
+++ app/assets/javascripts/views/avaliation_recovery_lowest_notes/form.js
@@ -221,8 +221,8 @@ $(function () {
   $(document).on('keydown', '.nested-fields input.decimal', function(e) {
     if (e.keyCode === 13) {
       e.preventDefault();
-      var inputs = $('.nested-fields input.decimal:not([readonly])');
-      var currentIndex = inputs.index(this);
+      let inputs = $('.nested-fields input.decimal:not([readonly])');
+      let currentIndex = inputs.index(this);
 
       if (currentIndex < inputs.length - 1) {
         inputs.eq(currentIndex + 1).focus();
