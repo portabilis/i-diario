2509661585c81ce7154560b9185ab5c84bb111ec
“mateusinacioo” <mateus.inacio321@gmail.com>
2025-08-19 16:54:11 -0300

    Refatora o serviço de vinculação de usuários a professores.
    - Removendo métodos desnecessários e melhorando a lógica de atualização com base nos registros de professores passados como argumento.

--- app/services/user_teacher_linker_service.rb
+++ app/services/user_teacher_linker_service.rb
@@ -1,63 +1,57 @@
 class UserTeacherLinkerService
-  def self.call
-    new.call
+  def self.call(teacher_records)
+    new.call(teacher_records)
   end
 
-  def call
-    users = load_users_with_matching_cpf
+  def call(teacher_records)
+    @teacher_records = teacher_records
+    teachers, users = load_teachers_and_users
 
-    if users.empty?
+    update_users_and_teachers(teachers, users)
+  end
+
+  def load_teachers_and_users
+    teachers = Teacher.where(api_code: @teacher_records.map(&:servidor_id), active: true, discarded_at: nil)
+                     .index_by { |t| t.api_code.to_s }
+    
+    users = User.joins(:user_roles).where(teacher_id: nil).distinct.index_by(&:cpf)
+    
+    [teachers, users]
+  end
+
+  def update_users_and_teachers(teachers, users)
+    user_teacher_id_updates = []
+
+    @teacher_records.each do |record|
+      next if record.cpf.blank?
+
+      user = users[record.cpf]
+      teacher = teachers[record.servidor_id.to_s]
+
+      next unless user && teacher
+
+      user_teacher_id_updates << { user: user, teacher_id: teacher.id } if user.teacher_id != teacher.id
+    end
+
+    if user_teacher_id_updates.empty?
       Rails.logger.info 'Nenhum usuário encontrado para vinculação automática usuário-professor por CPF'
       return
     end
 
-    users_by_teacher = users.group_by(&:teacher_id)
-    used_teacher_ids = load_used_teacher_ids
-
-    update_users_and_teachers(users_by_teacher, used_teacher_ids)
-
-    Rails.logger.info 'Vinculação automática usuário-professor por CPF executada'
-  end
-
-  private
-
-  def load_users_with_matching_cpf
-    User.joins(<<-SQL)
-      INNER JOIN teachers ON
-        REGEXP_REPLACE(users.cpf, '[^\\d]+', '', 'g') = REGEXP_REPLACE(teachers.cpf, '[^\\d]+', '', 'g')
-        AND teachers.active = true
-        AND teachers.discarded_at IS NULL
-    SQL
-        .where(teacher_id: nil)
-        .where.not(cpf: [nil, ''])
-        .select('users.id as user_id, teachers.id as teacher_id')
-  end
-
-  def load_used_teacher_ids
-    User.where.not(teacher_id: nil).pluck(:teacher_id).to_set
-  end
-
-  def update_users_and_teachers(users_by_teacher, used_teacher_ids)
     User.transaction do
-      users_by_teacher.each do |teacher_id, user_matches|
-        next if used_teacher_ids.include?(teacher_id)
-
-        first_user = user_matches.first
-        user = User.find(first_user.user_id)
-
-        next unless user_has_valid_roles?(user)
+      user_teacher_id_updates.each do |user_teacher_id_update|
+        user = user_teacher_id_update[:user]
+        teacher_id = user_teacher_id_update[:teacher_id]
 
         if user.update(teacher_id: teacher_id)
-          used_teacher_ids << teacher_id
           Rails.logger.info "Usuário #{user.id} (#{user.name}) vinculado ao professor #{teacher_id} pelo CPF"
         else
           Rails.logger.warn "Falha ao vincular usuário #{user.id}: #{user.errors.full_messages.join(', ')}"
         end
       end
     end
-  end
-
-  def user_has_valid_roles?(user)
-    user.user_roles.any?
+    
+    Rails.logger.info 'Vinculação automática usuário-professor por CPF executada'
   end
 end
+
