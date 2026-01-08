ded8237db595bd2cd451daf509dd8095ace949d3
“mateusinacioo” <mateus.inacio321@gmail.com>
2026-01-08 10:24:05 -0300

    Adiciona filtro dinâmico de etapas por turma na tela de notas conceituais
    - Refatora index.js para incluir atualização de etapas via AJAX
    - Altera campo de filtro de by_step para by_step_id na view
    - Etapas são carregadas via fetch_steps e agrupadas por turma
    - Limpa seleção de etapa ao trocar turma

--- app/assets/javascripts/views/conceptual_exams/index.js
+++ app/assets/javascripts/views/conceptual_exams/index.js
@@ -1,9 +1,10 @@
-$(function () {
+$(function() {
   'use strict';
 
   var flashMessages = new FlashMessages();
-  var $classroom = $("#filter_by_classroom_id");
-  var $student = $(".conceptual-exam-student-filter");
+  var $classroomFilter = $('#filter_by_classroom_id');
+  var $stepFilter = $('#filter_by_step_id');
+  var $studentFilter = $('.conceptual-exam-student-filter');
 
   function setupSelect2(field, data, message) {
     var options = { data: data };
@@ -15,11 +16,11 @@ $(function () {
     field.select2(options);
   }
 
-  async function fetchStudents(classroom_id) {
+  function fetchStudents(classroom_id) {
     return $.ajax({
       url: Routes.fetch_students_by_classroom_conceptual_exams_pt_br_path({
         classroom_id: classroom_id,
-        format: "json"
+        format: 'json'
       }),
       success: handleFetchStudentsSuccess,
       error: handleFetchStudentsError
@@ -33,28 +34,78 @@ $(function () {
 
     studentOptions.unshift({ id: 'empty', text: '' });
 
-    setupSelect2($student, studentOptions);
+    setupSelect2($studentFilter, studentOptions);
   }
 
   function handleFetchStudentsError() {
-    flashMessages.error("Ocorreu um erro ao buscar os alunos da turma selecionada.");
+    flashMessages.error('Ocorreu um erro ao buscar os alunos da turma selecionada.');
   }
 
   function setupEmptyFields() {
-    setupSelect2($student, [], 'Selecione uma turma para carregar os alunos');
+    setupSelect2($studentFilter, [], 'Selecione uma turma para carregar os alunos');
   }
 
-  $classroom.on("change", async function () {
-    var classroom_id = $(this).val();
+  function isEmptyValue(value) {
+    return !value || value === 'empty' || value === '';
+  }
 
-    if (classroom_id) {
-      await fetchStudents(classroom_id);
+  function updateStepsFilter(classroomId) {
+    if ($stepFilter.length === 0) {
+      return;
+    }
+
+    var params = {};
+
+    if (!isEmptyValue(classroomId)) {
+      params.classroom_id = classroomId;
+    }
+
+    $.getJSON('/conceptual_exams/fetch_steps', params, function(data) {
+      $stepFilter.select2('destroy');
+      $stepFilter.data('elements', data);
+
+      $stepFilter.select2({
+        formatResult: function(el) {
+          if (el.children) {
+            return "<div class='select2-result-label'><strong>" + (el.text || el.name) + "</strong></div>";
+          }
+          return "<div class='select2-user-result'>" + (el.name || el.text) + "</div>";
+        },
+        formatSelection: function(el) {
+          return "<div class='select2-user-result'>" + (el.text || el.name) + "</div>";
+        },
+        data: data,
+        allowClear: true,
+        theme: 'classic'
+      });
+    });
+  }
+
+  // Limpa o step antes do form ser serializado pelo index.js global
+  $classroomFilter.on('select2-selecting select2-clearing', function() {
+    if ($stepFilter.length > 0) {
+      $stepFilter.val('');
+    }
+  });
+
+  $classroomFilter.on('change', function(e) {
+    var classroomId = isEmptyValue(e.val) ? '' : e.val;
+    updateStepsFilter(classroomId);
+
+    if (classroomId) {
+      fetchStudents(classroomId);
     } else {
       setupEmptyFields();
     }
   });
 
-  if ($classroom.val()) {
-    fetchStudents($classroom.val());
+  // Atualiza etapas agrupadas se nenhuma turma estiver selecionada
+  if (isEmptyValue($classroomFilter.val())) {
+    updateStepsFilter('');
+  }
+
+  // Carrega alunos se já houver turma selecionada
+  if ($classroomFilter.val()) {
+    fetchStudents($classroomFilter.val());
   }
 });
