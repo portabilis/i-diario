9f1a057ff8939c8637323c2d283c67180a89897e
Eder Soares <edersoares@me.com>
2026-03-06 14:07:36 -0300

    refactor: preloader TeachersSynchronizer

--- app/services/ieducar_synchronizers/teachers_synchronizer.rb
+++ app/services/ieducar_synchronizers/teachers_synchronizer.rb
@@ -14,6 +14,8 @@ class TeachersSynchronizer < BaseSynchronizer
   end
 
   def update_teachers(teachers)
+    preload_teachers(teachers.map(&:servidor_id))
+
     teachers.each do |teacher_record|
       next if teacher_record.nome.blank?
 
@@ -24,7 +26,10 @@ class TeachersSynchronizer < BaseSynchronizer
   end
 
   def update_teacher_record(teacher_record)
-    Teacher.with_discarded.find_or_initialize_by(api_code: teacher_record.servidor_id).tap do |teacher|
+    (
+      teacher(teacher_record.servidor_id) ||
+      Teacher.new(api_code: teacher_record.servidor_id)
+    ).tap do |teacher|
       teacher.name = teacher_record.nome
       teacher.active = teacher_record.ativo.to_s == IeducarBooleanState::ACTIVE
       teacher.save! if teacher.changed?
