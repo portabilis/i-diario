e785e34aaba3e1022a2e9edc5813746dcb5240f5
tiagocamargo <tiago.camargo@portabilis.com.br>
2026-04-17 12:06:21 -0300

    Ajustada sintaxe das migrations para compatibilidade com Rails 5.0.

--- db/migrate/20260417141316_add_trgm_index_to_audits_audited_changes.rb
+++ db/migrate/20260417141316_add_trgm_index_to_audits_audited_changes.rb
@@ -2,9 +2,9 @@ class AddTrgmIndexToAuditsAuditedChanges < ActiveRecord::Migration[5.0]
   disable_ddl_transaction!
   def change
     enable_extension 'pg_trgm' unless extension_enabled?('pg_trgm')
-    add_index :audits, :audited_changes,
+
+    add_index :audits, 'audited_changes gin_trgm_ops',
               using: :gin,
-              opclass: :gin_trgm_ops,
               algorithm: :concurrently,
               name: 'index_audits_on_audited_changes_trgm'
   end
