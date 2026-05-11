8ce049a37ffffcb9f79356e465bf280b3d78e62d
“mateusinacioo” <mateus.inacio321@gmail.com>
2026-05-11 15:17:29 -0300

    fix: corrige versão da migration

--- db/migrate/20260312160000_add_allow_active_search_frequency_to_general_configurations.rb
+++ db/migrate/20260312160000_add_allow_active_search_frequency_to_general_configurations.rb
@@ -1,4 +1,4 @@
-class AddAllowActiveSearchFrequencyToGeneralConfigurations < ActiveRecord::Migration[4.2]
+class AddAllowActiveSearchFrequencyToGeneralConfigurations < ActiveRecord::Migration[5.0]
   def change
     add_column :general_configurations, :allow_active_search_frequency, :boolean, default: false
   end
